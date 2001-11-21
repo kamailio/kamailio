78cb379804f5b89fe992f8da17f4e86d3fc14f04
Bogdan-Andrei Iancu <bogdan@voice-system.ro>
2001-11-21 17:04:40 +0000

    *** empty log message ***

--- modules/tm/lock.h
+++ modules/tm/lock.h
@@ -5,14 +5,15 @@
 #include <sys/ipc.h>
 #include <sys/sem.h>
 
+/* typedef to structure we use for mutexing;
+   currently, index to a semaphore set identifier now */
+typedef int lock_t;
+
 #include "h_table.h"
 
 /* Uni*x permissions for IPC */
 #define IPC_PERMISSIONS 0666
 
-/* typedef to structure we use for mutexing;
-   currently, index to a semaphore set identifier now */
-typedef int lock_t;
 
 /* try to allocate as long set as possible;
    return set size or -1 on error
@@ -27,13 +28,13 @@ int unlock( lock_t s );
 
 int change_semaphore( int semaphore_id , int val );
 
-void init_cell_lock( struct cell *cell );
-void init_entry_lock( struct entry *entry );
-void init_timerlist_lock( struct timer *timerlist );
+int init_cell_lock( struct cell *cell );
+int init_entry_lock( struct entry *entry );
+int init_timerlist_lock( struct timer *timerlist );
 
-void release_cell_lock( struct cell *cell );
-void release_entry_lock( struct entry *entry );
-void release_timerlist_lock( struct timer *timerlist );
+int release_cell_lock( struct cell *cell );
+int release_entry_lock( struct entry *entry );
+int release_timerlist_lock( struct timer *timerlist );
 
 #endif
 
