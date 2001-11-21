5a9aff8e00755edaf97eefd12cf0db106bd8eccb
Jiri Kuthan <jiri@iptel.org>
2001-11-21 12:30:59 +0000

    added lock.h

---
+++ modules/tm/lock.h
@@ -0,0 +1,38 @@
+#ifndef _SEMAPHORE_H
+#define _SEMAPHORE_H
+
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+
+#include "h_table.h"
+
+/* Uni*x permissions for IPC */
+#define IPC_PERMISSIONS
+
+/* typedef to structure we use for mutexing; 
+   currently, index to a semaphore set identifier now */
+typedef int lock_t;
+
+/* try to allocate as long set as possible;
+   return set size or -1 on error
+*/
+
+int lock_initialize();
+
+int lock_cleanup();
+
+int lock( lock_t s );
+
+int change_sem( int semaphore_id , int val );
+
+init_cell_lock( struct cell *cell );
+init_entry_lock( struct entry *entry );
+init_timerlist_lock( struct timer *timerlist );
+
+release_cell_lock( struct cell *cell );
+release_entry_lock( struct entry *entry );
+release_timerlist_lock( struct timer *timerlist );
+
+#endif
+
