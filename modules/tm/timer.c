ee3c00b2ca345681353a837beecedffebbee3912
Bogdan-Andrei Iancu <bogdan@voice-system.ro>
2001-11-26 19:11:51 +0000

    *** empty log message ***

--- modules/tm/timer.c
+++ modules/tm/timer.c
@@ -19,11 +19,11 @@ void add_to_tail_of_timer_list( struct s_table* hash_table , struct timer_link* 
 {
    struct timer* timer_list = &(hash_table->timers[ list_id ]);
 
-   /* the entire timer list is locked now -- noone else can manipulate it */
-   lock( timer_list->mutex );
    tl->time_out = time_out + hash_table->time;
    tl->next_tl= 0;
 
+   /* the entire timer list is locked now -- noone else can manipulate it */
+   lock( timer_list->mutex );
    if (timer_list->last_tl)
    {
        tl->prev_tl=timer_list->last_tl;
@@ -43,9 +43,45 @@ void add_to_tail_of_timer_list( struct s_table* hash_table , struct timer_link* 
 
 /*
   */
-void insert_into_timer_list( struct s_table* hash_table , struct timer_link* tl, int list_id , unsigned int time_out )
+void insert_into_timer_list( struct s_table* hash_table , struct timer_link* new_tl, int list_id , unsigned int time_out )
 {
-   struct timer* timer_list = &(hash_table->timers[ list_id ]);
+   struct timer          *timer_list = &(hash_table->timers[ list_id ]);
+   struct timer_link  *tl;
+
+   new_tl->time_out = time_out + hash_table->time;
+
+    /* if we have an empty list*/
+   if ( !timer_list->first_tl )
+   {
+      new_tl->next_tl= 0;
+      new_tl->prev_tl = 0;
+      lock( timer_list->mutex );
+      timer_list->first_tl = new_tl;
+      timer_list->last_tl = new_tl;
+      unlock( timer_list->mutex );
+   }
+
+   for( tl=timer_list->first_tl ; tl && tl->time_out<new_tl->time_out ; tl=tl->next_tl );
+
+   lock( timer_list->mutex );
+   if ( tl )
+   {
+      /* insert before tl*/
+      new_tl->prev_tl = tl->prev_tl;
+      tl->prev_tl = new_tl;
+      if ( new_tl->prev_tl )
+         new_tl->prev_tl->next_tl = new_tl;
+      new_tl->next_tl = tl;
+   }
+   else
+   {
+      /* insert at the end */
+      new_tl->next_tl = 0;
+      new_tl->prev_tl = timer_list->last_tl;
+      timer_list->last_tl->next_tl = new_tl;
+      timer_list->last_tl = new_tl ;
+    }
+    unlock( timer_list->mutex );
 }
 
 
@@ -104,8 +140,6 @@ struct timer_link  *remove_from_timer_list_from_head( struct s_table* hash_table
 
 
 
-
-
 void * timer_routine(void * attr)
 {
    struct s_table      *hash_table = (struct s_table *)attr;
@@ -130,7 +164,6 @@ void * timer_routine(void * attr)
             tl = remove_from_timer_list_from_head( hash_table, id );
             timers[id].timeout_handler( tl->payload );
          }
-
    } /* while (1) */
 }
 
