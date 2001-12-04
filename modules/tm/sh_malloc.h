d80dc2b86d64de95f75b7846265a2d9e8b815f2a
Jiri Kuthan <jiri@iptel.org>
2001-12-04 21:10:28 +0000

    SHMEM support commented out

--- modules/tm/sh_malloc.h
+++ modules/tm/sh_malloc.h
@@ -1,6 +1,26 @@
 #ifndef _SH_MALLOC_H
 #define _SH_MALLOC_H
 
+
+/*
+#if defined SHM_MEM
+
+#include "../../shm_mem.h"
+
+#define sh_malloc(size)		shm_malloc((size))
+#define sh_free(ptr)		shm_free((ptr))
+
+#else
+*/
+
+#include <stdlib.h>
+
+#define sh_malloc(size)		malloc((size))
+#define sh_free(ptr)		free((ptr))
+
+#endif
+
+/*
 #ifdef MEM_DBG
 
 #include <stdlib.h>
@@ -11,24 +31,6 @@
 
 #define sh_free(ptr)		({ printf("MEMDBG: free: %p\n", (ptr)); free((ptr)); })
 
-/*
-#elif defined SHM_MEM
 
-#include "../../shm_mem.h"
-
-#define sh_malloc(size)		shm_malloc((size))
-#define sh_free(ptr)		shm_free((ptr))
-
+#endif
 */
-
-#else
-
-#include <stdlib.h>
-
-#define sh_malloc(size)		malloc((size))
-#define sh_free(ptr)		free((ptr))
-
-#endif
-
-
-#endif
