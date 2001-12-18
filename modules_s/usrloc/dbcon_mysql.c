64e9e528e7899bbea81c15680368625142417948
Jan Janak <jan@iptel.org>
2001-12-18 00:29:18 +0000

    Separated database module

--- modules_s/usrloc/dbcon_mysql.c
+++ modules_s/usrloc/dbcon_mysql.c
@@ -1,4 +1,3 @@
-#define __USE_XOPEN /* Because of strptime */
 #include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,16 +5,22 @@
 #include "dbcon_mysql.h"
 #include "utils.h"
 #include <mysql/mysql.h>
+#include "db_utils.h"
+
+#define SQL_BUF_LEN 1024
 
 
 /*
  * Declarations
  */
-static int        connect_db    (const char* _db_url);
-static int        disconnect_db (void);
-static inline int is_connected  (void);
+static int        connect_db    (db_con_t* _h, const char* _db_url);
+static int        disconnect_db (db_con_t* _h);
 static void       time2mysql    (time_t _time, char* _result, int _res_len);
 static time_t     mysql2time    (const char* _str);
+static int        submit_query  (db_con_t* _h, const char* _s);
+static db_row_t*  get_result    (db_con_t* _h);
+static int        free_result   (db_con_t* _h, db_row_t* _r);
+static int        convert_result(db_con_t* _h, db_row_t* _r);
 
 
 static char ins_buf   [SQL_BUF_LEN];
@@ -25,56 +30,51 @@ static char update_buf[SQL_BUF_LEN];
 
 
 /*
- * MySQL connection handle
- */
-static MYSQL      db_con;
-static MYSQL_RES* db_res;
-static MYSQL_ROW  cur_row;
-
-
-/*
- * MySQL connection handle
- */
-static int connected = FALSE;
-
-
-/*
  * Establish database connection,
  * returns 1 on success, 0 otherwise
- * _handle is a handle used in communication with database
+ * _h is a handle used in communication with database
  *
  * URL is in form sql://user:password@host:port/database
  */
-static int connect_db(const char* _db_url)
+static int connect_db(db_con_t* _h, const char* _db_url)
 {
 	int res;
 	int p;
 	char* user, *password, *host, *port, *database;
+	char* buf;
 
+#ifdef PARANOID
+	if (!_h) return FALSE;
+	if (!_db_url) return FALSE;
+#endif
 	     /* Make a scratch pad copy of given SQL URL */
-	char* buf = strdup(_db_url);
+	buf = pkg_malloc(strlen(_db_url) + 1);
+	if (!buf) {
+		log(L_ERR, "connect_db(): Not enough memory\n");
+		return FALSE;
+	}
 
-	mysql_init(&db_con);
+	mysql_init(&(_h->con));
 	res = parse_sql_url(buf, &user, &password, &host, &port, &database);
 	if (port) {
 		p = atoi(port);
 	} else {
 		p = 0;
 	}
-
+	
 	if (res == FALSE) {
-		printf("connect_db(): Error while parsing SQL URL\n");
-		mysql_close(&db_con);
+		log(L_ERR, "connect_db(): Error while parsing SQL URL\n");
+		mysql_close(&(_h->con));
 		return FALSE;
 	}
-	if (!mysql_real_connect(&db_con, host, user, password, database, p, NULL, 0)) {
-		printf("connect_db(): %s\n", mysql_error(&db_con));
-		mysql_close(&db_con);
+	if (!mysql_real_connect(&(_h->con), host, user, password, database, p, NULL, 0)) {
+		log(L_ERR, "connect_db(): %s\n", mysql_error(&(_h->con)));
+		mysql_close(&(_h->con));
 		return FALSE;
 	}
 
-	free(buf);
-	connected = TRUE;
+	pkg_free(buf);
+	h->connected = TRUE;
 	return TRUE;
 }
 
@@ -85,11 +85,14 @@ static int connect_db(const char* _db_url)
  * disconnects database connection represented by _handle
  * returns 1 on success, 0 otherwise
  */
-static int disconnect_db(void)
+static int disconnect_db(db_con_t* _h)
 {
-	if (is_connected()) {
-		mysql_close(&db_con);
-		connected = FALSE;
+#ifdef PARANOID
+	if (!_h) return FALSE;
+#endif
+	if (_h->connected == TRUE) {
+		mysql_close(&(_h->con));
+		_h->connected = FALSE;
 		return TRUE;
 	} else {
 		return FALSE;
@@ -97,56 +100,33 @@ static int disconnect_db(void)
 }
 
 
-static inline int is_connected(void)
-{
-	return (connected == TRUE);
-}
-
-
-/*
- * Convert time_t structure to format accepted by MySQL database
- */
-static void time2mysql(time_t _time, char* _result, int _res_len)
-{
-	struct tm* t;
-
-	     /*
-	       if (_time == MAX_TIME_T) {
-	       snprintf(_result, _res_len, "0000-00-00 00:00:00");
-	       }
-	     */
-
-	t = gmtime(&_time);
-	strftime(_result, _res_len, "%Y-%m-%d %H:%M:%S", t);
-}
-
-
-/*
- * Convert MySQL time representation to time_t structure
- */
-static time_t mysql2time(const char* _str)
-{
-	struct tm time;
-
-	     /* It is neccessary to zero tm structure first */
-	memset(&time, '\0', sizeof(struct tm));
-	strptime(_str, "%Y-%m-%d %H:%M:%S", &time);
-	return mktime(&time);
-}
-
 
 /*
  * Initialize database module
  * No function should be called before this
  */
-int db_init(const char* _sqlurl)
+db_con_t* db_init(const char* _sqlurl)
 {
-	if (!connect_db(_sqlurl)) {
-		printf("db_init(): Error while trying to connect database\n");
-		return FALSE;
+	db_con_t* res;
+#ifdef PARANOID
+	if (!_sqlurl) return NULL;
+#endif
+
+	res = pkg_malloc(sizeof(db_con_t));
+	if (!res) {
+		log(L_ERR, "db_init(): No memory left\n");
+		return NULL;
+	} else {
+		memset(res, 0, sizeof(db_con_t));
 	}
 
-	return TRUE;
+	if (!connect_db(res, _sqlurl)) {
+		log(L_ERR, "db_init(): Error while trying to connect database\n");
+		pkg_free(res);
+		return NULL;
+	}
+
+	return res;
 }
 
 
@@ -154,13 +134,19 @@ int db_init(const char* _sqlurl)
  * Shut down database module
  * No function should be called after this
  */
-void db_close(void)
+void db_close(db_con_t* _h)
 {
-	disconnect_db();
-	if (db_res) {
-		mysql_free_result(db_res);
-		db_res = NULL;
+#ifdef PARANOID
+	if (!_h) return;
+#endif
+	disconnect_db(_h);
+	if (_h->res) {
+		mysql_free_result(_h->res);
 	}
+	if (_h->table) {
+		pkg_free(_h->table);
+	}
+	pkg_free(_h);
 }
 
 
@@ -350,71 +336,250 @@ int update_contact(const char* _tab, const contact_t* _con)
 
 
 
-/*
- * SQL URL parser
- */
-int parse_sql_url(char* _url, char** _user, char** _pass, 
-		  char** _host, char** _port, char** _db)
+
+static int submit_query(db_con_t* _h, const char* _s)
+{	
+#ifdef PARANOID
+	if (!_h) return FALSE;
+	if (!_s) return FALSE;
+#endif
+	if (mysql_query(&(_h->con), _s));
+	{
+ 		log(L_ERR, "submit_query(): %s\n", mysql_error(&(_h->con)));
+		return FALSE;
+	} else {
+		return TRUE;
+	}
+}
+
+
+db_res_t* get_result(db_con_t* _h)
 {
-	char* slash, *dcolon, *at, *db_slash;
+	db_res_t* res;
+	int n, i;
+#ifdef PARANOID
+	if (!_h) return NULL;
+#endif
 
-	*_user = '\0';
-	*_pass = '\0';
-	*_host = '\0';
-	*_port = '\0';
-	*_db   = '\0';
+	_h->res = mysql_store_result(&(_h->con));
+	if (!_h->res) {
+		log(L_ERR,"get_result(): %s\n", mysql_error(&(_h->con)));
+		return NULL;
+	}
 
-	     /* Remove any leading and trailing spaces and tab */
-	_url = trim(_url);
+	res = new_result();
+        if (convert_result(_h, _res) == FALSE) {
+		log(L_ERR, "get_result(): Error while converting result\n");
+		free_result(res);
+		return NULL;
+	}
+	
+	return res;
+}
 
-	if (strlen(_url) < 6) return FALSE;
 
-	if (*_url == '\0') return FALSE; /* Empty string */
 
-	slash = strchr(_url, '/');
-	if (!slash) return FALSE;   /* Invalid string, slashes not found */
+int free_result(db_con_t* _h, db_res_t* _r)
+{
+#ifdef PARANOID
+     if (!_h) return FALSE;
+     if (!_r) return FALSE;
+#endif
+     if (free_result(_r) == FALSE) {
+	     log(L_ERR, "free_result(): Unable to free result structure\n");
+	     return FALSE;
+     }
+     mysql_free_result(_h->res);
+     return TRUE;
+}
 
-	if ((*(++slash)) != '/') {  /* Invalid URL, 2nd slash not found */
+
+/*
+ * Query table for specified rows
+ * _h: structure representing database connection
+ * _k: key names
+ * _v: values of the keys that must match
+ * _c: column names to return
+ * _n: nmber of key=values pairs to compare
+ * _nc: number of columns to return
+ * _o: order by the specified column
+ */
+db_res_t* query_table(db_con_t* _h, db_key_t* _k, 
+		      db_val_t* _v, db_key_t* _c, int* _n, int _nc)
+{
+	int off;
+#ifdef PARANOID
+	if (!_h) return NULL;
+#endif
+	if (!_c) {
+		off = snprintf(query_buf, SQL_BUF_LEN, "select * from %s ", _h->table);
+	} else {
+		off = snprintf(query_buf, SQL_BUF_LEN, "select ");
+		off += print_columns(query_buf + off, SQL_BUF_LEN - off, _c, _nc);
+		off += snprintf(query_buf + off, SQL_BUF_LEN - off, "from %s ", _h->table);
+	}
+	if (_n) {
+		off += snprintf(query_buf + off, SQL_BUF_LEN - off, "where ");
+		off += print_cond(query_buf + off, SQL_BUF_LEN - off, _k, _v, _n);
+	}
+	if (submit_query(_h, query_buf) == FALSE) {
+		log(L_ERR, "query_table(): Error while submitting query\n");
+		return NULL;
+	}
+
+	return get_result(_h);
+}
+
+
+static int print_columns(char* _b, int _l, db_key_t* _c, int _n)
+{
+	int i;
+	int res = 0;
+#ifdef PARANOID
+	if (!_c) return 0;
+	if (!_n) return 0;
+	if (!_b) return 0;
+	if (!_l) return 0;
+#endif
+	for(i = 0; i < _n; i++) {
+		if (i == (_n - 1)) {
+			res += snprintf(_b + res, _l - res, "%s ", _c[i]);
+		} else {
+			res += snprintf(_b + res, _l - res, "%s,", _c[i]);
+		}
+	}
+	return res;
+}
+
+
+static int print_values(char* _b, int _l, db_value_t* _v, int _n)
+{
+	int i, res = 0, l;
+#ifdef PARANOID
+	if (!_b) return 0;
+	if (!_l) return 0;
+	if (!_v) return 0;
+	if (!_n) return 0;
+#endif
+
+	for(i = 0; i < _n; i++) {
+		l = _l - res;
+		res += val2str(_v, _b + res, &l);
+		res += l;
+		if (i != (_n - 1) {
+			*(_b + res) = ',';
+			res++;
+		}
+	}
+	return res;
+}
+
+
+static int print_cond(char* _b, int _l, db_key_t* _k, db_val_t* _v, int _n)
+{
+	int i;
+	int res = 0;
+	int l;
+#ifdef PARANOID
+	if (!_b) return 0;
+	if (!_l) return 0;
+	if (!_k) return 0;
+	if (!_v) return 0;
+	if (!_n) return 0;
+#endif
+	for(i = 0; i < _n; i++) {
+		res += snprintf(_b + res, _l - res, "%s=", _k[i]);
+		l = _l - res;
+		val2str(_v[i], _b + res, &l);
+		res += l;
+		if (i != (_n - 1)) {
+			res += snprintf(_b + res, _l - res, " AND ");
+		}
+	}
+	return res;
+}
+
+
+/*
+ * Insert a row into specified table
+ * _h: structure representing database connection
+ * _k: key names
+ * _v: values of the keys
+ * _n: number of key=value pairs
+ */
+int insert_row(db_con_t* _h, db_key_t* _k, db_val_t* _v, int _n)
+{
+	int off;
+#ifdef PARANOID
+	if ((!_h) || (!_k) || (!_v) || (!_n)) return FALSE;
+#endif
+	off = snprintf(ins_buf, SQL_BUF_LEN, "insert into %s (", _h->table);
+	off += print_columns(ins_buf + off, SQL_BUF_LEN - off, _k, _n);
+	off += snprintf(ins_buf + off, SQL_BUF_LEN - off, ") values (");
+	off += print_values(ins_buf + off, SQL_BUF_LEN - off, _v, _n);
+	*(ins_buf + off++) = ')';
+	*(ins_buf + off) = '\0';
+
+	if (submit_query(_h, ins_buf) == FALSE) {
+		log(L_ERR, "insert_row(): Error while submitting query\n");
 		return FALSE;
 	}
+	return TRUE;
+}
 
-	slash++;
 
-	at = strchr(slash, '@');
+/*
+ * Delete a row from the specified table
+ * _h: structure representing database connection
+ * _k: key names
+ * _v: values of the keys that must match
+ * _n: number of key=value pairs
+ */
+int delete_row(db_con_t* _h, db_key_t* _k, db_val_t* _v, int _n)
+{
+	int off;
+#ifdef PARANOID
+	if ((!_h) || (!_k) || (!_v) || (!_n)) return FALSE;
+#endif
+	off = snprintf(del_buf, SQL_BUF_LEN, "delete from %s where ", _h->table);
+	off += print_cond(del_buf + off, SQL_BUF_LEN - off, _k, _v, _n);
+	
+	if (submit_query(_h, del_buf) == FALSE) {
+		log(L_ERR, "delete_row(): Error while submitting query\n");
+		return FALSE;
+	}
+	return TRUE;
+}
 
-	db_slash = strchr(slash, '/');
-	if (db_slash) {
-		*db_slash++ = '\0';
-		*_db = trim(db_slash);
+
+/*
+ * Update some rows in the specified table
+ * _h: structure representing database connection
+ * _k: key names
+ * _v: values of the keys that must match
+ * _uk: updated columns
+ * _uv: updated values of the columns
+ * _n: number of key=value pairs
+ * _un: number of columns to update
+ */
+int update_table(db_con_t* _h, db_key_t* _k, db_val_t* _v,
+		 db_key_t* _uk, db_val_t* _uv, int _n, int _un)
+{
+	int off;
+#ifdef PARANOID
+	if ((!_h) || (!_uk) || (!_uv) || (!_un)) return FALSE;
+#endif
+	off = snprintf(update_buf, SQL_BUF_LEN, "update %s set ", _h->table);
+	off += print_cond(update_buf + off, SQL_BUF_LEN - off, _uk, _uv, _un);
+	if (_n) {
+		off += snprintf(update_buf + off, SQL_BUF_LEN - off, " where ");
+		off += print_cond(update_buf + off, SQL_BUF_LEN - off, _k, _v, _n);
 	}
 
-	if (!at) {
-		dcolon = strchr(slash, ':');
-		if (dcolon) {
-			*dcolon++ = '\0';
-			*_port = trim(dcolon);
-		}
-		*_host = trim(slash);
-	} else {
-		dcolon = strchr(slash, ':');
-	        *at++ = '\0';
-		if (dcolon) {
-			*dcolon++ = '\0';
-			if (dcolon < at) {   /* user:passwd */
-				*_pass = trim(dcolon);
-				dcolon = strchr(at, ':');
-				if (dcolon) {  /* host:port */
-					*dcolon++ = '\0';
-					*_port = trim(dcolon);
-				}
-			} else {            /* host:port */
-				*_port = trim(dcolon);
-			}
-		}
-		*_host = trim(at);
-		*_user = trim(slash);
+	if (submit_query(_h, update_buf) == FALSE) {
+		log(L_ERR, "update_row(): Error while submitting query\n");
+		return FALSE;
 	}
-
 	return TRUE;
 }
 
--- modules_s/usrloc/dbcon_mysql.h
+++ modules_s/usrloc/dbcon_mysql.h
@@ -3,20 +3,73 @@
 
 #include "location.h"
 #include "contact.h"
-
-int  db_init         (const char* _sqlurl);
-void db_close        (void); 
-int  query_location  (const char* _tab, const char* _aor, location_t** _res);
-int  insert_location (const char* _tab, const location_t* _loc);
-int  insert_contact  (const char* _tab, const contact_t* _con);
-int  delete_location (const char* _tab, const location_t* _loc);
-int  update_location (const char* _tab, const location_t* _loc);
-int  update_contact  (const char* _tab, const contact_t* _con);
-int parse_sql_url(char* _url, char** _user, char** _pass,
-		  char** _host, char** _port, char** _db);
+#include "db_con.h"
+#include "db_val.h"
+#include "db_row.h"
+#include "db_key.h"
+#include "db_res.h"
 
 
-#define SQL_BUF_LEN 1024
+/*
+ * Initialize database connection and
+ * obtain the connection handle
+ */
+db_con_t* db_init(const char* _sqlurl);
+
+
+/*
+ * Close a database connection and free
+ * all memory used
+ */
+void db_close (db_con_t* _h); 
+
+
+/*
+ * Query table for specified rows
+ * _h: structure representing database connection
+ * _k: key names
+ * _v: values of the keys that must match
+ * _c: column names to return
+ * _n: nmber of key=values pairs to compare
+ * _nc: number of columns to return
+ * _o: order by the specified column
+ */
+db_res_t* query_table (db_con_t* _h, db_key_t* _k, 
+		       db_val_t* _v, db_key_t* _c, int* _n, int _nc);
+
+
+/*
+ * Insert a row into specified table
+ * _h: structure representing database connection
+ * _k: key names
+ * _v: values of the keys
+ * _n: number of key=value pairs
+ */
+int insert_row (db_con_t* _h, db_key_t* _k, db_val_t* _v, int _n);
+
+
+/*
+ * Delete a row from the specified table
+ * _h: structure representing database connection
+ * _k: key names
+ * _v: values of the keys that must match
+ * _n: number of key=value pairs
+ */
+int delete_row (db_con_t* _h, db_key_t* _k, db_val_t* _v, int _n);
+
+
+/*
+ * Update some rows in the specified table
+ * _h: structure representing database connection
+ * _k: key names
+ * _v: values of the keys that must match
+ * _uk: updated columns
+ * _uv: updated values of the columns
+ * _n: number of key=value pairs
+ * _un: number of columns to update
+ */
+int update_table (db_con_t* _h, db_key_t* _k, db_val_t* _v,
+		  db_key_t* _uk, db_val_t* _uv, int _n, int _un);
 
 
 #endif
