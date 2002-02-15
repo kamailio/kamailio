34b052f1dcda28f81efa4e6336b4aa9df272aa7e
Andrei Pelinescu-Onciul <andrei@iptel.org>
2002-02-15 18:12:01 +0000

    - sun compile fixes - kill_sem.sh sun update

--- modules/tm/test/kill_sem.sh
+++ modules/tm/test/kill_sem.sh
@@ -1,4 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
-ipcrm sem `cat /proc/sysvipc/sem | awk '{ print $2; }'`
-ipcrm shm `cat /proc/sysvipc/shm | awk '{ print $2; }'` 
+OS=`uname -s`
+echo $OS
+if [ "$OS"  == "Linux" ] 
+then
+	ipcrm sem `cat /proc/sysvipc/sem | awk '{ print $2; }'`
+	ipcrm shm `cat /proc/sysvipc/shm | awk '{ print $2; }'` 
+elif [ "$OS" == "SunOS" ]
+then	
+	whoami=`whoami`
+	for r in `ipcs|grep ${whoami}| awk '{ print $2; }' `
+	do
+		echo "deleting semid $r"
+		ipcrm -s $r
+	done
+
+
+
+fi
