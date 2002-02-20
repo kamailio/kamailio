3bbaef787cd4dce0f1dce222261597001f2b3975
Bogdan-Andrei Iancu <bogdan@voice-system.ro>
2002-02-20 21:21:08 +0000

    *** empty log message ***

--- modules_s/maxfwd/maxfwd.c
+++ modules_s/maxfwd/maxfwd.c
@@ -19,40 +19,46 @@ static int w_decrement_maxfwd(struct sip_msg* msg, char* str, char* str2);
 static int w_add_maxfwd_header(struct sip_msg* msg, char* str, char* str2);
 static int w_is_maxfwd_zero(struct sip_msg* msg, char* str, char* str2);
 static int w_reply_to_maxfwd_zero(struct sip_msg* msg, char* str, char* str2);
-static int fixup_add_maxfwd_header(void** param, int param_no);
+static int fixup_maxfwd_header(void** param, int param_no);
 static int w_is_maxfwd_present(struct sip_msg* msg, char* str, char* str2);
+static int w_process_maxfwd_header(struct sip_msg* msg,char* str,char* str2);
+
 
 
 static struct module_exports mf_exports= {
 	"maxfwd_module",
-	(char*[]){			"mf_decrement_maxfwd",
+	(char*[]){	"mf_decrement_maxfwd",
 				"mf_add_maxfwd_header",
 				"mf_is_maxfwd_zero",
 				"mf_reply_to_maxfwd_zero",
-				"mf_is_maxfwd_present"
+				"mf_is_maxfwd_present",
+				"mf_process_maxfwd_header"
 			},
 	(cmd_function[]){
 					w_decrement_maxfwd,
 					w_add_maxfwd_header,
 					w_is_maxfwd_zero,
 					w_reply_to_maxfwd_zero,
-					w_is_maxfwd_present
+					w_is_maxfwd_present,
+					w_process_maxfwd_header
 					},
 	(int[]){
 				0,
 				1,
 				0,
 				0,
-				0
+				0,
+				1
 			},
 	(fixup_function[]){
 				0,
-				fixup_add_maxfwd_header,
+				fixup_maxfwd_header,
 				0,
 				0,
-				0
+				0,
+				fixup_maxfwd_header
 		},
-	5,
+	6,
 	(response_function) 0,
 	(destroy_function) 0,
 	0
@@ -72,7 +78,7 @@ struct module_exports* mod_register()
 
 
 
-static int fixup_add_maxfwd_header(void** param, int param_no)
+static int fixup_maxfwd_header(void** param, int param_no)
 {
 	unsigned int code;
 	int err;
@@ -81,7 +87,7 @@ static int fixup_add_maxfwd_header(void** param, int param_no)
 		code=str2s(*param, strlen(*param), &err);
 		if (err==0){
 			if (code>255){
-				LOG(L_ERR, "MAXFWD module:fixup_add_maxfwd_header: "
+				LOG(L_ERR, "MAXFWD module:fixup_maxfwd_header: "
 					"number to big <%d> (max=255)\n",code);
 				return E_UNSPEC;
 			}
@@ -89,7 +95,7 @@ static int fixup_add_maxfwd_header(void** param, int param_no)
 			*param=(void*)code;
 			return 0;
 		}else{
-			LOG(L_ERR, "MAXFWD module:fixup_add_maxfwd_header: bad  number <%s>\n",
+			LOG(L_ERR, "MAXFWD module:fixup_maxfwd_header: bad  number <%s>\n",
 					*param);
 			return E_UNSPEC;
 		}
@@ -122,6 +128,31 @@ static int w_is_maxfwd_present(struct sip_msg* msg, char* str, char* str2)
 {
 	return is_maxfwd_present( msg );
 }
+
+static int w_process_maxfwd_header(struct sip_msg* msg, char* str,char* str2)
+{
+	if (is_maxfwd_present(msg)==1)
+	{
+		if ( decrement_maxfwd(msg)!=1 )
+		{
+			LOG( L_ERR,"ERROR: MAX_FWD module : unable to decrement header\n");
+			goto OK;
+		}
+		if (is_maxfwd_zero(msg)==1 )
+		{
+			LOG( L_INFO,"INFO: MAX_FWD module : zero value found\n!");
+			goto error;
+		}
+	}
+	else
+		add_maxfwd_header( msg , (unsigned int)str );
+
+OK:
+	return 1;
+error:
+	return -1;
+}
+
 
 
 
