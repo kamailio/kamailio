e4d9031512c5969a9545345c333dbb7f51bde5d7
Jan Janak <jan@iptel.org>
2002-02-24 17:12:36 +0000

    New table schema

--- scripts/mysql-create.sql
+++ scripts/mysql-create.sql
@@ -2,11 +2,13 @@ create database ser;
 use ser;
 
 create table location (
-    user varchar(255)    not null,
+    user varchar(50)    not null,
     contact varchar(255) not null,
-    expire datetime,
+    expires datetime,
     q float(10,2),
+    callid varchar(255),
+    cseq integer,
     last_modified timestamp(14),
-    key(user)
+    key(user,contact)
 );
 
