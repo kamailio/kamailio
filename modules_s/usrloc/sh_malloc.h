32e2d19e4275fb49e643258b1945647e37f94b3c
Jan Janak <jan@iptel.org>
2002-03-25 11:10:31 +0000

    Shared memory wrapper

---
+++ modules_s/usrloc/sh_malloc.h
@@ -0,0 +1,30 @@
+/*
+ * $Id$
+ */
+
+
+#ifndef _SH_MALLOC_H
+#define _SH_MALLOC_H
+
+#include "../../mem/shm_mem.h"
+
+#if defined SHM_MEM
+
+#include "../../mem/shm_mem.h"
+
+#define sh_malloc(size)	shm_malloc((size))
+#define sh_free(ptr)	shm_free((ptr))
+#define sh_status()	shm_status()
+
+#else
+
+#include <stdlib.h>
+
+#warn "You should define SHM_MALLOC !!!!"
+#define sh_malloc(size)	 malloc((size))
+#define sh_free(ptr)	 free((ptr))
+#define sh_status()
+
+#endif
+
+#endif
