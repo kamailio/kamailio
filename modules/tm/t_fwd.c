e78806c007e4ae0dc442847beca66feee5a84b34
Bogdan-Andrei Iancu <bogdan@voice-system.ro>
2002-05-16 20:47:49 +0000

    *** empty log message ***

--- modules/tm/t_fwd.c
+++ modules/tm/t_fwd.c
@@ -195,7 +195,7 @@ int t_forward_nonack( struct sip_msg* p_msg , unsigned int dest_ip_param ,
 	}
 	p_msg->new_uri.s = backup_uri.s;
 	p_msg->new_uri.len = backup_uri.len;
-	//t_clear_forks();
+	t_clear_forks();
 	return 1;
 
 error:
