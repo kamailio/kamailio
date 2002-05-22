9d393c960c09d46f42039dbc2db96a7306a82433
Bogdan-Andrei Iancu <bogdan@voice-system.ro>
2002-05-22 14:05:15 +0000

    *** empty log message ***

---
+++ modules_s/sms/sms.c
@@ -0,0 +1,77 @@
+/*
+ * $Id$
+ *
+ * MAXFWD module
+ *
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "../../sr_module.h"
+#include "../../error.h"
+#include "../../ut.h"
+#include "sms_funcs.h"
+
+
+
+static int mod_init(void);
+static int sms_send_message(struct sip_msg*, char*, char* );
+
+
+struct module_exports exports= {
+	"sms_module",
+	(char*[]){
+				"sms_send_message"
+			},
+	(cmd_function[]){
+					sms_send_message
+					},
+	(int[]){
+				0
+			},
+	(fixup_function[]){
+				0
+		},
+	1,
+
+	NULL,   /* Module parameter names */
+	NULL,   /* Module parameter types */
+	NULL,   /* Module parameter variable pointers */
+	0,      /* Number of module paramers */
+
+	mod_init,   /* module initialization function */
+	(response_function) 0,
+	(destroy_function) 0,
+	0,
+	0  /* per-child init function */
+};
+
+
+static int mod_init(void)
+{
+	printf("sms - initializing\n");
+	//sms_startup();
+	return 0;
+}
+
+
+
+
+static int sms_send_message(struct sip_msg *msg, char* foo1, char * foo2)
+{
+	str body;
+
+	if ( !sms_extract_body(msg,&body) )
+	{
+		LOG(L_ERR,"ERROR: sms_send_message:cannot extract body from msg!\n");
+		goto error;
+	}
+
+	return 1;
+error:
+	return -1;
+}
+
+
---
+++ modules_s/sms/sms_funcs.c
@@ -0,0 +1,26 @@
+#include "sms_funcs.h"
+
+
+int sms_extract_body(struct sip_msg *msg, str *body )
+{
+	if ( parse_headers(msg,HDR_EOH)==-1 )
+	{
+		LOG(L_ERR,"ERROR: sms_extract_body:unable to parse all headers!\n");
+		goto error;
+	}
+
+	if ( strncmp(CRLF,msg->unparsed,CRLF_LEN)!=0 )
+	{
+		LOG(L_ERR,"ERROR: sms_extract_body:unable to detect the beginning"
+			" of message body!\n ");
+		goto error;
+	}
+
+	body->s = msg->unparsed + CRLF_LEN;
+	body->len = msg->buf + msg->len - body->s;
+	DBG("------------- body =\n|%.*s|\n %d\n",body->len,body->s);
+
+	return 1;
+error:
+	return -1;
+}
