1dafdf72b30d82e5af8975ae33c4956dcb7197bf
Jiri Kuthan <jiri@iptel.org>
2002-05-25 17:56:21 +0000

    changed MySQL structure

--- scripts/mysql-create.sql
+++ scripts/mysql-create.sql
@@ -1,22 +1,42 @@
+# $Id$
+
 create database ser;
 use ser;
 
-create table location (
-    user varchar(50)    not null,
-    contact varchar(255) not null,
-    expires datetime,
-    q float(10,2),
-    callid varchar(255),
-    cseq integer,
-    last_modified timestamp(14),
-    key(user,contact)
+CREATE TABLE grp (
+   user varchar(50) NOT NULL,
+   grp varchar(50) NOT NULL
 );
 
-
-create table auth (
-	realm varchar(255) not null,
-	user varchar(50) not null,
-	ha1 varchar(33) not null,
-	last_modified timestamp(14),
-	key(user,realm)
+CREATE TABLE location (
+   user varchar(50) NOT NULL,
+   contact varchar(255) NOT NULL,
+   expires datetime,
+   q float(10,2),
+   callid varchar(255),
+   cseq int(11),
+   last_modified timestamp(14),
+   KEY user (user, contact)
 );
+
+CREATE TABLE subscriber (
+   USER_ID varchar(100) NOT NULL,
+   PASSWORD varchar(25) NOT NULL,
+   FIRST_NAME varchar(25) NOT NULL,
+   LAST_NAME varchar(45) NOT NULL,
+   PHONE varchar(15) NOT NULL,
+   EMAIL_ADDRESS varchar(50) NOT NULL,
+   DATETIME_CREATED datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+   DATETIME_MODIFIED datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+   confirmation varchar(64) NOT NULL,
+   flag char(1) DEFAULT 'o' NOT NULL,
+   SendNotification varchar(50) NOT NULL,
+   Greeting varchar(50) NOT NULL,
+   HA1 varchar(128) NOT NULL,
+   REALM varchar(128) NOT NULL,
+   ha1b varchar(128) NOT NULL,
+   UNIQUE USER_ID (USER_ID),
+   KEY USER_ID_2 (USER_ID)
+   key(user_id,realm)
+);
+
--- scripts/mysql-drop.sql
+++ scripts/mysql-drop.sql
@@ -1,5 +1,9 @@
+# $Id$
+
 use ser;
 
 drop table location;
+drop table subscriber;
+drop table grp;
 drop database ser;
 
