d440f863b35ffa3e9b019aefd09091acb92b88c0
ssi <ssi>
2002-08-08 17:15:41 +0000

    header file for parse_from ssi

---
+++ parser/parse_from.h
@@ -0,0 +1,15 @@
+/*
+ * $Id$
+ */
+
+#ifndef PARSE_FROM
+#define PARSE_FROM
+
+#include "../str.h"
+#include "parse_to.h"
+/*
+ * To header field parser
+ */
+char* parse_from(char* buffer, char *end, struct from_body *from_b);
+
+#endif
