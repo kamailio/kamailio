3828f8411334e5e21a092fecee0d187c87a0e03c
Jiri Kuthan <jiri@iptel.org>
2002-08-15 12:57:47 +0000

    bigbang

---
+++ hash_func.h
@@ -0,0 +1,20 @@
+/*
+ * $Id$
+ */
+
+
+#ifndef _HASH_H
+#define _HASH_H
+
+#include "str.h"
+
+/* always use a power of 2 for hash table size */
+#define T_TABLE_POWER    12
+#define TABLE_ENTRIES    (1 << (T_TABLE_POWER))
+
+int new_hash( str  call_id, str cseq_nr );
+int old_hash( str  call_id, str cseq_nr );
+
+#define hash( cid, cseq) new_hash( cid, cseq )
+
+#endif
