9d36d775648ff06296c5c0c54fc757343373b55e
Daniel-Constantin Mierla <daniel@voice-system.ro>
2002-08-22 18:17:53 +0000

    MSILO module - docs

---
+++ modules_s/msilo/doc/msilo.sql
@@ -0,0 +1,21 @@
+-- SQL script for MSILO module
+
+DROP DATABASE IF EXISTS msilo;
+
+CREATE DATABASE msilo;
+
+USE msilo;
+
+-- create table
+CREATE TABLE silo(
+	mid INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	iuri VARCHAR(128),
+	ouri VARCHAR(128) NOT NULL,
+	to_h VARCHAR(128),
+	from_h VARCHAR(128),
+	inc_time INTEGER NOT NULL DEFAULT 0,
+	exp_time INTEGER NOT NULL,
+	ctype VARCHAR(32) NOT NULL DEFAULT "text/plain",
+	body BLOB NOT NULL
+);
+
