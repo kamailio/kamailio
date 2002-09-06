40ac69d46623d9d86c35b14502014a538ab26944
Jiri Kuthan <jiri@iptel.org>
2002-09-06 10:37:31 +0000

    DBG_MSG_QA introduced

--- modules/tm/t_fwd.c
+++ modules/tm/t_fwd.c
@@ -54,6 +54,12 @@ char *print_uac_request( struct cell *t, struct sip_msg *i_req,
 
 	/* ... and build it now */
 	buf=build_req_buf_from_sip_req( i_req, len, send_sock );
+#ifdef DBG_MSG_QA
+	if (buf[*len-1]==0) {
+		LOG(L_ERR, "ERROR: print_uac_request: sanity check failed\n");
+		abort();
+	}
+#endif
 	if (!buf) {
 		LOG(L_ERR, "ERROR: print_uac_request: no pkg_mem\n"); 
 		ser_error=E_OUT_OF_MEM;
