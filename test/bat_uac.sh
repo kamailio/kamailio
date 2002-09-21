b426bf8bb83f78e868e3d741772fe4c07ce2d32f
Jiri Kuthan <jiri@iptel.org>
2002-09-21 08:04:25 +0000

    cleanups

--- test/bat_uac.sh
+++
@@ -1,1 +0,0 @@
-java -classpath TurboSip.jar AutoTestApps.TurboSip.TSipC 195.37.77.101 5080 195.37.77.100 5000 1000
--- test/bat_uas.sh
+++
@@ -1,1 +0,0 @@
-java -classpath TurboSip.jar AutoTestApps.TurboSip.TSipS 195.37.77.101 5080  195.37.77.100 5090 1000
