faa9e46f23d3d405a0ef6dae5c119cdd2a937871
Bogdan-Andrei Iancu <bogdan@voice-system.ro>
2002-10-24 14:21:08 +0000

    Line 321 - a comma was missing! ;-)

--- forward.c
+++ forward.c
@@ -317,7 +317,7 @@ int forward_reply(struct sip_msg* msg)
 		if (check_self(&msg->via1->host,
 					msg->via1->port?msg->via1->port:SIP_PORT)!=1){
 			LOG(L_NOTICE, "ERROR: forward_reply: host in first via!=me :"
-					" %.*s:%d\n", msg->via1->host.len, msg->via1->host.s
+					" %.*s:%d\n", msg->via1->host.len, msg->via1->host.s,
 									msg->via1->port);
 			/* send error msg back? */
 			goto error;
