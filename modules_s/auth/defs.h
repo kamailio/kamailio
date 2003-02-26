c37e463a7bab0756ad58b12a823af9b92d36d085
Jan Janak <jan@iptel.org>
2003-02-26 14:27:22 +0000

    - README updated - USER_DOMAIN_HACK removed from defs.h

--- modules_s/auth/defs.h
+++ modules_s/auth/defs.h
@@ -38,13 +38,6 @@
  */
 
 /*
- * the module will accept and authorize also username
- * of form user@domain which some broken clients send
- */
-#define USER_DOMAIN_HACK
-
-
-/*
  * If the method is ACK, it is always authorized
  */
 #define ACK_CANCEL_HACK
