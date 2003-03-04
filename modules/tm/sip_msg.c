3d24a36a58edd1f4db27ca3a41412bb8959a7166
Jan Janak <jan@iptel.org>
2003-03-04 14:38:31 +0000

    DIGEST_DOMAIN ifdef

--- modules/tm/sip_msg.c
+++ modules/tm/sip_msg.c
@@ -167,6 +167,9 @@ static inline struct auth_body* auth_body_cloner(char* new_buf, char *org_buf, s
 	
 	     /* authorized field must be cloned elsewhere */
 	new_auth->digest.username.s = translate_pointer(new_buf, org_buf, auth->digest.username.s);
+#ifdef DIGEST_DOMAIN
+	new_auth->digest.domain.s = translate_pointer(new_buf, org_buf, auth->digest.domain.s);
+#endif
 	new_auth->digest.realm.s = translate_pointer(new_buf, org_buf, auth->digest.realm.s);
 	new_auth->digest.nonce.s = translate_pointer(new_buf, org_buf, auth->digest.nonce.s);
 	new_auth->digest.uri.s = translate_pointer(new_buf, org_buf, auth->digest.uri.s);
