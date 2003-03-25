420b6bb040b42ebca4f9c82044c11048dfb76134
Jan Janak <jan@iptel.org>
2003-03-25 13:11:07 +0000

    - parameter parser extracted from contact parser - added support for Route & Record-Route parameters - added support for Double-Record-Routes - will be shared by Contact and Route & Record-Route parsers

---
+++ parser/parse_param.c
@@ -0,0 +1,413 @@
+/* 
+ * $Id$
+ *
+ * Generic Parameter Parser
+ *
+ * Copyright (C) 2001-2003 Fhg Fokus
+ *
+ * This file is part of ser, a free SIP server.
+ *
+ * ser is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version
+ *
+ * For a license to use the ser software under conditions
+ * other than those described here, or to purchase support for this
+ * software, please contact iptel.org by e-mail at the following addresses:
+ *    info@iptel.org
+ *
+ * ser is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software 
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * History:
+ * -------
+ * 2003-03-24 Created by janakj
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include "../str.h"
+#include "../ut.h"
+#include "../dprint.h"
+#include "../trim.h"
+#include "../mem/mem.h"
+#include "parse_param.h"
+
+
+/*
+ * Try to find out parameter name, recognized parameters
+ * are lr and r2
+ */
+static inline void parse_rr_class(param_hooks_t* _h, param_t* _p)
+{
+	switch(_p->name.s[0]) {
+	case 'l':
+	case 'L':
+		if ((_p->name.len == 2) &&
+		    ((_p->name.s[1] == 'r') || (_p->name.s[1] == 'R'))) {
+			_p->type = P_LR;
+			_h->rr.lr = _p;
+		}
+		break;
+ 
+	case 'r':
+	case 'R':
+		if ((_p->name.len == 3) &&
+		    (_p->name.s[1] == '2')) {
+			_p->type = P_R2;
+			_h->rr.r2 = _p;
+		}
+		break;
+	}
+}
+
+
+/*
+ * Try to find out parameter name, recognized parameters
+ * are q, expires and method
+ */
+static inline void parse_contact_class(param_hooks_t* _h, param_t* _p)
+{
+	switch(_p->name.s[0]) {
+	case 'q':
+	case 'Q':
+		if (_p->name.len == 1) {
+			_p->type = P_Q;
+			_h->contact.q = _p;
+		}
+		break;
+		
+	case 'e':
+	case 'E':
+		if ((_p->name.len == 7) &&
+		    (!strncasecmp(_p->name.s + 1, "xpires", 6))) {
+			_p->type = P_EXPIRES;
+			_h->contact.expires = _p;
+		}
+		break;
+		
+	case 'm':
+	case 'M':
+		if ((_p->name.len == 6) &&
+		    (!strncasecmp(_p->name.s + 1, "ethod", 5))) {
+			_p->type = P_METHOD;
+			_h->contact.method = _p;
+		}
+		break;
+	}
+}
+
+
+/*
+ * Parse quoted string in a parameter body
+ * return the string without quotes in _r
+ * parameter and update _s to point behind the
+ * closing quote
+ */
+static inline int parse_quoted_param(str* _s, str* _r)
+{
+	char* end_quote;
+
+	     /* The string must have at least
+	      * surrounding quotes
+	      */
+	if (_s->len < 2) {
+		return -1;
+	}
+
+	     /* Skip opening quote */
+	_s->s++;
+	_s->len--;
+
+
+	     /* Find closing quote */
+	end_quote = q_memchr(_s->s, '\"', _s->len);
+
+	     /* Not found, return error */
+	if (!end_quote) {
+		return -2;
+	}
+
+	     /* Let _r point to the string without
+	      * surrounding quotes
+	      */
+	_r->s = _s->s;
+	_r->len = end_quote - _s->s;
+
+	     /* Update _s parameter to point
+	      * behind the closing quote
+	      */
+	_s->len -= (end_quote - _s->s + 1);
+	_s->s = end_quote + 1;
+
+	     /* Everything went OK */
+	return 0;
+}
+
+
+/*
+ * Parse unquoted token in a parameter body
+ * let _r point to the token and update _s
+ * to point right behind the token
+ */
+static inline int parse_token_param(str* _s, str* _r)
+{
+	int i;
+
+	     /* There is nothing to parse,
+	      * return error
+	      */
+	if (_s->len == 0) {
+		return -1;
+	}
+
+	     /* Save the begining of the
+	      * token in _r->s
+	      */
+	_r->s = _s->s;
+
+	     /* Iterate throught the
+	      * token body
+	      */
+	for(i = 0; i < _s->len; i++) {
+
+		     /* All these characters
+		      * mark end of the token
+		      */
+		switch(_s->s[i]) {
+		case ' ':
+		case '\t':
+		case '\r':
+		case '\n':
+		case ',':
+		case ';':
+			     /* So if you find
+			      * any of them
+			      * stop iterating
+			      */
+			goto out;
+		}
+	}
+ out:
+	if (i == 0) {
+		return -1;
+        }
+
+	     /* Save length of the token */
+        _r->len = i;
+
+	     /* Update _s parameter so it points
+	      * right behind the end of the token
+	      */
+	_s->s = _s->s + i;
+	_s->len -= i;
+
+	     /* Everything went OK */
+	return 0;
+}
+
+
+/*
+ * Parse a parameter name
+ */
+static inline void parse_param_name(str* _s, pclass_t _c, param_hooks_t* _h, param_t* _p)
+{
+	_p->name.s = _s->s;
+
+	while(_s->len) {
+		switch(_s->s[0]) {
+		case ' ':
+		case '\t':
+		case '\r':
+		case '\n':
+		case ';':
+		case ',':
+		case '=':
+			goto out;
+		}
+		_s->s++;
+		_s->len--;
+	}
+
+ out:
+	_p->name.len = _s->s - _p->name.s;
+	
+	switch(_c) {
+	case CLASS_RR:      parse_rr_class(_h, _p);      break;
+	case CLASS_CONTACT: parse_contact_class(_h, _p); break;
+	default: break;
+	}
+}
+
+
+/*
+ * Parse body of a parameter. It can be quoted string or
+ * a single token.
+ */
+static inline int parse_param_body(str* _s, param_t* _c)
+{
+	if (_s->s[0] == '\"') {
+		if (parse_quoted_param(_s, &(_c->body)) < 0) {
+			LOG(L_ERR, "parse_param_body(): Error while parsing quoted string\n");
+			return -2;
+		}
+	} else {
+		if (parse_token_param(_s, &(_c->body)) < 0) {
+			LOG(L_ERR, "parse_param_body(): Error while parsing token\n");
+			return -3;
+		}
+	}
+
+	return 0;
+}
+
+
+/*
+ * Parse parameters
+ * _s is string containing parameters, it will be updated to point behind the parameters
+ * _c is class of parameters
+ * _h is pointer to structure that will be filled with pointer to well known parameters
+ * linked list of parsed parameters will be stored in
+ * the variable _p is pointing to
+ * The function returns 0 on success and negative number
+ * on an error
+ */
+int parse_params(str* _s, pclass_t _c, param_hooks_t* _h, param_t** _p)
+{
+	param_t* t;
+
+	if (!_s || !_h || !_p) {
+		LOG(L_ERR, "parse_params(): Invalid parameter value\n");
+		return -1;
+	}
+
+	while(1) {
+		t = (param_t*)pkg_malloc(sizeof(param_t));
+		if (t == 0) {
+			LOG(L_ERR, "parse_params(): No memory left\n");
+			goto error;
+		}
+		memset(t, 0, sizeof(param_t));
+
+		parse_param_name(_s, _c, _h, t);
+		trim_leading(_s);
+		
+		if (_s->len == 0) { /* The last parameter without body */
+			goto ok;
+		}
+		
+		if (_s->s[0] == '=') {
+			_s->s++;
+			_s->len--;
+			trim_leading(_s);
+
+			if (_s->len == 0) {
+				LOG(L_ERR, "parse_params(): Body missing\n");
+				goto error;
+			}
+
+			if (parse_param_body(_s, t) < 0) {
+				LOG(L_ERR, "parse_params(): Error while parsing param body\n");
+				goto error;
+			}
+
+			trim_leading(_s);
+			if (_s->len == 0) {
+				goto ok;
+			}
+		}
+
+		if (_s->s[0] == ',') goto ok;
+
+		if (_s->s[0] != ';') {
+			LOG(L_ERR, "parse_params(): Invalid character, ; expected\n");
+			goto error;
+		}
+
+		_s->s++;
+		_s->len--;
+		trim_leading(_s);
+		
+		if (_s->len == 0) {
+			LOG(L_ERR, "parse_params(): Param name missing after ;\n");
+			goto error;
+		}
+
+		t->next = *_p;
+		*_p = t;
+	}
+
+ error:
+	if (t) pkg_free(t);
+	free_params(*_p);
+	return -2;
+
+ ok:
+	t->next = *_p;
+	*_p = t;
+	return 0;
+}
+
+
+/*
+ * Free linked list of parameters
+ */
+void free_params(param_t* _p)
+{
+	param_t* ptr;
+	
+	while(_p) {
+		ptr = _p;
+		_p = _p->next;
+		pkg_free(ptr);
+	}
+}
+
+
+/*
+ * Print a parameter structure, just for debugging
+ */
+static inline void print_param(param_t* _p)
+{
+	char* type;
+
+	printf("---param(%p)---\n", _p);
+	
+	switch(_p->type) {
+	case P_OTHER:   type = "P_OTHER";   break;
+	case P_LR:      type = "P_LR";      break;
+	case P_R2:      type = "P_R2";      break;
+	case P_Q:       type = "P_Q";       break;
+	case P_EXPIRES: type = "P_EXPIRES"; break;
+	case P_METHOD:  type = "P_METHOD";  break;
+	default:        type = "UNKNOWN";   break;
+	}
+	
+	printf("type: %s\n", type);
+	printf("name: \'%.*s\'\n", _p->name.len, _p->name.s);
+	printf("body: \'%.*s\'\n", _p->body.len, _p->body.s);
+	
+	printf("---/cparam---\n");
+}
+
+
+/*
+ * Print linked list of parameters, just for debugging
+ */
+void print_params(param_t* _p)
+{
+	param_t* ptr;
+	
+	ptr = _p;
+	while(ptr) {
+		print_param(ptr);
+		ptr = ptr->next;
+	}
+}
---
+++ parser/parse_param.h
@@ -0,0 +1,125 @@
+/* 
+ * $Id$
+ *
+ * Generic Parameter Parser
+ *
+ * Copyright (C) 2001-2003 Fhg Fokus
+ *
+ * This file is part of ser, a free SIP server.
+ *
+ * ser is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version
+ *
+ * For a license to use the ser software under conditions
+ * other than those described here, or to purchase support for this
+ * software, please contact iptel.org by e-mail at the following addresses:
+ *    info@iptel.org
+ *
+ * ser is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software 
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * History:
+ * -------
+ * 2003-03-24 Created by janakj
+ */
+
+#ifndef PARSE_PARAM_H
+#define PARSE_PARAM_H
+
+
+/*
+ * Supported types of parameters
+ */
+typedef enum ptype {
+	P_OTHER = 0, /* Unknown parameter */
+	P_LR,        /* Route & Record-Route: lr parameter */
+	P_R2,        /* Route & Record-Route: r2 parameter - Route & Record-Route */
+	P_Q,         /* Contact: q parameter */
+	P_EXPIRES,   /* Contact: expires parameter */
+	P_METHOD     /* Contact: method parameter */
+} ptype_t;
+
+
+/*
+ * Class of parameters
+ */
+typedef enum pclass {
+	CLASS_ANY = 0,  /* Any parameters, well-known hooks will be not used */
+	CLASS_RR,       /* Route & Record-Route parameters */
+	CLASS_CONTACT   /* Contact parameters */
+} pclass_t;
+
+
+/*
+ * Structure representing a parameter
+ */
+typedef struct param {
+	ptype_t type;         /* Type of the parameter */
+	str name;             /* Parameter name */
+	str body;             /* Parameter body */
+	struct param* next;   /* Next parameter in the list */
+} param_t;
+
+
+/*
+ * Hooks to well known parameters for Route & Record-Route class of parameters
+ */
+struct rr_hooks {
+	struct param* lr; /* lr parameter */
+	struct param* r2; /* r2 parameters - ser specific */
+};
+
+
+/*
+ * Hooks to well known parameters for contact class of parameters
+ */
+struct contact_hooks {
+	struct param* expires; /* expires parameter */
+	struct param* q;       /* q parameter */
+	struct param* method;  /* method parameter */
+};
+
+
+/*
+ * Union of hooks structures for all classes
+ */
+typedef union param_hooks {
+ 	struct rr_hooks rr;           /* Route & Record-Route hooks */
+	struct contact_hooks contact; /* Contact hooks */
+} param_hooks_t;
+
+
+/*
+ * Parse parameters
+ * _s is string containing parameters
+ * _c is class of parameters
+ * _h is pointer to structure that will be filled with pointer to well known parameters
+ * linked list of parsed parameters will be stored in
+ * the variable _p is pointing to
+ * The function returns 0 on success and negative number
+ * on an error
+ */
+int parse_params(str* _s, pclass_t _c, param_hooks_t* _h, param_t** _p);
+
+
+/*
+ * Free linked list of parameters
+ */
+void free_params(param_t* _p);
+
+
+/*
+ * Print linked list of parameters, just for debugging
+ */
+void print_params(param_t* _p);
+
+
+#endif /* PARSE_PARAM_H */
