03e488499b70738a5f08722e3eba770a869a87f1
Jiri Kuthan <jiri@iptel.org>
2003-04-07 06:36:56 +0000

    s/reply_route/failure_route, onreply_route introduced

--- modules_s/textops/textops.c
+++ modules_s/textops/textops.c
@@ -46,6 +46,7 @@
  *  2003-03-10  module export interface updated to the new format (andrei)
  *  2003-03-16  flags export parameter added (janakj)
  *  2003-03-19  replaced all mallocs/frees w/ pkg_malloc/pkg_free (andrei)
+ *  2003-04-97  actions permitted to be used from failure/reply routes (jiri)
  */
 
 
@@ -80,13 +81,20 @@ static int mod_init(void);
 
 
 static cmd_export_t cmds[]={
-	{"search",           search_f,          1, fixup_regex, REQUEST_ROUTE},
-	{"search_append",    search_append_f,   2, fixup_regex, REQUEST_ROUTE},
-	{"replace",          replace_f,         2, fixup_regex, REQUEST_ROUTE},
-	{"replace_all",      replace_all_f,     2, fixup_regex, REQUEST_ROUTE},
-	{"append_to_reply",  append_to_reply_f, 1, 0,           REQUEST_ROUTE},
-	{"append_hf",        append_hf,         1, str_fixup,   REQUEST_ROUTE},
-	{"append_urihf",     append_urihf,      2, str_fixup,   REQUEST_ROUTE},
+	{"search",           search_f,          1, fixup_regex, 
+			REQUEST_ROUTE|ONREPLY_ROUTE|FAILURE_ROUTE}, 
+	{"search_append",    search_append_f,   2, fixup_regex, 
+			REQUEST_ROUTE|ONREPLY_ROUTE|FAILURE_ROUTE}, 
+	{"replace",          replace_f,         2, fixup_regex, 
+			REQUEST_ROUTE|ONREPLY_ROUTE|FAILURE_ROUTE}, 
+	{"replace_all",      replace_all_f,     2, fixup_regex, 
+			REQUEST_ROUTE|ONREPLY_ROUTE|FAILURE_ROUTE}, 
+	{"append_to_reply",  append_to_reply_f, 1, 0, 
+			REQUEST_ROUTE},
+	{"append_hf",        append_hf,         1, str_fixup,
+			REQUEST_ROUTE|ONREPLY_ROUTE|FAILURE_ROUTE}, 
+	{"append_urihf",     append_urihf,      2, str_fixup,   
+			REQUEST_ROUTE|FAILURE_ROUTE},
 	{0,0,0,0,0}
 };
 
