50d7560030a05673d51777541c9f908eb38e9878
Jan Janak <jan@iptel.org>
2003-04-08 12:31:40 +0000

    - forgot to initialize the timer process when forking is enabled

--- main.c
+++ main.c
@@ -795,6 +795,11 @@ int main_loop()
 			close(sockfd[0]);
 			unix_tcp_sock=sockfd[1];
 #endif
+			if (init_child(PROC_TIMER) < 0) {
+				LOG(L_ERR, "timer: init_child failed\n");
+				goto error;
+			}
+			
 			for(;;){
 				/* debug:  instead of doing something usefull */
 				/* (placeholder for timers, etc.) */
