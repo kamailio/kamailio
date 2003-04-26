12119ff098d30da0100b622db8bc7ea19f33d809
Jiri Kuthan <jiri@iptel.org>
2003-04-26 18:25:46 +0000

    <radiusclient> moved to bottom so that its PATH_MAX definition does not override limit.h's (sr_module.h->msg_parser.h->ip_addr.h->socket.h->limits.h)

--- modules_s/auth_radius/authrad_mod.c
+++ modules_s/auth_radius/authrad_mod.c
@@ -38,13 +38,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <radiusclient.h>
 #include "../../sr_module.h"
 #include "../../error.h"
 #include "../../dprint.h"
 #include "../../mem/mem.h"
 #include "authrad_mod.h"
 #include "authorize.h"
+#include <radiusclient.h>
 
 pre_auth_f pre_auth_func = 0;   /* Pre authorization function from auth module */
 post_auth_f post_auth_func = 0; /* Post authorization function from auth module */
