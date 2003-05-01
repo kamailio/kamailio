9a88daf78930847bbd72ed4b48a9b33ae2d653a1
Maxim Sobolev <sobomax@sippysoft.com>
2003-05-01 09:25:39 +0000

    Provide portable memmem() implementation from NetBSD and replace call to strnstr() with call to memmem().

--- modules_s/nathelper/nathelper.c
+++ modules_s/nathelper/nathelper.c
@@ -50,11 +50,6 @@
 #include <string.h>
 
 
-/* FIXME */
-#warning CAUTION: nathelper uses unknown strnstr; TEMPORARILY REMOVED
-#define strnstr(_a,_b,_c) 0
-
-
 static int fix_nated_contact_f(struct sip_msg*, char*, char*);
 static int fix_nated_sdp_f(struct sip_msg*, char*, char*);
 
@@ -87,6 +82,38 @@ static int
 mod_init(void)
 {
 	return 0;
+}
+
+/*
+ * ser_memmem() returns the location of the first occurence of data
+ * pattern b2 of size len2 in memory block b1 of size len1 or
+ * NULL if none is found. Obtained from NetBSD.
+ */
+static void *
+ser_memmem(const void *b1, const void *b2, size_t len1, size_t len2)
+{
+	/* Initialize search pointer */
+	char *sp = (char *) b1;
+
+	/* Initialize pattern pointer */
+	char *pp = (char *) b2;
+
+	/* Intialize end of search address space pointer */
+	char *eos = sp + len1 - len2;
+
+	/* Sanity check */
+	if(!(b1 && b2 && len1 && len2))
+		return NULL;
+
+	while (sp <= eos) {
+		if (*sp == *pp)
+			if (memcmp(sp, pp, len2) == 0)
+				return sp;
+
+			sp++;
+	}
+
+	return NULL;
 }
 
 /*
@@ -262,7 +289,7 @@ fix_nated_sdp_f(struct sip_msg* msg, char* str1, char* str2)
 
 	if (level & FIX_MEDIAIP) {
 		for (cp = body.s; (len = body.s + body.len - cp) > 0;) {
-			cp1 = strnstr(cp, "c=", len);
+			cp1 = ser_memmem(cp, "c=", len, 2);
 			if (cp1 == NULL || cp1[-1] == '\n' || cp1[-1] == '\r')
 				break;
 			cp = cp1 + 2;
