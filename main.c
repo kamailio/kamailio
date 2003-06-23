be6f6ec9bf56fb408631941dd26d2c5385842697
Jiri Kuthan <jiri@iptel.org>
2003-06-23 22:13:10 +0000

    ser made more noisy: default log level lifted to L_NOTICE

--- main.c
+++ main.c
@@ -270,7 +270,7 @@ int tcp_disable = 0; /* 1 if tcp is disabled */
 struct process_table *pt=0;		/*array with childrens pids, 0= main proc,
 									alloc'ed in shared mem if possible*/
 int sig_flag = 0;              /* last signal received */
-int debug = 0;
+int debug = L_NOTICE;
 int dont_fork = 0;
 int log_stderr = 0;
 /* check if reply first via host==us */
