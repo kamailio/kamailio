c99c5f78a43a6197f7806ba96eff04a2567baf83
Daniel-Constantin Mierla <daniel@voice-system.ro>
2003-06-24 13:38:05 +0000

    - docbook documentation added - README updated accordingly

--- modules_s/msilo/doc/msilo.sql
+++ modules_s/msilo/doc/msilo.sql
@@ -1,20 +1,30 @@
+
 -- SQL script for MSILO module
 
 DROP DATABASE IF EXISTS msilo;
 
+-- create a database for storage
 CREATE DATABASE msilo;
 
 USE msilo;
 
--- create table
+-- create the table
 CREATE TABLE silo(
-	mid INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	src_addr VARCHAR(255) NOT NULL DEFAULT "",
-	dst_addr VARCHAR(255) NOT NULL DEFAULT "",
-	r_uri VARCHAR(255) NOT NULL DEFAULT "",
-	inc_time INTEGER NOT NULL DEFAULT 0,
-	exp_time INTEGER NOT NULL DEFAULT 0,
-	ctype VARCHAR(32) NOT NULL DEFAULT "text/plain",
-	body BLOB NOT NULL DEFAULT ""
+      -- unique ID per message
+    mid INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+      -- src address - From URI
+    src_addr VARCHAR(255) NOT NULL DEFAULT "",
+      -- dst address - To URI
+    dst_addr VARCHAR(255) NOT NULL DEFAULT "",
+      -- Request-URI
+    r_uri VARCHAR(255) NOT NULL DEFAULT "",
+      -- incoming time
+    inc_time INTEGER NOT NULL DEFAULT 0,
+      -- expiration time
+    exp_time INTEGER NOT NULL DEFAULT 0,
+      -- content type
+    ctype VARCHAR(32) NOT NULL DEFAULT "text/plain",
+      -- body of the message
+    body BLOB NOT NULL DEFAULT ""
 );
 
