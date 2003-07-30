daaa79fe94eee7cf32c58bce93cf2e1a27fedfd8
Jan Janak <jan@iptel.org>
2003-07-30 14:04:30 +0000

    - preferences table added.

--- scripts/ser_mysql.sh
+++ scripts/ser_mysql.sh
@@ -527,7 +527,13 @@ CREATE TABLE server_monitoring (
 ) $TABLE_TYPE;
 
 
-
+CREATE TABLE preferences (
+  $USERCOL varchar(50) NOT NULL default '',
+  domain varchar(100) NOT NULL default '',
+  attribute varchar(50) NOT NULL default '',
+  value varchar(100) NOT NULL default '',
+  PRIMARY KEY ($USERCOL, domain, attribute)
+) $TABLE_TYPE;
 
 #
 # Table structure for table 'server_monitoring_ul'
