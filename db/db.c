ebc3080f4c4132589542bf5dd202414b2fa98979
Jan Janak <jan@iptel.org>
2003-10-08 13:07:21 +0000

    - Got rid of all the ~ at the beginning of functions that are not supposed   to be called from scripts. It's a relict.

--- db/db.c
+++ db/db.c
@@ -34,31 +34,31 @@ db_func_t dbf;
 
 int bind_dbmod(void)
 {
-	db_use_table = (db_use_table_f)find_export("~db_use_table", 2, 0);
+	db_use_table = (db_use_table_f)find_export("db_use_table", 2, 0);
 	if (db_use_table == 0) return -1;
 
-	db_init = (db_init_f)find_export("~db_init", 1, 0);
+	db_init = (db_init_f)find_export("db_init", 1, 0);
 	if (db_init == 0) return -1;
 
-	db_close = (db_close_f)find_export("~db_close", 2, 0);
+	db_close = (db_close_f)find_export("db_close", 2, 0);
 	if (db_close == 0) return -1;
 
-	db_query = (db_query_f)find_export("~db_query", 2, 0);
+	db_query = (db_query_f)find_export("db_query", 2, 0);
 	if (db_query == 0) return -1;
 
-	db_raw_query = (db_raw_query_f)find_export("~db_raw_query", 2, 0);
+	db_raw_query = (db_raw_query_f)find_export("db_raw_query", 2, 0);
 	if (db_raw_query == 0) return -1;
 
-	db_free_query = (db_free_query_f)find_export("~db_free_query", 2, 0);
+	db_free_query = (db_free_query_f)find_export("db_free_query", 2, 0);
 	if (db_free_query == 0) return -1;
 
-	db_insert = (db_insert_f)find_export("~db_insert", 2, 0);
+	db_insert = (db_insert_f)find_export("db_insert", 2, 0);
 	if (db_insert == 0) return -1;
 
-	db_delete = (db_delete_f)find_export("~db_delete", 2, 0);
+	db_delete = (db_delete_f)find_export("db_delete", 2, 0);
 	if (db_delete == 0) return -1;
 
-	db_update = (db_update_f)find_export("~db_update", 2, 0);
+	db_update = (db_update_f)find_export("db_update", 2, 0);
 	if (db_update == 0) return -1;
 
 	return 0;
