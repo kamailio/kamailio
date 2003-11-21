c7808e4325bb58028e1c815ac0e830df9aa5b2e5
Andrei Pelinescu-Onciul <andrei@iptel.org>
2003-11-21 16:41:14 +0000

    - cleanup

--- modules_s/group_radius/group_mod.d
+++
@@ -1,9 +0,0 @@
-group_mod.o group_mod.d : group_mod.c ../../sr_module.h ../../parser/msg_parser.h \
-  ../../comp_defs.h ../../str.h ../../data_lump.h ../../flags.h \
-  ../../ip_addr.h ../../dprint.h ../../md5utils.h ../../config.h \
-  ../../types.h ../../parser/parse_def.h ../../parser/parse_cseq.h \
-  ../../parser/parse_to.h ../../parser/parse_via.h \
-  ../../parser/parse_fline.h ../../parser/hf.h ../../ut.h ../../error.h \
-  ../../mem/mem.h ../../mem/q_malloc.h group_mod.h \
-  ../../parser/digest/digest.h ../../parser/digest/digest_parser.h \
-  group.h
