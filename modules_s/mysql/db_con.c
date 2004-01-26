89c08fcce4a93dbbe2b1835fb50fa871c000d07a
Jan Janak <jan@iptel.org>
2004-01-26 13:10:35 +0000

    Shallow copy of db_con->table instead of deep.

--- modules_s/mysql/db_con.c
+++ modules_s/mysql/db_con.c
@@ -48,15 +48,6 @@ int use_table(db_con_t* _h, const char* _t)
 		return -1;
 	}
 
-	l = strlen(_t) + 1;
-	ptr = (char*)pkg_malloc(l);
-	if (!ptr) {
-		LOG(L_ERR, "use_table(): No memory left\n");
-		return -2;
-	}
-	memcpy(ptr, _t, l);
-
-	if (CON_TABLE(_h)) pkg_free(CON_TABLE(_h));
-	CON_TABLE(_h) = ptr;
+	CON_TABLE(_h) = _t;
 	return 0;
 }
