6386c11bcd1e0d8640d8dbc733f56075a739b24b
Jiri Kuthan <jiri@iptel.org>
2004-02-23 04:34:34 +0000

    - 487 no longer generated localy (hopefuly, UACs are well-behaving   and no longer go crazy if it takes time to get the 487 due to   another branch which is unresponsive) -> race conditions (200   coming from upstream) eliminated; - messages complaining about UAC status rewrites removed in   special cases: a) negative reply retransmission b) receipt   of 487 on a branch which was localy 408-ed

--- modules/tm/t_fwd.c
+++ modules/tm/t_fwd.c
@@ -362,6 +362,15 @@ void e2e_cancel( struct sip_msg *cancel_msg,
 		DBG("DEBUG: e2e_cancel: e2e cancel -- no more pending branches\n");
 		t_reply( t_cancel, cancel_msg, 200, CANCEL_DONE );
 	}
+
+#ifdef LOCAL_487
+
+	/* local 487s have been deprecated -- it better handles
+	 * race conditions (UAS sending 200); hopefuly there are
+	 * no longer UACs who go crazy waiting for the 487 whose
+	 * forwarding is being blocked by other unreponsive branch
+	 */
+
 	/* we could await downstream UAS's 487 replies; however,
 	   if some of the branches does not do that, we could wait
 	   long time and annoy upstream UAC which wants to see 
@@ -378,6 +387,7 @@ void e2e_cancel( struct sip_msg *cancel_msg,
 	   "can't reply twice"
 	*/
 	t_reply(t_invite, t_invite->uas.request, 487, CANCELLED );
+#endif
 }
 
 
--- modules/tm/t_reply.c
+++ modules/tm/t_reply.c
@@ -624,6 +624,19 @@ static enum rps t_should_relay_response( struct cell *Trans , int new_code,
 	/* if final response received at this branch, allow only INVITE 2xx */
 	if (Trans->uac[branch].last_received>=200
 			&& !(inv_through && Trans->uac[branch].last_received<300)) {
+		/* don't report on retranmissions */
+		if (Trans->uac[branch].last_received==new_code) {
+			DBG("DEBUG: final reply retrasnmission\n");
+			goto discard;
+		}
+		/* if you FR-timed-out, faked a local 408 and 487 came, don't
+		 * report on it either */
+		if (Trans->uac[branch].last_received==408 && new_code==487) {
+			DBG("DEBUG: 487 came for a timed-out branch\n");
+			goto discard;
+		}
+		/* this looks however how a very strange status rewrite attempt;
+		 * report on it */
 		LOG(L_ERR, "ERROR: t_should_relay: status rewrite by UAS: "
 			"stored: %d, received: %d\n",
 			Trans->uac[branch].last_received, new_code );
