e6c028f0b8e76b3c9cbd22758bdc9ea9c1be895a
Bogdan-Andrei Iancu <bogdan@voice-system.ro>
2004-03-16 19:51:27 +0000

    - support for more generic callbacks (not triggered yet)

---
+++ modules_s/usrloc/ul_callback.c
@@ -0,0 +1,120 @@
+/*
+ * $Id$
+ *
+ * Copyright (C) 2001-2003 Fhg Fokus
+ *
+ * This file is part of ser, a free SIP server.
+ *
+ * ser is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version
+ *
+ * For a license to use the ser software under conditions
+ * other than those described here, or to purchase support for this
+ * software, please contact iptel.org by e-mail at the following addresses:
+ *    info@iptel.org
+ *
+ * ser is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software 
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+/*
+ * History:
+ * --------
+ *  2004-03-16  created (bogdan)
+ */
+
+
+
+#include "stdlib.h"
+#include "../../dprint.h"
+#include "../../error.h"
+#include "../../mem/shm_mem.h"
+#include "ul_callback.h"
+
+
+struct ulcb_head_list* ulcb_list = 0;
+
+
+
+int init_ulcb_list()
+{
+	ulcb_list = (struct ulcb_head_list*)shm_malloc
+		( sizeof(struct ulcb_head_list) );
+	if (ulcb_list==0) {
+		LOG(L_CRIT,"ERROR:usrloc:init_ulcb_list: no more shared mem\n");
+		return -1;
+	}
+	ulcb_list->first = 0;
+	ulcb_list->reg_types = 0;
+	return 1;
+}
+
+
+void destroy_ulcb_list()
+{
+	struct ul_callback *cbp, *cbp_tmp;
+
+	if (!ulcb_list)
+		return;
+
+	for( cbp=ulcb_list->first; cbp ; ) {
+		cbp_tmp = cbp;
+		cbp = cbp->next;
+		if (cbp_tmp->param) shm_free( cbp_tmp->param );
+		shm_free( cbp_tmp );
+	}
+
+	shm_free(ulcb_list);
+}
+
+
+
+/* register a callback function 'f' for 'types' mask of events;
+*/
+int register_ulcb( int types, ul_cb f, void *param )
+{
+	struct ul_callback *cbp;
+
+	/* are the callback types valid?... */
+	if ( types<0 || types>ULCB_MAX ) {
+		LOG(L_CRIT, "BUG:usrloc:register_ulcb: invalid callback types: "
+			"mask=%d\n",types);
+		return E_BUG;
+	}
+	/* we don't register null functions */
+	if (f==0) {
+		LOG(L_CRIT, "BUG:usrloc:register_ulcb: null callback function\n");
+		return E_BUG;
+	}
+
+	/* build a new callback structure */
+	if (!(cbp=(struct ul_callback*)shm_malloc(sizeof( struct ul_callback)))) {
+		LOG(L_ERR, "ERROR:usrloc:register_ulcb: out of shm. mem\n");
+		return E_OUT_OF_MEM;
+	}
+
+	/* link it into the proper place... */
+	cbp->next = ulcb_list->first;
+	ulcb_list->first = cbp;
+	ulcb_list->reg_types |= types;
+	/* ... and fill it up */
+	cbp->callback = f;
+	cbp->param = param;
+	cbp->types = types;
+	if (cbp->next)
+		cbp->id = cbp->next->id+1;
+	else
+		cbp->id = 0;
+
+	return 1;
+}
+
+
+
---
+++ modules_s/usrloc/ul_callback.h
@@ -0,0 +1,94 @@
+/*
+ * $Id$
+ *
+ * Copyright (C) 2001-2003 Fhg Fokus
+ *
+ * This file is part of ser, a free SIP server.
+ *
+ * ser is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version
+ *
+ * For a license to use the ser software under conditions
+ * other than those described here, or to purchase support for this
+ * software, please contact iptel.org by e-mail at the following addresses:
+ *    info@iptel.org
+ *
+ * ser is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software 
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * History:
+ * --------
+ *  2004-03-16  created (bogdan)
+ */
+
+
+#ifndef _UL_CALLBACKS_H
+#define _UL_CALLBACKS_H
+
+#include "ucontact.h"
+
+
+#define UL_CONTACT_ADD         (1<<0)
+#define UL_CONTACT_UPDATE      (1<<1)
+#define UL_CONTACT_DELETE      (1<<2)
+#define UL_CONTACT_EXPIRE      (1<<3)
+#define ULCB_MAX               ((1<<4)-1)
+
+/* callback function prototype */
+typedef void (ul_cb) (ucontact_t *c, int type, void *param);
+/* register callback function prototype */
+typedef int (*register_ulcb_f)( int cb_types, ul_cb f, void *param);
+
+
+struct ul_callback {
+	int id;                      /* id of this callback - useless */
+	int types;                   /* types of events that trigger the callback*/
+	ul_cb* callback;             /* callback function */
+	void *param;                 /* param to be passed to callback function */
+	struct ul_callback* next;
+};
+
+struct ulcb_head_list {
+	struct ul_callback *first;
+	int reg_types;
+};
+
+
+extern struct ulcb_head_list*  ulcb_list;
+
+
+#define exists_ulcb_type(_T_, _types_) \
+	( (ulcb_list->reg_types)|(_types_) )
+
+
+int init_tmcb_lists();
+
+void destroy_tmcb_lists();
+
+
+/* register a callback for several types of events */
+int register_ulcb( int types, ul_cb f, void *param );
+
+/* run all transaction callbacks for an event type */
+static inline void run_ul_callbacks( int type , ucontact_t *c)
+{
+	struct ul_callback    *cbp;
+
+	for (cbp=ulcb_list->first; cbp; cbp=cbp->next)  {
+		DBG("DBG:usrloc: contact=%p, callback type %d, id %d entered\n",
+			c, cbp->types, cbp->id );
+		cbp->callback( c, type, cbp->param );
+	}
+}
+
+
+
+#endif
