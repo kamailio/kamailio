f030237d4ec8fba90b40463af77eeb749b2838b8
Jan Janak <jan@iptel.org>
2004-04-13 14:58:05 +0000

    - Broken headers fixed - Missing prototypes added (Reported by Maxim Sobolev)

--- modules_s/postgres/dbase.h
+++ modules_s/postgres/dbase.h
@@ -106,4 +106,16 @@ int db_update(db_con_t* _h, db_key_t* _k, db_op_t* _o, db_val_t* _v,
 	      db_key_t* _uk, db_val_t* _uv, int _n, int _un);
 
 
+/*
+ * Store name of table that will be used by
+ * subsequent database functions
+ */
+int use_table(db_con_t* _h, const char* _t);
+
+int val2str(db_val_t* _v, char* _s, int* _len);
+
+int free_result(db_res_t* _r);
+
+int convert_result(db_con_t* _h, db_res_t* _r);
+
 #endif /* DBASE_H */
