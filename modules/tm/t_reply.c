6e7c897f1ea8ca5e052d038a29163b1ed80f5b20
Jiri Kuthan <jiri@iptel.org>
2004-05-31 17:12:39 +0000

    additional logs which should help to explain why we sometimes see errors like these: May 31 14:05:49 proxy00 /usr/local/sbin/ser[9043]: Warning: run_failure_handlers: no UAC support

--- modules/tm/t_reply.c
+++ modules/tm/t_reply.c
@@ -608,12 +608,19 @@ static inline int run_failure_handlers(struct cell *t, struct sip_msg *rpl,
 
 	/* failure_route for a local UAC? */
 	if (!shmem_msg) {
-		LOG(L_WARN, "Warning: run_failure_handlers: no UAC support\n");
-		return 0;
-	}
+		LOG(L_WARN, 
+			"Warning: run_failure_handlers: no UAC support (%d, %d) \n",
+			t->on_negative, 
+			t->tmcb_hl.reg_types);
+                return 0;
+        }
 
 	/* don't start faking anything if we don't have to */
 	if ( !has_tran_tmcbs( t, TMCB_ON_FAILURE) && !t->on_negative ) {
+		LOG(L_WARN, 
+			"Warning: run_failure_handlers: no negative handler (%d, %d)\n",
+			t->on_negative, 
+			t->tmcb_hl.reg_types);
 		return 1;
 	}
 
