ca85b625d46b4420b15e218dfbf8f90f59f27465
Maxim Sobolev <sobomax@sippysoft.com>
2004-07-07 14:56:49 +0000

    Fix ipv6 compilation on FreeBSD and probably other systems with Kame-derived IPv6 stacks: IN6_IS_ADDR_MULTICAST() doesn't automagically casts its argument to struct in6_addr * there.

--- ip_addr.c
+++ ip_addr.c
@@ -176,7 +176,7 @@ int is_mcast(struct ip_addr* ip)
 		return IN_MULTICAST(htonl(ip->u.addr32[0]));
 #ifdef USE_IPV6
 	} else if (ip->af==AF_INET6){
-		return IN6_IS_ADDR_MULTICAST(ip->u.addr);
+		return IN6_IS_ADDR_MULTICAST((struct in6_addr *)ip->u.addr);
 #endif /* USE_IPV6 */
 	} else {
 		LOG(L_ERR, "ERROR: is_mcast: Unsupported protocol family\n");
