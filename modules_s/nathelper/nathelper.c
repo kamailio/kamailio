0d44f9e365234283435d78602feb66484387ab8a
Jan Janak <jan@iptel.org>
2004-09-03 18:46:40 +0000

    Allocated buffer too short -- fixed

--- modules_s/nathelper/nathelper.c
+++ modules_s/nathelper/nathelper.c
@@ -1669,7 +1669,7 @@ add_rcv_param_f(struct sip_msg* msg, char* str1, char* str2)
 	}
 
 	while(c) {
-		param = (char*)pkg_malloc(RECEIVED_LEN + uri.len);
+		param = (char*)pkg_malloc(RECEIVED_LEN + 2 + uri.len);
 		if (!param) {
 			LOG(L_ERR, "add_rcv_param: No memory left\n");
 			return -1;
