11342eed4bebbe8dae8e4bc0fb589797fed889f2
Bogdan-Andrei Iancu <bogdan@voice-system.ro>
2004-10-18 16:01:32 +0000

    - finishing the fixup

--- usr_avp.c
+++ usr_avp.c
@@ -266,7 +266,7 @@ struct usr_avp *search_next_avp( struct usr_avp *avp,  int_str *val )
 	if (avp->flags&AVP_NAME_STR)
 		avp = internal_search_name_avp( avp->next, avp->id, get_avp_name(avp));
 	else
-		avp = internal_search_ID_avp( avp, avp->id );
+		avp = internal_search_ID_avp( avp->next, avp->id );
 
 	if (avp && val)
 		get_avp_val(avp, val);
