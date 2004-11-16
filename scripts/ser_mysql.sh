5b9692c5b4faaf0c34790807587546ca8126e152
Daniel-Constantin Mierla <daniel@voice-system.ro>
2004-11-16 10:12:38 +0000

    - version for table silo set to 3

--- scripts/ser_mysql.sh
+++ scripts/ser_mysql.sh
@@ -246,7 +246,7 @@ INSERT INTO version VALUES ( 'aliases', '6');
 INSERT INTO version VALUES ( 'active_sessions', '1');
 INSERT INTO version VALUES ( 'acc', '2');
 INSERT INTO version VALUES ( 'config', '1');
-INSERT INTO version VALUES ( 'silo', '2');
+INSERT INTO version VALUES ( 'silo', '3');
 INSERT INTO version VALUES ( 'realm', '1');
 INSERT INTO version VALUES ( 'domain', '1');
 INSERT INTO version VALUES ( 'uri', '1');
