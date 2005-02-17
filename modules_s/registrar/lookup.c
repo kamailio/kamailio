feb92a239907bdbe54ae58c43ebd14a20eacad61
Jan Janak <jan@iptel.org>
2005-02-17 15:56:15 +0000

    - Registrar now properly initializes q parameter in the destination set   from the user location database.

--- modules_s/registrar/lookup.c
+++ modules_s/registrar/lookup.c
@@ -101,6 +101,8 @@ int lookup(struct sip_msg* _m, char* _t, char* _s)
 			}
 		}
 
+		set_ruri_q(ptr->q);
+
 		nat |= ptr->flags & FL_NAT;
 		ptr = ptr->next;
 	} else {
@@ -114,7 +116,7 @@ int lookup(struct sip_msg* _m, char* _t, char* _s)
 
 	while(ptr) {
 		if (VALID_CONTACT(ptr, act_time)) {
-			if (append_branch(_m, ptr->c.s, ptr->c.len, ptr->received.s, ptr->received.len, Q_UNSPECIFIED) == -1) {
+			if (append_branch(_m, ptr->c.s, ptr->c.len, ptr->received.s, ptr->received.len, ptr->q) == -1) {
 				LOG(L_ERR, "lookup(): Error while appending a branch\n");
 				     /* Return 1 here so the function succeeds even if appending of
 				      * a branch failed
