b2d2a4ec86e8b67f468bab522d8b10bc1c909e8e
Karel Kozlik <karel@iptel.org>
2005-04-26 18:10:03 +0000

    added columns fname and lname to table speed_dial

--- scripts/ser_mysql.sh
+++ scripts/ser_mysql.sh
@@ -682,6 +682,8 @@ CREATE TABLE speed_dial (
   sd_username varchar(64) NOT NULL default '',
   sd_domain varchar(128) NOT NULL default '',
   new_uri varchar(192) NOT NULL default '',
+  fname varchar(128) NOT NULL default '',
+  lname varchar(128) NOT NULL default '',
   description varchar(64) NOT NULL default '',
   PRIMARY KEY  ($USERCOL,domain,sd_username,sd_domain)
 ) $TABLE_TYPE;
