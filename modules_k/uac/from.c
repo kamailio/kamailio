081af2f360fa21ca4828e36556c9e43d5826b2ad
Bogdan-Andrei Iancu <bogdan@voice-system.ro>
2005-08-09 10:10:53 +0000

    fixed the B64 encoding of the orginal FROM URI (reported by Tony Jago)
    git-svn-id: https://openser.svn.sourceforge.net/svnroot/openser/trunk@173 689a6050-402a-0410-94f2-e92a70836424

--- modules_k/uac/from.c
+++ modules_k/uac/from.c
@@ -46,7 +46,7 @@ static char enc_table64[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 static int dec_table64[256];
 
 
-#define text3B64_len(_l)   ( ( ((_l)+1)/3 ) << 2 )
+#define text3B64_len(_l)   ( ( ((_l)+2)/3 ) << 2 )
 
 
 void init_from_replacer()
