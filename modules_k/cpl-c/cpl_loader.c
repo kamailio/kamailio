1edcc12923c4289a910b339eff2a5d9763811bd3
Bogdan-Andrei Iancu <bogdan@voice-system.ro>
2005-08-09 10:07:09 +0000

    include fixes for FreeBSD (thanks to Jesus Rodriguez)
    git-svn-id: https://openser.svn.sourceforge.net/svnroot/openser/trunk@172 689a6050-402a-0410-94f2-e92a70836424

--- modules_k/cpl-c/cpl_loader.c
+++ modules_k/cpl-c/cpl_loader.c
@@ -28,10 +28,10 @@
 
 
 #include <stdio.h>
+#include <sys/types.h>
 #include <sys/uio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <sys/uio.h>
--- modules_k/rr/rr_mod.c
+++ modules_k/rr/rr_mod.c
@@ -34,6 +34,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
 #include <regex.h>
 
 #include "../../sr_module.h"
