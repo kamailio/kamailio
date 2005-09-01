adfd3163edc1b0b878255581d9bd1416ba2f4210
Andrei Pelinescu-Onciul <andrei@iptel.org>
2005-09-01 12:19:07 +0000

    -sleep_us typo fix, thanks to Tomas Mandys

--- ut.h
+++ ut.h
@@ -261,7 +261,7 @@ inline static int string2hex(
 inline static void sleep_us( unsigned int nusecs )
 {
 	struct timeval tval;
-	tval.tv_sec=nusecs/100000;
+	tval.tv_sec =nusecs/1000000;
 	tval.tv_usec=nusecs%1000000;
 	select(0, NULL, NULL, NULL, &tval );
 }
