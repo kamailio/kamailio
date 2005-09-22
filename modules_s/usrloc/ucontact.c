8664f64b1d7385fb5f4a7dd4d5780dc7bdae46e5
Nils Ohlmeier <nils@iptel.org>
2005-09-22 22:05:04 +0000

    fixed memory leak introduce by sip.instance; thanks Andreas Granig; closes #SER-74

--- modules_s/usrloc/ucontact.c
+++ modules_s/usrloc/ucontact.c
@@ -133,6 +133,7 @@ error:
  		if ((*_c)->user_agent.s) shm_free((*_c)->user_agent.s);
  		if ((*_c)->callid.s) shm_free((*_c)->callid.s);
  		if ((*_c)->c.s) shm_free((*_c)->c.s);
+		if ((*_c)->instance.s) shm_free((*_c)->instance.s);
  		shm_free(*_c);
 	}
 	return -1;	
@@ -149,6 +150,7 @@ void free_ucontact(ucontact_t* _c)
 	if (_c->user_agent.s) shm_free(_c->user_agent.s);
 	if (_c->callid.s) shm_free(_c->callid.s);
 	if (_c->c.s) shm_free(_c->c.s);
+	if (_c->instance.s) shm_free(_c->instance.s);
 	shm_free(_c);
 }
 
