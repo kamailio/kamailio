01125e700960ce73d5c197b3296e8ffad151a95c
Bogdan-Andrei Iancu <bogdan@voice-system.ro>
2005-11-29 12:59:27 +0000

    - bug fixed in matching local ACKs (versus e2e ACKs) - credits go to   Cesc Santasusana <cesc.santa@gmail.com>
    git-svn-id: https://openser.svn.sourceforge.net/svnroot/openser/trunk@410 689a6050-402a-0410-94f2-e92a70836424

--- modules_k/tm/t_lookup.c
+++ modules_k/tm/t_lookup.c
@@ -284,7 +284,7 @@ static int matching_3261( struct sip_msg *p_msg, struct cell **trans,
 	struct via_body *via1;
 	int is_ack;
 	int dlg_parsed;
-	int ret;
+	int ret = 0;
 	struct cell *e2e_ack_trans;
 
 	e2e_ack_trans=0;
@@ -329,6 +329,7 @@ static int matching_3261( struct sip_msg *p_msg, struct cell **trans,
 			ret=ack_matching(p_cell /* t w/invite */, p_msg /* ack */);
 			if (ret>0) {
 				e2e_ack_trans=p_cell;
+				break;
 			}
 			/* this ACK is neither local "negative" one, nor a proxied
 			 * end-2-end one, nor an end-2-end one for a UAS transaction
@@ -351,7 +352,7 @@ static int matching_3261( struct sip_msg *p_msg, struct cell **trans,
 	/* just check if it we found an e2e ACK previously */
 	if (e2e_ack_trans) {
 		*trans=e2e_ack_trans;
-		return 2;
+		return ret;
 	}
 	DBG("DEBUG: RFC3261 transaction matching failed\n");
 	return 0;
