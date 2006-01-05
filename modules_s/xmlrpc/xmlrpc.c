ddbc8d2060cea1043a8c6e5a98479f80734fc209
Jan Janak <jan@iptel.org>
2006-01-05 09:36:01 +0000

    - removed XML_PARSE_COMPACT flag (not present in older libxml versions)

--- modules_s/xmlrpc/xmlrpc.c
+++ modules_s/xmlrpc/xmlrpc.c
@@ -1099,8 +1099,7 @@ static int open_doc(rpc_ctx_t* ctx, struct sip_msg* msg)
 	ctx->doc = xmlReadMemory(doc.s, doc.len, 0, 0, 
 				 XML_PARSE_NOBLANKS |
 				 XML_PARSE_NONET |
-				 XML_PARSE_NOCDATA |
-				 XML_PARSE_COMPACT);
+				 XML_PARSE_NOCDATA);
 	
 	if (!ctx->doc) {
 		set_fault(reply, 400, "Invalid XML-RPC Document");
