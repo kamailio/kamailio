5b1455d07009b537559e7fef9743f58c923d12c5
Tomas Mandys <tomas.mandys@iptel.org>
2006-01-19 10:29:10 +0000

    -changed "str str" declaration to "struct _str str" to make gcc happy when compiling with "-x c++" options (required when a module uses c++ .h files)

--- route_struct.h
+++ route_struct.h
@@ -100,7 +100,7 @@ enum { NOSUBTYPE=0, STRING_ST, NET_ST, NUMBER_ST, IP_ST, RE_ST, PROXY_ST,
 /* Expression operand */
 union exp_op {
 	struct expr* expr;
-	str str;
+	struct _str str;
 	char* string;
 	void* param;
 	int intval;
@@ -123,7 +123,7 @@ typedef struct {
 	union {
 		long number;
 		char* string;
-		str str;
+		struct _str str;
 		void* data;
 		avp_spec_t* attr;
 		select_t* select;
