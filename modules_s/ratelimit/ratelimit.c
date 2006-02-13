e47f5547d418ee387db26e76c805228a201113f0
Hendrik Scholz <hscholz@raisdorf.net>
2006-02-13 19:57:11 +0000

    - add GPL license - remove parser/parse_methods.[ch] includes
    suggested by andrei@

--- modules_s/ratelimit/ratelimit.c
+++ modules_s/ratelimit/ratelimit.c
@@ -1,19 +1,31 @@
-/******************************************************************************
+/*
+ * $Id$
  *
  * ratelimit module
  *
- * This modules tracks the number of requests traversing SER and allows
- * dropping of packets if a given threshold is reached.
- * Two algorithms are implemented: a trivial tail drop one and a probably
- * better Random Early Detection (RED) based.
+ * Copyright (C) 2006 Hendrik Scholz <hscholz@raisdorf.net>
  *
- * define RL_WITH_RED if you like to use the RED algorithm (default)
+ * This file is part of ser, a free SIP server.
  *
- * (c) 2006 Hendrik Scholz <hscholz@raisdorf.net>
+ * ser is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version
  *
- * $Id$
+ * For a license to use the ser software under conditions
+ * other than those described here, or to purchase support for this
+ * software, please contact iptel.org by e-mail at the following addresses:
+ *    info@iptel.org
  *
- *****************************************************************************/
+ * ser is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 
 #include <stdio.h>
 
@@ -22,8 +34,6 @@
 #include "../../dprint.h"
 #include "../../timer.h"
 #include "../../ut.h"
-#include "../../parser/parse_methods.h"
-#include "../../parser/parse_methods.c"
 
 MODULE_VERSION
 
