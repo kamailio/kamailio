9c2b45c7b429551201b13d3c4db351800730b70a
Jan Janak <jan@iptel.org>
2006-02-16 14:29:34 +0000

    - remove test function

--- modules/db_postgres/db_mod.c
+++ modules/db_postgres/db_mod.c
@@ -181,7 +181,7 @@ struct module_exports exports = {
 	cmds,
 	0,         /* RPC methods */
 	params,    /*  module parameters */
-        pg_test,   /* module initialization function */
+        0,         /* module initialization function */
 	0,         /* response function*/
 	0,         /* destroy function */
 	0,         /* oncancel function */
