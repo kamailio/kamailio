3cfe232ae8b9eb285d037165381c77102512d635
Andrei Pelinescu-Onciul <andrei@iptel.org>
2006-03-30 10:37:54 +0000

    - removed unused code

--- parser/parse_event.c
+++ parser/parse_event.c
@@ -81,7 +81,6 @@ static inline int event_parser(char* _s, int _l, event_t* _e)
 {
 	str tmp;
 	char* end;
-	char buf[128];
 
 	tmp.s = _s;
 	tmp.len = _l;
@@ -98,9 +97,6 @@ static inline int event_parser(char* _s, int _l, event_t* _e)
 	end = skip_token(tmp.s, tmp.len);
 
 	_e->text.len = end - tmp.s;
-
-	strncpy(buf, tmp.s, tmp.len);
-	buf[tmp.len] = 0;
 
 	if ((_e->text.len == PRES_STR_LEN) && 
 	    !strncasecmp(PRES_STR, tmp.s, _e->text.len)) {
