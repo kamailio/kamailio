b49cb497f7871c5f85268d4d2708e5298b03a15a
Jan Janak <jan@iptel.org>
2006-04-19 12:49:22 +0000

    - cmdline option support

--- scripts/dbtext/ser_dbtext.sh
+++ scripts/dbtext/ser_dbtext.sh
@@ -6,162 +6,235 @@
 #
 # TODO: Check if user and group exist
 #
+# Copyright (C) 2006 iptelorg GmbH
+#
 
 #################################################################
-# config vars
+# configuration variables
 #################################################################
-DEFAULT_ROOTDIR="/var/local/lib/ser"
-DEFAULT_DBNAME="ser"
-DEFAULT_OWNER="ser"
-DEFAULT_GROUP="ser"
+DEFAULT_ROOTDIR="/var/local/lib/ser" # Default dbtext root directory
+DEFAULT_DBNAME="ser"                 # Default database name
+DEFAULT_OWNER="ser"                  # The owner of dbtext files
+DEFAULT_GROUP="ser"                  # The group of dbtext files
+DEFAULT_SRCDB="ser_db"      # Source data generated from XML description
 
-DEFAULT_SRCDB="ser_db"
 
 usage() {
 cat <<EOF
-Usage: $COMMAND create 
-       $COMMAND drop   
-       $COMMAND backup [database] <file.tar> 
-       $COMMAND restore [database] <file.tar>
-       $COMMAND copy <source_db> <dest_db>
-   
-  Command 'create' creates database named '${DBNAME}' containing tables 
-  needed for SER and SERWeb. 
+NAME
+  $COMMAND - SER dbtext database administration tool
 
-  Commmand 'drop' deletes database named '${DBNAME}'.
+SYNOPSIS
+  $COMMAND [options] create
+  $COMMAND [options] drop
+  $COMMAND [options] backup [filename.tar] 
+  $COMMAND [options] restore [filename.tar]
 
-  Command 'backup' Dumps the contents of the database in <file>. If no
-  database name is provided on the command line then the default '${DBNAME}'
-  database will be used.
+DESCRIPTION
+  This tool is a simple shell wrapper that can be used to create, drop, or 
+  backup SER database stored in plain-text files on the filesystem (used
+  by dbtext SER module). See section COMMANDS for brief overview of supported 
+  actions.
 
-  Command 'restore' will load the datata previously saved with 'backup'
-  command in the database. If no database name is provided on the command
-  line then '${DBNAME}' database will be loaded.
-    WARNING: Any existing data in the database being restored will be
-             overwritten. Make sure that SER is not running when performing
-	     restore command, otherwise it could corrupt the database !
+  The database template for SER dbtext database is stored in dbtext_template
+  directory which can usualy be found in /usr/lib/ser (depending on
+  installation). You can use the template to create SER database manually
+  if you cannot or do not want to use this shell wrapper.
 
-  Command 'copy' will copy the contents of <source_db> to database <dest_db>.
-  The destination database must not exist -- it will be created.
+COMMANDS
+  create
+    Create a new SER database from scratch. The database must not exist.
+    This command creates the database, the default name of the database
+    is '${DEFAULT_DBNAME}' (the default name can be changed using a command line
+    parameter, see below). The database will be created in the default dbtext 
+    database directory (${DEFAULT_ROOTDIR}) unless changed using -d command
+    line option (see below). You can use command line options to change the
+    default database name, owner username and group. 
 
-  Environment variables:
-    ROOTDIR   The root directory of all dbtext databases (${ROOTDIR})
-    DBNAME    Default name of SER database (${DBNAME})
-    OWNER     The username of owner of all database files. This is usualy
-              the username SER runs under.
-    GROUP     THe group of all database files. This should be same as
-              the group of SER processes.
-           
-Report bugs to <ser-bugs@iptel.org>
+  drop
+    This command can be used to delete SER database. WARNING: This command
+    will delete all data in the database and this action cannot be undone
+    afterwards. Make sure that you have backups if you want to keep the data
+    from the database.
+
+  backup <filename>
+    Backup the contents of SER database. If you specify a filename then the
+    contents of the database will be saved in that file, otherwise the tool
+    will dumps the contents on the standard output.
+
+  restore <filename>
+    Load the contents of SER database from a file (if you specify one) or
+    from the standard input. 
+
+OPTIONS
+  -h, --help
+      Display this help text.
+
+  -n NAME, --name=NAME
+      Database name of SER database.
+      (Default value is '$DEFAULT_DBNAME')
+
+  -d DIR, --dir=DIR
+      Root directory of dbtext databases.
+      (Default value is '$DEFAULT_ROOTDIR')
+
+  -o USERNAME, --owner=USERNAME
+      Owner of files in the database.
+      (Default value is '$DEFAULT_OWNER')
+
+  -g GROUP, --group=GROUP
+      Group of files in the database.
+      (Default value is '$DEFAULT_GROUP')
+
+  -v, --verbose
+      Enable verbose mode. This option can be given multiple times
+      to produce more and more output.
+        
+AUTHOR
+  Written by Jan Janak <jan@iptel.org>
+
+COPYRIGHT
+  Copyright (C) 2006 iptelorg GmbH
+  This is free software. You may redistribute copies of it under the
+  termp of the GNU General Public License. There is NO WARRANTY, to the
+  extent permitted by law.
+
+FILES
+  $SRCDB
+    
+REPORTING BUGS
+  Report bugs to <ser-bugs@iptel.org>             
 EOF
 } #usage
 
+dbg()
+{
+    if [ ! -z $VERBOSE ] ; then
+	echo $@
+    fi
+}
+
+err()
+{
+    echo "ERROR: $@"
+}
+
 
 # Dump the contents of the database to stdout
-db_save() 
+backup_db() 
 {
-    if [ $# -ne 2 ] ; then
-	echo "ERROR: Bug in $COMMAND"
-	exit 1
+    if [ ! -z $VERBOSE ] ; then
+	(cd "${ROOTDIR}/$1"; tar cfv - *)
+    else
+	(cd "${ROOTDIR}/$1"; tar cf - * 2>/dev/null)
     fi
-    (cd $ROOTDIR/$1; tar cf - *) > $2
 }
 
 
-# Load the contents of the database from a file
-db_load() #pars: <database name> <filename>
+# Re-create the contents of database from a tar file
+restore_db()
 {
-    if [ $# -ne 2 ] ; then
-	echo "ERROR: Bug in $COMMAND"
+    if [ ! -d "${ROOTDIR}/$1" ] ; then
+	err "Database $1 does not exist, create it first"
 	exit 1
     fi
-    rm -rf $ROOTDIR/$1
-    mkdir -p $ROOTDIR/$1
-    chown $USER:$GROUP $ROOTDIR/$1
-    tar xf $2 -C $ROOTDIR/$1 
-}
-
-
-# copy a database to database_bak
-db_copy() # par: <source_database> <destination_database>
-{
-	if [ $# -ne 2 ] ; then
-		echo  "ERROR: Bug in $COMMAND"
-		exit 1
-	fi
-	cp -a $ROOTDIR/$1 $ROOTDIR/$2
+    if [ ! -z $VERBOSE ] ; then
+	(cd $ROOTDIR/$1; rm -f *; tar xfv -)
+    else
+	(cd $ROOTDIR/$1; rm -f *; tar xf -)
+    fi
 }
 
 
 # Drop SER database
-ser_drop()
+drop_db()
 {
-    # Drop dabase
-    # Revoke user permissions
-
-    echo "Dropping SER database"
-    rm -rf $ROOTDIR/$DBNAME
-} #ser_drop
+    dbg "Removing dbtext database directory ${ROOTDIR}/${DBNAME}"
+    rm -rf "${ROOTDIR}/${DBNAME}"
+} # drop_db
 
 
 # Create SER database
-ser_create ()
+create_db ()
 {
-    echo "Creating SER database"
-    mkdir -p $ROOTDIR/$DBNAME
-    cp -a $SRCDB/* $ROOTDIR/$DBNAME
-    chown -R $OWNER:$GROUP $ROOTDIR/$DBNAME
-    chmod -R 0660 $ROOTDIR/$DBNAME
-} # ser_create
+    dbg "Creating database directory ${ROOTDIR}/${DBNAME}"
+    mkdir -p "${ROOTDIR}/${DBNAME}"
+    if [ ! -d ] ; then
+	err "Could not create directory ${ROOTDIR}/${DBNAME}"
+	exit 1
+    fi
+    
+    dbg "Copying template files from ${SRCDB} to ${ROOTDIR}/${DBNAME}"
+    cp -a ${SRCDB}/* "${ROOTDIR}/${DBNAME}"
+
+    dbg "Setting owner and group of new files to ${OWNER}:${GROUP}"
+    chown -R "${OWNER}:${GROUP}" "${ROOTDIR}/${DBNAME}"
+    chmod -R 0660 ${ROOTDIR}/${DBNAME}/*
+} # create_db
 
 
 
 # Main program
-
 COMMAND=`basename $0`
 
-if [ -z "$ROOTDIR" ]; then
-    ROOTDIR=$DEFAULT_ROOTDIR;
-fi
+if [ -z "$DBNAME" ] ; then DBNAME=$DEFAULT_DBNAME; fi;
+if [ -z "$OWNER" ]  ; then OWNER=$DEFAULT_OWNER; fi;
+if [ -z "$GROUP" ]  ; then GROUP=$DEFAULT_GROUP; fi;
+if [ -z "$SRCDB" ]  ; then SRCDB=`dirname $0`"/"$DEFAULT_SRCDB; fi
 
-if [ -z "$DBNAME" ]; then
-    DBNAME=$DEFAULT_DBNAME;
-fi
+TEMP=`getopt -o hn:d:o:g:v --long help,name:,dir:,owner:,group:,verbose -n $COMMAND -- "$@"`
+if [ $? != 0 ] ; then exit 1; fi
+eval set -- "$TEMP"
 
-if [ -z "$OWNER" ]; then
-    OWNER=$DEFAULT_OWNER;
-fi
-
-if [ -z "$GROUP" ]; then
-    GROUP=$DEFAULT_GROUP;
-fi
-
-if [ -z "$SRCDB" ]; then
-    SRCDB=$DEFAULT_SRCDB;
-fi
+while true ; do
+    case "$1" in
+	-h|--help)    usage; exit 0 ;;
+	-n|--name)    DBNAME=$2; shift 2 ;;
+        -d|--dir)     ROOTDIR=$2; shift 2 ;;
+	-o|--owner)   OWNER=$2;  shift 2 ;;
+	-g|--group)   GROUP=$2;  shift 2 ;;
+        -v|--verbose) export OPTS="${OPTS} -v "; VERBOSE=1; shift ;;
+	--)           shift; break ;;
+	*)            echo "Internal error"; exit 1 ;;
+    esac
+done
 
 if [ $# -eq 0 ]; then
     usage
     exit 1
 fi
 
+TEMP=`id $OWNER > /dev/null 2>&1`
+if [ $? != 0 ] ; then
+    err "User '$OWNER' does not exist"
+    exit 1
+fi
+
+TEMP=`id -g $GROUP > /dev/null 2>&1`
+if [ $? != 0 ] ; then
+    err "Group '$GROUP' does not exist"
+    exit 1
+fi
+
 case $1 in
-    create) # Create SER database and users
-	ser_create
+    create) # Create SER database
+	create_db
 	exit $?
-		;;
+	;;
     
-    drop) # Drop SER database and users
-	ser_drop
+    drop) # Drop SER database
+	drop_db
 	exit $?
 	;;
 
     backup) # backup SER database
 	shift
 	if [ $# -eq 1 ]; then
-	    db_save $DBNAME $1
-	elif [ $# -eq 2 ]; then
-	    db_save $1 $2
+	    dbg "Backing up ${ROOTDIR}/$DBNAME"
+	    backup_db $DBNAME > $1
+	elif [ $# -eq 0 ]; then
+	    dbg "Backing up ${ROOTDIR}/$DBNAME"
+	    backup_db $DBNAME
 	else
 	    usage
 	    exit 1
@@ -172,9 +245,11 @@ case $1 in
     restore) # restore SER database
 	shift
 	if [ $# -eq 1 ]; then
-	    db_load $DBNAME $1
-	elif [ $# -eq 2 ]; then
-	    db_load $1 $2
+	    dbg "Restoring ${ROOTDIR}/$DBNAME"
+	    cat $1 | restore_db $DBNAME
+	elif [ $# -eq 0 ]; then
+	    dbg "Restoring ${ROOTDIR}/$DBNAME"
+	    restore_db $DBNAME
 	else
 	    usage
 	    exit 1
@@ -182,17 +257,8 @@ case $1 in
 	exit $?
 	;;
         
-    copy)
-	shift
-	if [ $# -ne 2 ]; then
-	    usage
-	    exit 1
-	fi
-	db_copy $1 $2
-	exit $?
-	;;
-    
     *)
+	echo "Unknown command '$1'"
 	usage
 	exit 1;
 	;;
