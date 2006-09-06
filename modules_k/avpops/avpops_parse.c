b9ffa4795a9320b21ece4cc78df5f36c6fd4fd9f
Bogdan-Andrei Iancu <bogdan@voice-system.ro>
2006-09-06 11:05:17 +0000

    - syncronize the copyright text with the rest of openser.  - remove from indication of "office@voice-system.ro" from copyright text
    git-svn-id: https://openser.svn.sourceforge.net/svnroot/openser/trunk@1089 689a6050-402a-0410-94f2-e92a70836424

--- modules_k/avpops/avpops_parse.c
+++ modules_k/avpops/avpops_parse.c
@@ -1,16 +1,16 @@
 /*
  * $Id$
  *
- * Copyright (C) 2004 Voice Sistem SRL
+ * Copyright (C) 2004-2006 Voice Sistem SRL
  *
- * This file is part of Open SIP Express Router.
+ * This file is part of Open SIP Express Router (openser).
  *
- * AVPOPS OpenSER-module is free software; you can redistribute it and/or
+ * openser is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
  *
- * AVPOPS OpenSER-module is distributed in the hope that it will be useful,
+ * openser is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
@@ -18,11 +18,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- * For any questions about this software and its license, please contact
- * Voice Sistem at following e-mail address:
- *         office@voice-sistem.ro
- *
  *
  * History:
  * ---------
