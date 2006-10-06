35bfdd613a71f6838752807ec3c0e50dfc1bf037
Bogdan-Andrei Iancu <bogdan@voice-system.ro>
2006-10-06 13:23:27 +0000

    - reverted some changes that were pushed on cvs by mistake
    git-svn-id: https://openser.svn.sourceforge.net/svnroot/openser/trunk@1153 689a6050-402a-0410-94f2-e92a70836424

--- modules_k/domain/domain.c
+++ modules_k/domain/domain.c
@@ -175,36 +175,35 @@ int is_from_local(struct sip_msg* _msg, char* _s1, char* _s2)
  */
 int is_uri_host_local(struct sip_msg* _msg, char* _s1, char* _s2)
 {
-/*  str branch;
-    qvalue_t q;
-    struct sip_uri puri;
-*/
+	str branch;
+	qvalue_t q;
+	struct sip_uri puri;
+
 	if ((route_type == REQUEST_ROUTE) || (route_type == BRANCH_ROUTE)
 			|| (route_type == FAILURE_ROUTE)) {
 		if (parse_sip_msg_uri(_msg) < 0) {
-		    LOG(L_ERR, "is_uri_host_local(): Error while parsing R-URI\n");
+			LOG(L_ERR, "is_uri_host_local(): Error while parsing R-URI\n");
 			return -1;
 		}
 		return is_domain_local(&(_msg->parsed_uri.host));
-/*    } else if (route_type == FAILURE_ROUTE) {
-			branch.s = get_branch(0, &branch.len, &q, 0, 0, 0);
+	} else if (route_type == FAILURE_ROUTE) {
+			branch.s = get_branch(0, &branch.len, &q, 0, 0, 0, 0);
 			if (branch.s) {
-			    if (parse_uri(branch.s, branch.len, &puri) < 0) {
+				if (parse_uri(branch.s, branch.len, &puri) < 0) {
 					LOG(L_ERR, "is_uri_host_local():"
 					" Error while parsing branch URI\n");
 					return -1;
-			    }
+				}
 				return is_domain_local(&(puri.host));
 			} else {
-			    LOG(L_ERR, "is_uri_host_local(): Branch is missing, "
-				" error in script\n");
-			    return -1;
+				LOG(L_ERR, "is_uri_host_local(): Branch is missing, "
+					" error in script\n");
+				return -1;
 			}
-*/
-    } else {
-	LOG(L_ERR, "is_uri_host_local(): Unsupported route type\n");
-	return -1;
-    }
+	} else {
+		LOG(L_ERR, "is_uri_host_local(): Unsupported route type\n");
+		return -1;
+	}
 }
 
 
