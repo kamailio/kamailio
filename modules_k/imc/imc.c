a18ee1b2790d0bf79a628f6f418b9c77779a8da1
Bogdan-Andrei Iancu <bogdan@voice-system.ro>
2006-10-10 13:15:02 +0000

    - history added to the C files
    git-svn-id: https://openser.svn.sourceforge.net/svnroot/openser/trunk@1162 689a6050-402a-0410-94f2-e92a70836424

--- modules_k/imc/imc.c
+++ modules_k/imc/imc.c
@@ -21,6 +21,9 @@
  * along with this program; if not, write to the Free Software 
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
+ * History:
+ * ---------
+ *  2006-10-06  first version (anca)
  */
 
 #include <stdio.h>
