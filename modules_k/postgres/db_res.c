b6e8ecbb57dede9b8b50427e417fe6dc3f70aaba
Bogdan-Andrei Iancu <bogdan@voice-system.ro>
2006-11-01 16:22:17 +0000

    added Norman's copyright on the changed files
    git-svn-id: https://openser.svn.sourceforge.net/svnroot/openser/trunk@1217 689a6050-402a-0410-94f2-e92a70836424

--- modules_k/postgres/db_res.c
+++ modules_k/postgres/db_res.c
@@ -5,6 +5,7 @@
  * the mysql module, thus it's similarity.
  *
  * Copyright (C) 2003 August.Net Services, LLC
+ * Copyright (C) 2006 Norman Brandinger
  *
  * This file is part of openser, a free SIP server.
  *
