0863c68c5e0693c24bdbda7b1b35a08db7b6bb6b
Vaclav Kubart <vaclav.kubart@iptel.org>
2006-11-01 14:51:53 +0000

    removed route serialization functions - used functions from CDS library

--- modules_s/dialog/serialize_dlg.c
+++ modules_s/dialog/serialize_dlg.c
@@ -28,6 +28,7 @@
 
 #include <cds/logger.h>
 #include <cds/memory.h>
+#include <cds/rr_serialize.h>
 
 static int serialize_dlg_state(sstream_t *ss, dlg_state_t *state)
 {
@@ -256,84 +257,6 @@ static int serialize_route_ex(sstream_t *ss, rr_t **_r)
 	return res;
 }
 #endif
-
-static void rr_dup(rr_t **dst, rr_t *pkg_rr)
-{
-	rr_t *tmp = NULL;
-	int res;
-	
-	tmp = pkg_rr;
-	
-	while (pkg_rr) {
-		res = shm_duplicate_rr(dst, pkg_rr);
-		dst = &(*dst)->next;
-		pkg_rr = pkg_rr->next;
-	}
-	if (tmp) free_rr(&tmp);
-}
-
-static int serialize_route(sstream_t *ss, rr_t **_r)
-{
-	int do_it = 0;
-	int res = 0;
-	if (is_input_sstream(ss)) { /* read route */
-		if (serialize_int(ss, &do_it) != 0) return -1;
-		*_r = NULL;
-	}
-	else { /* store route */
-		if (*_r) do_it = 1;
-		else do_it = 0;
-		if (serialize_int(ss, &do_it) != 0) return -1;
-	}
-		
-	if (do_it) {
-		str s;
-		if (*_r) {
-			s.s = (*_r)->nameaddr.name.s;
-			s.len = (*_r)->len;
-		}
-
-		res = serialize_str(ss, &s) | res;
-		if (is_input_sstream(ss)) {
-			rr_t *pkg_rr = NULL;
-			parse_rr_body(s.s, s.len, &pkg_rr);
-			rr_dup(_r, pkg_rr);
-		}
-	}
-	
-	return res;
-}
-
-static int serialize_route_set(sstream_t *ss, rr_t **route_set)
-{
-	rr_t *r, *first = NULL, *last = NULL;
-	int res = 0;
-
-	if (is_input_sstream(ss)) { /* read */
-		do {
-			res = serialize_route(ss, &r) | res;
-			if (last) last->next = r; 
-			else first = r;
-			last = r;
-			if (last) {
-				/* due to parsing rr (may be more rr_t than 1) */
-				while (last->next) last = last->next; 
-			}
-		} while (r);
-		*route_set = first;
-	}
-	else {	/* store */
-		r = *route_set;
-		while (r) {
-			serialize_route(ss, &r);
-			r = r->next;
-		}
-		r = NULL;
-		serialize_route(ss, &r); /* store terminating route */
-	}
-	
-	return 0;
-}
 
 /*static void trace_dlg(const char *s, dlg_t *d)
 {
