cf819e54bfdc59b35d6d9c5d18417b3800d1b95b
Juha Heinanen <jh@tutpro.com>
2006-12-05 13:48:23 +0000

    * Added subnet support to allow_address functions.
    git-svn-id: https://openser.svn.sourceforge.net/svnroot/openser/trunk@1304 689a6050-402a-0410-94f2-e92a70836424

--- tools/mysqldb.sh
+++ tools/mysqldb.sh
@@ -286,7 +286,7 @@ INSERT INTO version VALUES ( 'gw', '3');
 INSERT INTO version VALUES ( 'gw_grp', '1');
 INSERT INTO version VALUES ( 'lcr', '2');
 INSERT INTO version VALUES ( 'sip_trace', '1');
-INSERT INTO version VALUES ( 'address', '2');
+INSERT INTO version VALUES ( 'address', '3');
 INSERT INTO version VALUES ( 'domainpolicy', '2');
 
 
@@ -588,6 +588,7 @@ CREATE TABLE address (
   id bigint(20) NOT NULL auto_increment,
   grp smallint(5) unsigned NOT NULL default '0',
   ip_addr varchar(15) NOT NULL,
+  mask tinyint NOT NULL default 32,
   port smallint(5) unsigned NOT NULL default '0',
   PRIMARY KEY (id)
 ) $TABLE_TYPE;
