662971e1f29bc789f83ca5653fc48bbc9387f4dd
Jan Janak <jan@iptel.org>
2006-12-13 16:51:49 +0000

    - Implemented radius_load_attrs function which can load the values   of SER attributes from RADIUS server. The function synopsis is   same as in avp_db.

--- modules_s/avp_radius/avp_radius.c
+++ modules_s/avp_radius/avp_radius.c
@@ -39,50 +39,34 @@
 #include "../../parser/digest/digest.h"
 #include "../../parser/parse_uri.h"
 #include "../../parser/parse_from.h"
+#include "../domain/domain.h"
 #include "../../usr_avp.h"
 #include "../../ut.h"
+#include "../../config.h"
 
 
 MODULE_VERSION
 
-#define CALLER_PREFIX "caller_"
-#define CALLER_PREFIX_LEN (sizeof(CALLER_PREFIX) - 1)
-
-#define CALLEE_PREFIX "callee_"
-#define CALLEE_PREFIX_LEN (sizeof(CALLEE_PREFIX) - 1)
-
-
-typedef enum load_avp_param {
-	LOAD_CALLER,       /* Use the caller's username and domain as the key */
-	LOAD_CALLER_FROM_RURI, /* Load caller's avpairs, but use ruri (callee) as key */
-	LOAD_CALLEE,       /* Use the callee's username and domain as the key */
-	LOAD_DIGEST
-} load_avp_param_t;
-
 
 static int mod_init(void);
-static int load_avp_radius(struct sip_msg*, char*, char*);
-static int load_avp_fixup(void**, int);
-
+static int radius_load_attrs(struct sip_msg*, char*, char*);
+static int attrs_fixup(void**, int);
 
 static char *radius_config = "/usr/local/etc/radiusclient/radiusclient.conf";
-static int caller_service_type = -1;
-static int callee_service_type = -1;
-static str caller_prefix = STR_STATIC_INIT(CALLER_PREFIX);
-static str callee_prefix = STR_STATIC_INIT(CALLEE_PREFIX);
 
 static void *rh;
 struct attr attrs[A_MAX];
 struct val vals[V_MAX];
 
+static domain_get_did_t dm_get_did = NULL;
 
 
 /*
  * Exported functions
  */
 static cmd_export_t cmds[] = {
-	{"avp_load_radius", load_avp_radius, 1, load_avp_fixup, REQUEST_ROUTE | FAILURE_ROUTE},
-	{0, 0, 0, 0, 0}
+    {"radius_load_attrs", radius_load_attrs, 2, attrs_fixup, REQUEST_ROUTE | FAILURE_ROUTE},
+    {0, 0, 0, 0, 0}
 };
 
 
@@ -90,285 +74,358 @@ static cmd_export_t cmds[] = {
  * Exported parameters
  */
 static param_export_t params[] = {
-	{"radius_config",       PARAM_STRING, &radius_config      },
-	{"caller_service_type", PARAM_INT,    &caller_service_type},
-	{"callee_service_type", PARAM_INT,    &callee_service_type},
-	{0, 0, 0}
+    {"radius_config", PARAM_STRING, &radius_config },
+    {0, 0, 0}
 };
 
 
 struct module_exports exports = {
-	"avp_radius",
-	cmds,      /* Exported commands */
-	0,         /* RPC methods */
-	params,    /* Exported parameters */
-	mod_init,  /* module initialization function */
-	0,         /* response function*/
-	0,         /* destroy function */
-	0,         /* oncancel function */
-	0          /* per-child init function */
+    "avp_radius",
+    cmds,      /* Exported commands */
+    0,         /* RPC methods */
+    params,    /* Exported parameters */
+    mod_init,  /* module initialization function */
+    0,         /* response function*/
+    0,         /* destroy function */
+    0,         /* oncancel function */
+    0          /* per-child init function */
 };
 
 
 static int mod_init(void)
 {
-        DBG("avp_radius - Initializing\n");
+    DICT_VENDOR *vend;
+    memset(attrs, 0, sizeof(attrs));
+    memset(vals, 0, sizeof(vals));
+    
+    attrs[A_USER_NAME].n        = "User-Name";
+    attrs[A_SER_SERVICE_TYPE].n = "SER-Service-Type";
+    attrs[A_SER_ATTR].n	        = "SER-Attr";
+    attrs[A_SER_DID].n          = "SER-DID";
+    attrs[A_SER_URI_SCHEME].n   = "SER-Uri-Scheme";
+    
+    vals[V_GET_URI_ATTRS].n  = "Get-URI-Attrs";
+    vals[V_GET_USER_ATTRS].n = "Get-User-Attrs";
 
-	memset(attrs, 0, sizeof(attrs));
-	memset(vals, 0, sizeof(vals));
+	 /* open log */
+    rc_openlog("ser");
+    
+	 /* read config */
+    if ((rh = rc_read_config(radius_config)) == NULL) {
+	LOG(L_ERR, "avp_radius: Error opening radius config file: %s\n",
+	    radius_config);
+	return -1;
+    }
+    
+	 /* read dictionary */
+    if (rc_read_dictionary(rh, rc_conf_str(rh, "dictionary")) != 0) {
+	LOG(L_ERR, "avp_radius: Error reading radius dictionary\n");
+	return -1;
+    }
+    
+    vend = rc_dict_findvend(rh, "iptelorg");
+    if (vend == NULL) {
+	ERR("RADIUS dictionary is missing required vendor 'iptelorg'\n");
+	return -1;
+    }
 
-	attrs[A_USER_NAME].n	  = "User-Name";
-	attrs[A_SERVICE_TYPE].n	  = "Service-Type";
-
-	attrs[A_SER_ATTRS].n	  = "SER-Attrs";
-	vals[V_SER_CALLER_AVPS].n = "SER-Caller-AVPs";
-	vals[V_SER_CALLEE_AVPS].n = "SER-Callee-AVPs";
-
-	     /* open log */
-	rc_openlog("ser");
-
-	     /* read config */
-	if ((rh = rc_read_config(radius_config)) == NULL) {
-		LOG(L_ERR, "avp_radius: Error opening radius config file: %s\n",
-		    radius_config);
-		return -1;
-	}
-
-	     /* read dictionary */
-	if (rc_read_dictionary(rh, rc_conf_str(rh, "dictionary")) != 0) {
-		LOG(L_ERR, "avp_radius: Error reading radius dictionary\n");
-		return -1;
-	}
-
-	INIT_AV(rh, attrs, vals, "avp", -1, -1);
-
-	if (caller_service_type != -1) {
-		vals[V_SER_CALLER_AVPS].v = caller_service_type;
-	}
-
-	if (callee_service_type != -1) {
-		vals[V_SER_CALLEE_AVPS].v = callee_service_type;
-	}
-
-	return 0;
+    INIT_AV(rh, attrs, vals, "avp", -1, -1);
+    return 0;
 }
 
 
-static inline void attr_name_value(VALUE_PAIR* vp, str* name, str* value)
+static void attr_name_value(str* name, str* value, VALUE_PAIR* vp)
 {
-	int i;
-
-
-	for (i = 0; i < vp->lvalue; i++) {
-		if (vp->strvalue[i] == ':') {
-			name->s = vp->strvalue;
-			name->len = i;
-
-			if (i == (vp->lvalue - 1)) {
-				value->s = (char*)0;
-				value->len = 0;
-			} else {
-				value->s = vp->strvalue + i + 1;
-				value->len = vp->lvalue - i - 1;
-			}
-			return;
-		}
+    int i;
+    
+    for (i = 0; i < vp->lvalue; i++) {
+	if (vp->strvalue[i] == ':' || vp->strvalue[i] == '=') {
+	    name->s = vp->strvalue;
+	    name->len = i;
+	    
+	    if (i == (vp->lvalue - 1)) {
+		value->s = (char*)0;
+		value->len = 0;
+	    } else {
+		value->s = vp->strvalue + i + 1;
+		value->len = vp->lvalue - i - 1;
+	    }
+	    return;
 	}
+    }
 
-	name->len = value->len = 0;
-	name->s = value->s = (char*)0;
+    name->len = value->len = 0;
+    name->s = value->s = (char*)0;
 }
 
 
-static int load_avp_user(struct sip_msg* msg, str* prefix, load_avp_param_t type)
+/*
+ * Generate AVPs from the database result
+ */
+static int generate_avps(unsigned int flags, VALUE_PAIR* received)
 {
-	static char rad_msg[4096];
-	str user_domain, name_str;
-	str* user, *domain, *uri;
-	struct hdr_field* h;
-	dig_cred_t* cred = 0;
-	int_str name, val;
-
-	VALUE_PAIR* send, *received, *vp;
-	UINT4 service;
-	struct sip_uri puri;
-
-	send = received = 0;
-	user_domain.s = 0;
-
-	switch(type) {
-	case LOAD_CALLER:
-		     /* Use From header field */
-		if (parse_from_header(msg) < 0) {
-			LOG(L_ERR, "load_avp_user: Error while parsing From header field\n");
-			return -1;
-		}
-
-		uri = &get_from(msg)->uri;
-		if (parse_uri(uri->s, uri->len, &puri) == -1) {
-			LOG(L_ERR, "load_avp_user: Error while parsing From URI\n");
-			return -1;
-		}
-
-		user = &puri.user;
-		domain = &puri.host;
-		service = vals[V_SER_CALLER_AVPS].v;
-		break;
-
-	case LOAD_CALLEE:
-	case LOAD_CALLER_FROM_RURI:
-		     /* Use the Request-URI */
-		if (parse_sip_msg_uri(msg) < 0) {
-			LOG(L_ERR, "load_avp_user: Error while parsing Request-URI\n");
-			return -1;
-		}
-
-		if (msg->parsed_uri.user.len == 0) {
-			LOG(L_ERR, "load_avp_user: Request-URI user is missing\n");
-			return -1;
-		}
-
-		user = &msg->parsed_uri.user;
-		domain = &msg->parsed_uri.host;
-		if(type == LOAD_CALLER_FROM_RURI) {
-		  service = vals[V_SER_CALLER_AVPS].v;
-		} else {
-		  service = vals[V_SER_CALLEE_AVPS].v;
-		}
-		break;
-
-	case LOAD_DIGEST:
-		     /* Use digest credentials */
-		get_authorized_cred(msg->proxy_auth, &h);
-		if (!h) {
-			LOG(L_ERR, "load_avp_user: No authoried credentials\n");
-			return -1;
-		}
-
-		cred = &((auth_body_t*)(h->parsed))->digest;
-		user = &cred->username.user;
-		domain = &cred->realm;
-		service = vals[V_SER_CALLER_AVPS].v;
-		break;
-
-	default:
-		LOG(L_ERR, "load_avp_user: Unknown type\n");
-		return -1;
-
+    int_str name, val;
+    VALUE_PAIR *vp;
+    
+    vp = received;
+    while ((vp = rc_avpair_get(vp, ATTRID(attrs[A_SER_ATTR].v), VENDOR(attrs[A_SER_ATTR].v)))) {
+	attr_name_value(&name.s, &val.s, vp);
+	if (name.s.len == 0) {
+	    ERR("Missing attribute name\n");
+	    return -1;
 	}
 
-	user_domain.len = user->len + 1 + domain->len;
-	user_domain.s = (char*)pkg_malloc(user_domain.len);
-	if (!user_domain.s) {
-		LOG(L_ERR, "avp_load_user: No memory left\n");
-		return -1;
+	if (add_avp(flags | AVP_NAME_STR | AVP_VAL_STR, name, val) < 0) {
+	    ERR("Unable to create a new SER attribute\n");
+	    return -1;
 	}
+	vp = vp->next;
+    }
+    
+    return 0;
+}
 
-	memcpy(user_domain.s, user->s, user->len);
-	user_domain.s[user->len] = '@';
-	memcpy(user_domain.s + user->len + 1, domain->s, domain->len);
 
-	if (!rc_avpair_add(rh, &send, attrs[A_USER_NAME].v,
-			   user_domain.s, user_domain.len, 0)) {
-		LOG(L_ERR, "avp_load_user: Error adding PW_USER_NAME\n");
-		goto error;
-	}
+static int load_user_attrs(struct sip_msg* msg, unsigned long flags, fparam_t* fp)
+{
+    static char rad_msg[4096];
+    str uid;
+    UINT4 service;
+    VALUE_PAIR* send, *received;
 
-	if (!rc_avpair_add(rh, &send, attrs[A_SERVICE_TYPE].v, &service, -1, 0)) {
-		LOG(L_ERR, "avp_load_user: Error adding PW_SERVICE_TYPE\n");
-		goto error;
-	}
+    send = NULL;
+    received = NULL;
 
-	if (rc_auth(rh, 0, send, &received, rad_msg) == OK_RC) {
-		DBG("avp_load_user: Success\n");
-		rc_avpair_free(send);
-		pkg_free(user_domain.s);
+    if (get_str_fparam(&uid, msg, (fparam_t*)fp) < 0) {
+	ERR("Unable to get UID\n");
+	return -1;
+    }
 
-		vp = received;
-		while ((vp = rc_avpair_get(vp, attrs[A_SER_ATTRS].v, 0))) {
-			attr_name_value(vp, &name_str, &val.s);
-			if (name.s.len == 0) {
-			    LOG(L_ERR, "avp_load_user: Missing attribute name\n");
-			    return -1;
-			}
+    service = vals[V_GET_USER_ATTRS].v;
 
-			name.s.s = (char*)pkg_malloc(prefix->len + name_str.len);
-			if (!name.s.s) {
-				LOG(L_ERR, "avp_load_user: No memory left\n");
-				return -1;
-			}
-			name.s.len = prefix->len + name_str.len;
-			memcpy(name.s.s, prefix->s, prefix->len);
-			memcpy(name.s.s + prefix->len, name_str.s, name_str.len);
+    if (!rc_avpair_add(rh, &send, ATTRID(attrs[A_USER_NAME].v), 
+		       uid.s, uid.len,
+		       VENDOR(attrs[A_USER_NAME].v))) {
+	ERR("Error while adding A_USER_NAME\n");
+	goto error;
+    }
+    
+    if (!rc_avpair_add(rh, &send, ATTRID(attrs[A_SER_SERVICE_TYPE].v), 
+		       &vals[V_GET_USER_ATTRS].v, -1, 
+		       VENDOR(attrs[A_SER_SERVICE_TYPE].v))) {
+	ERR("Error adding A_SERVICE_TYPE\n");
+	goto error;
+    }
 
-			add_avp(AVP_TRACK_FROM | AVP_CLASS_USER | AVP_NAME_STR | AVP_VAL_STR, name, val);
-			DBG("avp_load_user: AVP '%.*s'='%.*s' has been added\n",
-			    name_str.len, ZSW(name_str.s),
-			    val.s.len, ZSW(val.s.s));
 
-			pkg_free(name.s.s);
-			vp = vp->next;
-		}
+    if (rc_auth(rh, 0, send, &received, rad_msg) != OK_RC) {
+	DBG("load_user_attrs: Failure\n");
+	goto error;
+    }
 
-		rc_avpair_free(received);
-		return 1;
-	} else {
-		DBG("avp_load_user: Failure\n");
-	}
+    DBG("load_user_attrs: Success\n");
+    rc_avpair_free(send);
+
+    if (generate_avps(flags, received) < 0) {
+	rc_avpair_free(received);
+	goto error;
+    }
+
+    rc_avpair_free(received);
+    return 1;
 
  error:
-	if (send) rc_avpair_free(send);
-	if (received) rc_avpair_free(received);
-	if (user_domain.s) pkg_free(user_domain.s);
+    if (send) rc_avpair_free(send);
+    if (received) rc_avpair_free(send);
+    return -1;
+}
+
+
+static void uri_type_to_str(uri_type type, str *s) {
+    static str	s_sip  = STR_STATIC_INIT("sip");
+    static str	s_sips = STR_STATIC_INIT("sips");
+    static str	s_tel  = STR_STATIC_INIT("tel");
+    static str	s_tels = STR_STATIC_INIT("tels");
+    static str	s_null = STR_STATIC_INIT("");
+    
+    switch (type) {
+	
+    case SIP_URI_T:	*s = s_sip;	break;
+    case SIPS_URI_T:	*s = s_sips;	break;
+    case TEL_URI_T:	*s = s_tel;	break;
+    case TELS_URI_T:	*s = s_tels;	break;
+    default:		*s = s_null;
+    }
+}
+
+
+static int load_uri_attrs(struct sip_msg* msg, unsigned long flags, fparam_t* fp)
+{
+    static char rad_msg[4096];
+    struct sip_uri puri;
+    str uri, did, scheme;
+    UINT4 service;
+    VALUE_PAIR* send, *received;
+
+    send = NULL;
+    received = NULL;
+
+    if (get_str_fparam(&uri, msg, (fparam_t*)fp) != 0) {
+	ERR("Unable to get URI\n");
 	return -1;
+    }
+
+    if (parse_uri(uri.s, uri.len, &puri) < 0) {
+	ERR("Error while parsing URI '%.*s'\n", uri.len, uri.s);
+	return -1;
+    }
+
+    if (puri.host.len) {
+	/* domain name is present */
+	if (dm_get_did(&did, &puri.host) < 0) {
+		DBG("Cannot lookup DID for domain %.*s, using default value\n", puri.host.len, ZSW(puri.host.s));
+		did.s = DEFAULT_DID;
+		did.len = sizeof(DEFAULT_DID) - 1;
+	}
+    } else {
+	/* domain name is missing -- can be caused by tel: URI */
+	DBG("There is no domain name, using default value\n");
+	did.s = DEFAULT_DID;
+	did.len = sizeof(DEFAULT_DID) - 1;
+    }
+
+    uri_type_to_str(puri.type, &scheme);
+    service = vals[V_GET_URI_ATTRS].v;
+
+    if (scheme.len) {
+	if (!rc_avpair_add(rh, &send, ATTRID(attrs[A_SER_URI_SCHEME].v), 
+			   scheme.s, scheme.len,
+			   VENDOR(attrs[A_SER_URI_SCHEME].v))) {
+	    ERR("Error while adding A_SER_URI_SCHEME\n");
+	    goto error;
+	}
+    }
+
+    if (!rc_avpair_add(rh, &send, ATTRID(attrs[A_USER_NAME].v), 
+		       puri.user.s, puri.user.len,
+		       VENDOR(attrs[A_USER_NAME].v))) {
+	ERR("Error while adding A_USER_NAME\n");
+	goto error;
+    }
+
+    if (!rc_avpair_add(rh, &send, ATTRID(attrs[A_SER_DID].v), 
+		       did.s, did.len,
+		       VENDOR(attrs[A_SER_DID].v))) {
+	ERR("Error while adding A_SER_DID\n");
+	goto error;
+    }
+    
+    if (!rc_avpair_add(rh, &send, ATTRID(attrs[A_SER_SERVICE_TYPE].v), 
+		       &vals[V_GET_URI_ATTRS].v, -1,
+		       VENDOR(attrs[A_SER_SERVICE_TYPE].v))) {
+	ERR("Error adding A_SERVICE_TYPE\n");
+	goto error;
+    }
+
+    if (rc_auth(rh, 0, send, &received, rad_msg) != OK_RC) {
+	DBG("load_uri_attrs: Failure\n");
+	goto error;
+    }
+
+    DBG("load_uri_attrs: Success\n");
+    rc_avpair_free(send);
+
+    if (generate_avps(flags, received) < 0) {
+	rc_avpair_free(received);
+	goto error;
+    }
+
+    rc_avpair_free(received);
+    return 1;
+
+ error:
+    if (send) rc_avpair_free(send);
+    if (received) rc_avpair_free(send);
+    return -1;
 }
 
 
-static int load_avp_radius(struct sip_msg* msg, char* attr, char* _dummy)
+/*
+ * Load user attributes
+ */
+static int radius_load_attrs(struct sip_msg* msg, char* fl, char* fp)
 {
-	switch((load_avp_param_t)attr) {
-	case LOAD_CALLER:
-		return load_avp_user(msg, &caller_prefix, LOAD_CALLER);
-		break;
+    unsigned long flags;
+    
+    flags = (unsigned long)fl;
 
-	case LOAD_CALLER_FROM_RURI:
-		return load_avp_user(msg, &caller_prefix, LOAD_CALLER_FROM_RURI);
-		break;
+    if (flags & AVP_CLASS_URI) {
+       	return load_uri_attrs(msg, flags, (fparam_t*)fp);
+    } else {
+	return load_user_attrs(msg, flags, (fparam_t*)fp);
+    }
+}
 
-	case LOAD_CALLEE:
-		return load_avp_user(msg, &callee_prefix, LOAD_CALLEE);
-		break;
 
-	case LOAD_DIGEST:
-		return load_avp_user(msg, &caller_prefix, LOAD_DIGEST);
-
+static int attrs_fixup(void** param, int param_no)
+{
+    unsigned long flags;
+    char* s;
+    
+    if (param_no == 1) {
+	     /* Determine the track and class of attributes to be loaded */
+	s = (char*)*param;
+	flags = 0;
+	if (*s != '$' || (strlen(s) != 3)) {
+	    ERR("Invalid parameter value, $xy expected\n");
+	    return -1;
+	}
+	switch((s[1] << 8) + s[2]) {
+	case 0x4655: /* $fu */
+	case 0x6675:
+	case 0x4675:
+	case 0x6655:
+	    flags = AVP_TRACK_FROM | AVP_CLASS_USER;
+	    break;
+	    
+	case 0x4652: /* $fr */
+	case 0x6672:
+	case 0x4672:
+	case 0x6652:
+	    flags = AVP_TRACK_FROM | AVP_CLASS_URI;
+	    break;
+	    
+	case 0x5455: /* $tu */
+	case 0x7475:
+	case 0x5475:
+	case 0x7455:
+	    flags = AVP_TRACK_TO | AVP_CLASS_USER;
+	    break;
+	    
+	case 0x5452: /* $tr */
+	case 0x7472:
+	case 0x5472:
+	case 0x7452:
+	    flags = AVP_TRACK_TO | AVP_CLASS_URI;
+	    break;
+	    
 	default:
-		LOG(L_ERR, "load_avp_radius: Unknown parameter value\n");
+	    ERR("Invalid parameter value: '%s'\n", s);
+	    return -1;
+	}
+
+	if ((flags & AVP_CLASS_URI) && !dm_get_did) {
+	    dm_get_did = (domain_get_did_t)find_export("get_did", 0, 0);
+	    if (!dm_get_did) {
+		ERR("Domain module required but not found\n");
 		return -1;
+	    }
 	}
-}
-
-
-static int load_avp_fixup(void** param, int param_no)
-{
-	long id = 0;
-
-	if (param_no == 1) {
-		if (!strcasecmp(*param, "caller")) {
-			id = LOAD_CALLER;
-		} else if (!strcasecmp(*param, "caller_from_ruri")) {
-		  id = LOAD_CALLER_FROM_RURI;
-		} else if (!strcasecmp(*param, "callee")) {
-			id = LOAD_CALLEE;
-		} else if (!strcasecmp(*param, "digest")) {
-			id = LOAD_DIGEST;
-		} else {
-			LOG(L_ERR, "load_avp_fixup: Unknown parameter\n");
-			return -1;
-		}
-	}
-
+	
 	pkg_free(*param);
-	*param=(void*)id;
-	return 0;
+	*param = (void*)flags;
+    } else if (param_no == 2) {
+	return fixup_var_str_12(param, 2);
+    }
+    return 0;
 }
