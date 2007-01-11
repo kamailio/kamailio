726c4aea3132988535d8ca50bf3db83cac7923ab
Bogdan-Andrei Iancu <bogdan@voice-system.ro>
2007-01-11 16:52:36 +0000

    - sql table format update to use an id column as PK - SQL creation script listed in the documentation
    git-svn-id: https://openser.svn.sourceforge.net/svnroot/openser/trunk@1486 689a6050-402a-0410-94f2-e92a70836424

--- modules_k/cpl-c/doc/cpl-c.sql
+++ modules_k/cpl-c/doc/cpl-c.sql
@@ -1,9 +1,11 @@
 USE openser;
 
 CREATE TABLE `cpl` (
+  `id` int(10) NOT NULL auto_increment,
   `username` varchar(64) NOT NULL,
   `domain` varchar(64) NOT NULL default '',
   `cpl_xml` blob,
   `cpl_bin` blob,
-  PRIMARY KEY  (`username`,`domain`)
+  UNIQUE KEY ud_cpl (`username`,`domain`),
+  PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM;
