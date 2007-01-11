296d7eaf85d9a9891674f68fe1f0c261b853e32e
Bogdan-Andrei Iancu <bogdan@voice-system.ro>
2007-01-11 16:33:06 +0000

    - removed sql script for creating module table - it is created by default - removed Installation section from readme as it is not relevant any more
    git-svn-id: https://openser.svn.sourceforge.net/svnroot/openser/trunk@1485 689a6050-402a-0410-94f2-e92a70836424

--- modules_k/alias_db/doc/alias_db.sql
+++
@@ -1,7 +0,0 @@
-create table dbaliases (
-  alias_username varchar(64) not null default '',
-  alias_domain varchar(128) not null default '',
-  username varchar(64) not null default '',
-  domain varchar(128) not null default '',
-  unique key alias_key(alias_username, alias_domain)
-);
