e7431aabaf9b85ee134fac3597cc24dc6d7ce8fe
Bogdan-Andrei Iancu <bogdan@voice-system.ro>
2007-02-05 15:30:23 +0000

    missing file uploaded - description of the DB tables
    git-svn-id: https://openser.svn.sourceforge.net/svnroot/openser/trunk@1575 689a6050-402a-0410-94f2-e92a70836424

---
+++ modules_k/pua/doc/pua.sql
@@ -0,0 +1,19 @@
+
+use openser;
+
+CREATE TABLE pua (
+  id int(10) unsigned NOT NULL auto_increment,
+  pres_uri varchar(128) NOT NULL,
+  pres_id varchar(128) NOT NULL,
+  expires int(11) NOT NULL,
+  flag int(11) NOT NULL,
+  etag varchar(128) NOT NULL,
+  tuple_id varchar(128) NOT NULL,
+  watcher_uri varchar(128) NOT NULL,
+  call_id varchar(128) NOT NULL,
+  to_tag varchar(128) NOT NULL,
+  from_tag varchar(128) NOT NULL,
+  cseq int(11) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM;
+
