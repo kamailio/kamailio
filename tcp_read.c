2dc17e99e0bc0b5d10e4844803904f3808d2a2d5
Andrei Pelinescu-Onciul <andrei@iptel.org>
2007-02-20 19:15:20 +0000

    [backport from unstable]
    - minor: nicer error handling
    - fixed wrong expire time calculation (not correct when tick oveflow,
       an timeout == some value before the overflow).
       This should fix the connection lives forever bug reported by Alfred E.
       Heggestad <aeh@db.org>.

--- tcp_read.c
+++ tcp_read.c
@@ -36,6 +36,7 @@
  * 2005-07-05  migrated to the new io_wait code (andrei)
  * 2006-02-03  use tsend_stream instead of send_all (andrei)
  * 2006-10-13  added STUN support - state machine for TCP (vlada)
+ * 2007-02-20  fixed timeout calc. bug (andrei)
  */
 
 #ifdef USE_TCP
@@ -799,7 +800,7 @@ skip:
 					}
 				}else{
 					/* timeout */
-					if (con->timeout<=ticks){
+					if ((s_ticks_t)(ticks-con->timeout)>=0){
 						/* expired, return to "tcp main" */
 						DBG("tcp_receive_loop: %p expired (%d, %d)\n",
 								con, con->timeout, ticks);
@@ -856,11 +857,11 @@ again:
 			}
 			if (n==0){
 				LOG(L_ERR, "WARNING: tcp_receive: handle_io: 0 bytes read\n");
-				break;
+				goto error;
 			}
 			if (con==0){
 					LOG(L_CRIT, "BUG: tcp_receive: handle_io null pointer\n");
-					break;
+					goto error;
 			}
 			con->fd=s;
 			if (s==-1) {
@@ -943,7 +944,7 @@ static inline void tcp_receive_timeout()
 			release_tcpconn(con, CONN_ERROR, tcpmain_sock);
 			continue;
 		}
-		if (con->timeout<=ticks){
+		if ((s_ticks_t)(ticks-con->timeout)>=0){
 			/* expired, return to "tcp main" */
 			DBG("tcp_receive_loop: %p expired (%d, %d)\n",
 					con, con->timeout, ticks);
