aaf285a65e0bd4d406ac30c5b1a86cf5dcc95716
Daniel-Constantin Mierla <miconda@gmail.com>
2007-03-11 19:20:59 +0000

    - added license and copyright
    git-svn-id: https://openser.svn.sourceforge.net/svnroot/openser/trunk@1819 689a6050-402a-0410-94f2-e92a70836424

--- modules_k/seas/seas_error.h
+++ modules_k/seas/seas_error.h
@@ -1,7 +1,29 @@
+/**
+ * $Id$
+ *
+ * Copyright (C) 2006-2007 VozTelecom Sistemas S.L
+ *
+ * This file is part of openser, a free SIP server.
+ *
+ * openser is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version
+ *
+ * openser is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software 
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 #include "../../error.h"
 
 
-/** SER ERRORS ARE NEGATIVE, SEAS ERROR CODES ARE POSITIVE */
+/** OPENSER ERRORS ARE NEGATIVE, SEAS ERROR CODES ARE POSITIVE */
 
 #define SE_CANCEL_MSG "500 SEAS cancel error"
 #define SE_CANCEL_MSG_LEN (sizeof(SE_CANCEL_MSG)-1)
