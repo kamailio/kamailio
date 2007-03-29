67fa78807696d3339ca1d4542241e1470e8fda0b
Jan Janak <jan@iptel.org>
2007-03-29 11:10:27 +0000

    - Templates for driver extension data

---
+++ db/db_drv.h
@@ -0,0 +1,84 @@
+/* 
+ * $Id$ 
+ *
+ * Copyright (C) 2001-2003 FhG FOKUS
+ * Copyright (C) 2006-2007 iptelorg GmbH
+ *
+ * This file is part of ser, a free SIP server.
+ *
+ * ser is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version
+ *
+ * For a license to use the ser software under conditions
+ * other than those described here, or to purchase support for this
+ * software, please contact iptel.org by e-mail at the following addresses:
+ *    info@iptel.org
+ *
+ * ser is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software 
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef _DB_DRV_H
+#define _DB_DRV_H  1
+
+#include "../str.h"
+#include "../list.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+struct db_drv;
+
+typedef void db_drv_free_t(struct db_drv* data);
+
+
+/*
+ * Generic function exported by DB driver modules
+ */
+typedef int (*db_drv_func_t)(void* db_struct, ...);
+
+
+/* Template for driver specific data that can be attached to various
+ * DB API structure and where drivers can store driver-specific data
+ */
+typedef struct db_drv {
+	db_drv_free_t* free;  /* Drivers must provide this function */
+} db_drv_t;
+
+
+int db_drv_init(struct db_drv* ptr, void* free_func);
+
+void db_drv_free(struct db_drv* ptr);
+
+/*
+ * Find function with given name in module named <module>
+ * RETURNS: -1 on error
+ *           0 if a function has been found
+ *           1 if no function has been found
+ */
+int db_drv_func(db_drv_func_t* func, str* module, char* func_name);
+
+/*
+ * Call function with name <func_name> in DB driver <module>, give
+ * it pointer <db_struct> as the pointer to the corresponding DB structure
+ * (type of the structure is function-specific) and <offset> is the offset
+ * of the driver/connection within the context (used to make DB_DRV_ATTACH 
+ * and DB_DRV_DATA macros work)
+ */
+int db_drv_call(str* module, char* func_name, void* db_struct, int offset);
+
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* _DB_DRV_H */
