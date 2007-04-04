fcaa16f68b3dd3ecf9886d799300a0184bc1b7c1
Anca Vamanu <anca@voice-system.ro>
2007-04-04 09:14:17 +0000

    - updated to the changes in pua ( new filed event) - use of static buffers
    git-svn-id: https://openser.svn.sourceforge.net/svnroot/openser/trunk@1960 689a6050-402a-0410-94f2-e92a70836424

--- modules_k/pua_mi/mi_func.c
+++ modules_k/pua_mi/mi_func.c
@@ -50,8 +50,7 @@ struct mi_root* mi_pua_publish(struct mi_root* cmd, void* param)
 	str pres_uri, expires;
 	str body= {0, 0};
 	struct sip_uri uri;
-	publ_info_t* publ= NULL;
-	int size= 0;
+	publ_info_t publ;
 
 	DBG("pua_mi: pua_mi_publish ..\n");
 	node = cmd->node.kids;
@@ -115,52 +114,28 @@ struct mi_root* mi_pua_publish(struct mi_root* cmd, void* param)
 
 	/* creating the publ_info_t structure */
 	
-	size= sizeof(publ_info_t)+ sizeof(str)+ pres_uri.len *sizeof(char); 
+	memset(&publ, 0, sizeof(publ_info_t));
 	
-	if(body.s)
-		size+= body.len* sizeof(char) +sizeof(str);
-
-	publ= (publ_info_t*)pkg_malloc(size);
-	if(publ== NULL)
-	{
-		LOG(L_ERR, "pua_mi: pua_mi_publish: Error no more share memory\n");
-		return 0;
-	}
-	memset(publ, 0, size);
-	size= sizeof(publ_info_t);
-
-	publ->pres_uri= (str*)( (char*)publ + size);
-	size+= sizeof(str);
-	publ->pres_uri->s= (char*)publ+ size;
-	memcpy(publ->pres_uri->s, pres_uri.s, pres_uri.len);
-	publ->pres_uri->len= pres_uri.len;
-	size+= pres_uri.len;
-
+	publ.pres_uri= &pres_uri;
 	if(body.s)
 	{
-		publ->body= (str*)( (char*)publ + size);
-		size+= sizeof(str);
-
-		publ->body->s= (char*)publ + size;
-		memcpy(publ->body->s, body.s, body.len);
-		publ->body->len= body.len;
-		size+= body.len;
-		DBG("pua_mi: pua_mi_publish: body= %.*s\n",publ->body->len, 
-				publ->body->s);
+		publ.body= &body;
+		DBG("pua_mi: pua_mi_publish: body= %.*s\n",publ.body->len, 
+				publ.body->s);
 	}
-	publ->expires= len;
-	publ->flag|= INSERT_TYPE;
-	publ->source_flag|= MI_PUBLISH;
-
+	publ.event= PRESENCE_EVENT;
+	publ.expires= len;
+	publ.flag|= INSERT_TYPE;
+	publ.source_flag|= MI_PUBLISH;
+	
 	DBG("pua_mi: pua_mi_publish: send publish\n");
 
-	if(pua_send_publish(publ)< 0)
+	if(pua_send_publish(&publ)< 0)
 	{
 		LOG(L_ERR, "pua_mi: pua_mi_publish:ERROR while sending publish\n");
 		goto error;
 	}	
 
-	pkg_free(publ);
 	rpl = init_mi_tree( 202, "accepted", 8);
 	if(rpl == NULL)
 		return 0;
@@ -169,7 +144,6 @@ struct mi_root* mi_pua_publish(struct mi_root* cmd, void* param)
 
 error:
 
-	pkg_free(publ);
 	return 0;
 
 }
@@ -188,8 +162,7 @@ struct mi_root* mi_pua_subscribe(struct mi_root* cmd, void* param)
 	struct mi_node* node= NULL;
 	struct mi_root* rpl= NULL;
 	struct sip_uri uri;
-	subs_info_t* subs= NULL;
-	int size;
+	subs_info_t subs;
 
 	node = cmd->node.kids;
 	if(node == NULL)
@@ -243,42 +216,25 @@ struct mi_root* mi_pua_subscribe(struct mi_root* cmd, void* param)
 		return 0;
 	}
 	
-	size= sizeof(subs_info_t)+ sizeof(str)* 2+ (pres_uri.len +
-			watcher_uri.len)*sizeof(char); 
+	memset(&subs, 0, sizeof(subs_info_t));
+
 	
-	subs= (subs_info_t*)pkg_malloc(size);
-	if(subs== NULL)
-	{
-		LOG(L_ERR, "pua_mi: pua_mi_subscribe: Error no more share memory\n");
-		return 0;
-	}
-	memset(subs, 0, size);
-	size= sizeof(subs_info_t);
+	subs.pres_uri= &pres_uri;
 
-	subs->pres_uri= (str*)((char*)subs + size);
-	size+= sizeof(str);
-	subs->pres_uri->s= (char*)subs+ size;
-	memcpy(subs->pres_uri->s, pres_uri.s, pres_uri.len);
-	subs->pres_uri->len= pres_uri.len;
-	size+= pres_uri.len;
+	subs.watcher_uri= &watcher_uri;
 
-	subs->watcher_uri= (str*)((char*)subs + size);
-	size+= sizeof(str);
-	subs->watcher_uri->s= (char*)subs+ size;
-	memcpy(subs->watcher_uri->s, watcher_uri.s, watcher_uri.len);
-	subs->watcher_uri->len= watcher_uri.len;
-	size+= watcher_uri.len;
+	subs.contact= &watcher_uri;
+	
+	subs.expires= exp;
+	subs.source_flag |= MI_SUBSCRIBE;
+	subs.event= PRESENCE_EVENT;
 
-	subs->expires= exp;
-	subs->source_flag |= MI_SUBSCRIBE;
-
-	if(pua_send_subscribe(subs)< 0)
+	if(pua_send_subscribe(&subs)< 0)
 	{
 		LOG(L_ERR, "pua_mi:pua_mi_subscribe: ERROR while sending subscribe\n");
 		goto error;
 	}
 	
-	pkg_free(subs);
 
 	rpl= init_mi_tree(202, "accepted", 8);
 	if(rpl == NULL)
@@ -288,7 +244,6 @@ struct mi_root* mi_pua_subscribe(struct mi_root* cmd, void* param)
 
 error:
 
-	pkg_free(subs);
 	return 0;
 
 }
