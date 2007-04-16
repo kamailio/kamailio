ba0c86c7498323d879dd938b986b7fcf30e1ed23
Miklos Tirpak <miklos@iptel.org>
2007-04-16 13:48:26 +0000

    wrong column was used for domain name

--- modules_s/domain/domain.c
+++ modules_s/domain/domain.c
@@ -282,7 +282,7 @@ int load_domains(domain_t** dest)
 		d = domain_search(list, &rec->fld[0].v.str);
 		if (d) {
 			/* DID exists in the list, update it */
-			if (domain_add(d, &rec->fld[0].v.str, flags) < 0) goto error;
+			if (domain_add(d, &rec->fld[1].v.str, flags) < 0) goto error;
 		} else {
 			     /* DID does not exist yet, create a new entry */
 			d = new_domain(&rec->fld[0].v.str, &rec->fld[1].v.str, flags);
