b9e527a75baa4835c370f1ca3772851abe220bf7
Daniel-Constantin Mierla <miconda@gmail.com>
2007-07-16 09:23:07 +0000

    - imported LDAP module submitted by Christian Schlatter, University of North Carolina, #1748544 - formatted to aling to 80char long line - removed ldap_mod.h - useless and was containing an extern to SL API which is no longer used - "ldap.h" => <ldap.h> to look it up directly in standard include path rather than local folder - README was generated - Christian Schlatter to be the maintainer of the module
    git-svn-id: https://openser.svn.sourceforge.net/svnroot/openser/trunk@2453 689a6050-402a-0410-94f2-e92a70836424

---
+++ modules_k/ldap/ldap_connect.h
@@ -0,0 +1,56 @@
+/*
+ * $Id: $
+ *
+ * OpenSER LDAP Module
+ *
+ * Copyright (C) 2007 University of North Carolina
+ *
+ * Original author: Christian Schlatter, cs@unc.edu
+ *
+ *
+ * This file is part of openser, a free SIP server.
+ *
+ * openser is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version
+ *
+ * openser is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * History:
+ * --------
+ * 2007-02-18: Initial version
+ */
+
+
+#ifndef LDAP_CONNECT_H
+#define LDAP_CONNECT_H
+
+#include "../../str.h"
+
+/* define L_LDAP_DBG, 
+* setting LDAP_DBG will set log_level to L_DBG for this module only
+*/
+#include "../../dprint.h"
+
+#ifdef LDAP_DBG
+#define L_LDAP_DBG L_INFO
+#else
+#define L_LDAP_DBG L_DBG
+#endif
+
+
+extern int ldap_connect(char* _ld_name);
+extern int ldap_disconnect(char* _ld_name);
+extern int ldap_reconnect(char* _ld_name);
+extern int ldap_get_vendor_version(char** _version);
+
+#endif /* LDAP_CONNECT_H */
+
---
+++ modules_k/ldap/ldap_exp_fn.c
@@ -0,0 +1,385 @@
+/*
+ * $Id: $
+ *
+ * OpenSER LDAP Module
+ *
+ * Copyright (C) 2007 University of North Carolina
+ *
+ * Original author: Christian Schlatter, cs@unc.edu
+ *
+ *
+ * This file is part of openser, a free SIP server.
+ *
+ * openser is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version
+ *
+ * openser is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * History:
+ * --------
+ * 2007-02-18: Initial version
+ */
+
+
+#include <string.h>
+#include <stdio.h>
+
+#include <ldap.h>
+
+#include "../../ut.h"
+#include "../../str.h"
+#include "../../items.h"
+#include "../../usr_avp.h"
+#include "../../mem/mem.h"
+#include "ldap_exp_fn.h"
+#include "ldap_connect.h"
+#include "ldap_api_fn.h"
+#include "ldap_escape.h"
+
+
+#define STR_BUF_SIZE 1024
+#define ESC_BUF_SIZE 65536
+
+static char str_buf[STR_BUF_SIZE];
+static char esc_buf[ESC_BUF_SIZE];
+
+
+/*
+* exported functions
+*/ 
+
+int ldap_search_impl(
+	struct sip_msg* _msg,
+	xl_elem_t* _ldap_url)
+{
+	str ldap_url;
+	int ld_result_count = 0;
+	
+	/*
+	* do variable substitution for _ldap_url (xl_printf_s)
+	*/
+	if (_ldap_url) {
+		if (xl_printf_s(_msg, _ldap_url, &ldap_url) != 0) {
+			LOG(
+				L_ERR, 
+				"ERROR:ldap:ldap_search: xl_printf_s failed\n");
+			return -2;
+		}
+	} else {
+		LOG(
+			L_ERR,
+			"ERROR:ldap:ldap_search: empty ldap_url\n");
+		return -2;
+	}
+
+	/*
+	* perform LDAP search
+	*/
+	if (ldap_url_search(ldap_url.s, &ld_result_count) != 0)
+	{
+		/* LDAP search error */
+		return -2;
+	}
+	if (ld_result_count < 1)
+	{
+		/* no LDAP entry found */
+		LOG(L_INFO, "ldap:ldap_search: no LDAP entry found\n");
+		return -1;
+	}
+	return ld_result_count;
+}
+
+int ldap_write_result(
+	struct sip_msg* _msg,
+	struct ldap_result_params* _lrp,
+	struct subst_expr* _se)
+{
+	int_str                    dst_avp_name, dst_avp_val;
+	unsigned short             dst_avp_type;
+	int                        nmatches, rc, i, added_avp_count = 0;
+	struct berval              **attr_vals;
+	str                        avp_val_str, *subst_result = NULL;
+	int                        avp_val_int;
+	
+	/*
+	* get dst AVP name (dst_avp_name)
+	*/
+	
+	if (xl_get_avp_name(	_msg, 
+				&_lrp->dst_avp_spec, 
+				&dst_avp_name, 
+				&dst_avp_type)
+			!= 0) 
+	{
+		LOG(L_ERR, "ERROR:ldap:ldap_result: error getting dst AVP name\n");
+		return -2;
+	}
+	if (dst_avp_type & AVP_NAME_STR)
+	{
+		if (dst_avp_name.s.len >= STR_BUF_SIZE)
+		{
+			LOG(L_ERR, "ERROR:ldap:ldap_result: dst AVP name too long\n");
+			return -2;
+		}
+		strncpy(str_buf, dst_avp_name.s.s, dst_avp_name.s.len);
+		str_buf[dst_avp_name.s.len] = '\0';
+		dst_avp_name.s.s = str_buf;
+	}
+
+	/*
+	* get LDAP attr values
+	*/
+	if ((rc = ldap_get_attr_vals(&_lrp->ldap_attr_name, &attr_vals)) != 0)
+	{
+		if (rc > 0) {
+			return -1;
+		} else {
+			return -2;
+		}
+	}
+
+	/*
+	* add AVPs
+	*/
+	for (i = 0; attr_vals[i] != NULL; i++)
+	{
+		if (_se == NULL)
+		{
+			avp_val_str.s = attr_vals[i]->bv_val;
+			avp_val_str.len = attr_vals[i]->bv_len;
+		}
+		else
+		{
+			subst_result = subst_str(attr_vals[i]->bv_val, _msg, _se,
+					&nmatches);
+			if ((subst_result == NULL) || (nmatches < 1))
+			{
+				continue;
+			}
+			avp_val_str = *subst_result;
+		}
+
+		if (_lrp->dst_avp_val_type == 1)
+		{
+			/* try to convert ldap value to integer */
+			if (!str2sint(&avp_val_str, &avp_val_int)) 
+			{
+				dst_avp_val.n = avp_val_int;
+				rc = add_avp(dst_avp_type, dst_avp_name, dst_avp_val);
+			} else
+			{
+				continue;
+			}
+		} else
+		{
+			/* save ldap value as string */
+			dst_avp_val.s = avp_val_str;
+			rc = add_avp(dst_avp_type|AVP_VAL_STR, dst_avp_name, dst_avp_val);
+		}
+		
+		if (subst_result != NULL) {
+			if (subst_result->s != 0) {
+				pkg_free(subst_result->s);
+			}
+			pkg_free(subst_result);
+			subst_result = NULL;
+		}
+		
+		if (rc < 0) 
+		{
+			LOG(L_ERR, "ERROR:ldap:ldap_result: failed to create new AVP\n");
+			ldap_value_free_len(attr_vals);
+			return -2;
+		}
+		added_avp_count++;
+	}
+	ldap_value_free_len(attr_vals);
+	
+	if (added_avp_count > 0)
+	{
+		return added_avp_count;
+	} else
+	{
+		return -1;
+	}
+}
+
+int ldap_result_next()
+{
+	int rc;
+
+	rc = ldap_inc_result_pointer();
+	switch (rc)
+	{
+	case 1:
+		return -1;
+	case 0:
+		return 1;
+	case -1:
+	default:
+		return -2;
+	}
+}
+
+int ldap_result_check(
+	struct sip_msg* _msg,
+	struct ldap_result_check_params* _lrp,
+	struct subst_expr* _se)
+{
+	str check_str, *subst_result = NULL;
+	int rc, i, nmatches;
+	char *attr_val;
+	struct berval **attr_vals;
+
+	/*
+	* do variable substitution for check_str 
+	*/
+	
+	if (_lrp->check_str_elem_p)
+	{
+		if (xl_printf_s(_msg, _lrp->check_str_elem_p, &check_str) != 0)
+		{
+			LOG(
+				L_ERR,
+				"ERROR:ldap:ldap_result_check: xl_printf_s failed\n");
+			return -2;
+		}
+	} else 
+	{
+		LOG(L_ERR, "ERROR:ldap:ldap_result_check: empty check string\n");
+		return -2;
+	}
+
+	LOG(L_LDAP_DBG, "ldap:ldap_result_check: check_str [%s]\n", check_str.s);
+	
+	/*
+	* get LDAP attr values
+	*/
+	
+	if ((rc = ldap_get_attr_vals(&_lrp->ldap_attr_name, &attr_vals)) != 0)
+	{
+		if (rc > 0) {
+			return -1;
+		} else {
+			return -2;
+		}
+	}
+
+	/*
+	* loop through attribute values
+	*/
+	
+	for (i = 0; attr_vals[i] != NULL; i++)
+	{
+		if (_se == NULL)
+		{
+			attr_val = attr_vals[i]->bv_val;
+		} else
+		{	
+			subst_result = subst_str(attr_vals[i]->bv_val, _msg, _se,
+					&nmatches);
+			if ((subst_result == NULL) || (nmatches < 1))
+			{
+				continue;
+			}
+			attr_val = subst_result->s;
+		}
+		
+		LOG(L_LDAP_DBG, "ldap:ldap_result_check: attr_val [%s]\n", attr_val);
+		rc = strncmp(check_str.s, attr_val, check_str.len);
+		if (_se != NULL) 
+		{
+			pkg_free(subst_result->s);
+		}
+		if (rc == 0)
+		{
+			ldap_value_free_len(attr_vals);
+			return 1;
+		}
+	}
+
+	ldap_value_free_len(attr_vals);
+	return -1;
+}
+
+int ldap_filter_url_encode(
+	struct sip_msg* _msg,
+	xl_elem_t* _filter_component,
+	xl_spec_t* _dst_avp_spec)
+{
+	str             filter_component_str, esc_str;	
+	int_str         dst_avp_name;
+	unsigned short  dst_avp_type;
+
+	/*
+	* variable substitution for _filter_component
+	*/
+	if (_filter_component) {
+		if (xl_printf_s(_msg, _filter_component, &filter_component_str) != 0) {
+			LOG(
+				L_ERR,
+				"ERROR:ldap:ldap_filter_url_encode: xl_printf_s failed\n");
+			return -1;
+		}
+	} else {
+		LOG(
+			L_ERR,
+			"ERROR:ldap:ldap_filter_url_encode: empty first argument\n");
+		return -1;
+	}
+
+	/*
+	* get dst AVP name (dst_avp_name)
+	*/
+	if (xl_get_avp_name(_msg, _dst_avp_spec, &dst_avp_name, &dst_avp_type) != 0)
+	{
+		LOG(L_ERR,
+			"ERROR:ldap:ldap_filter_url_encode: error getting dst AVP name\n");
+		return -1;
+	}
+	if (dst_avp_type & AVP_NAME_STR)
+	{
+		if (dst_avp_name.s.len >= STR_BUF_SIZE)
+		{
+			LOG(L_ERR,
+				"ERROR:ldap:ldap_filter_url_encode: dst AVP name too long\n");
+			return -1;
+		}
+		strncpy(str_buf, dst_avp_name.s.s, dst_avp_name.s.len);
+		str_buf[dst_avp_name.s.len] = '\0';
+		dst_avp_name.s.s = str_buf;
+	}
+
+	/*
+	* apply LDAP filter escaping rules
+	*/
+	esc_str.s = esc_buf;
+	esc_str.len = ESC_BUF_SIZE;
+	if (ldap_rfc4515_escape(&filter_component_str, &esc_str, 1) != 0)
+	{
+		LOG(L_ERR,
+		"ERROR:ldap:ldap_filter_url_encode: ldap_rfc4515_escape() failed\n");
+		return -1;
+	}
+
+	/*
+	* add dst AVP
+	*/
+	if (add_avp(dst_avp_type|AVP_VAL_STR, dst_avp_name, (int_str)esc_str) != 0)
+	{
+		LOG(L_ERR,
+			"ERROR:ldap:ldap_filter_url_encode: failed to add new AVP\n");
+		return -1;
+	}
+
+	return 1;
+}
