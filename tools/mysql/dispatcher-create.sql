9d1000c62403c4ff796524f7cee76f0dabb07fbe
Daniel-Constantin Mierla <miconda@gmail.com>
2007-07-27 11:45:36 +0000

    - compiled version for dispatcher table - included in default db creation - dev version increased
    git-svn-id: https://openser.svn.sourceforge.net/svnroot/openser/trunk@2534 689a6050-402a-0410-94f2-e92a70836424

---
+++ tools/mysql/dispatcher-create.sql
@@ -0,0 +1,9 @@
+INSERT INTO version (table_name, table_version) values ('dispatcher','1');
+CREATE TABLE dispatcher (
+    id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    setid INT NOT NULL DEFAULT 0,
+    destination VARCHAR(192) NOT NULL DEFAULT '',
+    description VARCHAR(64) NOT NULL DEFAULT ''
+) ENGINE=MyISAM;
+
+ 
---
+++ tools/postgres/dispatcher-create.sql
@@ -0,0 +1,9 @@
+INSERT INTO version (table_name, table_version) values ('dispatcher','1');
+CREATE TABLE dispatcher (
+    id SERIAL PRIMARY KEY NOT NULL,
+    setid INTEGER NOT NULL DEFAULT 0,
+    destination VARCHAR(192) NOT NULL DEFAULT '',
+    description VARCHAR(64) NOT NULL DEFAULT ''
+);
+
+ 
