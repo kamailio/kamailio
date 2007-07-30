1c3889068b044c63eb02f095a4d4b922dde61fb9
Andrei Pelinescu-Onciul <andrei@iptel.org>
2007-07-30 20:19:31 +0000

    - Retry-After parser missing files (from older commit)

---
+++ parser/case_retr.h
@@ -0,0 +1,74 @@
+/* 
+ * $Id$ 
+ *
+ * Expires Header Field Name Parsing Macros
+ *
+ * Copyright (C) 2007 iptelorg GmbH
+ *
+ * This file is part of ser, a free SIP server.
+ *
+ * ser is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version
+ *
+ * For a license to use the ser software under conditions
+ * other than those described here, or to purchase support for this
+ * software, please contact iptel.org by e-mail at the following addresses:
+ *    info@iptel.org
+ *
+ * ser is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software 
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * History:
+ * ---------
+ *  2007-07-27  created by andrei
+ */
+
+
+#ifndef CASE_RETR_H
+#define CASE_RETR_H
+
+#include "../comp_defs.h"
+#include "keys.h"
+
+#define RETR_TER_CASE				\
+	switch(LOWER_DWORD(val)){	\
+		case _ter1_:				\
+			hdr->type = HDR_RETRY_AFTER_T;	\
+			hdr->name.len = 11;				\
+			return (p + 4);					\
+		\
+		case _ter2_:                     \
+			hdr->type = HDR_RETRY_AFTER_T;	\
+			p += 4;							\
+			goto dc_end;					\
+	}
+
+
+
+#define RETR_Y_AF_CASE				\
+	if (LOWER_DWORD(val)==_y_af_){	\
+		p+=4;						\
+		val=READ(p);				\
+		RETR_TER_CASE;				\
+		goto other;					\
+	}
+
+
+
+
+#define retr_CASE		\
+	p+=4;			\
+	val=READ(p);	\
+	RETR_Y_AF_CASE;	\
+	goto other;
+
+
+#endif /* CASE_RETR_H */
