ed62a70cb22d399ef33eaa9e4897ebae15f56006
Henning Westerholt <henning.westerholt@1und1.de>
2007-09-28 11:26:26 +0000

    - activate sl_send_reply also for ONREPLY_ROUTEs
    git-svn-id: https://openser.svn.sourceforge.net/svnroot/openser/trunk@2835 689a6050-402a-0410-94f2-e92a70836424

--- modules_k/sl/sl.c
+++ modules_k/sl/sl.c
@@ -72,7 +72,7 @@ stat_var *rcv_acks;
 
 static cmd_export_t cmds[]={
 	{"sl_send_reply",   w_sl_send_reply,            2,  fixup_sl_send_reply,
-			REQUEST_ROUTE | ERROR_ROUTE},
+			REQUEST_ROUTE | ERROR_ROUTE | ONREPLY_ROUTE },
 	{"sl_reply_error",  w_sl_reply_error,           0,  0,
 			REQUEST_ROUTE},
 	{"register_slcb",  (cmd_function)register_slcb, 0,  0,
