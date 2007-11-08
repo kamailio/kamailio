c54d84dc16e4f5d1c9f1e4bb5cf240b0d9efaab8
Henning Westerholt <henning.westerholt@1und1.de>
2007-11-08 17:16:55 +0000

    - fix output of script
    git-svn-id: https://openser.svn.sourceforge.net/svnroot/openser/trunk@3067 689a6050-402a-0410-94f2-e92a70836424

--- test/unit/3.sh
+++ test/unit/3.sh
@@ -19,11 +19,11 @@ sed -i "s/# INSTALL_SERWEB_TABLES=ask/INSTALL_SERWEB_TABLES=yes/g" openserctlrc
 cp openserdbctl.mysql openserdbctl.mysql.bak
 sed -i "s/#PW=""/PW=""/g" openserdbctl.mysql
 
-./openserdbctl create $tmp_name #> /dev/null
+./openserdbctl create $tmp_name > /dev/null
 ret=$?
 
 if [ "$ret" -eq 0 ] ; then
-	./openserdbctl drop $tmp_name #> /dev/null
+	./openserdbctl drop $tmp_name > /dev/null
 	ret=$?
 fi ;
 
