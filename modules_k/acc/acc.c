e1ca467bf0dae05fd2a8610b526c72b2cf956d75
Henning Westerholt <henning.westerholt@1und1.de>
2007-12-17 18:17:36 +0000

    - revert acc extension for now, will be later added again after some extension   from Bogdan to the logging system, makes this functionality much more easier   to achieve
    git-svn-id: https://openser.svn.sourceforge.net/svnroot/openser/trunk@3416 689a6050-402a-0410-94f2-e92a70836424

--- modules_k/acc/acc.c
+++ modules_k/acc/acc.c
@@ -248,7 +248,7 @@ int acc_log_request( struct sip_msg *rq)
 	*(p++) = '\n';
 	*(p++) = 0;
 
-	LOGACC(log_level, "%.*stimestamp=%lu%s",
+	LOG(log_level, "%.*stimestamp=%lu%s",
 		acc_env.text.len, acc_env.text.s,(unsigned long) acc_env.ts, log_msg);
 
 	return 1;
