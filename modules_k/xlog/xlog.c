c4bb0bd75bf7743b4ecaaed97dfcd3204bd01964
Henning Westerholt <henning.westerholt@1und1.de>
2007-12-18 17:36:20 +0000

    - add some cast to match the new module interface
    git-svn-id: https://openser.svn.sourceforge.net/svnroot/openser/trunk@3430 689a6050-402a-0410-94f2-e92a70836424

--- modules_k/xlog/xlog.c
+++ modules_k/xlog/xlog.c
@@ -73,11 +73,11 @@ static pv_export_t mod_items[] = {
 
 
 static cmd_export_t cmds[]={
-	{"xlog",  xlog_1,  1, xdbg_fixup, 0, REQUEST_ROUTE | FAILURE_ROUTE |
+	{"xlog",  (cmd_function)xlog_1,  1, xdbg_fixup, 0, REQUEST_ROUTE | FAILURE_ROUTE |
 		ONREPLY_ROUTE | BRANCH_ROUTE | ERROR_ROUTE},
-	{"xlog",  xlog_2,  2, xlog_fixup, 0, REQUEST_ROUTE | FAILURE_ROUTE |
+	{"xlog",  (cmd_function)xlog_2,  2, xlog_fixup, 0, REQUEST_ROUTE | FAILURE_ROUTE |
 		ONREPLY_ROUTE | BRANCH_ROUTE | ERROR_ROUTE},
-	{"xdbg",  xdbg,    1, xdbg_fixup, 0, REQUEST_ROUTE | FAILURE_ROUTE | 
+	{"xdbg",  (cmd_function)xdbg,    1, xdbg_fixup, 0, REQUEST_ROUTE | FAILURE_ROUTE | 
 		ONREPLY_ROUTE | BRANCH_ROUTE | ERROR_ROUTE},
 	{0,0,0,0,0,0}
 };
