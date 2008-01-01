fe34937922aa2af78f463e4fd4c7000f2599e1f5
Juha Heinanen <jh@tutpro.com>
2008-01-01 19:10:19 +0000

    * Fixed bug that was introduced in is_uri_host_local function by   http://openser.svn.sourceforge.net/viewvc/openser?view=rev&revision=1153   As result of the bug, is_uri_host_local function did not work if   it was called from failure route.
    git-svn-id: https://openser.svn.sourceforge.net/svnroot/openser/trunk@3496 689a6050-402a-0410-94f2-e92a70836424

--- modules_k/domain/domain.c
+++ modules_k/domain/domain.c
@@ -169,8 +169,7 @@ int is_uri_host_local(struct sip_msg* _msg, char* _s1, char* _s2)
 	qvalue_t q;
 	struct sip_uri puri;
 
-	if ((route_type == REQUEST_ROUTE) || (route_type == BRANCH_ROUTE)
-			|| (route_type == FAILURE_ROUTE)) {
+	if ((route_type == REQUEST_ROUTE) || (route_type == BRANCH_ROUTE)) {
 		if (parse_sip_msg_uri(_msg) < 0) {
 			LM_ERR("Error while parsing R-URI\n");
 			return -1;
