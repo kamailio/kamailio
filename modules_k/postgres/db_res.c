640212fca4c5d3b00fdc9c197bf3af90369927e9
Henning Westerholt <henning.westerholt@1und1.de>
2008-01-25 11:40:23 +0000

    - cleanups in postgres driver:   summarize comments, remove commented out code, remove most of the verbatim   copies from the postgres documentation, small formatting changes
    git-svn-id: https://openser.svn.sourceforge.net/svnroot/openser/trunk@3584 689a6050-402a-0410-94f2-e92a70836424

--- modules_k/postgres/db_res.c
+++ modules_k/postgres/db_res.c
@@ -6,6 +6,7 @@
  *
  * Copyright (C) 2003 August.Net Services, LLC
  * Copyright (C) 2006 Norman Brandinger
+ * Copyright (C) 2008 1&1 Internet AG
  *
  * This file is part of openser, a free SIP server.
  *
@@ -88,11 +89,11 @@ int db_postgres_get_columns(const db_con_t* _con, db_res_t* _res)
 		return -1;
 	}
 
-	/* PQntuples: Returns the number of rows (tuples) in the query result. */
+	/* Get the number of rows (tuples) in the query result. */
 	RES_NUM_ROWS(_res) = PQntuples(CON_RESULT(_con));
 
 
-	/* PQnfields: Returns the number of columns (fields) in each row of the query result. */
+	/* Get the number of columns (fields) in each row of the query result. */
 	cols = PQnfields(CON_RESULT(_con));
 
 	if (!cols) {
@@ -128,22 +129,12 @@ int db_postgres_get_columns(const db_con_t* _con, db_res_t* _res)
 	}
 
 	/* Save number of columns in the result structure */
-        RES_COL_N(_res) = cols;
+	RES_COL_N(_res) = cols;
 
-	/* 
-	 * For each column both the name and the OID number of the data type are saved.
-	 */
+	/* For each column both the name and the OID number of the data type are saved. */
 	for(col = 0; col < cols; col++) {
 		int ft;
 
-		/*
-		 * PQfname: Returns the column name associated with the given column number.
-		 * Column numbers start at 0. 
-		 * The caller should not free the result directly. 
-		 * It will be freed when the associated PGresult handle is passed to PQclear.
-		 * NULL is returned if the column number is out of range.
-		 *
-		 */
 		RES_NAMES(_res)[col] = (str*)pkg_malloc(sizeof(str));
 		if (! RES_NAMES(_res)[col]) {
 			LM_ERR("no private memory left\n");
@@ -152,7 +143,7 @@ int db_postgres_get_columns(const db_con_t* _con, db_res_t* _res)
 			// FIXME we should also free all previous allocated RES_NAMES[col]
 			return -5;
 		}
-
+		/* The pointer that is here returned is part of the PGResult structure. */
 		len = strlen(PQfname(CON_RESULT(_con),col));
 		RES_NAMES(_res)[col]->s = pkg_malloc(len);
 		LM_DBG("%p=pkg_malloc(%d) RES_NAMES[%d]->s\n", RES_NAMES(_res)[col]->s, len, col);
@@ -162,6 +153,7 @@ int db_postgres_get_columns(const db_con_t* _con, db_res_t* _res)
 			// FIXME we should also free all previous allocated memory here
 			return -1;
 		}
+		/* copy the name, because it will freed when the PGResult is freed */
 		memset((char *)RES_NAMES(_res)[col]->s, 0, len);
 		strncpy((char *)RES_NAMES(_res)[col]->s, PQfname(CON_RESULT(_con),col), len);
 		RES_NAMES(_res)[col]->len = len;
@@ -169,10 +161,7 @@ int db_postgres_get_columns(const db_con_t* _con, db_res_t* _res)
 		LM_DBG("RES_NAMES(%p)[%d]=[%.*s]\n", RES_NAMES(_res)[col], col,
 				RES_NAMES(_res)[col]->len, RES_NAMES(_res)[col]->s);
 
-		/* PQftype: Returns the data type associated with the given column number.
-		 * The integer returned is the internal OID number of the type.
-		 * Column numbers start at 0.
-		 */
+		/* get the datatype of the column */
 		switch(ft = PQftype(CON_RESULT(_con),col))
 		{
 			case INT2OID:
@@ -265,46 +254,8 @@ int db_postgres_convert_rows(const db_con_t* _con, db_res_t* _res, int row_start
 	cols = RES_COL_N(_res);
 
 	/*
-	 * I'm not sure of the usefulness of the following two blocks of code.
-	 * It appears that if, anywhere in the result, a zero length field exists, 
-	 * this routine quits and sets the number of rows in this Fetch to zero.
-	 * norm
-	 */
-
-	/*
-	 * Check the data length of each column and each row in the query result.
-	 * If a zero length is found ANYWHERE, set the value flag.
-	 * 
-	 * PQgetlength: Returns the actual length of a field value in bytes. 
-	 * Row and column numbers start at 0. 
-	 * This is the actual data length for the particular data value, that is,
-	 * the size of the object pointed to by PQgetvalue.
-	 * For text data format this is the same as strlen().
-	 * For binary format this is essential information.
-	 * Note that one should not rely on PQfsize to obtain the actual data length.
-	 *
-	 */
-
-	// value = 0;
-
-	// for(row = row_start; row < (row_start + row_count); row++) {
-	//	for(col = 0; col < cols; col++) {
-	//		if(PQgetlength(CON_RESULT(_con), row, col))
-	//			value = 1;
-	//	}
-	//}
-	/* Looking for the row instance with no values */
-	//if(!value){
-	//	LM_ERR("row instance, does not have a column value!\n");
-	//	/* Set the number of rows in this Fetch to zero */
-	//	RES_ROW_N(_res) = 0;
-	//	return 0;
-	//}
-
-	/*
-	 * Allocate an array of pointers one per column.
-	 * It that will be used to hold the address of the string representation of each column.
-	 *
+	 * Allocate an array of pointers one per column. It that will be used to hold
+	 * the address of the string representation of each column.
 	 */
 	len = sizeof(char *) * cols;
 	row_buf = (char **)pkg_malloc(len);
@@ -331,71 +282,28 @@ int db_postgres_convert_rows(const db_con_t* _con, db_res_t* _res, int row_start
 	fetch_count = 0;
 	for(row = row_start; row < (row_start + row_count); row++) {
 		for(col = 0; col < cols; col++) {
-			/* 
-			 * PQgetisnull: Tests a field for a null value. Row and column 
-			 * numbers start at 0.
-			 * This function returns 1 if the field is null and 0 if it 
-			 * contains a non-null value.
-			 * (Note that PQgetvalue will return an empty string, not a 
-			 * null pointer, for a null field.)
-			 *
-			 * Not sure of the usefullness of the following code used to 
-			 * set a NULL to an empty
-			 * string.  The doc for PQgetvalue (below) seems to indicate 
-			 * that this is taken care of
-			 * by PostgreSQL. norm 
-			 *
-			 */
-			// if(PQgetisnull(CON_RESULT(_con), row, col)) {
 				/*
-				** I don't believe there is a NULL
-				** representation, so, I'll cheat
-				*/
-		 	//	s = "";
-			// } else {
-				/*
-				 * PQgetvalue: Returns a single field value of one row of a PGresult.
-				 * Row and column numbers start at 0.
-				 * The caller should not free the result directly.
-				 * It will be freed when the associated PGresult 
-				 * handle is passed to PQclear. 
-				 * For data in text format, the value returned by 
-				 * PQgetvalue is a null-terminated
-				 * character string representation of the field value.
-				 * For data in binary format,
-				 * the value is in the binary representation determined 
-				 * by the data type's typsend
-				 * and typreceive functions. (The value is actually 
-				 * followed by a zero byte in this
-				 * case too, but that is not ordinarily useful, since
-				 * the value is likely to contain
-				 * embedded nulls.)
-				 * An empty string is returned if the field value is null.
-				 * See PQgetisnull to distinguish null values from 
-				 * empty-string values.
-				 * The pointer returned by PQgetvalue points to storage 
-				 * that is part of the PGresult structure.
-				 * One should not modify the data it points to, and one 
-				 * must explicitly copy the data into
-				 * other storage if it is to be used past the lifetime 
-				 * of the PGresult structure itself.
+				 * The row data pointer returned by PQgetvalue points to storage
+				 * that is part of the PGresult structure. One should not modify
+				 * the data it points to, and one must explicitly copy the data
+				 * into other storage if it is to be used past the lifetime of
+				 * the PGresult structure itself.
 				 */
 				s = PQgetvalue(CON_RESULT(_con), row, col);
-			// }
-			LM_DBG("PQgetvalue(%p,%d,%d)=[%s]\n", _con, row, col, s);
-			len = strlen(s);
-			row_buf[col] = pkg_malloc(len+1);
-			if (!row_buf[col]) {
-				LM_ERR("failed to allocate %d bytes in pkg memory "
-					"for row_buf[%d]\n", len+1, col);
-				return -1;
-			}
-			memset(row_buf[col], 0, len+1);
-			LM_DBG("%p=pkg_malloc(%d) row_buf[%d]\n", row_buf[col], len, col);
+				LM_DBG("PQgetvalue(%p,%d,%d)=[%s]\n", _con, row, col, s);
+				len = strlen(s);
+				row_buf[col] = pkg_malloc(len+1);
+				if (!row_buf[col]) {
+					LM_ERR("failed to allocate %d bytes in pkg memory "
+						"for row_buf[%d]\n", len+1, col);
+					return -1;
+				}
+				memset(row_buf[col], 0, len+1);
+				LM_DBG("%p=pkg_malloc(%d) row_buf[%d]\n", row_buf[col], len, col);
 
-			strncpy(row_buf[col], s, len);
-			LM_DBG("[%d][%d] Column[%.*s]=[%s]\n",
-				row, col, RES_NAMES(_res)[col]->len, RES_NAMES(_res)[col]->s, row_buf[col]);
+				strncpy(row_buf[col], s, len);
+				LM_DBG("[%d][%d] Column[%.*s]=[%s]\n",
+					row, col, RES_NAMES(_res)[col]->len, RES_NAMES(_res)[col]->s, row_buf[col]);
 		}
 
 		/*
@@ -412,31 +320,24 @@ int db_postgres_convert_rows(const db_con_t* _con, db_res_t* _res, int row_start
 			pkg_free(row_buf);
 			return -4;
 		}
-		
-		/* pkg_free() must be done for the above allocations 
-		 * now that the row has been converted.
-		 * During pg_convert_row (and subsequent pg_str2val) 
-		 * processing, data types that don't need to be
-		 * converted (namely STRINGS and STR) have their addresses saved.
-		 * These data types should not have
-		 * their pkg_malloc() allocations freed here because 
-		 * they are still needed.  However, some data types
-		 * (ex: INT, DOUBLE) should have their pkg_malloc() 
-		 * allocations freed because during the conversion
-		 * process, their converted values are saved in the union 
-		 * portion of the db_val_t structure
-		 * BLOB will be copied during PQunescape in str2val, thus it 
+		/*
+		 * pkg_free() must be done for the above allocations now that the row
+		 * has been converted. During pg_convert_row (and subsequent pg_str2val)
+		 * processing, data types that don't need to be converted (namely STRINGS
+		 * and STR) have their addresses saved. These data types should not have
+		 * their pkg_malloc() allocations freed here because they are still
+		 * needed.  However, some data types (ex: INT, DOUBLE) should have their
+		 * pkg_malloc() allocations freed because during the conversion process,
+		 * their converted values are saved in the union portion of the db_val_t
+		 * structure. BLOB will be copied during PQunescape in str2val, thus it
 		 * has to be freed here AND in pg_free_row().
 		 *
-		 * Warning: when the converted row is no longer needed, 
-		 * the data types whose addresses
-		 * were saved in the db_val_t structure must be freed or
-		 * a memory leak will happen.
-		 * This processing should happen in the pg_free_row() subroutine.
-		 * The caller of
-		 * this routine should ensure that pg_free_rows(), 
-		 * pg_free_row() or pg_free_result()
-		 * is eventually called.
+		 * Warning: when the converted row is no longer needed, the data types
+		 * whose addresses were saved in the db_val_t structure must be freed
+		 * or a memory leak will happen. This processing should happen in the
+		 * pg_free_row() subroutine. The caller of this routine should ensure
+		 * that pg_free_rows(), pg_free_row() or pg_free_result() is eventually
+		 * called.
 		 */
 		for (col=0; col<cols; col++) {
 			switch (RES_TYPES(_res)[col]) {
@@ -452,17 +353,14 @@ int db_postgres_convert_rows(const db_con_t* _con, db_res_t* _res, int row_start
 						(char *)row_buf[col], col);
 					pkg_free((char *)row_buf[col]);
 			}
-			/* The following housekeeping may not be technically 
-			 * required, but it is a good practice
-			 * to NULL pointer fields that are no longer valid.  
-			 * Note that DB_STRING fields have not
-			 * been pkg_free(). NULLing DB_STRING fields would normally 
-			 * not be good to do because a memory
-			 * leak would occur.  However, the pg_convert_row() routine 
-			 * has saved the DB_STRING pointer
-			 * in the db_val_t structure.  The db_val_t structure will 
-			 * eventually be used to pkg_free()
-			 * the DB_STRING storage.
+			/*
+			 * The following housekeeping may not be technically required, but it
+			 * is a good practice to NULL pointer fields that are no longer valid.
+			 * Note that DB_STRING fields have not been pkg_free(). NULLing DB_STRING
+			 * fields would normally not be good to do because a memory leak would
+			 * occur.  However, the pg_convert_row() routine  has saved the DB_STRING
+			 * pointer in the db_val_t structure.  The db_val_t structure will 
+			 * eventually be used to pkg_free() the DB_STRING storage.
 			 */
 			row_buf[col] = (char *)NULL;
 		}
@@ -488,8 +386,10 @@ int db_postgres_convert_row(const db_con_t* _con, db_res_t* _res, db_row_t* _row
 		return -1;
 	}
 
-	/* Allocate storage to hold the data type value converted from a string */
-	/* because PostgreSQL returns (most) data as strings */
+	/*
+	 * Allocate storage to hold the data type value converted from a string
+	 * because PostgreSQL returns (most) data as strings
+	 */
 	len = sizeof(db_val_t) * RES_COL_N(_res);
 	ROW_VALUES(_row) = (db_val_t*)pkg_malloc(len);
 	LM_DBG("%p=pkg_malloc(%d) ROW_VALUES for %d columns\n",
@@ -567,11 +467,10 @@ int db_postgres_free_row(db_row_t* _row)
 		return -1;
 	}
 
-	/* 
-	 * Loop thru each columm, then check to determine if the storage 
-	 * pointed to by db_val_t structure must be freed.
-	 * This is required for all data types which use a pointer to a buffer 
-	 * like DB_STRING, DB_STR and DB_BLOB.
+	/*
+	 * Loop thru each columm, then check to determine if the storage pointed to
+	 * by db_val_t structure must be freed. This is required for all data types
+	 * which use a pointer to a buffer like DB_STRING, DB_STR and DB_BLOB.
 	 * If this is not done, a memory leak will happen.
 	 */
 	for (col = 0; col < ROW_N(_row); col++) {
