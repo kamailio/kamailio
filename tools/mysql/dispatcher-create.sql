dbbf5470a06971351da1a5fd95744616cb527a84
Daniel-Constantin Mierla <miconda@gmail.com>
2008-02-07 13:03:47 +0000

    - regeneration of scripts to create dispatcher table
    git-svn-id: https://openser.svn.sourceforge.net/svnroot/openser/trunk@3661 689a6050-402a-0410-94f2-e92a70836424

--- tools/mysql/dispatcher-create.sql
+++ tools/mysql/dispatcher-create.sql
@@ -1,8 +1,9 @@
-INSERT INTO version (table_name, table_version) values ('dispatcher','1');
+INSERT INTO version (table_name, table_version) values ('dispatcher','2');
 CREATE TABLE dispatcher (
     id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
     setid INT NOT NULL DEFAULT 0,
     destination VARCHAR(192) NOT NULL DEFAULT '',
+    flags INT NOT NULL DEFAULT 0,
     description VARCHAR(64) NOT NULL DEFAULT ''
 ) ENGINE=MyISAM;
 
--- tools/postgres/dispatcher-create.sql
+++ tools/postgres/dispatcher-create.sql
@@ -1,8 +1,9 @@
-INSERT INTO version (table_name, table_version) values ('dispatcher','1');
+INSERT INTO version (table_name, table_version) values ('dispatcher','2');
 CREATE TABLE dispatcher (
     id SERIAL PRIMARY KEY NOT NULL,
     setid INTEGER NOT NULL DEFAULT 0,
     destination VARCHAR(192) NOT NULL DEFAULT '',
+    flags INTEGER NOT NULL DEFAULT 0,
     description VARCHAR(64) NOT NULL DEFAULT ''
 );
 
