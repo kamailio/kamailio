5e0a031e9ade7839f292475f69aba0748c3c0c93
Bogdan-Andrei Iancu <bogdan@voice-system.ro>
2008-02-28 09:54:16 +0000

    - removed from the subscriber table the following fields: 	- first_name 	- last_name 	- email_address 	- datetime_created 	- timezone   as these fields are not used by openser itself (but rather by whatever provisioning tools)
    git-svn-id: https://openser.svn.sourceforge.net/svnroot/openser/trunk@3782 689a6050-402a-0410-94f2-e92a70836424

--- tools/mysql/auth_db-create.sql
+++ tools/mysql/auth_db-create.sql
@@ -4,13 +4,9 @@ CREATE TABLE subscriber (
     username VARCHAR(64) NOT NULL DEFAULT '',
     domain VARCHAR(64) NOT NULL DEFAULT '',
     password VARCHAR(25) NOT NULL DEFAULT '',
-    first_name VARCHAR(64) NOT NULL DEFAULT '',
-    last_name VARCHAR(64) NOT NULL DEFAULT '',
     email_address VARCHAR(64) NOT NULL DEFAULT '',
-    datetime_created DATETIME NOT NULL DEFAULT '1900-01-01 00:00:01',
     ha1 VARCHAR(64) NOT NULL DEFAULT '',
     ha1b VARCHAR(64) NOT NULL DEFAULT '',
-    timezone VARCHAR(64) DEFAULT NULL,
     rpid VARCHAR(64) DEFAULT NULL,
     UNIQUE KEY account_idx (username, domain),
     KEY username_idx (username)
