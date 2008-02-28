3e8e2f20d63e0e678f7722220b2e00a4e5d8b010
Henning Westerholt <henning.westerholt@1und1.de>
2008-02-28 15:45:33 +0000

    - major reworking of carrierroute module - make exported, user visible interface much more flexible - add a new table 'carrierfailureroute' for database supported failure routing - credits for this work goes to Hardy Kahl, hardy dot kahl at 1und1 dot de
    git-svn-id: https://openser.svn.sourceforge.net/svnroot/openser/trunk@3789 689a6050-402a-0410-94f2-e92a70836424

--- tools/mysql/carrierroute-create.sql
+++ tools/mysql/carrierroute-create.sql
@@ -12,6 +12,20 @@ CREATE TABLE carrierroute (
     comment VARCHAR(255) DEFAULT NULL
 ) ENGINE=MyISAM;
 
+INSERT INTO version (table_name, table_version) values ('carrierfailureroute','1');
+CREATE TABLE carrierfailureroute (
+    id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    carrier INT(10) UNSIGNED NOT NULL DEFAULT 0,
+    domain VARCHAR(64) NOT NULL DEFAULT '',
+    scan_prefix VARCHAR(64) NOT NULL DEFAULT '',
+    host_name VARCHAR(128) NOT NULL DEFAULT '',
+    reply_code VARCHAR(3) NOT NULL DEFAULT '',
+    flags INT(11) UNSIGNED NOT NULL DEFAULT 0,
+    mask INT(11) UNSIGNED NOT NULL DEFAULT 0,
+    next_domain VARCHAR(64) NOT NULL DEFAULT '',
+    comment VARCHAR(255) DEFAULT NULL
+) ENGINE=MyISAM;
+
 INSERT INTO version (table_name, table_version) values ('route_tree','1');
 CREATE TABLE route_tree (
     id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
--- tools/postgres/carrierroute-create.sql
+++ tools/postgres/carrierroute-create.sql
@@ -12,6 +12,20 @@ CREATE TABLE carrierroute (
     comment VARCHAR(255) DEFAULT NULL
 );
 
+INSERT INTO version (table_name, table_version) values ('carrierfailureroute','1');
+CREATE TABLE carrierfailureroute (
+    id SERIAL PRIMARY KEY NOT NULL,
+    carrier INTEGER NOT NULL DEFAULT 0,
+    domain VARCHAR(64) NOT NULL DEFAULT '',
+    scan_prefix VARCHAR(64) NOT NULL DEFAULT '',
+    host_name VARCHAR(128) NOT NULL DEFAULT '',
+    reply_code VARCHAR(3) NOT NULL DEFAULT '',
+    flags INTEGER NOT NULL DEFAULT 0,
+    mask INTEGER NOT NULL DEFAULT 0,
+    next_domain VARCHAR(64) NOT NULL DEFAULT '',
+    comment VARCHAR(255) DEFAULT NULL
+);
+
 INSERT INTO version (table_name, table_version) values ('route_tree','1');
 CREATE TABLE route_tree (
     id SERIAL PRIMARY KEY NOT NULL,
