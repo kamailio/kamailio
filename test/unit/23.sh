b1bdf4a2d51bc0c95b206184546d4414b02edb2d
Henning Westerholt <henning.westerholt@1und1.de>
2008-02-29 13:03:50 +0000

    - fix and extend carrierroute loading tests
    git-svn-id: https://openser.svn.sourceforge.net/svnroot/openser/trunk@3807 689a6050-402a-0410-94f2-e92a70836424

--- test/unit/23.sh
+++ test/unit/23.sh
@@ -19,7 +19,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-CFG=14.cfg
+CFG=13.cfg
 
 if [ ! -e ../modules/carrierroute/carrierroute.so ] ; then
 	echo "carrierroute not found, not run"
