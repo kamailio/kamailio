884c4c982aaa416f39126b3c975918a91c0a3402
Jan Janak <jan@iptel.org>
2008-03-06 12:15:51 +0000

    - base64 functions moved to libcds - updated Makefile to use libcds

--- modules_s/rr/avp_cookie.c
+++ modules_s/rr/avp_cookie.c
@@ -29,6 +29,7 @@
 
 #include <stdio.h>
 #include "avp_cookie.h"
+#include "../../lib/cds/base64.h"
 #include "../../crc.h"
 #include "../../usr_avp.h"
 #include "../../mem/mem.h"
@@ -40,74 +41,6 @@
 unsigned short crc_secret = 0;
 regex_t* cookie_filter_re = 0;
 avp_flags_t avp_flag_dialog = 0;
-
-void base64decode(char* src_buf, int src_len, char* tgt_buf, int* tgt_len) {
-	int pos, i, n;
-	unsigned char c[4];
-	for (pos=0, i=0, *tgt_len=0; pos < src_len; pos++) {
-		if (src_buf[pos] >= 'A' && src_buf[pos] <= 'Z')
-			c[i] = src_buf[pos] - 65;   /* <65..90>  --> <0..25> */
-		else if (src_buf[pos] >= 'a' && src_buf[pos] <= 'z')
-			c[i] = src_buf[pos] - 71;   /* <97..122>  --> <26..51> */
-		else if (src_buf[pos] >= '0' && src_buf[pos] <= '9')
-			c[i] = src_buf[pos] + 4;    /* <48..56>  --> <52..61> */
-		else if (src_buf[pos] == '+')
-			c[i] = 62;
-		else if (src_buf[pos] == '/')
-			c[i] = 63;
-		else  /* '=' */
-			c[i] = 64;
-		i++;
-		if (pos == src_len-1) {
-			while (i < 4) {
-				c[i] = 64;
-				i++;
-			}
-		}
-		if (i==4) {
-			if (c[0] == 64)
-				n = 0;
-			else if (c[2] == 64)
-				n = 1;
-			else if (c[3] == 64)
-				n = 2;
-			else
-				n = 3;
-			switch (n) {
-				case 3:
-					tgt_buf[*tgt_len+2] = (char) (((c[2] & 0x03) << 6) | c[3]);
-					/* no break */
-				case 2:
-					tgt_buf[*tgt_len+1] = (char) (((c[1] & 0x0F) << 4) | (c[2] >> 2));
-					/* no break */
-				case 1:
-					tgt_buf[*tgt_len+0] = (char) ((c[0] << 2) | (c[1] >> 4));
-					break;
-			}
-			i=0;
-			*tgt_len+= n;
-		}
-	}
-}
-
-void base64encode(char* src_buf, int src_len, char* tgt_buf, int* tgt_len) {
-	static char code64[64+1] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-	int pos;
-	for (pos=0, *tgt_len=0; pos < src_len; pos+=3,*tgt_len+=4) {
-		tgt_buf[*tgt_len+0] = code64[(unsigned char)src_buf[pos+0] >> 2];
-		tgt_buf[*tgt_len+1] = code64[(((unsigned char)src_buf[pos+0] & 0x03) << 4) | ((pos+1 < src_len)?((unsigned char)src_buf[pos+1] >> 4):0)];
-		if (pos+1 < src_len)
-			tgt_buf[*tgt_len+2] = code64[(((unsigned char)src_buf[pos+1] & 0x0F) << 2) | ((pos+2 < src_len)?((unsigned char)src_buf[pos+2] >> 6):0)];
-		else
-			(*tgt_len)--;
-			/* tgt_buf[*tgt_len+2] = '='; do not add trailing equals - not header compliant without quotes */
-		if (pos+2 < src_len)
-			tgt_buf[*tgt_len+3] = code64[(unsigned char)src_buf[pos+2] & 0x3F];
-		else
-			(*tgt_len)--;
-			/* tgt_buf[*tgt_len+3] = '='; */
-	}
-}
 
 #define MAX_AVP_DIALOG_LISTS 4
 static unsigned short avp_dialog_lists[MAX_AVP_DIALOG_LISTS] = {AVP_CLASS_URI|AVP_TRACK_FROM, AVP_CLASS_URI|AVP_TRACK_TO,
@@ -204,7 +137,7 @@ brk:
 		crc = crcitt_string_ex(buf+sizeof(crc), len-sizeof(crc), crc_secret);
 		memcpy(&buf, &crc, sizeof(crc));
 
-		base64encode(buf, len, result->s, &result->len);
+		base64encode(buf, len, result->s, &result->len, 0);
 		DBG("avp_cookies: len=%d, crc=0x%x, base64(%u)='%.*s'\n", len, crc, result->len, result->len, result->s);
 	}
 	return result;
