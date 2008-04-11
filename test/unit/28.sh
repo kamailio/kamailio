07080c8ad2d80230bd9e1f2c80b1bc28f4b8a039
Henning Westerholt <henning.westerholt@1und1.de>
2008-04-11 09:47:17 +0000

    - rename presence test - add test with some simple cpl operations for mysql
    git-svn-id: https://openser.svn.sourceforge.net/svnroot/openser/trunk@4008 689a6050-402a-0410-94f2-e92a70836424

---
+++ test/unit/28.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+# tests simple cpl-c script operatons with mysql
+
+# Copyright (C) 2008 1&1 Internet AG
+#
+# This file is part of openser, a free SIP server.
+#
+# openser is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version
+#
+# openser is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+# needs the sipp utility to run
+which sipp > /dev/null
+ret=$?
+
+if [ ! $? -eq 0 ] ; then
+	echo "sipp not found, not run"
+	exit 0
+fi ;
+
+CFG=28.cfg
+CPL=cpl_ignore.xml
+TMPFILE=`mktemp -t openser-test.XXXXXXXXXX`
+
+
+../openser -w . -f $CFG &> /dev/null;
+ret=$?
+sleep 1
+
+../scripts/openserctl fifo LOAD_CPL sip:alice@127.0.0.1 $CPL
+
+if [ "$ret" -eq 0 ] ; then
+	sipp -m 1 -f 1 127.0.0.1:5060 -sf cpl_test.xml &> /dev/null;
+	ret=$?
+fi;
+
+if [ "$ret" -eq 0 ] ; then
+  ../scripts/openserctl fifo GET_CPL sip:alice@127.0.0.1 > $TMPFILE 
+  diff $TMPFILE $CPL 
+  ret=$?
+fi; 
+
+if [ "$ret" -eq 0 ] ; then
+  ../scripts/openserctl fifo REMOVE_CPL sip:alice@127.0.0.1
+  ../scripts/openserctl fifo GET_CPL sip:alice@127.0.0.1 > $TMPFILE
+fi;
+
+diff $TMPFILE $CPL &> /dev/null;
+ret=$?
+
+if [ ! "$ret" -eq 0 ] ; then
+  ret=0
+fi;
+
+#cleanup:
+killall -9 openser &> /dev/null;
+killall -9 sipp &> /dev/null;
+rm $TMPFILE
+
+exit $ret;
