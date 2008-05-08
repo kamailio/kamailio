47097ffefaf8c38cea7cb8fb8356f0b5fe9ae239
Henning Westerholt <henning.westerholt@1und1.de>
2008-05-08 15:47:43 +0000

    - postgres fetch_result test should also use a small fetch_rows parameter
    git-svn-id: https://openser.svn.sourceforge.net/svnroot/openser/trunk@4136 689a6050-402a-0410-94f2-e92a70836424

--- test/unit/32.sh
+++ test/unit/32.sh
@@ -32,6 +32,7 @@ cp $CFG $CFG.bak
 
 echo "loadmodule \"db_postgres/db_postgres.so\"" >> $CFG
 echo "modparam(\"usrloc\", \"db_url\", \"postgres://openser:openserrw@localhost/openser\")" >> $CFG
+echo "modparam(\"usrloc\", \"fetch_rows\", 13)" >> $CFG
 
 DOMAIN="local"
 
