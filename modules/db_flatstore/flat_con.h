3e87a030beddd8b55885bc1c1036e7571391afe7
Jan Janak <jan@iptel.org>
2008-05-27 00:22:02 +0000

    - new version of flatstore module for new db api in SER 2.1

--- modules/db_flatstore/flat_con.h
+++ modules/db_flatstore/flat_con.h
@@ -1,70 +1,92 @@
-/* 
+/*
  * $Id$
  *
- * Flatstore module connection structure
+ * Copyright (C) 2004 FhG FOKUS
+ * Copyright (C) 2008 iptelorg GmbH
+ * Written by Jan Janak <jan@iptel.org>
  *
- * Copyright (C) 2004 FhG Fokus
+ * This file is part of SER, a free SIP server.
  *
- * This file is part of ser, a free SIP server.
+ * SER is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
  *
- * ser is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version
+ * SER is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
  *
- * For a license to use the ser software under conditions
- * other than those described here, or to purchase support for this
- * software, please contact iptel.org by e-mail at the following addresses:
- *    info@iptel.org
- *
- * ser is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc., 
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
 #ifndef _FLAT_CON_H
 #define _FLAT_CON_H
 
-#include <stdio.h>
-#include <time.h>
-#include "flat_id.h"
+/** \addtogroup flatstore
+ * @{ 
+ */
 
-struct flat_con {
-	struct flat_id* id;    /* Connection identifier */
-	int ref;               /* Reference count */
-	FILE* file;            /* File descriptor structure */
-	struct flat_con* next; /* Next connection in the pool */
+/** \file 
+ * Inmplementation of flatstore "connections".
+ */
+
+#include "../../db/db_pool.h"
+#include "../../db/db_con.h"
+#include "../../db/db_uri.h"
+
+#include <stdio.h>
+
+
+/** 
+ * Per-connection flags for flatstore connections.
+ */
+enum flat_con_flags {
+	FLAT_OPENED      = (1 << 0), /**< Handle opened successfully */
 };
 
 
-/*
- * Some convenience wrappers
+struct flat_file {
+	char* filename; /**< Name of file within the directory */
+	str table;      /**< Table name the file belongs to */
+	FILE* f;        /**< File handle of the file */
+};
+
+
+/** A structure representing flatstore virtual connections.
+ * Flatstore module is writing data to files on a local filesystem only so
+ * there is no concept of real database connections.  In flatstore module a
+ * connection is related with a directory on the filesystem and it contains
+ * file handles to files in that directory. The file handles are then used
+ * from commands to write data in them.
  */
-#define CON_FILE(db_con) (((struct flat_con*)((db_con)->tail))->file)
+struct flat_con {
+	db_pool_entry_t gen;    /**< Generic part of the structure */
+	struct flat_file* file;
+	int n;                  /**< Size of the file array */
+	unsigned int flags;     /**< Flags */
+};
 
 
-/*
- * Create a new connection structure,
- * open the MySQL connection and set reference count to 1
+/** Create a new flat_con structure.
+ * This function creates a new flat_con structure and attachs the structure to
+ * the generic db_con structure in the parameter.
+ * @param con A generic db_con structure to be extended with flatstore payload
+ * @retval 0 on success
+ * @retval A negative number on error
  */
-struct flat_con* flat_new_connection(struct flat_id* id);
+int flat_con(db_con_t* con);
 
 
-/*
- * Close the connection and release memory
- */
-void flat_free_connection(struct flat_con* con);
+int flat_con_connect(db_con_t* con);
 
 
-/*
- * Reopen a connection
- */
-int flat_reopen_connection(struct flat_con* con);
+void flat_con_disconnect(db_con_t* con);
+
+
+int flat_open_table(int *idx, db_con_t* con, str* name);
 
 
 #endif /* _FLAT_CON_H */
