f9c15185aefb8d19e4476420139f4c6029780b00
Jan Janak <jan@iptel.org>
2008-06-01 09:35:48 +0000

    - license text reformatted - changed header field order in ld_uri.c

--- modules_s/ldap/ld_con.h
+++ modules_s/ldap/ld_con.h
@@ -10,11 +10,7 @@
  * SER is free software; you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
  * Foundation; either version 2 of the License, or (at your option) any later
- * version
- *
- * For a license to use the ser software under conditions other than those
- * described here, or to purchase support for this software, please contact
- * iptel.org by e-mail at the following addresses: info@iptel.org
+ * version.
  *
  * SER is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
@@ -22,8 +18,8 @@
  * details.
  *
  * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc., 59
- * Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * with this program; if not, write to the Free Software Foundation, Inc., 
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
  */
 
 #ifndef _LD_CON_H
@@ -75,9 +71,7 @@ int ld_con(db_con_t* con);
 
 /** Establish a new connection to server.  
  * This function is called when a SER module calls db_connect to establish a
- * new connection to the database server. After the connection is established
- * the function sends an SQL query to the server to determine the format of
- * timestamp fields and also obtains the list of supported field types.
+ * new connection to the database server.
  * @param con A structure representing database connection.
  * @retval 0 on success.
  * @retval A negative number on error.
