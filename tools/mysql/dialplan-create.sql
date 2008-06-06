38fbc1b82b390e40f8a374eef3381c1bd1382606
Anca Vamanu <anca@voice-system.ro>
2008-06-06 14:36:01 +0000

    -  uploaded new modules that implements generic string translations based on matching and replacement rules. It can be used to manipulate R-URI or a PV and to translated to a new format/value.
    git-svn-id: https://openser.svn.sourceforge.net/svnroot/openser/trunk@4331 689a6050-402a-0410-94f2-e92a70836424

---
+++ tools/mysql/dialplan-create.sql
@@ -0,0 +1,13 @@
+INSERT INTO version (table_name, table_version) values ('dialplan','1');
+CREATE TABLE dialplan (
+    id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    dpid INT(11) NOT NULL,
+    pr INT(11) NOT NULL,
+    match_op INT(11) NOT NULL,
+    match_exp VARCHAR(64) NOT NULL,
+    match_len INT(11) NOT NULL,
+    subst_exp VARCHAR(64) NOT NULL,
+    repl_exp VARCHAR(32) NOT NULL,
+    attrs VARCHAR(32) NOT NULL
+) ENGINE=MyISAM;
+
