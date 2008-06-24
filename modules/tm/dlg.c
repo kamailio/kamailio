fecc52cd4fd3121f2036332c690012f6e84c2783
Jan Janak <jan@iptel.org>
2008-06-24 16:12:09 +0000

    - reset pointes when route set is missing, patch provided by Dragos Vingarnzan   closes SER-266

--- modules/tm/dlg.c
+++ modules/tm/dlg.c
@@ -143,6 +143,13 @@ static inline int calculate_hooks(dlg_t* _d)
 		
 		if (_d->dst_uri.s) _d->hooks.next_hop = &_d->dst_uri;
 		else _d->hooks.next_hop = _d->hooks.request_uri;
+
+		/*
+		 * the routes in the hooks need to be reset because if the route_set was dropped somewhere else
+		 * then these will remain set without the actual routes existing any more
+		 */
+		_d->hooks.first_route = 0;
+		_d->hooks.last_route = 0; 
 	}
 
 	if ((_d->hooks.request_uri) && (_d->hooks.request_uri->s) && (_d->hooks.request_uri->len)) {
