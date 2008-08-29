131f4c1a8748cddf53b9839f0967b417dd29fda9
Henning Westerholt <henning.westerholt@1und1.de>
2008-08-29 13:59:47 +0000

    - add test for private memory effiency and eventual fragmentation with   ul_dump with and without PKG_MALLOC - especially with regards to the fragmentation this needs some extensions
    git-svn-id: https://openser.svn.sourceforge.net/svnroot/openser/trunk@4775 689a6050-402a-0410-94f2-e92a70836424

---
+++ test/unit/36.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+# private memory effiency and fragmentation with ul_dump w/ and w/o PKG_MALLOC
+
+# Copyright (C) 2008 1&1 Internet AG
+#
+# This file is part of Kamailio, a free SIP server.
+#
+# Kamailio is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version
+#
+# Kamailio is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+source include/common
+source include/require
+source include/database
+
+if ! (check_kamailio && check_module "db_mysql" && check_mysql); then
+	exit 0
+fi ;
+
+CFG=11.cfg
+
+cp $CFG $CFG.bak
+
+echo "loadmodule \"db_mysql/db_mysql.so\"" >> $CFG
+
+# 1778 contacts should fit into 1 MB of PKG memory
+../$BIN -V | grep "PKG_MALLOC" > /dev/null
+if [ $? -eq 0 ]; then
+	NR=1778
+else
+	NR=2778
+fi;
+
+COUNTER=0
+while [  $COUNTER -lt $NR ]; do
+	COUNTER=$(($COUNTER+1))
+	$MYSQL "insert into location (username, domain, contact, user_agent) values ('foobar-$COUNTER-$RANDOM', 'local', 'foobar-$COUNTER-$RANDOM@$DOMAIN', '___test___');"
+done
+
+../$BIN -w . -f $CFG > /dev/null
+ret=$?
+
+if [ $ret -eq 0 ]; then
+	sleep 1
+	for ((i=1;i<=100;i+=1)); do
+		tmp=`../scripts/$CTL fifo ul_dump | grep "AOR" | wc -l`
+		if ! [ $tmp -eq $NR ]; then
+			ret=1
+			break;
+		fi;
+	done
+fi;
+
+sleep 1
+killall -9 $BIN
+
+$MYSQL "delete from location where user_agent = '___test___'"
+
+mv $CFG.bak $CFG
+
+exit $ret
