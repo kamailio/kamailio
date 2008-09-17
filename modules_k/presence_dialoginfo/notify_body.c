fed075fac7914cba1548fa4a51a37bbf0109fa30
Klaus Darilion <klaus.darilion@pernau.at>
2008-09-17 11:45:48 +0000

    - add modules for dialog-info support to pua and presence module   This work was sponsored by Silver Server (www.sil.at)
    - add modules to debian presence package
    
    - add new modules to exclude_modules
    
    
    
    git-svn-id: https://openser.svn.sourceforge.net/svnroot/openser/trunk@4937 689a6050-402a-0410-94f2-e92a70836424

---
+++ modules_k/presence_dialoginfo/notify_body.c
@@ -0,0 +1,321 @@
+/*
+ * $Id: notify_body.c 1337 2006-12-07 18:05:05Z bogdan_iancu $
+ *
+ * presence_dialoginfo module -  
+ *
+ * Copyright (C) 2006 Voice Sistem S.R.L.
+ * Copyright (C) 2008 Klaus Darilion, IPCom
+ *
+ * This file is part of Kamailio, a free SIP server.
+ *
+ * Kamailio is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version
+ *
+ * Kamailio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * History:
+ * --------
+ *  2008-08-25  initial version (kd)
+ */
+/*! \file
+ * \brief Kamailio Presence_XML :: Notify BODY handling
+ * \ingroup presence_xml
+ */
+
+#define MAX_INT_LEN 11 /* 2^32: 10 chars + 1 char sign */
+
+#include <string.h>
+#include <stdlib.h>
+#include <libxml/parser.h>
+
+#include "../../mem/mem.h"
+#include "../presence/utils_func.h"
+#include "../presence/hash.h"
+#include "../presence/event_list.h"
+#include "../presence/presence.h"
+#include "../presence/presentity.h"
+#include "notify_body.h"
+#include "pidf.h"
+
+str* agregate_xmls(str* pres_user, str* pres_domain, str** body_array, int n);
+extern int force_single_dialog;
+
+void free_xml_body(char* body)
+{
+	if(body== NULL)
+		return;
+
+	xmlFree(body);
+	body= NULL;
+}
+
+
+str* dlginfo_agg_nbody(str* pres_user, str* pres_domain, str** body_array, int n, int off_index)
+{
+	str* n_body= NULL;
+
+	if(body_array== NULL)
+		return NULL;
+
+	n_body= agregate_xmls(pres_user, pres_domain, body_array, n);
+	if(n_body== NULL && n!= 0)
+	{
+		LM_ERR("while aggregating body\n");
+	}
+
+	xmlCleanupParser();
+    xmlMemoryDump();
+
+	return n_body;
+}	
+
+str* agregate_xmls(str* pres_user, str* pres_domain, str** body_array, int n)
+{
+	int i, j= 0;
+
+	xmlDocPtr  doc = NULL;
+	xmlNodePtr root_node = NULL;
+	xmlNsPtr   namespace = NULL;
+
+	xmlNodePtr p_root= NULL;
+	xmlDocPtr* xml_array ;
+	xmlNodePtr node = NULL;
+	char *state;
+	int winner_priority = -1, priority ;
+	xmlNodePtr winner_dialog_node = NULL ;
+	str *body= NULL;
+    char buf[MAX_URI_SIZE+1];
+
+	LM_DBG("[pres_user]=%.*s [pres_domain]= %.*s, [n]=%d\n",
+			pres_user->len, pres_user->s, pres_domain->len, pres_domain->s, n);
+
+	xml_array = (xmlDocPtr*)pkg_malloc( n*sizeof(xmlDocPtr));
+	if(xml_array== NULL)
+	{
+		LM_ERR("while allocating memory");
+		return NULL;
+	}
+	memset(xml_array, 0, n*sizeof(xmlDocPtr)) ;
+
+	/* parse all the XML documents */
+	for(i=0; i<n; i++)
+	{
+		if(body_array[i] == NULL )
+			continue;
+
+		xml_array[j] = NULL;
+		xml_array[j] = xmlParseMemory( body_array[i]->s, body_array[i]->len );
+		
+		/* LM_DBG("parsing XML body: [n]=%d, [i]=%d, [j]=%d xml_array[j]=%p\n", n, i, j, xml_array[j] ); */
+
+		if( xml_array[j]== NULL)
+		{
+			LM_ERR("while parsing xml body message\n");
+			goto error;
+		}
+		j++;
+
+	} 
+
+	if(j== 0)  /* no body */
+	{
+		if(xml_array)
+			pkg_free(xml_array);
+		return NULL;
+	}
+
+	/* n: number of bodies in total */
+	/* j: number of useful bodies; created XML structures */
+	/* i: loop counter */
+	/* LM_DBG("number of bodies in total [n]=%d, number of useful bodies [j]=%d\n", n, j ); */
+
+	/* create the new NOTIFY body  */
+    if ( (pres_user->len + pres_domain->len + 1) > MAX_URI_SIZE) {
+        LM_ERR("entity URI too long, maximum=%d\n", MAX_URI_SIZE);
+        return NULL;
+    }
+    memcpy(buf, pres_user->s, pres_user->len);
+    buf[pres_user->len] = '@';
+    memcpy(buf + pres_user->len + 1, pres_domain->s, pres_domain->len);
+    buf[pres_user->len + 1 + pres_domain->len]= '\0';
+
+    doc = xmlNewDoc(BAD_CAST "1.0");
+    if(doc==0)
+        return NULL;
+
+    root_node = xmlNewNode(NULL, BAD_CAST "dialog-info");
+    if(root_node==0)
+        goto error;
+
+    xmlDocSetRootElement(doc, root_node);
+	namespace = xmlNewNs(root_node, BAD_CAST "urn:ietf:params:xml:ns:dialog-info", NULL);
+	if (!namespace) {
+		LM_ERR("creating namespace failed\n");
+	}
+	xmlSetNs(root_node, namespace);
+	/* The version must be increased for each new document and is a 32bit int.
+       As the version is different for each watcher, we can not set here the
+       correct value. Thus, we just put here a placeholder which will be 
+	   replaced by the correct value in the aux_body_processing callback.
+	   Thus we have CPU intensive XML aggregation only once and can use
+	   quick search&replace in the per-watcher aux_body_processing callback.
+	   We use 11 chracters as an signed int (although RFC says unsigned int we
+	   use signed int as presence module stores "version" in DB as
+	   signed int) has max. 10 characters + 1 character for the sign
+	*/
+    xmlNewProp(root_node, BAD_CAST "version", BAD_CAST "00000000000");
+    xmlNewProp(root_node, BAD_CAST  "state",  BAD_CAST "full" );
+    xmlNewProp(root_node, BAD_CAST "entity",  BAD_CAST buf);
+
+	/* loop over all bodies and create the aggregated body */
+	for(i=0; i<j; i++)
+	{
+		/* LM_DBG("[n]=%d, [i]=%d, [j]=%d xml_array[i]=%p\n", n, i, j, xml_array[j] ); */
+		p_root= xmlDocGetRootElement(xml_array[i]);
+			if(p_root ==NULL) {
+				LM_ERR("while geting the xml_tree root element\n");
+				goto error;
+			}
+			if (p_root->children) {
+			for (node = p_root->children; node; node = node->next) {
+				if (node->type == XML_ELEMENT_NODE) {
+					LM_DBG("node type: Element, name: %s\n", node->name);
+					/* we do not copy the node, but unlink it and then add it ot the new node
+					 * this destroys the original document but we do not need it anyway.
+					 * using "copy" instead of "unlink" would also copy the namespace which 
+					 * would then be declared redundant (libxml unfortunately can not remove 
+					 * namespaces)
+					 */
+					if (!force_single_dialog || (j==1)) {
+						xmlUnlinkNode(node);
+						if(xmlAddChild(root_node, node)== NULL) {
+							LM_ERR("while adding child\n");
+							goto error;
+						}
+					} else {
+						/* try to put only the most important into the XML document
+						 * order of importance: terminated->trying->proceeding->confirmed->early
+						 */
+						state = xmlNodeGetNodeContentByName(node, "state", NULL);
+						if (state) {
+							LM_DBG("state element content = %s\n", state);
+							priority = get_dialog_state_priority(state);
+							if (priority > winner_priority) {
+								winner_priority = priority;
+								LM_DBG("new winner priority = %s (%d)\n", state, winner_priority);
+								winner_dialog_node = node;
+							}
+							xmlFree(state);
+						}
+					}
+				}
+			}
+		}
+	}
+
+	if (force_single_dialog && (j!=1)) {
+		xmlUnlinkNode(winner_dialog_node);
+		if(xmlAddChild(root_node, winner_dialog_node)== NULL) {
+			LM_ERR("while adding winner-child\n");
+			goto error;
+		}
+	}
+
+	body = (str*)pkg_malloc(sizeof(str));
+	if(body == NULL) {
+		ERR_MEM(PKG_MEM_STR);
+	}
+
+	xmlDocDumpFormatMemory(doc,(xmlChar**)(void*)&body->s, 
+			&body->len, 1);	
+
+  	for(i=0; i<j; i++)
+	{
+		if(xml_array[i]!=NULL)
+			xmlFreeDoc( xml_array[i]);
+	}
+	if (doc)
+		xmlFreeDoc(doc);
+	if(xml_array!=NULL)
+		pkg_free(xml_array);
+    
+	xmlCleanupParser();
+    xmlMemoryDump();
+
+	return body;
+
+error:
+	if(xml_array!=NULL)
+	{
+		for(i=0; i<=j; i++)
+		{
+			if(xml_array[i]!=NULL)
+				xmlFreeDoc( xml_array[i]);
+		}
+		pkg_free(xml_array);
+	}
+	if(body)
+		pkg_free(body);
+
+	return NULL;
+}
+
+
+int get_dialog_state_priority(char *state) {
+	if (strcasecmp(state,"terminated") == 0)
+		return 0;
+	if (strcasecmp(state,"trying") == 0)
+		return 1;
+	if (strcasecmp(state,"proceeding") == 0)
+		return 2;
+	if (strcasecmp(state,"confirmed") == 0)
+		return 3;
+	if (strcasecmp(state,"early") == 0)
+		return 4;
+
+	return 0;
+}
+
+
+str *dlginfo_body_setversion(subs_t *subs, str *body) {
+	char *version_start=0;
+	char version[MAX_INT_LEN + 2]; /* +2 becasue of trailing " and \0 */
+	int version_len;
+
+	if (!body) {
+		return NULL;
+	}
+
+	/* xmlDocDumpFormatMemory creates \0 terminated string */
+	/* version parameters starts at minimum at character 34 */
+	version_start = strstr(body->s + 34, "version=");
+	if (!version_start) {
+	    LM_ERR("version string not found!\n");
+		return NULL;
+	}
+	version_start += 9;
+
+	version_len = snprintf(version, MAX_INT_LEN + 2,"%d\"", subs->version);
+	if (version_len >= MAX_INT_LEN + 2) {
+		LM_ERR("failed to convert 'version' to string\n");
+		memcpy(version_start, "00000000000\"", 12);
+		return NULL;
+	}
+	/* Replace the placeholder 00000000000 with the version.
+	 * Put the padding behind the ""
+	 */
+	LM_DBG("replace version with \"%s\n",version);
+	memcpy(version_start, version, version_len);
+	memset(version_start + version_len, ' ', 12 - version_len);
+
+	return NULL;
+}
---
+++ modules_k/pua_dialoginfo/pua_dialoginfo.c
@@ -0,0 +1,336 @@
+/*
+ * $Id: pua_usrloc.c 4657 2008-08-10 22:51:44Z henningw $
+ *
+ * pua_dialoginfo module - publish dialog-info from dialo module
+ *
+ * Copyright (C) 2006 Voice Sistem S.R.L.
+ * Copyright (C) 2007-2008 Dan Pascu
+ * Copyright (C) 2008 Klaus Darilion IPCom
+ *
+ * This file is part of Kamailio, a free SIP server.
+ *
+ * Kamailio is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version
+ *
+ * Kamailio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software 
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * History:
+ * --------
+ *  2008-08-25  initial version (kd)
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <libxml/parser.h>
+#include <time.h>
+
+#include "../../script_cb.h"
+#include "../../sr_module.h"
+#include "../../parser/parse_expires.h"
+#include "../../dprint.h"
+#include "../../mem/shm_mem.h"
+#include "../../parser/msg_parser.h"
+#include "../../str.h"
+#include "../../mem/mem.h"
+#include "../../pt.h"
+#include "../dialog/dlg_load.h"
+#include "../dialog/dlg_hash.h"
+#include "../pua/pua_bind.h"
+#include "pua_dialoginfo.h"
+
+MODULE_VERSION
+
+/* Default module parameter values */
+#define DEF_INCLUDE_CALLID 1
+#define DEF_INCLUDE_LOCALREMOTE 1
+#define DEF_INCLUDE_TAGS 1
+#define DEF_INITIAL_LIFETIME 125
+#define DEF_CALLER_ALWAYS_CONFIRMED 0
+
+/* define PUA_DIALOGINFO_DEBUG to activate more verbose 
+ * logging and dialog info callback debugging
+ */
+/* #define PUA_DIALOGINFO_DEBUG 1 */
+
+pua_api_t pua;
+
+struct dlg_binds dlg_api;
+
+/* Module parameter variables */
+int include_callid      = DEF_INCLUDE_CALLID;
+int include_localremote = DEF_INCLUDE_LOCALREMOTE;
+int include_tags        = DEF_INCLUDE_TAGS;
+int initial_lifetime    = DEF_INITIAL_LIFETIME;
+int caller_confirmed    = DEF_CALLER_ALWAYS_CONFIRMED;
+
+
+/** module functions */
+
+static int mod_init(void);
+static void destroy(void);
+
+
+static cmd_export_t cmds[]=
+{
+	{0, 0, 0, 0, 0, 0} 
+};
+
+static param_export_t params[]={
+	{"include_callid",      INT_PARAM, &include_callid },
+	{"include_localremote", INT_PARAM, &include_localremote },
+	{"include_tags",        INT_PARAM, &include_tags },
+	{"initial_lifetime",    INT_PARAM, &initial_lifetime },
+	{"caller_confirmed",    INT_PARAM, &caller_confirmed },
+	{0, 0, 0 }
+};
+
+struct module_exports exports= {
+	"pua_dialoginfo",		/* module name */
+	DEFAULT_DLFLAGS,		/* dlopen flags */
+	cmds,					/* exported functions */
+	params,					/* exported parameters */
+	0,						/* exported statistics */
+	0,						/* exported MI functions */
+	0,						/* exported pseudo-variables */
+	0,						/* extra processes */
+	mod_init,				/* module initialization function */
+	(response_function) 0,	/* response handling function */
+	destroy,				/* destroy function */
+	NULL					/* per-child init function */
+};
+	
+
+#ifdef PUA_DIALOGINFO_DEBUG
+static void
+__dialog_cbtest(struct dlg_cell *dlg, int type, struct dlg_cb_params *_params)
+{
+	str tag;
+	LM_ERR("dialog callback received, from=%.*s, to=%.*s\n", dlg->from_uri.len, dlg->from_uri.s, dlg->to_uri.len, dlg->to_uri.s);
+	if (dlg->tag[0].len && dlg->tag[0].s ) {
+		LM_ERR("dialog callback: tag[0] = %.*s", dlg->tag[0].len, dlg->tag[0].s);
+	}
+	if (dlg->tag[0].len && dlg->tag[1].s ) {
+		LM_ERR("dialog callback: tag[1] = %.*s", dlg->tag[1].len, dlg->tag[1].s);
+	}
+
+if (type != DLGCB_DESTROY) {
+	/* get to tag*/
+	if ( !_params->msg->to) {
+		// to header not defined, parse to header
+		LM_ERR("to header not defined, parse to header\n");
+		if (parse_headers(_params->msg, HDR_TO_F,0)<0) {
+			//parser error
+			LM_ERR("parsing of to-header failed\n");
+			tag.s = 0;
+			tag.len = 0;
+		} else if (!_params->msg->to) {
+			// to header still not defined
+			LM_ERR("no to although to-header is parsed: bad reply or missing TO hdr :-/\n");
+			tag.s = 0;
+			tag.len = 0;
+		} else 
+			tag = get_to(_params->msg)->tag_value;
+	} else {
+		tag = get_to(_params->msg)->tag_value;
+		if (tag.s==0 || tag.len==0) {
+			LM_ERR("missing TAG param in TO hdr :-/\n");
+			tag.s = 0;
+			tag.len = 0;
+		}
+	}
+	if (tag.s) {
+		LM_ERR("dialog callback: _params->msg->to->parsed->tag_value = %.*s", tag.len, tag.s);
+	}
+}
+
+	switch (type) {
+	case DLGCB_FAILED:
+		LM_ERR("dialog callback type 'DLGCB_FAILED' received, from=%.*s\n", dlg->from_uri.len, dlg->from_uri.s);
+		break;
+	case DLGCB_CONFIRMED:
+		LM_ERR("dialog callback type 'DLGCB_CONFIRMED' received, from=%.*s\n", dlg->from_uri.len, dlg->from_uri.s);
+		break;
+	case DLGCB_REQ_WITHIN:
+		LM_ERR("dialog callback type 'DLGCB_REQ_WITHIN' received, from=%.*s\n", dlg->from_uri.len, dlg->from_uri.s);
+		break;
+	case DLGCB_TERMINATED:
+		LM_ERR("dialog callback type 'DLGCB_TERMINATED' received, from=%.*s\n", dlg->from_uri.len, dlg->from_uri.s);
+		break;
+	case DLGCB_EXPIRED:
+		LM_ERR("dialog callback type 'DLGCB_EXPIRED' received, from=%.*s\n", dlg->from_uri.len, dlg->from_uri.s);
+		break;
+	case DLGCB_EARLY:
+		LM_ERR("dialog callback type 'DLGCB_EARLY' received, from=%.*s\n", dlg->from_uri.len, dlg->from_uri.s);
+		break;
+	case DLGCB_RESPONSE_FWDED:
+		LM_ERR("dialog callback type 'DLGCB_RESPONSE_FWDED' received, from=%.*s\n", dlg->from_uri.len, dlg->from_uri.s);
+		break;
+	case DLGCB_RESPONSE_WITHIN:
+		LM_ERR("dialog callback type 'DLGCB_RESPONSE_WITHIN' received, from=%.*s\n", dlg->from_uri.len, dlg->from_uri.s);
+		break;
+	case DLGCB_MI_CONTEXT:
+		LM_ERR("dialog callback type 'DLGCB_MI_CONTEXT' received, from=%.*s\n", dlg->from_uri.len, dlg->from_uri.s);
+		break;
+	case DLGCB_DESTROY:
+		LM_ERR("dialog callback type 'DLGCB_DESTROY' received, from=%.*s\n", dlg->from_uri.len, dlg->from_uri.s);
+		break;
+	default:
+		LM_ERR("dialog callback type 'unknown' received, from=%.*s\n", dlg->from_uri.len, dlg->from_uri.s);
+	}
+}
+#endif
+
+static void
+__dialog_sendpublish(struct dlg_cell *dlg, int type, struct dlg_cb_params *_params)
+{
+	str tag = {0,0};
+
+	switch (type) {
+	case DLGCB_FAILED:
+	case DLGCB_TERMINATED:
+	case DLGCB_EXPIRED:
+		LM_DBG("dialog over, from=%.*s\n", dlg->from_uri.len, dlg->from_uri.s);
+		dialog_publish("terminated", &(dlg->from_uri), &(dlg->to_uri), &(dlg->callid), 1, initial_lifetime, 0, 0);
+		dialog_publish("terminated", &(dlg->to_uri), &(dlg->from_uri), &(dlg->callid), 0, initial_lifetime, 0, 0);
+		break;
+	case DLGCB_CONFIRMED:
+	case DLGCB_REQ_WITHIN:
+		LM_DBG("dialog confirmed, from=%.*s\n", dlg->from_uri.len, dlg->from_uri.s);
+		dialog_publish("confirmed", &(dlg->from_uri), &(dlg->to_uri), &(dlg->callid), 1, dlg->lifetime, 0, 0);
+		dialog_publish("confirmed", &(dlg->to_uri), &(dlg->from_uri), &(dlg->callid), 0, dlg->lifetime, 0, 0);
+		break;
+	case DLGCB_EARLY:
+		LM_DBG("dialog is early, from=%.*s\n", dlg->from_uri.len, dlg->from_uri.s);
+		if (include_tags) {
+			/* get to tag*/
+			if ( !_params->msg->to && ((parse_headers(_params->msg, HDR_TO_F,0)<0) || !_params->msg->to) ) {
+				LM_ERR("bad reply or missing TO hdr :-/\n");
+				tag.s = 0;
+				tag.len = 0;
+			} else {
+				tag = get_to(_params->msg)->tag_value;
+				if (tag.s==0 || tag.len==0) {
+					LM_ERR("missing TAG param in TO hdr :-/\n");
+					tag.s = 0;
+					tag.len = 0;
+				}
+			}
+			if (caller_confirmed) {
+				dialog_publish("confirmed", &(dlg->from_uri), &(dlg->to_uri), &(dlg->callid), 1, initial_lifetime, &(dlg->tag[0]), &tag);
+			} else {
+				dialog_publish("early", &(dlg->from_uri), &(dlg->to_uri), &(dlg->callid), 1, initial_lifetime, &(dlg->tag[0]), &tag);
+			}
+			dialog_publish("early", &(dlg->to_uri), &(dlg->from_uri), &(dlg->callid), 0, initial_lifetime, &(dlg->tag[0]), &tag);
+		} else {
+			if (caller_confirmed) {
+				dialog_publish("confirmed", &(dlg->from_uri), &(dlg->to_uri), &(dlg->callid), 1, initial_lifetime, 0, 0);
+			} else {
+				dialog_publish("early", &(dlg->from_uri), &(dlg->to_uri), &(dlg->callid), 1, initial_lifetime, 0, 0);
+			}
+			dialog_publish("early", &(dlg->to_uri), &(dlg->from_uri), &(dlg->callid), 0, initial_lifetime, 0, 0);
+		}
+		break;
+	default:
+		LM_ERR("unhandled dialog callback type %d received, from=%.*s\n", type, dlg->from_uri.len, dlg->from_uri.s);
+		dialog_publish("terminated", &(dlg->from_uri), &(dlg->to_uri), &(dlg->callid), 1, initial_lifetime, 0, 0);
+		dialog_publish("terminated", &(dlg->to_uri), &(dlg->from_uri), &(dlg->callid), 0, initial_lifetime, 0, 0);
+	}
+}
+
+static void
+__dialog_created(struct dlg_cell *dlg, int type, struct dlg_cb_params *_params)
+{
+	struct sip_msg *request = _params->msg;
+
+	if (request->REQ_METHOD != METHOD_INVITE)
+		return;
+
+	LM_DBG("new INVITE dialog created: from=%.*s\n", dlg->from_uri.len, dlg->from_uri.s);
+
+	/* dialog callbacks which triggers sending PUBLISH */
+	if (dlg_api.register_dlgcb(dlg, 
+		DLGCB_FAILED| DLGCB_CONFIRMED | DLGCB_TERMINATED | DLGCB_EXPIRED | 
+		DLGCB_REQ_WITHIN | DLGCB_EARLY,
+		__dialog_sendpublish, NULL, NULL) != 0) {
+		LM_ERR("cannot register callback for interesting dialog types\n");
+		return;
+	}
+
+
+#ifdef PUA_DIALOGINFO_DEBUG
+	/* dialog callback testing (registered last to be executed frist) */
+	if (dlg_api.register_dlgcb(dlg, 
+		DLGCB_FAILED| DLGCB_CONFIRMED | DLGCB_REQ_WITHIN | DLGCB_TERMINATED | DLGCB_EXPIRED | 
+		DLGCB_EARLY | DLGCB_RESPONSE_FWDED | DLGCB_RESPONSE_WITHIN  | 
+		DLGCB_MI_CONTEXT | DLGCB_DESTROY,
+		__dialog_cbtest, NULL, NULL) != 0) {
+		LM_ERR("cannot register callback for all dialog types\n");
+		return;
+	}
+#endif
+
+	dialog_publish("Trying", &(dlg->from_uri), &(dlg->to_uri), &(dlg->callid), 1, initial_lifetime, 0, 0);
+}
+
+
+
+
+/**
+ * init module function
+ */
+static int mod_init(void)
+{
+	bind_pua_t bind_pua;
+	
+	bind_pua= (bind_pua_t)find_export("bind_pua", 1,0);
+	if (!bind_pua)
+	{
+		LM_ERR("Can't bind pua\n");
+		return -1;
+	}
+	
+	if (bind_pua(&pua) < 0)
+	{
+		LM_ERR("Can't bind pua\n");
+		return -1;
+	}
+	if(pua.send_publish == NULL)
+	{
+		LM_ERR("Could not import send_publish\n");
+		return -1;
+	}
+	pua_send_publish= pua.send_publish;
+
+	/* bind to the dialog API */
+	if (load_dlg_api(&dlg_api)!=0) {
+		LM_ERR("failed to find dialog API - is dialog module loaded?\n");
+		return -1;
+	}
+	/* register dialog creation callback */
+	if (dlg_api.register_dlgcb(NULL, DLGCB_CREATED, __dialog_created, NULL, NULL) != 0) {
+		LM_ERR("cannot register callback for dialog creation\n");
+		return -1;
+	}
+
+	return 0;
+}
+
+static void destroy(void)
+{	
+	LM_DBG("destroying module ...\n");
+
+	return ;
+}
+
+
+
