f0bd64366c6aa8118d1370f378c9c7a129e6acff
Andrei Pelinescu-Onciul <andrei@iptel.org>
2008-10-14 14:13:14 +0000

    sctp: if SCTP support not compiled, next_proto() shouldn't return SCTP
    - minor fix: if no SCTP support was compiled, init_proto_order() didn't
     remove PROTO_SCTP references and this caused a BUG message on ser start

--- socket_info.c
+++ socket_info.c
@@ -1458,11 +1458,11 @@ void init_proto_order()
 		}
 #ifdef USE_SCTP
 	if (sctp_disable)
+#endif
 		for(r=PROTO_NONE; r<=PROTO_LAST; r++){
 			if (nxt_proto[r]==PROTO_SCTP)
 				nxt_proto[r]=nxt_proto[PROTO_SCTP];
 		}
-#endif
 }
 
 
