e37837a0b9d020325265c94699ae069f4eeb0888
Henning Westerholt <henning.westerholt@1und1.de>
2008-11-13 16:17:34 +0000

    - extend registrar test for db_mysql and db_postgres: test also for contact   persistence accross server restarts (the NULL value behaviour differ between   the two database types)
    git-svn-id: https://openser.svn.sourceforge.net/svnroot/openser/trunk@5198 689a6050-402a-0410-94f2-e92a70836424

--- test/unit/11.sh
+++ test/unit/11.sh
@@ -149,12 +149,40 @@ if [ "$ret" -eq 0 ]; then
 	fi;
 fi;
 
+$MYSQL "delete from location where username like '49721123456789%';"
+
+if [ "$ret" -eq 0 ]; then
+	# register again
+	sipsak -U -C sip:foobar@localhost -s sip:49721123456789@localhost -H localhost &> /dev/null
+	ret=$?
+fi;
+
+$KILL
+
+# restart to test preload_udomain functionality
+../$BIN -w . -f ../test/$CFG > /dev/null
+ret=$?
+
+sleep 1
+
+if [ "$ret" -eq 0 ]; then
+	# check if the contact is still registered
+	sipsak -U -C empty -s sip:49721123456789@127.0.0.1 -H localhost -q "Contact: <sip:foobar@localhost>" &> /dev/null
+	ret=$?
+fi;
+
+# check if the methods value is correct
+if [ "$ret" -eq 0 ]; then
+	./$CTL ul show | grep "Methods:: 4294967295" &> /dev/null
+	ret=$?
+fi;
+
 # cleanup
 $MYSQL "delete from location where username like '49721123456789%';"
 
-cd ../test
-
 $KILL
+
+cd ../test
 
 mv $CFG.bak $CFG
 
--- test/unit/22.sh
+++ test/unit/22.sh
@@ -74,7 +74,37 @@ if [ "$ret" -eq 0 ]; then
 	ret=`$PSQL "select COUNT(*) from location where username='49721123456789';" | tail -n 1`
 fi;
 
+$PSQL "delete from location where username like '49721123456789%';"
+
+if [ "$ret" -eq 0 ]; then
+	# register again
+	sipsak -U -C sip:foobar@localhost -s sip:49721123456789@localhost -H localhost &> /dev/null
+	ret=$?
+fi;
+
 $KILL
+
+# restart to test preload_udomain functionality
+../$BIN -w . -f ../test/$CFG > /dev/null
+ret=$?
+
+sleep 1
+
+if [ "$ret" -eq 0 ]; then
+	# check if the contact is still registered
+	sipsak -U -C empty -s sip:49721123456789@127.0.0.1 -H localhost -q "Contact: <sip:foobar@localhost>" &> /dev/null
+	ret=$?
+fi;
+
+# check if the methods value is correct
+if [ "$ret" -eq 0 ]; then
+	./$CTL ul show | grep "Methods:: 4294967295" &> /dev/null
+	ret=$?
+fi;
+
+$KILL
+
+$PSQL "delete from location where username like '49721123456789%';"
 
 cd ../test
 mv $CFG.tmp $CFG
