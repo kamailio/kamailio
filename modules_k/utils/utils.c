ddbacf7a5f938afc1ad1e1afb784a385582f821c
Henning Westerholt <henning.westerholt@1und1.de>
2008-11-14 14:25:52 +0000

    - add new module 'utils', to hold various SIP message related functionality - add a function to convert 183 messages (for example in parallel call)   to 180 (derived from some internal module that we implemented long ago) - Credits for the original code belongs to Hardy Kahl, hardy dot kahl at 1und1 dot de - write some doxygen comments, write user documentation, add timeout value   as parameter, add new module to test 2 - in the near term some other functions of small modules will be moved to   this place, e.g. maxfwd, some new contributions are also planned to be   placed here
    git-svn-id: https://openser.svn.sourceforge.net/svnroot/openser/trunk@5202 689a6050-402a-0410-94f2-e92a70836424

---
+++ modules_k/utils/utils.c
@@ -0,0 +1,108 @@
+/*
+ * $Id$
+ *
+ * Copyright (C) 2008-2009 1&1 Internet AG
+ *
+ * This file is part of Kamailio, a free SIP server.
+ *
+ * Kamailio is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version
+ *
+ * Kamailio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software 
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/**
+ * \file
+ * \brief Module with several utiltity functions related to SIP messages handling
+ * \ingroup utils
+ * - Module; \ref utils
+ */
+
+/*!
+ * \defgroup utils UTILS :: Module definitions
+ */
+
+#include <assert.h>
+
+#include "../../sr_module.h"
+#include "../../script_cb.h"
+#include "../../locking.h"
+
+#include "ring.h"
+
+
+MODULE_VERSION
+
+gen_lock_t *ring_lock = NULL;
+unsigned int ring_timeout = 30;
+
+static int mod_init(void);
+static void mod_destroy(void);
+
+
+static cmd_export_t cmds[]={
+	{ "ring_insert_callid", (cmd_function)ring_insert_callid, 0, 0, 0, REQUEST_ROUTE|FAILURE_ROUTE },
+	{0,0,0,0,0,0}
+};
+
+static param_export_t params[] = {
+	{"ring_timeout", INT_PARAM, &ring_timeout },
+	{0, 0, 0}
+};
+
+
+struct module_exports exports= {
+	"utils",
+	DEFAULT_DLFLAGS, /* dlopen flags */
+	cmds,            /* Exported functions */
+	params,          /* param exports */
+	0,               /* exported statistics */
+	0,               /* exported MI functions */
+	0,               /* exported pseudo-variables */
+	0,               /* extra processes */
+	mod_init,        /* initialization function */
+	0,               /* Response function */
+	mod_destroy,     /* Destroy function */
+	0,               /* Child init function */
+};
+
+
+static int mod_init(void)
+{
+	ring_init_hashtable();
+
+	ring_lock = lock_alloc();
+	assert(ring_lock);
+	if (lock_init(ring_lock) == 0) {
+		LM_CRIT("cannot initialize lock.\n");
+		return -1;
+	}
+
+	if (register_script_cb(ring_filter, PRE_SCRIPT_CB|RPL_TYPE_CB, 0) != 0) {
+		LM_ERR("could not insert callback");
+		return -1;
+	}
+
+	return 0;
+}
+
+
+static void mod_destroy(void)
+{
+	if (ring_lock) {
+		lock_destroy(ring_lock);
+		lock_dealloc((void *)ring_lock);
+		ring_lock = NULL;
+	}
+
+	ring_destroy_hashtable();
+}
