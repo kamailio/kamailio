3963fa380653b8c48c530499e987cbfce58cadb2
Daniel-Constantin Mierla <miconda@gmail.com>
2009-01-14 11:54:41 +0000

    - new column to dispatcher table to hold priority - regenerated scripts to create the table
    git-svn-id: https://openser.svn.sourceforge.net/svnroot/openser/trunk@5457 689a6050-402a-0410-94f2-e92a70836424

--- tools/mysql/dispatcher-create.sql
+++ tools/mysql/dispatcher-create.sql
@@ -1,9 +1,10 @@
-INSERT INTO version (table_name, table_version) values ('dispatcher','2');
+INSERT INTO version (table_name, table_version) values ('dispatcher','3');
 CREATE TABLE dispatcher (
     id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
     setid INT DEFAULT 0 NOT NULL,
     destination VARCHAR(192) DEFAULT '' NOT NULL,
     flags INT DEFAULT 0 NOT NULL,
+    priority INT DEFAULT 0 NOT NULL,
     description VARCHAR(64) DEFAULT '' NOT NULL
 ) ENGINE=MyISAM;
 
--- tools/oracle/dispatcher-create.sql
+++ tools/oracle/dispatcher-create.sql
@@ -1,9 +1,10 @@
-INSERT INTO version (table_name, table_version) values ('dispatcher','2');
+INSERT INTO version (table_name, table_version) values ('dispatcher','3');
 CREATE TABLE dispatcher (
     id NUMBER(10) PRIMARY KEY,
     setid NUMBER(10) DEFAULT 0 NOT NULL,
     destination VARCHAR2(192) DEFAULT '',
     flags NUMBER(10) DEFAULT 0 NOT NULL,
+    priority NUMBER(10) DEFAULT 0 NOT NULL,
     description VARCHAR2(64) DEFAULT ''
 );
 
