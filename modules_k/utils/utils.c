33011db426a2f61af9645aa34e41923b353b5599
Henning Westerholt <henning.westerholt@1und1.de>
2009-01-26 09:06:35 +0000

    - add functionality for runtime message forwarding, this is useful   for example to transfer SIP traffic from your balancers to testing   machines - forwarding can be modified with some FIFO commands, only certain   message types can be matched - message are forwarded before the config script is executed, with   some callbacks - derived of a stand-alone module, written from Hardy Kahl,   hardy dot kahl at 1und1 dot de, some functions were removed - not activated per default, thus no run-time overhead when not used - TODO: FIFO commands documentation missing - add a test for the functionality
    git-svn-id: https://openser.svn.sourceforge.net/svnroot/openser/trunk@5510 689a6050-402a-0410-94f2-e92a70836424

--- modules_k/utils/utils.c
+++ modules_k/utils/utils.c
@@ -2,6 +2,7 @@
  * utils Module
  *
  * Copyright (C) 2008 Juha Heinanen
+ * Copyright (C) 2009 1&1 Internet AG
  *
  * This file is part of Kamailio, a free SIP server.
  *
@@ -31,11 +32,35 @@
 #include "../../mod_fix.h"
 #include "../../sr_module.h"
 #include "../../ut.h"
+#include "../../forward.h"
+#include "../../resolve.h"
+#include "../../locking.h"
+#include "../../script_cb.h"
+#include "../../mem/shm_mem.h"
 
 #include "functions.h"
+#include "conf.h"
 
 
 MODULE_VERSION
+
+
+/* module parameters */
+static int forward_active = 0;
+static int   mp_max_id = 0;
+static char* mp_switch = "";
+static char* mp_filter = "";
+static char* mp_proxy  = "";
+
+/* lock for configuration access */
+static gen_lock_t *conf_lock = NULL;
+
+
+/* FIFO interface functions */
+static struct mi_root* forward_fifo_list(struct mi_root* cmd_tree, void *param);
+static struct mi_root* forward_fifo_switch(struct mi_root* cmd_tree, void* param);
+static struct mi_root* forward_fifo_filter(struct mi_root* cmd_tree, void* param);
+static struct mi_root* forward_fifo_proxy(struct mi_root* cmd_tree, void* param);
 
 
 /* Module management function prototypes */
@@ -51,6 +76,8 @@ int http_query_timeout = 4;
 static int fixup_http_query(void** param, int param_no);
 static int fixup_free_http_query(void** param, int param_no);
 
+/* forward function */
+int utils_forward(struct sip_msg *msg, int id, int proto);
 
 /* Exported functions */
 static cmd_export_t cmds[] = {
@@ -64,9 +91,17 @@ static cmd_export_t cmds[] = {
 /* Exported parameters */
 static param_export_t params[] = {
     {"http_query_timeout", INT_PARAM, &http_query_timeout},
+    {"forward_active",     INT_PARAM, &forward_active},
     {0, 0, 0}
 };
 
+static mi_export_t mi_cmds[] = {
+	{ "forward_list",   forward_fifo_list,   MI_NO_INPUT_FLAG, 0,  0 },
+	{ "forward_switch", forward_fifo_switch, 0, 0,  0 },
+	{ "forward_filter", forward_fifo_filter, 0, 0,  0 },
+	{ "forward_proxy",  forward_fifo_proxy,  0, 0,  0 },
+	{ 0, 0, 0, 0, 0}
+};
 
 /* Module interface */
 struct module_exports exports = {
@@ -75,7 +110,7 @@ struct module_exports exports = {
     cmds,      /* Exported functions */
     params,    /* Exported parameters */
     0,         /* exported statistics */
-    0,         /* exported MI functions */
+    mi_cmds,   /* exported MI functions */
     0,         /* exported pseudo-variables */
     0,         /* extra processes */
     mod_init,  /* module initialization function */
@@ -85,23 +120,100 @@ struct module_exports exports = {
 };
 
 
+static int init_shmlock(void)
+{
+	conf_lock = lock_alloc();
+	if (conf_lock == NULL) {
+		LM_CRIT("cannot allocate memory for lock.\n");
+		return -1;
+	}
+	if (lock_init(conf_lock) == 0) {
+		LM_CRIT("cannot initialize lock.\n");
+		return -1;
+	}
+
+	return 0;
+}
+
+
+static int pre_script_filter(struct sip_msg *msg, void *unused)
+{
+	/* use id 0 for pre script callback */
+	utils_forward(msg, 0, PROTO_UDP);
+
+	/* always return 1 so that routing skript is called for msg */
+	return 1;
+}
+
+
+static void destroy_shmlock(void)
+{
+	if (conf_lock) {
+		lock_destroy(conf_lock);
+		lock_dealloc((void *)conf_lock);
+		conf_lock = NULL;
+	}
+}
+
+
 /* Module initialization function */
 static int mod_init(void)
 {
-    /* Initialize curl */
-    if (curl_global_init(CURL_GLOBAL_ALL)) {
-	LM_ERR("curl_global_init failed\n");
-	return -1;
-    }
+	/* Initialize curl */
+	if (curl_global_init(CURL_GLOBAL_ALL)) {
+		LM_ERR("curl_global_init failed\n");
+		return -1;
+	}
 
-    return 0;
+
+	if (init_shmlock() != 0) {
+		LM_CRIT("cannot initialize shmlock.\n");
+		return -1;
+	}
+
+	if (conf_init(mp_max_id) < 0) {
+		LM_CRIT("cannot initialize configuration.\n");
+		return -1;
+	}
+
+	/* read module parameters and update configuration structure */
+	if (conf_parse_proxy(mp_proxy) < 0) {
+		LM_CRIT("cannot parse proxy module parameter.\n");
+		return -1;
+	}
+	if (conf_parse_filter(mp_filter) < 0) {
+		LM_CRIT("cannot parse filter module parameter.\n");
+		return -1;
+	}
+	if (conf_parse_switch(mp_switch) < 0) {
+		LM_CRIT("cannot parse switch module parameter.\n");
+		return -1;
+	}
+
+	if (forward_active == 1) {
+		/* register callback for id 0 */
+		if (register_script_cb(pre_script_filter, PRE_SCRIPT_CB|REQ_TYPE_CB, 0) < 0) {
+			LM_CRIT("cannot register script callback for requests.\n");
+			return -1;
+		}
+		if (register_script_cb(pre_script_filter, PRE_SCRIPT_CB|RPL_TYPE_CB, 0) < 0) {
+			LM_CRIT("cannot register script callback for replies.\n");
+			return -1;
+		}
+	} else {
+		LM_INFO("forward functionality disabled");
+	}
+	return 0;
 }
 
 
 static void destroy(void)
 {
-    /* Cleanup curl */
-    curl_global_cleanup();
+	/* Cleanup curl */
+	curl_global_cleanup();
+	/* Cleanup forward */
+	conf_destroy();
+	destroy_shmlock();
 }
 
 
@@ -150,4 +262,203 @@ static int fixup_free_http_query(void** param, int param_no)
     
     LM_ERR("invalid parameter number <%d>\n", param_no);
     return -1;
+}
+
+
+/*!
+ * \brief checks precondition, switch, filter and forwards msg if necessary
+ * \param msg the message to be forwarded
+ * \param id use configuration with this ID when checking switch, filter, proxy.
+ * \param proto protocol to be used. Should be PROTO_UDP.
+ *  \return 0 on success, -1 otherwise
+ */
+int utils_forward(struct sip_msg *msg, int id, int proto)
+{
+	int ret = -1;
+	union sockaddr_union* to = NULL;
+	struct socket_info *send_sock = NULL;
+
+	to = (union sockaddr_union*)pkg_malloc(sizeof(union sockaddr_union));
+	if (to == NULL) {
+		PKG_MEM_ERROR;
+		return -1;
+	}
+
+	// critical section start:
+	// avoids dirty reads when updating configuration.
+	lock_get(conf_lock);
+
+	struct proxy_l *proxy = conf_needs_forward(msg, id);
+
+	if (proxy != NULL) {
+		hostent2su(to, &proxy->host, proxy->addr_idx, (proxy->port)?proxy->port:SIP_PORT);
+		do {
+			send_sock=get_send_socket(msg, to, proto);
+
+			if (send_sock==0) {
+				LM_ERR("cannot forward to af %d, "
+					"proto %d no corresponding listening socket\n", to->s.sa_family, proto);
+				continue;
+			}
+			LM_DBG("Sending:\n%.*s.\n", (int)msg->len,msg->buf);
+
+			if (msg_send(send_sock, proto, to, 0, msg->buf,msg->len)<0){
+				LM_ERR("Error sending message!\n");
+				continue;
+			}
+			ret = 0;
+        		break;
+		} while( get_next_su( proxy, to, 0) == 0 );
+	}
+
+	// critical section end
+	lock_release(conf_lock);
+	pkg_free(to);
+
+	return ret;
+}
+
+
+/* FIFO functions */
+
+/*!
+ * \brief fifo command for listing configuration
+ * \return pointer to the mi_root on success, 0 otherwise
+ */
+static struct mi_root* forward_fifo_list(struct mi_root* cmd_tree, void *param)
+{
+	struct mi_node *node = NULL;
+	struct mi_root * ret = init_mi_tree(200, MI_OK_S, MI_OK_LEN);
+	if(ret == NULL)
+		return 0;
+
+	node = addf_mi_node_child( &ret->node, 0, 0, 0, "Printing forwarding information:");
+	if(node == NULL)
+		goto error;
+
+	// critical section start:
+	// avoids dirty reads when updating configuration.
+	lock_get(conf_lock);
+
+	conf_show(ret);
+
+	// critical section end
+	lock_release(conf_lock);
+
+	return ret;
+
+error:
+	free_mi_tree(ret);
+	return 0;
+}
+
+
+/*!
+ * \brief fifo command for configuring switch
+ * \return pointer to the mi_root on success, 0 otherwise
+ */
+static struct mi_root* forward_fifo_switch(struct mi_root* cmd_tree, void* param)
+{
+	struct mi_node *node = NULL;
+	struct mi_root * ret = init_mi_tree(200, MI_OK_S, MI_OK_LEN);
+	if(ret == NULL)
+		return 0;
+
+	node = cmd_tree->node.kids;
+	if (node==NULL || node->next!=NULL)
+		ret = init_mi_tree(400, MI_MISSING_PARM_S, MI_MISSING_PARM_LEN);
+
+	/* look for command */
+	char* buf = node->value.s;
+	if (buf==NULL)
+	ret = init_mi_tree(400, MI_MISSING_PARM_S, MI_MISSING_PARM_LEN);
+
+	// critical section start:
+	// avoids dirty reads when updating configuration.
+	lock_get(conf_lock);
+
+	int result = conf_parse_switch(buf);
+
+	// critical section end
+	lock_release(conf_lock);
+
+	if (result < 0) {
+		LM_ERR("cannot parse parameter.\n");
+		ret = init_mi_tree( 400, MI_BAD_PARM_S, MI_BAD_PARM_LEN);
+	}
+	return ret;
+}
+
+
+/*!
+ * \brief fifo command for configuring filter
+ * \return pointer to the mi_root on success, 0 otherwise
+ */
+static struct mi_root* forward_fifo_filter(struct mi_root* cmd_tree, void* param)
+{
+	struct mi_node *node = NULL;
+	struct mi_root * ret = init_mi_tree(200, MI_OK_S, MI_OK_LEN);
+	if(ret == NULL)
+		return 0;
+
+	node = cmd_tree->node.kids;
+	if (node==NULL || node->next!=NULL)
+		ret = init_mi_tree(400, MI_MISSING_PARM_S, MI_MISSING_PARM_LEN);
+
+	/* look for command */
+	char* buf = node->value.s;
+	if (buf==NULL)
+		ret = init_mi_tree(400, MI_MISSING_PARM_S, MI_MISSING_PARM_LEN);
+
+	// critical section start:
+	//   avoids dirty reads when updating configuration.
+	lock_get(conf_lock);
+
+	int result = conf_parse_filter(buf);
+
+	// critical section end
+	lock_release(conf_lock);
+
+	if (result < 0) {
+		LM_ERR("cannot parse parameter.\n");
+		ret = init_mi_tree( 400, MI_BAD_PARM_S, MI_BAD_PARM_LEN);
+	}
+	return ret;
+}
+
+
+/*!
+ * \brief fifo command for configuring proxy
+ * \return pointer to the mi_root on success, 0 otherwise
+ */
+static struct mi_root* forward_fifo_proxy(struct mi_root* cmd_tree, void* param)
+{
+	struct mi_node *node = NULL;
+	struct mi_root * ret = init_mi_tree(200, MI_OK_S, MI_OK_LEN);
+	if(ret == NULL)
+		return 0;
+
+	node = cmd_tree->node.kids;
+	if (node==NULL || node->next!=NULL)
+		ret = init_mi_tree(400, MI_MISSING_PARM_S, MI_MISSING_PARM_LEN);
+
+	/* look for command */
+	char* buf = node->value.s;
+	if (buf==NULL)
+		ret = init_mi_tree(400, MI_MISSING_PARM_S, MI_MISSING_PARM_LEN);
+
+	// critical section start:
+	//   avoids dirty reads when updating configuration.
+	lock_get(conf_lock);
+
+	int result = conf_parse_proxy(buf);
+
+	// critical section end
+	lock_release(conf_lock);
+
+	if (result < 0) {
+		LM_ERR("cannot parse parameter.\n");
+		ret = init_mi_tree( 400, MI_BAD_PARM_S, MI_BAD_PARM_LEN);
+	}
+	return ret;
 }
