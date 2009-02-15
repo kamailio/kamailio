536ced2783aa223bbabd10682df04c6eff7a47b2
Jan Janak <jan@iptel.org>
2009-02-15 18:45:58 +0100

    Make the module compile in the sip-router source tree.

--- modules/db_flatstore/flat_con.h
+++ modules/db_flatstore/flat_con.h
@@ -33,9 +33,9 @@
  * Inmplementation of flatstore "connections".
  */
 
-#include "../../db/db_pool.h"
-#include "../../db/db_con.h"
-#include "../../db/db_uri.h"
+#include "../../lib/srdb2/db_pool.h"
+#include "../../lib/srdb2/db_con.h"
+#include "../../lib/srdb2/db_uri.h"
 
 #include <stdio.h>
 
