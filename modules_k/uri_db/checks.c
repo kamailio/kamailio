da483ce40f8df2ee3fa1f1b6b77850d68feaf9a5
Jan Janak <jan@iptel.org>
2009-03-06 02:38:35 +0100

    Include parser_helpers.h and link with libkcore

--- modules_k/uri_db/checks.c
+++ modules_k/uri_db/checks.c
@@ -35,6 +35,7 @@
 #include "../../parser/digest/digest.h" /* get_authorized_cred */
 #include "../../parser/parse_from.h"
 #include "../../parser/parse_uri.h"
+#include "../../lib/kcore/parser_helpers.h"
 #include "../../ut.h"                   /* Handy utilities */
 #include "../../lib/srdb1/db.h"                /* Database API */
 #include "uridb_mod.h"
