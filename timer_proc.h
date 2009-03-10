45df9f7e49450e27507977fa71a67dd98d381d9e
Andrei Pelinescu-Onciul <andrei@iptel.org>
2009-03-10 22:35:33 +0100

    timers: helpers for forking separate timer procs
    Helper functions for forking simple separate timer processes:
    
     fork_dummy_timer(...) - forks a process that will just sleep()
        for the required interval and then will call the provided
        timer function, all that in a loop.
        Care must be taken to use it only when fork_process() is
        allowed (e.g.: module  child_init function, when
        rank==PROC_MAIN; see doc/modules_init.txt for more details)
        It can be used to replace kamailio register_timer_process()
        (but note that it's not a "drop-in" replacement).
    
     fork_local_timer_process() - forks a process, initializes a
        local_timer for it, and returns the local_timer handler both
        to the parent and to the child process, so both of them can
        add/del timers (if appropriate locking is used).
        Example usage:
          struct local_timer* lt_h;
    
          pid=fork_local_timer_process(...., &lt_h);
          if (pid==0){
              timer_init(&my_timer1, my_timer_f1, 0, 0);
              ...
              local_timer_add(&lt_h, &my_timer, S_TO_TICKS(10), get_ticks_raw());
              ...
              while(1) { sleep(1); local_timer_run(lt, get_ticks_raw()); }
          }
          (note that instead of sleep()-ing the process can do some useful work,
           like polling some fds a.s.o.)

---
+++ timer_proc.h
@@ -0,0 +1,43 @@
+/* 
+ * $Id$
+ * 
+ * Copyright (C) 2009 iptelorg GmbH
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+/*
+ * timer_proc.h - separate process timers
+ * (unrelated to the main fast and slow timers)
+ */
+/*
+ * History:
+ * --------
+ *  2009-03-10  initial version (andrei)
+*/
+
+#ifndef __timer_proc_h
+#define __timer_proc_h
+
+#include "local_timer.h"
+
+/* forks a separate simple sleep() periodic timer */
+int fork_dummy_timer(int child_id, char* desc, int make_sock,
+						timer_function* f, void* param, int interval);
+
+/* forks a timer process based on the local timer */
+int fork_local_timer_process(int child_id, char* desc, int make_sock,
+						struct local_timer** lt_h);
+
+#endif /*__timer_proc_h*/
+
+/* vi: set ts=4 sw=4 tw=79:ai:cindent: */
