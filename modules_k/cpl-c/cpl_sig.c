be1c4e558e4dcc1759e29e428bcb567bb9ce1cd8
Jan Janak <jan@iptel.org>
2009-03-29 18:16:07 +0200

    The sr core has a different name for the variable with route blocks

--- modules_k/cpl-c/cpl_sig.c
+++ modules_k/cpl-c/cpl_sig.c
@@ -102,7 +102,7 @@ int cpl_proxy_to_loc_set( struct sip_msg *msg, struct location **locs,
 	/* run what proxy route is set */
 	if (cpl_env.proxy_route) {
 		/* do not alter route type - it might be REQUEST or FAILURE */
-		run_top_route( rlist[cpl_env.proxy_route], msg);
+		run_top_route( main_rt.rlist[cpl_env.proxy_route], msg);
 	}
 
 	/* do t_forward */
