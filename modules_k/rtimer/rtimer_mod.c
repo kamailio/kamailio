39c1b57ac2e1943cd770bb6dda51b583b2612d2a
Daniel-Constantin Mierla <miconda@gmail.com>
2009-03-29 21:03:12 +0200

    Rename rlist to main_rt.rlist to match the sr core
    Signed-off-by: Jan Janak <jan@iptel.org>

--- modules_k/rtimer/rtimer_mod.c
+++ modules_k/rtimer/rtimer_mod.c
@@ -191,7 +191,7 @@ void stm_timer_exec(unsigned int ticks, void *param)
 		if (exec_pre_req_cb(&_stm_msg)==0 )
 			continue; /* drop the request */
 		set_route_type(REQUEST_ROUTE);
-		run_top_route(rlist[rt->route], &_stm_msg);
+		run_top_route(main_rt.rlist[rt->route], &_stm_msg);
 		exec_post_req_cb(&_stm_msg);
 	}
 }
