346bb9e79d68398a08fcb48c4911415c73aae519
Jan Janak <jan@iptel.org>
2009-04-03 14:25:46 +0200

    Include libkcore/km_ut.h for trim_spaces_lr

--- modules_k/dialog/dlg_profile.c
+++ modules_k/dialog/dlg_profile.c
@@ -36,6 +36,7 @@
 
 #include "../../mem/shm_mem.h"
 #include "../../lib/kcore/hash_func.h"
+#include "../../lib/kcore/km_ut.h"
 #include "../../dprint.h"
 #include "../../ut.h"
 #include "../../route.h"
