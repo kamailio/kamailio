1f01639cb73a6757eab5ef4059241d9cffdbe926
Daniel-Constantin Mierla <miconda@gmail.com>
2009-04-22 17:11:05 +0200

    core: statistics.h only includes lib/kcore/statistics.h
    - this should be removed in the future
    - mi and statistics API are libraries, initialization is done from
      modules, the fields in module_exports are not used by core

--- statistics.h
+++ statistics.h
@@ -28,46 +28,10 @@
 #ifndef _STATISTICS_H_
 #define _STATISTICS_H_
 
-#include "str.h"
-
-#define STAT_NO_RESET  1
-#define STAT_NO_SYNC   2
-#define STAT_SHM_NAME  4
-#define STAT_IS_FUNC   8
-
-
-
-typedef unsigned int stat_val;
-typedef unsigned long (*stat_function)(void);
-
-typedef struct stat_var_{
-	unsigned int mod_idx;
-	str name;
-	int flags;
-	union{
-		stat_val *val;
-		stat_function f;
-	}u;
-	struct stat_var_ *hnext;
-	struct stat_var_ *lnext;
-} stat_var;
-
-
-typedef struct stat_export_ {
-	char* name;                /* null terminated statistic name */
-	int flags;                 /* flags */
-	stat_var** stat_pointer;   /* pointer to the variable's shm mem location */
-} stat_export_t;
-
-#define get_stat(name)  0
-#define get_stat_val(v) 0
-#define get_stat_var_from_num_code(num_code, in_code) 0
-#define update_stat(v, n)
-#define reset_stat(v)
-#define if_update_stat(cond, v, n)
+#include "lib/kcore/statistics.h"
 
 #ifdef STATISTICS
-#warning "sorry sip-router does not support STATISTICS"
+#warning "sorry sip-router does not support all STATISTICS"
 #endif
 
 #endif /* _STATISTICS_H_ */
