29494fc266fc36959e0ef560e3d05bd4e827d644
Elena-Ramona Modroiu <ramona@rosdev.ro>
2009-04-23 14:00:08 +0300

    usrloc(k): register exported statistics from mod_init
    - k statistics API is now in kcore library, SR core doesn't register
      stats from module_exports

--- modules_k/usrloc/ul_mod.c
+++ modules_k/usrloc/ul_mod.c
@@ -214,6 +214,14 @@ struct module_exports exports = {
  */
 static int mod_init(void)
 {
+#ifdef STATISTICS
+	/* register statistics */
+	if (register_module_stats( exports.name, mod_stats)!=0 ) {
+		LM_ERR("failed to register core statistics\n");
+		return -1;
+	}
+#endif
+	
 	if(register_mi_mod(exports.name, mi_cmds)!=0)
 	{
 		LM_ERR("failed to register MI commands\n");
