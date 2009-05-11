a0da9a80aa1aa99c03c2f8e910f5403d1f65d8d0
Daniel-Constantin Mierla <miconda@gmail.com>
2009-05-11 11:26:43 +0300

    mi_fifo: fork process using PROC_NOCHLDINIT instead of 100
    - do not call modules' child init, MI commands register own proc init
      function

--- modules_k/mi_fifo/mi_fifo.c
+++ modules_k/mi_fifo/mi_fifo.c
@@ -191,7 +191,7 @@ static int mi_child_init(int rank)
 	}
 
 	if (rank==PROC_MAIN) {
-		pid=fork_process(100, "MI FIFO", 1);
+		pid=fork_process(PROC_NOCHLDINIT, "MI FIFO", 1);
 		if (pid<0)
 			return -1; /* error */
 		if(pid==0){
