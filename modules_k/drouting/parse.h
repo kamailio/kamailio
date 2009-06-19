0fbd2328d880d54cc2e4c68d09c7f5d52f4f6820
Raul Alexis Betancor Santana <rabs@dimension-virtual.com>
2009-06-20 00:49:23 +0100

    drouting: Updated all Copyright notices

--- modules_k/drouting/parse.h
+++ modules_k/drouting/parse.h
@@ -1,16 +1,16 @@
 /*
- * $Id: parse.h,v 1.1.1.1 2007/05/09 11:25:33 bogdan Exp $
+ * $Id$
  *
- * Copyright (C) 2005-2008 Voice Sistem SRL
+ * Copyright (C) 2005-2009 Voice Sistem SRL
  *
- * This file is part of Open SIP Server.
+ * This file is part of Kamailio, a free SIP server.
  *
- * DROUTING OpenSIPS-module is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * Kamailio is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- * DROUTING OpenSIPS-module is distributed in the hope that it will be useful,
+ * Kamailio is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
@@ -18,10 +18,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- * For any questions about this software and its license, please contact
- * Voice Sistem at following e-mail address:
- *         office@voice-system.ro
  *
  * History:
  * ---------
