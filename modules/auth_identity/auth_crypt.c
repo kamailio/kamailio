105d0ab0a15ef69b18eff9287b6a031a3e4404a3
oej <oej@edvina.net>
2009-10-10 16:02:06 +0200

    - updating licence templates - Adding doxygen headers

--- modules/auth_identity/auth_crypt.c
+++ modules/auth_identity/auth_crypt.c
@@ -3,19 +3,14 @@
  *
  * Copyright (c) 2007 iptelorg GmbH
  *
- * This file is part of ser, a free SIP server.
+ * This file is part of sip-router, a free SIP server.
  *
- * ser is free software; you can redistribute it and/or modify
+ * sip-router is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version
  *
- * For a license to use the ser software under conditions
- * other than those described here, or to purchase support for this
- * software, please contact iptel.org by e-mail at the following addresses:
- *    info@iptel.org
- *
- * ser is distributed in the hope that it will be useful,
+ * sip-router is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
@@ -24,6 +19,14 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
+
+/*!
+ * \file
+ * \brief SIP-router auth-identity :: Crypt
+ * \ingroup auth-identity
+ * Module: \ref auth-identity
+ */
+
 
 #define _GNU_SOURCE
 #include <stdio.h>
--- modules/auth_identity/auth_hdrs.c
+++ modules/auth_identity/auth_hdrs.c
@@ -3,19 +3,14 @@
  *
  * Copyright (c) 2007 iptelorg GmbH
  *
- * This file is part of ser, a free SIP server.
+ * This file is part of SIP-router, a free SIP server.
  *
- * ser is free software; you can redistribute it and/or modify
+ * SIP-router is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version
  *
- * For a license to use the ser software under conditions
- * other than those described here, or to purchase support for this
- * software, please contact iptel.org by e-mail at the following addresses:
- *    info@iptel.org
- *
- * ser is distributed in the hope that it will be useful,
+ * SIP-router is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
@@ -23,6 +18,13 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/*!
+ * \file
+ * \brief SIP-router auth-identity :: Authentication headers
+ * \ingroup auth-identity
+ * Module: \ref auth-identity
  */
 
 #include <errno.h>
