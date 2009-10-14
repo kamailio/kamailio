kamailio/kamailio

commit 9accd256fdc80e6a6207cb799c00528f6a4846be

Adding doxygen documentation

modules/tls/tls_mod.h
@@ -7,19 +7,19 @@
  * Copyright (C) 2004,2005 Free Software Foundation, Inc.
  * Copyright (C) 2005 iptelorg GmbH
  *
- * This file is part of ser, a free SIP server.
+ * This file is part of sip-router, a free SIP server.
  *
- * ser is free software; you can redistribute it and/or modify
+ * sip-router is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version
  *
- * For a license to use the ser software under conditions
+ * For a license to use the sip-router software under conditions
  * other than those described here, or to purchase support for this
  * software, please contact iptel.org by e-mail at the following addresses:
  *    info@iptel.org
  *
- * ser is distributed in the hope that it will be useful,
+ * sip-router is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
@@ -28,10 +28,17 @@
  * along with this program; if not, write to the Free Software 
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
+/*!
+ * \file
+ * \brief SIP-router TLS support :: module interface
+ * \ingroup tls
+ * Module: \ref tls
+ */
+
 
 
 #ifndef _TLS_MOD_H
-#define _TLS_MOD_H
+#iefine _TLS_MOD_H
 
 #include "../../str.h"
 #include "../../locking.h"

modules/tls/fixed_c_zlib.h
@@ -18,6 +18,13 @@
  *    version).
  *  -- andrei
  */
+/*!
+ * \file
+ * \brief SIP-router TLS support :: Zlib functions
+ * \ingroup tls
+ * Module: \ref tls
+ */
+
 
 
 #ifdef TLS_FIX_ZLIB_COMPRESSION

modules/tls/tls_config.c
@@ -29,6 +29,13 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  */
+/*!
+ * \file
+ * \brief SIP-router TLS support :: Configuration file parser
+ * \ingroup tls
+ * Module: \ref tls
+ */
+
 
 #include "tls_config.h"
 #include "tls_domain.h"

modules/tls/tls_config.h
@@ -29,6 +29,13 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  */
+/*!
+ * \file
+ * \brief SIP-router TLS support :: Configuration file parser
+ * \ingroup tls
+ * Module: \ref tls
+ */
+
 
 #ifndef _TLS_CONFIG_H
 #define _TLS_CONFIG_H

modules/tls/tls_domain.c
@@ -28,6 +28,13 @@
  * along with this program; if not, write to the Free Software 
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
+/*!
+ * \file
+ * \brief SIP-router TLS support :: Virtual domain configuration support
+ * \ingroup tls
+ * Module: \ref tls
+ */
+
 
 #include <stdlib.h>
 #include <openssl/ssl.h>

modules/tls/tls_domain.h
@@ -28,6 +28,13 @@
  * along with this program; if not, write to the Free Software 
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
+/*!
+ * \file
+ * \brief SIP-router TLS support :: virtual configuration domain support
+ * \ingroup tls
+ * Module: \ref tls
+ */
+
 
 #ifndef _TLS_DOMAIN_H
 #define _TLS_DOMAIN_H

modules/tls/tls_init.c
@@ -35,6 +35,24 @@
  *  2007-02-23  openssl low memory bugs workaround (andrei)
  */
 
+/*! \defgroup tls SIP-router TLS support
+ *
+ * This modules implements SIP over TCP with TLS encryption.
+ * Make sure you read the README file that describes configuration
+ * of TLS for single servers and servers hosting multiple domains,
+ * and thus using multiple SSL/TLS certificates.
+ *
+ *
+ */
+/*!
+ * \file
+ * \brief SIP-router TLS support :: Initialization
+ * \ingroup tls
+ * Module: \ref tls
+ */
+
+
+
 #include <stdio.h>
 #include <sys/types.h>
 #include <netinet/in_systm.h>

modules/tls/tls_init.h
@@ -28,6 +28,13 @@
  * along with this program; if not, write to the Free Software 
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
+/*!
+ * \file
+ * \brief SIP-router TLS support :: OpenSSL initialization funtions
+ * \ingroup tls
+ * Module: \ref tls
+ */
+
 
 #ifndef _TLS_INIT_H
 #define _TLS_INIT_H

commit 22595b7d37980bd16aff22c5bc24314563933e93

tls(sr): fix define error introduced in 9accd256fdc80e6a6207cb7

modules/tls/tls_mod.h
@@ -38,7 +38,7 @@
 
 
 #ifndef _TLS_MOD_H
-#iefine _TLS_MOD_H
+#define _TLS_MOD_H
 
 #include "../../str.h"
 #include "../../locking.h"