b90ac7da0de80d2ce997fc9586976b80e9aee788
Andrei Pelinescu-Onciul <andrei@iptel.org>
2009-10-15 20:06:29 +0200

    mi_rpc: Revert Changing mi_rpc licence
    Revert commit cbfe9a14fdb05e8212ba4a0b24472f5239850a77
     (changing mi_rpc_mod.c licence), at Daniel's request.

--- modules/mi_rpc/mi_rpc_mod.c
+++ modules/mi_rpc/mi_rpc_mod.c
@@ -5,34 +5,18 @@
  *
  * Copyright (C) 2009 Daniel-Constantin Mierla (asipto.com).
  *
- * This file is part of SIP-router, a free SIP server.
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
  *
- * SIP-router is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version
- *
- * SIP-router is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
-
-/*!
- * \file 
- * \brief Management interface :: RPC support
- * \ingroup mi_rpc
- */
-
-/*!
- * \defgroup mi_rpc Management interface :: RPC support
- */
-
 
 #include <stdlib.h>
 #include <string.h>
