kamailio/kamailio

commit 5a03489e539be9a42994533e55026cc7710d131c

- Doxygen updates on core files - Add project name to doxygen in Makefile

cfg_parser.c
@@ -25,12 +25,41 @@
  * \file
  * \brief SIP-router core :: 
  * \ingroup core
+ *
  * Module: \ref core
+ *
+ * See \ref ConfigEngine
+ *
+ * \page ConfigEngine
+ * In file \ref cfg_parser.c 
+ * Configuration examples
+ * - \ref ConfigExample1
+ * - \ref ConfigExample2
+ * - \ref ConfigExample3
+ * - \ref ConfigExample4
+ * - \ref ConfigExample5
+ * - \ref ConfigExample6
+ * - \ref ConfigExample7
+ * - \ref ConfigExample8
+ *
+ * <b>Run-time Allocated Destination Variables</b>
+ * - the destination variable pointers in arrays are assigned at compile time
+ * - The address of variables allocated on the heap (typically in dynamically allocated
+ *   structures) is not know and thus we need to assign NULL initially and change the pointer
+ *   at runtime.
+ *   (provide an example).
+ *
+ * <b>Built-in parsing functions</b>
+ *
+ * *_val functions parse the whole option body (including =)
+ */
+
  */
 
 
-/** Example 1: Options without values
+/*! \page ConfigExample1  Configuration engine Example 1: Options without values
  *
+\verbatim
  *	str file = STR_STATIC_INIT("test.cfg");
  *	cfg_parser_t* parser;
  *	int feature_a = 0, feature_b = 0;
@@ -56,17 +85,21 @@
  *	}
  *
  *	cfg_parser_close(parser);
+\endverbatim
  */
 
-/** Example 2: Options with integer values
+/*! \page ConfigExample2  Configuration engine Example 2: Options with integer values
+\verbatim
  * 	cfg_option_t options[] = {
  *		{"max_number",   .param = &max_number,   .f = cfg_parse_int_val },
  *		{"extra_checks", .param = &extra_checks, .f = cfg_parse_bool_val},
  *		{0}
  *	};
+\endverbatim
  */
 
-/** Example 3: Enum options
+/*! \page ConfigExample3 Configuration engine Example 3: Enum options
+\verbatim
  * int scope;
  *
  *	cfg_option_t scopes[] = {
@@ -83,10 +116,12 @@
  *		{"scope", .param = scopes, .f = cfg_parse_enum_val},
  *		{0}
  *	};
+\endverbatim
  */
 
-/** Example 4: Options with string values
- * 	str filename = STR_NULL;
+/*! \page ConfigExample4 Configuration engine Example 4: Options with string values
+\verbatim
+* 	str filename = STR_NULL;
  *
  *	cfg_option_t options[] = {
  *		{"filename", .param = &filename, .f = cfg_parse_str_val},
@@ -97,35 +132,25 @@
  *   by a subsequent call
  * - There are flags to tell the function to copy the resuting string in a pkg, shm, glibc,
  *   or static buffers
+\endverbatim
  */
 
-/** Example 5: Custom value parsing
+/*! \page ConfigExample5 Configuration engine Example 5: Custom value parsing
  * TBD
  */
 
-/** Example 6: Parsing Sections
+/*! \page ConfigExample6 Configuration engine Example 6: Parsing Sections
  * TBD
  */
 
-/** Example 7: Default Options
+/*! \page ConfigExample7 Configuration engine Example 7: Default Options
  * TBD
  */
 
-/** Example 8: Memory management of strings
- * - Data types with fixed size are easy, they can be copied into a pre-allocated memory
+/*! \page ConfigExample8 Configuration engine Example 8: Memory management of strings
+ *
+ * Data types with fixed size are easy, they can be copied into a pre-allocated memory
  * buffer, strings cannot because their length is unknown.
- */
-
-/** Run-time Allocated Destination Variables
- * - the destination variable pointers in arrays are assigned at compile time
- * - The address of variables allocated on the heap (typically in dynamically allocated
- *   structures) is not know and thus we need to assign NULL initially and change the pointer
- *   at runtime.
- *   (provide an example).
- */
-
-/** Built-in parsing functions
- * *_val functions parse the whole option body (including =)
  */
 
 #include "cfg_parser.h"
@@ -142,20 +167,20 @@
 #include <libgen.h>
 
 
-/* The states of the lexical scanner */
+/*! \brief The states of the lexical scanner */
 enum st {
-	ST_S,  /**< Begin */
-	ST_A,  /**< Alphanumeric */
-	ST_AE, /**< Alphanumeric escaped */
-	ST_Q,  /**< Quoted */
-	ST_QE, /**< Quoted escaped */
-	ST_C,  /**< Comment */
-	ST_CE, /**< Comment escaped */
-	ST_E,  /**< Escaped */
+	ST_S,  /*!< Begin */
+	ST_A,  /*!< Alphanumeric */
+	ST_AE, /*!< Alphanumeric escaped */
+	ST_Q,  /*!< Quoted */
+	ST_QE, /*!< Quoted escaped */
+	ST_C,  /*!< Comment */
+	ST_CE, /*!< Comment escaped */
+	ST_E,  /*!< Escaped */
 };
 
 
-/* Test for alphanumeric characters */
+/*! \brief Test for alphanumeric characters */
 #define IS_ALPHA(c) \
     (((c) >= 'a' && (c) <= 'z') || \
      ((c) >= 'A' && (c) <= 'Z') || \
@@ -163,7 +188,7 @@ enum st {
      (c) == '_')
 
 
-/* Test for delimiter characters */
+/*! \brief Test for delimiter characters */
 #define IS_DELIM(c) \
     ((c) == '=' || \
      (c) == ':' || \
@@ -188,7 +213,7 @@ enum st {
      (c) == '\'')
 
 
-/* Whitespace characters */
+/*! \brief Whitespace characters */
 #define IS_WHITESPACE(c) ((c) == ' ' || (c) == '\t' || (c) == '\r') 
 #define IS_QUOTE(c)      ((c) == '\"')  /* Quote characters */
 #define IS_COMMENT(c)    ((c) == '#')   /* Characters that start comments */
@@ -196,9 +221,8 @@ enum st {
 #define IS_EOL(c)        ((c) == '\n')  /* End of line */
 
 
-/*
- * Append character to the value of current
- * token
+/*! \brief
+ * Append character to the value of current token
  */
 #define PUSH(c)                            \
     if (token->val.len >= MAX_TOKEN_LEN) { \
@@ -213,7 +237,7 @@ enum st {
     token->val.s[token->val.len++] = (c);
 
 
-/*
+/*! \brief
  * Return current token from the lexical analyzer
  */
 #define RETURN(c)               \
@@ -224,7 +248,7 @@ enum st {
     return 0;
 
 
-/*
+/*! \brief
  * Get next character and update counters
  */
 #define READ_CHAR      \

Makefile.rules
@@ -280,6 +280,7 @@ doxygen:
 	# disable call graphes, because of the DOT dependencies
 	(cat ./$(COREPATH)/doc/doxygen/ser.doxygen; \
 	echo "HAVE_DOT=no" ;\
+	echo "PROJECT_NAME=SIP-ROUTER" ;\
 	echo "PROJECT_NUMBER=$(NAME)-$(RELEASE)" )| doxygen -
 	-@echo "Doxygen documentation created"
 

action.c
@@ -52,6 +52,7 @@
  *  2009-05-04  switched IF_T to rval_expr (andrei)
  *  2009-09-15  added SET_{FWD,RPL}_NO_CONNECT, SET_{FWD,RPL}_CLOSE (andrei)
  */
+
 /*!
  * \file
  * \brief SIP-router core :: 

action.h
@@ -26,6 +26,13 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+/*!
+ * \file
+ * \brief SIP-router core ::
+ * \ingroup core
+ * Module: \ref core
+ */
+
 
 #ifndef action_h
 #define action_h

atomic_ops.h
@@ -15,8 +15,18 @@
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
+
+/*!
+ * \file
+ * \brief SIP-router core :: Atomic operations and memory barriers
+ * \ingroup core
+ * Module: \ref core
+ * See \ref atomicops
+ */
+
 /*
- *  atomic operations and memory barriers
+ * \page atomicops  Atomic operations and memory barriers
+ *
  *  WARNING: atomic ops do not include memory barriers
  *  
  *  memory barriers:

atomic_ops_init.h
@@ -24,10 +24,15 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-/*
- * atomic_ops init functions
- * (needed for lock intializing if no native asm locks are available
- *  for the current arch./compiler combination, see atomic_ops.c)
+/*!
+ * \file
+ * \brief SIP-router core :: atomic_ops init functions
+ *
+ * \ingroup core
+ * Module: \ref core
+ *
+ * Needed for lock intializing if no native asm locks are available
+ *  for the current arch./compiler combination, see \ref atomic_ops.c
  */
 /* 
  * History:
@@ -38,9 +43,9 @@
 #ifndef __atomic_ops_init_h
 #define __atomic_ops_init_h
 
-/* init atomic ops */
+/*! \brief init atomic ops */
 int init_atomic_ops();
-/* destroy atomic ops (e.g. frees the locks, if locks are used) */
+/*! \brief destroy atomic ops (e.g. frees the locks, if locks are used) */
 void destroy_atomic_ops();
 
 #endif

basex.c
@@ -28,7 +28,7 @@
  */
 /*!
  * \file
- * \brief SIP-router core :: 
+ * \brief SIP-router core :: convert/decode to/from ascii using various bases
  * \ingroup core
  * Module: \ref core
  */

basex.h
@@ -17,42 +17,38 @@
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
-/*
+/*!
+ * \file
+ * \brief SIP-router core :: convert/decode to/from ascii using various bases
+ *
+ * \ingroup core
+ *
+ * Module: \ref core
+ *
+ *
  * Functions:
- *  init_basex()                              - inits internal lookup tables
- *  HEX_HI(unsigned char c)                   - returns the first 4 bits of
- *                                              c converted to a hex digit
- *  HEX_LOW(unsigned char c)                  - returns the low 4 bits of
- *                                              c converted to a hex digit
- *  UNHEX(unsigned char hex_digit)            - converts hex_digit to a
+ *  - init_basex()                    : inits internal lookup tables
+ *  - HEX_HI(unsigned char c)         : returns the first 4 bits of c converted to a hex digit
+ *  - HEX_LOW(unsigned char c)         : returns the low 4 bits of converted to a hex digit
+ *  - UNHEX(unsigned char hex_digit)            : converts hex_digit to a
  *                                              number (0..15); it might
  *                                              return 0xff for invalid 
  *                                              digit (but with some compile
  *                                              option it won't check)
  *
- *  base16_enc(src, src_len, dst, dst_len)    - encode to standard hex
- *  base16_dec(src, src_len, dst, dst_len)    - decode from standard hex
- *  base16_enc_len(len)                       - length needed to encode len
- *                                              bytes (macro)
- *  base16_max_dec_len(len)                   - length needed to decode a 
- *                                              string of size len
+ *  - base16_enc(src, src_len, dst, dst_len)    : encode to standard hex
+ *  - base16_dec(src, src_len, dst, dst_len)    : decode from standard hex
+ *  - base16_enc_len(len)                       : length needed to encode len bytes (macro)
+ *  - base16_max_dec_len(len)                   : length needed to decode a string of size len
  *
- *  base64_enc(src, src_len, dst, dst_len)    - encode to base64, standard
- *                                              alphabet
- *  base64_dec(src, src_len, dst, dst_len)    - decode from base64, standard
- *                                              alphabet
- *  base64_enc_len(len)                       - length needed to encode
- *                                               len bytes (macro)
- *  base64_max_dec_len(len)                   - maximum length needed to
- *                                               decode len bytes (macro)
- *  base64_dec_len(str, len)                  - size of the decoded str 
+ *  - base64_enc(src, src_len, dst, dst_len)    : encode to base64, standard alphabet
+ *  - base64_dec(src, src_len, dst, dst_len)    : decode from base64, standard  alphabet
+ *  - base64_enc_len(len)                       : length needed to encode len bytes (macro)
+ *  - base64_max_dec_len(len)                   : maximum length needed to decode len bytes (macro)
+ *  - base64_dec_len(str, len)                  : size of the decoded str 
+ *  - q_base64_enc(src, src_len, dst, dst_len)  : encode to special base64 alphabet (non standard)
+ *  - q_base64_dec(src, src_len, dst, dst_len)  - decode from special non-standard base64 alphabet
  *
- *
- *  q_base64_enc(src, src_len, dst, dst_len)  - encode to special base64
- *                                              alphabet (non standard)
- *
- *  q_base64_dec(src, src_len, dst, dst_len)  - decode from special non-
- *                                              standard base64 alphabet
  *  All the above functions return the size used (in dst) on success and
  *   0 or a negative number (which is -1*size_needed) on error.
  *
@@ -65,6 +61,7 @@
  *  for some interesting tests and ideeas).
  *
  *  Test results for 40 bytes  (typical ser nounce) in average cpu cycles:
+<verbatim>
  *                    lookup   lookup_large lookup8k no-lookup
  *  base16_enc           211/231  218/199      -       1331
  *  base16_dec           252/251  236          -       1226
@@ -73,22 +70,23 @@
  *  q_base64_enc         -                              288
  *  q_base64_dec         -                              281
  *  (see test/basex.txt for more results)
+</verbatim>
  *
  * Defines:
- *  BASE64_LOOKUP_TABLE/NO_BASE64_LOOKUP_TABLE - use (default)/don't use
+ *  - BASE64_LOOKUP_TABLE/NO_BASE64_LOOKUP_TABLE : use (default)/don't use
  *     small lookup tables for conversions (faster in general).
- *  BASE64_LOOKUP_LARGE    - use large lookup tables (2560 bytes for 
+ *  - BASE64_LOOKUP_LARGE    : use large lookup tables (2560 bytes for 
  *    encoding and 256 bytes for decoding; without it 64 bytes are used for
  *    encoding and 85 bytes for decoding.
- *  BASE64_LOOKUP_8K - use even larger lookup tables (8K for encoding and
+ *  - BASE64_LOOKUP_8K : use even larger lookup tables (8K for encoding and
  *    256 for decoding); also try to write 2 bytes at a time (short) if
  *    the destination is 2 byte aligned
  *
- *  BASE16_LOOKUP_TABLE/NO_BASE16_LOOKUP_TABLE - use (default)/don't use
+ *  - BASE16_LOOKUP_TABLE/NO_BASE16_LOOKUP_TABLE : use (default)/don't use
  *     small lookup tables for conversions (faster in general).
- *  BASE16_LOOKUP_LARGE  - use large lookup tables (512 bytes for 
+ *  - BASE16_LOOKUP_LARGE  : use large lookup tables (512 bytes for 
  *    encoding and 256 bytes for decoding
- *  BASE16_READ_WHOLE_INTS - read an int at a time
+ *  - BASE16_READ_WHOLE_INTS : read an int at a time
  *
  * History:
  * --------
@@ -129,7 +127,7 @@
 	defined BASE64_LOOKUP_8K
 #include "endianness.h"
 
-/* aligns p to a type* pointer, type must have a 2^k size */
+/*! \brief aligns p to a type* pointer, type must have a 2^k size */
 #define ALIGN_POINTER(p, type) \
 	((type*) ((long)((char*)(p)+sizeof(type)-1)&~(long)(sizeof(type)-1)))
 
@@ -141,7 +139,7 @@
 #ifdef BASE16_LOOKUP_TABLE
 
 #ifdef BASE16_LOOKUP_LARGE
-/* use large tables: 512 for lookup and 256 for decode */
+/*! \brief use large tables: 512 for lookup and 256 for decode */
 
 extern unsigned char _bx_hexdig_hi[256];
 extern unsigned char _bx_hexdig_low[256];
@@ -154,7 +152,7 @@ extern unsigned char _bx_unhexdig256[256];
 #define UNHEX(h)	_bx_unhexdig256[(h)]
 
 #else /* BASE16_LOOKUP_LARGE */
-/* use small tabes: 16 bytes for lookup and 32 for decode */
+/*! \brief use small tabes: 16 bytes for lookup and 32 for decode */
 
 extern unsigned char _bx_hexdig[16+1];
 
@@ -254,39 +252,40 @@ extern unsigned char _bx_ub64[0x54+1];
 
 
 
-/* lenght needed for encoding l bytes */
+/*! \brief lenght needed for encoding l bytes */
 #define base16_enc_len(l) (l*2)
-/* maximum lenght needed for decoding l bytes */
+/*! \brief maximum lenght needed for decoding l bytes */
 #define base16_max_dec_len(l) (l/2)
-/* actual space needed for decoding a string b of size l */
+/*! \brief actual space needed for decoding a string b of size l */
 #define base16_dec_len(b, l) base16_max_dec_len(l)
-/* minimum valid source len for decoding */
+/*! \brief minimum valid source len for decoding */
 #define base16_dec_min_len() 2
-/* minimum valid source len for encoding */
+/*! \brief minimum valid source len for encoding */
 #define base16_enc_min_len() 0
 
-/* space needed for encoding l bytes */
+/*! \brief space needed for encoding l bytes */
 #define base64_enc_len(l) (((l)+2)/3*4)
-/* maximum space needed for encoding l bytes */
+/*! \brief maximum space needed for encoding l bytes */
 #define base64_max_dec_len(l) ((l)/4*3)
-/* actual space needed for decoding a string b of size l, l>=4 */
+/*! \brief actual space needed for decoding a string b of size l, l>=4 */
 #define base64_dec_len(b, l) \
 	(base64_max_dec_len(l)-((b)[(l)-2]=='=') -((b)[(l)-1]=='='))
-/* minimum valid source len for decoding */
+/*! \brief minimum valid source len for decoding */
 #define base64_dec_min_len() 4
-/* minimum valid source len for encoding */
+/*! \brief minimum valid source len for encoding */
 #define base64_enc_min_len() 0
 
 
 #ifdef BASE16_READ_WHOLE_INTS
 
-/* params: 
- * returns: size used from the output buffer (dst) on success,
+/*! 
+ * \params: 
+ * \return: size used from the output buffer (dst) on success,
  *          -size_needed on error
+ *
  * WARNING: the output string is not 0-term
  */
-inline static int base16_enc(unsigned char* src, int slen,
-							 unsigned char*  dst, int dlen)
+inline static int base16_enc(unsigned char* src, int slen, unsigned char*  dst, int dlen)
 {
 	unsigned int* p;
 	unsigned char* end;
@@ -454,10 +453,11 @@ inline static int base16_enc(unsigned char* src, int slen,
 #else /* BASE16_READ_WHOLE_INTS */
 
 
-/* params: 
- * returns: size used from the output buffer (dst) on success,
+/*!
+ * \return : size used from the output buffer (dst) on success,
  *          -size_needed on error
- * WARNING: the output string is not 0-term
+ *
+ * \note WARNING: the output string is not 0-term
  */
 inline static int base16_enc(unsigned char* src, int slen,
 							 unsigned char*  dst, int dlen)
@@ -479,8 +479,7 @@ inline static int base16_enc(unsigned char* src, int slen,
 
 #endif /* BASE16_READ_WHOLE_INTS */
 
-inline static int base16_dec(unsigned char* src, int slen,
-							 unsigned char* dst, int dlen)
+inline static int base16_dec(unsigned char* src, int slen, unsigned char* dst, int dlen)
 {
 	unsigned char* end;
 	int osize;
@@ -498,8 +497,8 @@ inline static int base16_dec(unsigned char* src, int slen,
 
 
 
-/* helper internal function: encodes v (6 bits value)
- * returns char ascii encoding on success and 0xff on error
+/*! \brief helper internal function: encodes v (6 bits value)
+ * \return char ascii encoding on success and 0xff on error
  * (value out of range) */
 inline static unsigned char base64_enc_char(unsigned char v)
 {
@@ -519,8 +518,8 @@ inline static unsigned char base64_enc_char(unsigned char v)
 	return 0xff;
 }
 
-/* helper internal function: decodes a base64 "digit",
- * returns value on success (0-63) and 0xff on error (invalid)*/
+/*! \brief helper internal function: decodes a base64 "digit",
+ * \return value on success (0-63) and 0xff on error (invalid)*/
 inline static unsigned base64_dec_char(unsigned char v)
 {
 	switch(v){
@@ -557,10 +556,11 @@ inline static unsigned base64_dec_char(unsigned char v)
 
 
 #ifdef BASE64_LOOKUP_8K
-/* params: 
- * returns: size used from the output buffer (dst) on success ((slen+2)/3*4)
+/*!
+ * \return : size used from the output buffer (dst) on success ((slen+2)/3*4)
  *          -size_needed on error
- * WARNING: the output string is not 0-term
+ *
+ * \note WARNING: the output string is not 0-term
  */
 inline static int base64_enc(unsigned char* src, int slen,
 							unsigned char* dst,  int dlen)
@@ -614,10 +614,10 @@ inline static int base64_enc(unsigned char* src, int slen,
 	return osize;
 }
 #else /*BASE64_LOOKUP_8K*/
-/* params: 
- * returns: size used from the output buffer (dst) on success ((slen+2)/3*4)
+/*! \brief Convert to base64
+ * \return size used from the output buffer (dst) on success ((slen+2)/3*4)
  *          -size_needed on error
- * WARNING: the output string is not 0-term
+ * \note WARNING: the output string is not 0-term
  */
 inline static int base64_enc(unsigned char* src, int slen,
 							unsigned char* dst,  int dlen)
@@ -655,10 +655,10 @@ inline static int base64_enc(unsigned char* src, int slen,
 
 
 
-/* params: 
- * returns: size used from the output buffer (dst) on success (max: slen/4*3)
+/*! \brief
+ * \return size used from the output buffer (dst) on success (max: slen/4*3)
  *          -size_needed on error or 0 on bad base64 encoded string
- * WARNING: the output string is not 0-term
+ * \note WARNING: the output string is not 0-term
  */
 inline static int base64_dec(unsigned char* src, int slen,
 							unsigned char* dst,  int dlen)
@@ -721,13 +721,12 @@ inline static int base64_dec(unsigned char* src, int slen,
 
 
 
-/*
- * same as base64_enc but with a different alphabet, that allows simpler and
+/*! \brief
+ * same as \ref base64_enc() but with a different alphabet, that allows simpler and
  *  faster enc/dec
- * params: 
- * returns: size used from the output buffer (dst) on success ((slen+2)/3*4)
+ * \return size used from the output buffer (dst) on success ((slen+2)/3*4)
  *          -size_needed on error
- * WARNING: the alphabet includes ":;<>?@[]\`", so it might not be suited
+ * \note WARNING: the alphabet includes ":;<>?@[]\`", so it might not be suited
  *  in all cases (e.g. encoding something in a sip uri).
  */
 inline static int q_base64_enc(unsigned char* src, int slen,
@@ -769,14 +768,13 @@ inline static int q_base64_enc(unsigned char* src, int slen,
 
 
 
-/*
- * same as base64_enc but with a different alphabet, that allows simpler and
+/*! \brief
+ * same as \ref base64_enc() but with a different alphabet, that allows simpler and
  *  faster enc/dec
- * params: 
- * params: 
- * returns: size used from the output buffer (dst) on success (max: slen/4*3)
+ *
+ * \return size used from the output buffer (dst) on success (max: slen/4*3)
  *          -size_needed on error or 0 on bad base64 encoded string
- * WARNING: the output string is not 0-term
+ * \note WARNING: the output string is not 0-term
  */
 inline static int q_base64_dec(unsigned char* src, int slen,
 							unsigned char* dst,  int dlen)

commit 921de0b5b7f45a33a2bffc322a92bdfaa5783d02

core: fix commit error introduced in 5a03489e539be9a42994

cfg_parser.c
@@ -54,8 +54,6 @@
  * *_val functions parse the whole option body (including =)
  */
 
- */
-
 
 /*! \page ConfigExample1  Configuration engine Example 1: Options without values
  *