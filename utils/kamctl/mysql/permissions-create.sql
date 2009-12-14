b2656f5ae7f36345ea18a5ff82eb66f11c7f5cab
Daniel-Constantin Mierla <miconda@gmail.com>
2009-12-14 10:22:25 +0100

    dbschema: regenerate db creation scripts
    - get the updates to permissions and lcr tables
    (cherry picked from commit 0586447a736900832b116f3a3826ba01d9969b6a)

--- utils/kamctl/mysql/permissions-create.sql
+++ utils/kamctl/mysql/permissions-create.sql
@@ -1,10 +1,10 @@
-INSERT INTO version (table_name, table_version) values ('trusted','4');
+INSERT INTO version (table_name, table_version) values ('trusted','5');
 CREATE TABLE trusted (
     id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
     src_ip VARCHAR(50) NOT NULL,
     proto VARCHAR(4) NOT NULL,
     from_pattern VARCHAR(64) DEFAULT NULL,
-    tag VARCHAR(32)
+    tag VARCHAR(64)
 ) ENGINE=MyISAM;
 
 CREATE INDEX peer_idx ON trusted (src_ip);
