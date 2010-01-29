2cef8565864ab1963ea3c2a6b863727a566e4580
sobomax <sobomax@sippysoft.com>
2010-01-29 04:48:21 -0800

    Revert "Revert "Add Python module, still WIP, but should be usable. No xml"."
    This reverts commit e08eaddb181637f4fbb3a165fb0827b9a964f6bd.

---
+++ modules/app_python/python_exec.h
@@ -0,0 +1,31 @@
+/* $Id$
+ *
+ * Copyright (C) 2009 Sippy Software, Inc., http://www.sippysoft.com
+ *
+ * This file is part of SIP-Router, a free SIP server.
+ *
+ * SIP-Router is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version
+ *
+ * SIP-Router is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+*/
+
+#ifndef _PYTHON_EXEC_H
+#define  _PYTHON_EXEC_H
+
+#include "../../parser/msg_parser.h"
+
+int python_exec1(struct sip_msg *, char *, char *);
+int python_exec2(struct sip_msg *, char *, char *);
+
+#endif
