3303a5b1f7c4ead6b0edd21d20e65b9957359200
Marius Zbihlei <marius.zbihlei@1and1.ro>
2010-02-17 13:57:05 +0200

    Modules/registrar : update stats even if -DSTATISTICS was not passed to the compiles(this was the default)
    update_stat was called even if the statistics where not added to the stats framework.

--- modules_k/registrar/save.c
+++ modules_k/registrar/save.c
@@ -780,18 +780,16 @@ int save(struct sip_msg* _m, char* _d, char* _cflags)
 			goto error;
 		ret = (ret==0)?1:ret;
 	}
-#ifdef STATISTICS
+
 	update_stat(accepted_registrations, 1);
-#endif
+
 	/* Only send reply upon request, not upon reply */
 	if ((route_type == REQUEST_ROUTE) && !is_cflag_set(REG_SAVE_NORPL_FL) && (reg_send_reply(_m) < 0))
 		return -1;
 
 	return ret;
 error:
-#ifdef STATISTICS
 	update_stat(rejected_registrations, 1);
-#endif
 	if ((route_type == REQUEST_ROUTE) && !is_cflag_set(REG_SAVE_NORPL_FL) )
 		reg_send_reply(_m);
 
