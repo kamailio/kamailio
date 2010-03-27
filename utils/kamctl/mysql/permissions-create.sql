64fa789c3bc94b36e79a8d886c5c2742c8a1629d
Daniel-Constantin Mierla <miconda@gmail.com>
2010-03-27 23:29:01 +0100

    kamctl: regenerated db definition scripts
    - due to update of address table structure with new column 'tag'

--- utils/kamctl/mysql/permissions-create.sql
+++ utils/kamctl/mysql/permissions-create.sql
@@ -9,12 +9,13 @@ CREATE TABLE trusted (
 
 CREATE INDEX peer_idx ON trusted (src_ip);
 
-INSERT INTO version (table_name, table_version) values ('address','3');
+INSERT INTO version (table_name, table_version) values ('address','4');
 CREATE TABLE address (
     id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
     grp SMALLINT(5) UNSIGNED DEFAULT 0 NOT NULL,
     ip_addr VARCHAR(15) NOT NULL,
     mask TINYINT DEFAULT 32 NOT NULL,
-    port SMALLINT(5) UNSIGNED DEFAULT 0 NOT NULL
+    port SMALLINT(5) UNSIGNED DEFAULT 0 NOT NULL,
+    tag VARCHAR(64)
 ) ENGINE=MyISAM;
 
--- utils/kamctl/oracle/permissions-create.sql
+++ utils/kamctl/oracle/permissions-create.sql
@@ -17,13 +17,14 @@ BEGIN map2users('trusted'); END;
 /
 CREATE INDEX trusted_peer_idx  ON trusted (src_ip);
 
-INSERT INTO version (table_name, table_version) values ('address','3');
+INSERT INTO version (table_name, table_version) values ('address','4');
 CREATE TABLE address (
     id NUMBER(10) PRIMARY KEY,
     grp NUMBER(5) DEFAULT 0 NOT NULL,
     ip_addr VARCHAR2(15),
     mask NUMBER(5) DEFAULT 32 NOT NULL,
-    port NUMBER(5) DEFAULT 0 NOT NULL
+    port NUMBER(5) DEFAULT 0 NOT NULL,
+    tag VARCHAR2(64)
 );
 
 CREATE OR REPLACE TRIGGER address_tr
