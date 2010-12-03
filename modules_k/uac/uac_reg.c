9ec1efcf245dfa7d802d0356f8de70706e645bc5
Daniel-Constantin Mierla <miconda@gmail.com>
2010-12-03 10:59:05 +0100

    uac: safety checks for empty values in uacreg
    - the values in uacreg table must not be empty
    - reported by Bernhard Suttner
    (cherry picked from commit dd663a733845510a9ee0557b8d49f058c77ef02c)

--- modules_k/uac/uac_reg.c
+++ modules_k/uac/uac_reg.c
@@ -663,6 +663,11 @@ void uac_reg_timer(unsigned int ticks)
 			reg.attr.s = \
 				(char*)(RES_ROWS(db_res)[i].values[pos].val.string_val); \
 			reg.attr.len = strlen(reg.attr.s); \
+			if(reg.attr.len == 0) { \
+				LM_ERR("empty value not allowed for column[%d]=%.*s\n", \
+						pos, db_cols[pos]->len, db_cols[pos]->s); \
+				goto error; \
+			} \
 		} \
 	} while(0);
 
