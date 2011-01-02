fbee5125e1faa2e405e8be8bdf57ae2763f6880f
Daniel-Constantin Mierla <miconda@gmail.com>
2011-01-02 19:56:40 +0100

    dialog(k): open db connection for main process in db delayed mode
    - used at shutdown to write to db
    (cherry picked from commit 7c530f69da7368f87e78d7a3ad1f3439005402a5)

--- modules_k/dialog/dialog.c
+++ modules_k/dialog/dialog.c
@@ -612,9 +612,10 @@ static int child_init(int rank)
 		if_update_stat(dlg_enable_stats, early_dlgs, early_dlgs_cnt);
 	}
 
-	if ( ((dlg_db_mode==DB_MODE_REALTIME || dlg_db_mode==DB_MODE_DELAYED) &&
-	(rank>0 || rank==PROC_TIMER)) ||
-	(dlg_db_mode==DB_MODE_SHUTDOWN && (rank==PROC_MAIN)) ) {
+	if ( ((dlg_db_mode==DB_MODE_REALTIME || dlg_db_mode==DB_MODE_DELAYED)
+				&& (rank>0 || rank==PROC_TIMER || rank==PROC_MAIN))
+			|| (dlg_db_mode==DB_MODE_SHUTDOWN && (rank==PROC_MAIN)) )
+	{
 		if ( dlg_connect_db(&db_url) ) {
 			LM_ERR("failed to connect to database (rank=%d)\n",rank);
 			return -1;
