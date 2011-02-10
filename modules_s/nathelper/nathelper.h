564067f1a35a60d5a111f5f808c5f0f03fa5eab5
Ovidiu Sas <osas@voipembedded.com>
2011-02-10 11:11:55 -0500

    modules_s/nathelper: rtpproxy functionality removed
     - for rtpproxy functionality please use rtpproxy module from modules

--- modules_s/nathelper/nathelper.h
+++ modules_s/nathelper/nathelper.h
@@ -28,29 +28,8 @@
 #ifndef _NATHELPER_H
 #define _NATHELPER_H
 
-/* Handy macros */
-#define	STR2IOVEC(sx, ix)	do {(ix).iov_base = (sx).s; (ix).iov_len = (sx).len;} while(0)
-#define SZ2IOVEC(sx, ix)	do {char *_t_p = (ix).iov_base = (sx); (ix).iov_len = strlen(_t_p);} while(0)
-
-struct rtpp_node {
-    char                    *rn_url;        /* unparsed, deletable */
-    int                     rn_umode;
-    char                    *rn_address;    /* substring of rn_url */
-    int                     rn_fd;          /* control fd */
-    int                     rn_disabled;    /* found unaccessible? */
-    unsigned                rn_weight;      /* for load balancing */
-    int                     rn_recheck_ticks;
-    int                     rn_rep_supported;
-    int                     rn_ptl_supported;
-    struct rtpp_node        *rn_next;
-};
-
 /* Parameters from nathelper.c */
 extern struct socket_info* force_socket;
-
-/* Functions from nathelper */
-struct rtpp_node *select_rtpp_node(str, int, int);
-char *send_rtpp_command(struct rtpp_node *, struct iovec *, int);
 
 /* Functions from natping.c */
 int natpinger_init(void);
