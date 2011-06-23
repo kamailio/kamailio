6aef49b836f75d180a555973a03cf9e981e9994d
Henning Westerholt <henning.westerholt@1und1.de>
2011-06-23 23:58:13 +0200

    rand: fix warnings: call to function ‘..’ without a real prototype from gcc 4.5

--- rand/isaac/rand.h
+++ rand/isaac/rand.h
@@ -36,9 +36,9 @@ typedef  struct randctx  randctx;
  If (flag==TRUE), then use the contents of randrsl[0..RANDSIZ-1] as the seed.
 ------------------------------------------------------------------------------
 */
-void randinit(/*_ randctx *r, word flag _*/);
+void randinit(randctx *r, word flag);
 
-void isaac(/*_ randctx *r _*/);
+void isaac(randctx *r);
 
 
 /*
