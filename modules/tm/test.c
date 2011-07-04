a99aa2035994fd349479ebd2d903bfc99984c388
Henning Westerholt <henning.westerholt@1und1.de>
2011-07-05 00:21:39 +0200

    tm: deactivate compilation for not used test function _test_insert_to_reply

--- modules/tm/test.c
+++ modules/tm/test.c
@@ -26,6 +26,8 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#if 0
+
 #include "defs.h"
 
 
@@ -67,3 +69,4 @@ int _test_insert_to_reply( struct sip_msg *msg, char *str )
     return 1;
 }
 
+#endif
