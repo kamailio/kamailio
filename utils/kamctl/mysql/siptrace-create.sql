fce0a50f90d3fc7625f9d8d4984d34a01079a4d4
pd <peter.dunkley@crocodile-rcs.com>
2011-07-27 13:49:50 +0100

    srdb1: Fixed merge error made in commit 8623eeb1c2762d299cd88e0b7bc8d8fdb8fa2a72
    - When copying utils/kamctl/mysql/siptrace-create.sql from my local SVN I accidently
      copied across utils/kamctl/postgres/siptrace-create.sql.

--- utils/kamctl/mysql/siptrace-create.sql
+++ utils/kamctl/mysql/siptrace-create.sql
@@ -1,20 +1,20 @@
 INSERT INTO version (table_name, table_version) values ('sip_trace','2');
 CREATE TABLE sip_trace (
-    id SERIAL PRIMARY KEY NOT NULL,
-    time_stamp TIMESTAMP WITHOUT TIME ZONE DEFAULT '1900-01-01 00:00:01' NOT NULL,
+    id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    time_stamp DATETIME DEFAULT '1900-01-01 00:00:01' NOT NULL,
     callid VARCHAR(255) DEFAULT '' NOT NULL,
     traced_user VARCHAR(128) DEFAULT '' NOT NULL,
-    msg TEXT NOT NULL,
+    msg MEDIUMTEXT NOT NULL,
     method VARCHAR(50) DEFAULT '' NOT NULL,
     status VARCHAR(128) DEFAULT '' NOT NULL,
     fromip VARCHAR(50) DEFAULT '' NOT NULL,
     toip VARCHAR(50) DEFAULT '' NOT NULL,
     fromtag VARCHAR(64) DEFAULT '' NOT NULL,
     direction VARCHAR(4) DEFAULT '' NOT NULL
-);
+) ENGINE=MyISAM;
 
-CREATE INDEX sip_trace_traced_user_idx ON sip_trace (traced_user);
-CREATE INDEX sip_trace_date_idx ON sip_trace (time_stamp);
-CREATE INDEX sip_trace_fromip_idx ON sip_trace (fromip);
-CREATE INDEX sip_trace_callid_idx ON sip_trace (callid);
+CREATE INDEX traced_user_idx ON sip_trace (traced_user);
+CREATE INDEX date_idx ON sip_trace (time_stamp);
+CREATE INDEX fromip_idx ON sip_trace (fromip);
+CREATE INDEX callid_idx ON sip_trace (callid);
 
