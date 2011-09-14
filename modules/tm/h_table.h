1e83919bcb3f1a2fb2890a6766a007b8f50cb986
Daniel-Constantin Mierla <miconda@gmail.com>
2011-09-14 18:39:37 +0200

    tm: backup X/AVP lists when building new T in t_uac()
    - build_cell() is setting core X/AVP lists to new T structure, losing
      the lists that existed in case another T was set already. This is in
      t_uac() which is used for local generated requests, such as
      uac_req_send(), presence notifications, msilo...
    - a revious patch was backing up only X/AVPs lists for
      execution of event_route[tm:local-request]
    - reported by Vitaliy Aleksandrov

--- modules/tm/h_table.h
+++ modules/tm/h_table.h
@@ -299,13 +299,30 @@ struct totag_elem {
  * max retr. = 65 s which should be enough and saves us 2*2 bytes */
 typedef unsigned short retr_timeout_t;
 
-
 /**
  * extra data from SIP message context to transaction storage
  */
 typedef struct tm_xdata
 {
 	/* lists with avps */
+	struct usr_avp *uri_avps_from;
+	struct usr_avp *uri_avps_to;
+	struct usr_avp *user_avps_from;
+	struct usr_avp *user_avps_to;
+	struct usr_avp *domain_avps_from;
+	struct usr_avp *domain_avps_to;
+#ifdef WITH_XAVP
+	sr_xavp_t **xavps_list;
+#endif
+} tm_xdata_t;
+
+
+/**
+ * links to extra data from SIP message context to transaction storage
+ */
+typedef struct tm_xlinks
+{
+	/* links to lists with avps */
 	struct usr_avp **uri_avps_from;
 	struct usr_avp **uri_avps_to;
 	struct usr_avp **user_avps_from;
@@ -315,7 +332,7 @@ typedef struct tm_xdata
 #ifdef WITH_XAVP
 	sr_xavp_t **xavps_list;
 #endif
-} tm_xdata_t;
+} tm_xlinks_t;
 
 
 /* transaction context */
@@ -564,7 +581,9 @@ inline static void remove_from_hash_table_unsafe( struct cell * p_cell)
 /**
  * backup xdata from/to msg context to local var and use T lists
  */
-void tm_xdata_swap(tm_cell_t *t, tm_xdata_t *xd, int mode);
+void tm_xdata_swap(tm_cell_t *t, tm_xlinks_t *xd, int mode);
+
+void tm_xdata_replace(tm_xdata_t *newxd, tm_xlinks_t *bakxd);
 
 #endif
 
