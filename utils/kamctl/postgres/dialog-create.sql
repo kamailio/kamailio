dbb012c32ec34cafc7cef3e5af66f6dc3f61c303
Daniel-Constantin Mierla <miconda@gmail.com>
2011-10-09 13:05:36 +0200

    srdb1/kamctl: dialog_vars table added to dialog group
    - dialog_vars is used by dialog module and created at once with dialog
      table, otherwise the module does not start

--- utils/kamctl/postgres/dialog-create.sql
+++ utils/kamctl/postgres/dialog-create.sql
@@ -27,3 +27,14 @@ CREATE TABLE dialog (
 
 CREATE INDEX dialog_hash_idx ON dialog (hash_entry, hash_id);
 
+INSERT INTO version (table_name, table_version) values ('dialog_vars','1');
+CREATE TABLE dialog_vars (
+    id SERIAL PRIMARY KEY NOT NULL,
+    hash_entry INTEGER NOT NULL,
+    hash_id INTEGER NOT NULL,
+    dialog_key VARCHAR(128) NOT NULL,
+    dialog_value VARCHAR(512) NOT NULL
+);
+
+CREATE INDEX dialog_vars_hash_idx ON dialog_vars (hash_entry, hash_id);
+
