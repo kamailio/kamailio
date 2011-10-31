233b229e00aa1d1be3a6d9b1484aa2c9ece3407e
Daniel-Constantin Mierla <miconda@gmail.com>
2011-10-31 17:16:36 +0100

    ctl: use SRNAME define to build default ctl file path
    - this define is set by sercmd comilation (NAME is set to sercmd in this
      case)
    - otherwise use NAME, which is set by sip server compilation
    - reported by Henning Westerholt
    (cherry picked from commit b515ac33300f3c1d47195961409812d49fafb58f)

--- modules/ctl/ctl_defaults.h
+++ modules/ctl/ctl_defaults.h
@@ -4,7 +4,13 @@
 #ifndef __ctl_defaults_h
 #define __ctl_defaults_h
 /*listen by default on: */
+#ifdef SRNAME
+/* this is used when compiling sercmd tool */
+#define DEFAULT_CTL_SOCKET  "unixs:/tmp/" SRNAME "_ctl"
+#else
+/* this is used when compiling sip server */
 #define DEFAULT_CTL_SOCKET  "unixs:/tmp/" NAME "_ctl"
+#endif
 /* port used by default for tcp/udp if no port is explicitely specified */
 #define DEFAULT_CTL_PORT 2049
 
