958328f023cc5a150c7860d98bf5e682a0a5c1e9
Timo Reimann <sr@foo-lounge.de>
2011-11-15 00:37:48 +0100

    tm: Pass transaction as parameter to callback for CANCEL request.

--- modules/tm/t_cancel.c
+++ modules/tm/t_cancel.c
@@ -328,9 +328,9 @@ int cancel_branch( struct cell *t, int branch,
 	DBG("DEBUG: cancel_branch: sending cancel...\n");
 	if (SEND_BUFFER( crb )>=0){
 		if (unlikely (has_tran_tmcbs(t, TMCB_REQUEST_OUT)))
-			run_trans_callbacks_with_buf(TMCB_REQUEST_OUT, crb, 0, 0, TMCB_LOCAL_F);
+			run_trans_callbacks_with_buf(TMCB_REQUEST_OUT, crb, t->uas.request, 0, TMCB_LOCAL_F);
 		if (unlikely (has_tran_tmcbs(t, TMCB_REQUEST_SENT)))
-			run_trans_callbacks_with_buf(TMCB_REQUEST_SENT, crb, 0, 0, TMCB_LOCAL_F);
+			run_trans_callbacks_with_buf(TMCB_REQUEST_SENT, crb, t->uas.request, 0, TMCB_LOCAL_F);
 	}
 	/*sets and starts the FINAL RESPONSE timer */
 	if (start_retr( crb )!=0)
