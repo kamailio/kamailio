e593683639abe5192a5da0ac1f3b4a35c45e00f3
Daniel-Constantin Mierla <miconda@gmail.com>
2011-12-23 09:35:25 +0100

    srdb1: schema update for address table of permissions module
    - kamctl db scripts updated accordingly

--- utils/kamctl/db_sqlite/permissions-create.sql
+++ utils/kamctl/db_sqlite/permissions-create.sql
@@ -9,12 +9,12 @@ CREATE TABLE trusted (
 
 CREATE INDEX trusted_peer_idx ON trusted (src_ip);
 
-INSERT INTO version (table_name, table_version) values ('address','4');
+INSERT INTO version (table_name, table_version) values ('address','5');
 CREATE TABLE address (
     id INTEGER PRIMARY KEY NOT NULL,
-    grp SMALLINT DEFAULT 1 NOT NULL,
-    ip_addr VARCHAR(15) NOT NULL,
-    mask SMALLINT DEFAULT 32 NOT NULL,
+    grp INTEGER DEFAULT 1 NOT NULL,
+    ip_addr VARCHAR(48) NOT NULL,
+    mask INTEGER DEFAULT 32 NOT NULL,
     port SMALLINT DEFAULT 0 NOT NULL,
     tag VARCHAR(64)
 );
--- utils/kamctl/mysql/permissions-create.sql
+++ utils/kamctl/mysql/permissions-create.sql
@@ -9,12 +9,12 @@ CREATE TABLE trusted (
 
 CREATE INDEX peer_idx ON trusted (src_ip);
 
-INSERT INTO version (table_name, table_version) values ('address','4');
+INSERT INTO version (table_name, table_version) values ('address','5');
 CREATE TABLE address (
     id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    grp SMALLINT(5) UNSIGNED DEFAULT 1 NOT NULL,
-    ip_addr VARCHAR(15) NOT NULL,
-    mask TINYINT DEFAULT 32 NOT NULL,
+    grp INT(11) UNSIGNED DEFAULT 1 NOT NULL,
+    ip_addr VARCHAR(48) NOT NULL,
+    mask INT DEFAULT 32 NOT NULL,
     port SMALLINT(5) UNSIGNED DEFAULT 0 NOT NULL,
     tag VARCHAR(64)
 ) ENGINE=MyISAM;
