b347fd4ff4d1be946dba6e89d21a18bebe3c04da
Daniel-Constantin Mierla <miconda@gmail.com>
2012-03-12 18:08:53 +0100

    core: sip parser - added internal flags to mark replacement of To/From
    - used by uac module when updating From/To headers
    - part of patch by Jasmin Schnatterbeck

--- parser/msg_parser.h
+++ parser/msg_parser.h
@@ -119,6 +119,8 @@ enum request_method {
 #define FL_MTU_SCTP_FB  (1 << 10)
 #define FL_ADD_LOCAL_RPORT  (1 << 11) /*!< add 'rport' to local via hdr */
 #define FL_SDP_BODY     (1 << 12)  /*!< msg has SDP in body */
+#define FL_USE_UAC_FROM      (1<<13)  /* take FROM hdr from UAC instead of UAS*/
+#define FL_USE_UAC_TO        (1<<14)  /* take TO hdr from UAC instead of UAS */
 
 /* WARNING: Value (1 << 28) is temporarily reserved for use in kamailio call_control
  * module (flag  FL_USE_CALL_CONTROL )! */
