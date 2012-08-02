fbd4acae2d732908a9624c727b8b0306e1c166cd
Daniel-Constantin Mierla <miconda@gmail.com>
2012-08-02 10:13:00 +0200

    core: Use Colin Plumb's MD5 implementation
    Origin: http://anonscm.debian.org/gitweb/?p=dpkg/dpkg.git;a=summary
    Author: Tzafrir Cohen <tzafrir@debian.org>
    
    Borrowed the md5 code from dpkg instead of the existing md5.[ch]
    
    This makes the code compatible with Debian packing rules and
    restrictions regarding license for distribution
    (cherry picked from commit e6d912ad036aa92604ba3f2bf65cc4c4ba3e4d2f)

--- md5.h
+++ md5.h
@@ -1,102 +1,34 @@
+/*	$OpenBSD: md5.h,v 1.15 2004/05/03 17:30:14 millert Exp $	*/
+
 /*
- * Copyright (C) 1991-2, RSA Data Security, Inc. Created 1991. All
- * rights reserved.
- * 
- * License to copy and use this software is granted provided that it
- * is identified as the "RSA Data Security, Inc. MD5 Message-Digest
- * Algorithm" in all material mentioning or referencing this software
- * or this function.
- * 
- * License is also granted to make and use derivative works provided
- * that such works are identified as "derived from the RSA Data
- * Security, Inc. MD5 Message-Digest Algorithm" in all material
- * mentioning or referencing the derived work.
- * 
- * RSA Data Security, Inc. makes no representations concerning either
- * the merchantability of this software or the suitability of this
- * software for any particular purpose. It is provided "as is"
- * without express or implied warranty of any kind.
- * 
- * These notices must be retained in any copies of any part of this
- * documentation and/or software.
+ * This code implements the MD5 message-digest algorithm.
+ * The algorithm is due to Ron Rivest.  This code was
+ * written by Colin Plumb in 1993, no copyright is claimed.
+ * This code is in the public domain; do with it what you wish.
+ *
+ * Equivalent code is available from RSA Data Security, Inc.
+ * This code has been tested against that, and is equivalent,
+ * except that you don't need to include two pages of legalese
+ * with every copy.
  */
 
-/*!
- * \file
- * \brief SIP-router core :: md5 hash support
- * \ingroup core
- * Module: \ref core
- */
+#ifndef _MD5_H_
+#define _MD5_H_
 
-#ifndef MD5_H
-#define MD5_H
+#define	MD5_BLOCK_LENGTH		64
+#define	MD5_DIGEST_LENGTH		16
+#define	MD5_DIGEST_STRING_LENGTH	(MD5_DIGEST_LENGTH * 2 + 1)
 
-/**
- * \brief MD5 context
- */
-typedef struct {
-  unsigned int state[4];                                   /* state (ABCD) */
-  unsigned int count[2];        /* number of bits, modulo 2^64 (lsb first) */
-  unsigned char buffer[64];                         /* input buffer */
+typedef struct MD5Context {
+	u_int32_t state[4];			/* state */
+	u_int64_t count;			/* number of bits, mod 2^64 */
+	u_int8_t buffer[MD5_BLOCK_LENGTH];	/* input buffer */
 } MD5_CTX;
 
-/**
- * \brief MD5 context initialization
- * 
- * MD5 context initialization. Begins an MD5 operation, writing a new context.
- * \param context initialized context
- */
-void MD5Init (MD5_CTX *context);
+void	 MD5Init(MD5_CTX *);
+void	 MD5Update(MD5_CTX *, const u_int8_t *, size_t);
+void	 MD5Pad(MD5_CTX *);
+void	 MD5Final(u_int8_t [MD5_DIGEST_LENGTH], MD5_CTX *);
+void	 MD5Transform(u_int32_t [4], const u_int8_t [MD5_BLOCK_LENGTH]);
 
-/**
- * \brief MD5 block update operation
- * 
- * MD5 block update operation. Continues an MD5 message-digest
- * operation, processing another message block, and updating the
- * context.
- * \param context context
- * \param input input block
- * \param inputLen length of input block
- */
-void U_MD5Update (MD5_CTX *context, unsigned char *input, unsigned int inputLen);
-
-  /**
- * \brief MD5 finalization
- * 
- * MD5 finalization. Ends an MD5 message-digest operation, writing the
- * the message digest and zeroizing the context.
- * \param digest message digest
- * \param context context
- */
-void U_MD5Final (unsigned char digest[16], MD5_CTX *context);
-
-/*!
- * \brief Small wrapper around MD5Update
- *
- * Small wrapper around MD5Update, because everybody uses this on 'str' types
- * \param context MD5 context
- * \param input input block
- * \param inputLen length of input block
- * \note please not use this in new code
- * \todo review and fix all wrong usage
- */
-static inline void MD5Update (MD5_CTX *context, char *input, unsigned int inputLen)
-{
-	return U_MD5Update(context, (unsigned char *)input, inputLen);
-}
-
-/*!
- * \brief Small wrapper around MD5Final
- *
- * Small wrapper around MD5Final, because everybody uses this on 'str' types
- * \param digest message digest
- * \param context MD5 context
- * \note please not use this in new code
- * \todo review and fix all wrong usage
- */
-static inline void MD5Final (char digest[16], MD5_CTX *context)
-{
-	U_MD5Final((unsigned char *)digest, context);
-}
-
-#endif /* MD5_H */
+#endif /* _MD5_H_ */
