f728283da3473a80e65a9d32c36d0a5dc580a3cb
Andrew Mortensen <admorten@isc.upenn.edu>
2012-08-06 16:26:06 -0400

    Fix implementation by pointing dialog members to correct offsets.

--- sca_dialog.c
+++ sca_dialog.c
@@ -25,10 +25,18 @@ sca_dialog_build_from_tags( sca_dialog *dialog, int maxlen, str *call_id,
     }
 
     memcpy( dialog->id.s, call_id->s, call_id->len );
+    dialog->call_id.s = dialog->id.s;
+    dialog->call_id.len = call_id->len;
+
     memcpy( dialog->id.s + call_id->len, from_tag->s, from_tag->len );
+    dialog->from_tag.s = dialog->id.s + call_id->len;
+    dialog->from_tag.len = from_tag->len;
+
     if ( !SCA_STR_EMPTY( to_tag )) {
 	memcpy( dialog->id.s + call_id->len + from_tag->len,
 		to_tag->s, to_tag->len );
+	dialog->to_tag.s = dialog->id.s + call_id->len + from_tag->len;
+	dialog->to_tag.len = to_tag->len;
     }
     dialog->id.len = len;
 
