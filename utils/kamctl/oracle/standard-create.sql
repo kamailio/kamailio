e09b5bc8f7d17d8d9d9754152d78b0c835e94a9f
Daniel-Constantin Mierla <miconda@gmail.com>
2012-09-02 13:36:55 +0200

    kamctl: regenerated db creation scripts

--- utils/kamctl/oracle/standard-create.sql
+++ utils/kamctl/oracle/standard-create.sql
@@ -1,6 +1,7 @@
 CREATE TABLE version (
     table_name VARCHAR2(32),
-    table_version NUMBER(10) DEFAULT 0 NOT NULL
+    table_version NUMBER(10) DEFAULT 0 NOT NULL,
+    CONSTRAINT version_table_name_idx  UNIQUE (table_name)
 );
 
 BEGIN map2users('version'); END;
--- utils/kamctl/postgres/standard-create.sql
+++ utils/kamctl/postgres/standard-create.sql
@@ -1,5 +1,6 @@
 CREATE TABLE version (
     table_name VARCHAR(32) NOT NULL,
-    table_version INTEGER DEFAULT 0 NOT NULL
+    table_version INTEGER DEFAULT 0 NOT NULL,
+    CONSTRAINT version_table_name_idx UNIQUE (table_name)
 );
 
