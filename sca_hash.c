c43bb19418cbf6a80dcd935491d02d891e5651a6
Andrew Mortensen <admorten@isc.upenn.edu>
2012-10-11 16:21:36 -0400

    Remove debug sanity check.

--- sca_hash.c
+++ sca_hash.c
@@ -228,12 +228,6 @@ sca_hash_table_slot_unlink_entry_unsafe( sca_hash_slot *slot,
 	}
     }
 
-    for ( cur_e = &slot->entries; *cur_e != NULL; cur_e = &(*cur_e)->next ) {
-	if ( *cur_e == e ) {
-LM_INFO( "@@@@@@@@ ADMORTEN DEBUG: unlink didn't unlink!" );
-	}
-    }
-
     return( e );
 }
 
