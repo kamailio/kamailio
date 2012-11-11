79e03e5622ec28a5577bed700ee416b9630c7d58
Andrew Mortensen <admorten@isc.upenn.edu>
2012-11-10 20:36:54 -0500

    event should be INT. subscriber, not aor, should be UNIQUE.

--- sca.sql
+++ sca.sql
@@ -3,7 +3,7 @@ CREATE TABLE sca_subscriptions (
     id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
     subscriber VARCHAR(255) NOT NULL,
     aor VARCHAR(255) NOT NULL,
-    event VARCHAR(16) NOT NULL,
+    event INT(1) NOT NULL,
     expires INT(11) NOT NULL,
     state INT(1) NOT NULL,
     app_idx INT(4) NOT NULL,
@@ -13,5 +13,5 @@ CREATE TABLE sca_subscriptions (
     notify_cseq INT(11) NOT NULL,
     subscribe_cseq INT(11) NOT NULL,
 
-    CONSTRAINT sca_subscriptions_idx UNIQUE (aor, call_id, from_tag, to_tag)
+    CONSTRAINT sca_subscriptions_idx UNIQUE (subscriber, call_id, from_tag, to_tag)
 ) ENGINE=MyISAM;
