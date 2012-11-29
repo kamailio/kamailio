aca01e149445a0c6f87a9ed97974bcba612e426f
Ovidiu Sas <osas@voipembedded.com>
2012-11-29 09:57:48 -0500

    sca: adding missing sca db utils files

---
+++ utils/kamctl/db_sqlite/sca-create.sql
@@ -0,0 +1,20 @@
+INSERT INTO version (table_name, table_version) values ('sca_subscriptions','0');
+CREATE TABLE sca_subscriptions (
+    id INTEGER PRIMARY KEY NOT NULL,
+    subscriber VARCHAR(255) NOT NULL,
+    aor VARCHAR(255) NOT NULL,
+    event INTEGER DEFAULT 0 NOT NULL,
+    expires INTEGER DEFAULT 0 NOT NULL,
+    state INTEGER DEFAULT 0 NOT NULL,
+    app_idx INTEGER DEFAULT 0 NOT NULL,
+    call_id VARCHAR(255) NOT NULL,
+    from_tag VARCHAR(64) NOT NULL,
+    to_tag VARCHAR(64) NOT NULL,
+    notify_cseq INTEGER NOT NULL,
+    subscribe_cseq INTEGER NOT NULL,
+    CONSTRAINT sca_subscriptions_sca_subscriptions_idx UNIQUE (subscriber, call_id, from_tag, to_tag)
+);
+
+CREATE INDEX sca_subscriptions_sca_expires_idx ON sca_subscriptions (expires);
+CREATE INDEX sca_subscriptions_sca_subscribers_idx ON sca_subscriptions (subscriber, event);
+
