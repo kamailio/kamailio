c26533bb2b7f06e72366f2d920dc1e63d3eb159f
Elena-Ramona Modroiu <ramona@asipto.com>
2012-12-05 19:03:25 +0100

    modules_s/xprint: update defined value in header file

--- modules_s/xprint/xp_lib.h
+++ modules_s/xprint/xp_lib.h
@@ -25,8 +25,8 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#ifndef _XL_LIB_H_
-#define _XL_LIB_H_
+#ifndef _XP_LIB_H_
+#define _XP_LIB_H_
 
 #include "../../parser/msg_parser.h"
 
