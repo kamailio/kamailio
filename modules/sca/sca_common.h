6d0f635b0bbf338fca0ddc2c7d8e79bc686e5040
Andrew Mortensen <admorten@isc.upenn.edu>
2012-12-05 14:00:12 -0500

    sca: modify SCA_STR_EMPTY macro to fix -Waddress compiler warnings.
    - Reported by Ovidiu Sas

--- modules/sca/sca_common.h
+++ modules/sca/sca_common.h
@@ -93,7 +93,8 @@
 		memcmp((str1)->s, (str2)->s, (str1)->len) == 0)
 
 #define SCA_STR_EMPTY( str1 ) \
-	((str1) == NULL || (str1)->s == NULL || (str1)->len <= 0 )
+	(((str1) != NULL && ((str1)->s == NULL || (str1)->len <= 0 )) \
+		|| (str1) == NULL )
 
 #define SCA_HEADER_EMPTY( hdr1 ) \
 	((hdr1) == NULL || SCA_STR_EMPTY( &(hdr1)->body ))
