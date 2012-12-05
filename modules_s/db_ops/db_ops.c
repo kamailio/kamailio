88b7ea9f926605c98676dd6b039965dff295d9c7
Elena-Ramona Modroiu <ramona@asipto.com>
2012-12-05 19:09:37 +0100

    modules_s/db_ops: use xprint module instead of xlog

--- modules_s/db_ops/db_ops.c
+++ modules_s/db_ops/db_ops.c
@@ -38,7 +38,7 @@
 #include "../../lib/srdb2/db.h"
 #include "../../select.h"
 #include "../../script_cb.h"
-#include "../xlog/xl_lib.h"
+#include "../xprint/xp_lib.h"
 #include "../../route.h"
 #include "../../action.h"
 #include "../../ut.h"
