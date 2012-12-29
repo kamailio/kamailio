907e13b7080c3ecbbbf8fa35c1adb4b3d9137ff5
Daniel-Constantin Mierla <miconda@gmail.com>
2012-12-29 16:15:07 +0100

    srdb1/kamctl: added uid_uri and uid_uri_attrs tables

---
+++ utils/kamctl/postgres/uid_uri_db-create.sql
@@ -0,0 +1,26 @@
+INSERT INTO version (table_name, table_version) values ('uid_uri','3');
+CREATE TABLE uid_uri (
+    id SERIAL PRIMARY KEY NOT NULL,
+    uid VARCHAR(64) NOT NULL,
+    did VARCHAR(64) NOT NULL,
+    username VARCHAR(64) NOT NULL,
+    flags INTEGER DEFAULT 0 NOT NULL,
+    scheme VARCHAR(8) DEFAULT 'sip' NOT NULL
+);
+
+CREATE INDEX uid_uri_uri_idx1 ON uid_uri (username, did, scheme);
+CREATE INDEX uid_uri_uri_uid ON uid_uri (uid);
+
+INSERT INTO version (table_name, table_version) values ('uid_uri_attrs','2');
+CREATE TABLE uid_uri_attrs (
+    id SERIAL PRIMARY KEY NOT NULL,
+    username VARCHAR(64) NOT NULL,
+    did VARCHAR(64) NOT NULL,
+    name VARCHAR(32) NOT NULL,
+    value VARCHAR(128),
+    type INTEGER DEFAULT 0 NOT NULL,
+    flags INTEGER DEFAULT 0 NOT NULL,
+    scheme VARCHAR(8) DEFAULT 'sip' NOT NULL,
+    CONSTRAINT uid_uri_attrs_uriattrs_idx UNIQUE (username, did, name, value, scheme)
+);
+
