e5831f5896e79ad5b878208a408d61fb374f2715
Daniel-Constantin Mierla <miconda@gmail.com>
2012-12-29 16:39:29 +0100

    libsrdb1/kamctl: added uid_global_attrs table

---
+++ utils/kamctl/db_sqlite/uid_gflags-create.sql
@@ -0,0 +1,10 @@
+INSERT INTO version (table_name, table_version) values ('uid_global_attrs','1');
+CREATE TABLE uid_global_attrs (
+    id INTEGER PRIMARY KEY NOT NULL,
+    name VARCHAR(32) NOT NULL,
+    type INTEGER DEFAULT 0 NOT NULL,
+    value VARCHAR(128),
+    flags INTEGER DEFAULT 0 NOT NULL,
+    CONSTRAINT uid_global_attrs_global_attrs_idx UNIQUE (name, value)
+);
+
