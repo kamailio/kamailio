1ecbbbbe2c2ee63553b47316f454ede3633a2189
Jason Penton <jason.penton@smilecoms.com>
2012-12-30 20:29:22 +0200

    modules/auth_ims: first commit of auth_ims module 	- This module provides IMS specific authentication/authorisation functionality.

---
+++ modules/auth_ims/rfc2617.c
@@ -0,0 +1,189 @@
+/*
+ * $Id$
+ *
+ * Copyright (C) 2012 Smile Communications, jason.penton@smilecoms.com
+ * Copyright (C) 2012 Smile Communications, richard.good@smilecoms.com
+ * 
+ * The initial version of this code was written by Dragos Vingarzan
+ * (dragos(dot)vingarzan(at)fokus(dot)fraunhofer(dot)de and the
+ * Fruanhofer Institute. It was and still is maintained in a separate
+ * branch of the original SER. We are therefore migrating it to
+ * Kamailio/SR and look forward to maintaining it from here on out.
+ * 2011/2012 Smile Communications, Pty. Ltd.
+ * ported/maintained/improved by 
+ * Jason Penton (jason(dot)penton(at)smilecoms.com and
+ * Richard Good (richard(dot)good(at)smilecoms.com) as part of an 
+ * effort to add full IMS support to Kamailio/SR using a new and
+ * improved architecture
+ * 
+ * NB: Alot of this code was originally part of OpenIMSCore,
+ * FhG Fokus. 
+ * Copyright (C) 2004-2006 FhG Fokus
+ * Thanks for great work! This is an effort to 
+ * break apart the various CSCF functions into logically separate
+ * components. We hope this will drive wider use. We also feel
+ * that in this way the architecture is more complete and thereby easier
+ * to manage in the Kamailio/SR environment
+ *
+ * This file is part of Kamailio, a free SIP server.
+ *
+ * Kamailio is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version
+ *
+ * Kamailio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software 
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ */
+
+
+#include <sys/types.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "rfc2617.h"
+#include "../../md5.h"
+#include "../../dprint.h"
+
+
+inline void cvt_hex(HASH _b, HASHHEX _h)
+{
+	unsigned short i;
+	unsigned char j;
+	
+	for (i = 0; i < HASHLEN; i++) {
+		j = (_b[i] >> 4) & 0xf;
+		if (j <= 9) {
+			_h[i * 2] = (j + '0');
+		} else {
+			_h[i * 2] = (j + 'a' - 10);
+		}
+
+		j = _b[i] & 0xf;
+
+		if (j <= 9) {
+			_h[i * 2 + 1] = (j + '0');
+		} else {
+			_h[i * 2 + 1] = (j + 'a' - 10);
+		}
+	};
+
+	_h[HASHHEXLEN] = '\0';
+}
+
+
+/* 
+ * calculate H(A1) as per spec 
+ */
+void calc_HA1(ha_alg_t _alg, str* _username, str* _realm, str* _password,
+	      str* _nonce, str* _cnonce, HASHHEX _sess_key)
+{
+	MD5_CTX Md5Ctx;
+	HASH HA1;
+	
+	MD5Init(&Md5Ctx);
+	MD5Update(&Md5Ctx, _username->s, _username->len);
+	MD5Update(&Md5Ctx, ":", 1);
+	MD5Update(&Md5Ctx, _realm->s, _realm->len);
+	MD5Update(&Md5Ctx, ":", 1);
+	MD5Update(&Md5Ctx, _password->s, _password->len);
+	MD5Final(HA1, &Md5Ctx);
+
+	if (_alg == HA_MD5_SESS) {
+		MD5Init(&Md5Ctx);
+		MD5Update(&Md5Ctx, HA1, HASHLEN);
+		MD5Update(&Md5Ctx, ":", 1);
+		MD5Update(&Md5Ctx, _nonce->s, _nonce->len);
+		MD5Update(&Md5Ctx, ":", 1);
+		MD5Update(&Md5Ctx, _cnonce->s, _cnonce->len);
+		MD5Final(HA1, &Md5Ctx);
+	};
+
+	cvt_hex(HA1, _sess_key);
+}
+
+void calc_H(str *ent, HASHHEX hash)
+{
+	MD5_CTX Md5Ctx;
+	HASH HA1;
+	MD5Init(&Md5Ctx);
+	MD5Update(&Md5Ctx, ent->s, ent->len);
+	MD5Final(HA1, &Md5Ctx);
+	cvt_hex(HA1, hash);
+}
+
+
+/* 
+ * calculate request-digest/response-digest as per HTTP Digest spec 
+ */
+void calc_response(HASHHEX _ha1,      /* H(A1) */
+		   str* _nonce,       /* nonce from server */
+		   str* _nc,          /* 8 hex digits */
+		   str* _cnonce,      /* client nonce */
+		   str* _qop,         /* qop-value: "", "auth", "auth-int" */
+		   int _auth_int,     /* 1 if auth-int is used */
+		   str* _method,      /* method from the request */
+		   str* _uri,         /* requested URL */
+		   HASHHEX _hentity,  /* H(entity body) if qop="auth-int" */
+		   HASHHEX _response) /* request-digest or response-digest */
+{
+	LM_DBG("calc_response(_ha1=%.*s, _nonce=%.*s, _nc=%.*s,_cnonce=%.*s, _qop=%.*s, _auth_int=%d,_method=%.*s,_uri=%.*s,_hentity=%.*s)\n",
+			HASHHEXLEN, _ha1,
+			_nonce->len, _nonce->s,
+			_nc->len, _nc->s,
+			_cnonce->len, _cnonce->s,
+			_qop->len, _qop->s,
+			_auth_int,
+			_method ? _method->len : 4, _method ? _method->s : "null",
+			_uri->len, _uri->s,
+			_auth_int ? HASHHEXLEN : 0, _hentity);
+
+	MD5_CTX Md5Ctx;
+	HASH HA2;
+	HASH RespHash;
+	HASHHEX HA2Hex;
+	
+	     /* calculate H(A2) */
+	MD5Init(&Md5Ctx);
+	if (_method) { /* _method is NULL when calculating H(A2) for rspauth in Authentication-Info */
+		MD5Update(&Md5Ctx, _method->s, _method->len);
+	}
+	MD5Update(&Md5Ctx, ":", 1);
+	MD5Update(&Md5Ctx, _uri->s, _uri->len);
+
+	if (_auth_int) {
+		MD5Update(&Md5Ctx, ":", 1);
+		MD5Update(&Md5Ctx, _hentity, HASHHEXLEN);
+	};
+
+	MD5Final(HA2, &Md5Ctx);
+	cvt_hex(HA2, HA2Hex);
+
+	     /* calculate response */
+	MD5Init(&Md5Ctx);
+	MD5Update(&Md5Ctx, _ha1, HASHHEXLEN);
+	MD5Update(&Md5Ctx, ":", 1);
+	MD5Update(&Md5Ctx, _nonce->s, _nonce->len);
+	MD5Update(&Md5Ctx, ":", 1);
+
+	if (_qop->len) {
+		MD5Update(&Md5Ctx, _nc->s, _nc->len);
+		MD5Update(&Md5Ctx, ":", 1);
+		MD5Update(&Md5Ctx, _cnonce->s, _cnonce->len);
+		MD5Update(&Md5Ctx, ":", 1);
+		MD5Update(&Md5Ctx, _qop->s, _qop->len);
+		MD5Update(&Md5Ctx, ":", 1);
+	};
+	MD5Update(&Md5Ctx, HA2Hex, HASHHEXLEN);
+	MD5Final(RespHash, &Md5Ctx);
+	cvt_hex(RespHash, _response);
+	LM_DBG("H(A1) = %.*s, H(A2) = %.*s, rspauth = %.*s\n",
+			HASHHEXLEN, _ha1, HASHHEXLEN, HA2Hex, HASHHEXLEN, _response);
+}
---
+++ modules/auth_ims/utils.c
@@ -0,0 +1,404 @@
+/*
+ * $Id$
+ *
+ * Copyright (C) 2012 Smile Communications, jason.penton@smilecoms.com
+ * Copyright (C) 2012 Smile Communications, richard.good@smilecoms.com
+ * 
+ * The initial version of this code was written by Dragos Vingarzan
+ * (dragos(dot)vingarzan(at)fokus(dot)fraunhofer(dot)de and the
+ * Fruanhofer Institute. It was and still is maintained in a separate
+ * branch of the original SER. We are therefore migrating it to
+ * Kamailio/SR and look forward to maintaining it from here on out.
+ * 2011/2012 Smile Communications, Pty. Ltd.
+ * ported/maintained/improved by 
+ * Jason Penton (jason(dot)penton(at)smilecoms.com and
+ * Richard Good (richard(dot)good(at)smilecoms.com) as part of an 
+ * effort to add full IMS support to Kamailio/SR using a new and
+ * improved architecture
+ * 
+ * NB: Alot of this code was originally part of OpenIMSCore,
+ * FhG Fokus. 
+ * Copyright (C) 2004-2006 FhG Fokus
+ * Thanks for great work! This is an effort to 
+ * break apart the various CSCF functions into logically separate
+ * components. We hope this will drive wider use. We also feel
+ * that in this way the architecture is more complete and thereby easier
+ * to manage in the Kamailio/SR environment
+ *
+ * This file is part of Kamailio, a free SIP server.
+ *
+ * Kamailio is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version
+ *
+ * Kamailio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software 
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ */
+
+
+#include "utils.h"
+
+/*
+ * Find credentials with given realm in a SIP message header
+ */
+inline int ims_find_credentials(struct sip_msg* _m, str* _realm,
+		hdr_types_t _hftype, struct hdr_field** _h) {
+	struct hdr_field** hook, *ptr, *prev;
+	hdr_flags_t hdr_flags;
+	int res;
+	str* r;
+
+	LM_DBG("Searching credentials in realm [%.*s]\n", _realm->len, _realm->s);
+
+	/*
+	 * Determine if we should use WWW-Authorization or
+	 * Proxy-Authorization header fields, this parameter
+	 * is set in www_authorize and proxy_authorize
+	 */
+	switch (_hftype) {
+	case HDR_AUTHORIZATION_T:
+		hook = &(_m->authorization);
+		hdr_flags = HDR_AUTHORIZATION_F;
+		break;
+	case HDR_PROXYAUTH_T:
+		hook = &(_m->proxy_auth);
+		hdr_flags = HDR_PROXYAUTH_F;
+		break;
+	default:
+		hook = &(_m->authorization);
+		hdr_flags = HDR_T2F(_hftype);
+		break;
+	}
+
+	/*
+	 * If the credentials haven't been parsed yet, do it now
+	 */
+	if (*hook == 0) {
+		/* No credentials parsed yet */
+		LM_DBG("*hook == 0, No credentials parsed yet\n");
+		if (parse_headers(_m, hdr_flags, 0) == -1) {
+			LM_ERR("Error while parsing headers\n");
+			return -1;
+		}
+	}
+
+	ptr = *hook;
+	LM_DBG("*hook = %p\n", ptr);
+	/*
+	 * Iterate through the credentials in the message and
+	 * find credentials with given realm
+	 */
+	while (ptr) {
+		res = parse_credentials(ptr);
+		if (res < 0) {
+			LM_ERR("Error while parsing credentials\n");
+			return (res == -1) ? -2 : -3;
+		} else if (res == 0) {
+			LM_DBG("Credential parsed successfully\n");
+			if (_realm->len) {
+				r = &(((auth_body_t*) (ptr->parsed))->digest.realm);
+				LM_DBG("Comparing realm <%.*s> and <%.*s>\n", _realm->len, _realm->s, r->len, r->s);
+				if (r->len == _realm->len) {
+					if (!strncasecmp(_realm->s, r->s, r->len)) {
+						*_h = ptr;
+						return 0;
+					}
+				}
+			} else {
+				*_h = ptr;
+				return 0;
+			}
+
+		}
+
+		prev = ptr;
+		if (parse_headers(_m, hdr_flags, 1) == -1) {
+			LM_ERR("Error while parsing headers\n");
+			return -4;
+		} else {
+			if (prev != _m->last_header) {
+				if (_m->last_header->type == _hftype)
+					ptr = _m->last_header;
+				else
+					break;
+			} else
+				break;
+		}
+	}
+
+	/*
+	 * Credentials with given realm not found
+	 */
+	LM_DBG("Credentials with given realm not found\n");
+	return 1;
+}
+
+
+/**
+ * Returns the Private Identity extracted from the Authorization header.
+ * If none found there takes the SIP URI in To without the "sip:" prefix
+ * \todo - remove the fallback case to the To header
+ * @param msg - the SIP message
+ * @param realm - the realm to match in an Authorization header
+ * @param is_proxy_auth 0 if the header is Authorization, anything else for Proxy-Authorization
+ * @returns the str containing the private id, no mem dup
+ */
+str get_private_identity(struct sip_msg *msg, str realm, int is_proxy_auth)
+{
+	str pi={0,0};
+	struct hdr_field* h=0;
+	int ret,i;
+
+	if (parse_headers(msg, is_proxy_auth ? HDR_PROXYAUTH_F : HDR_AUTHORIZATION_F,0)!=0) {
+		return pi;
+	}
+
+	if (!(is_proxy_auth ? msg->proxy_auth : msg->authorization)){
+		goto fallback;
+	}
+
+	ret = find_credentials(msg, &realm, is_proxy_auth ? HDR_PROXYAUTH_F : HDR_AUTHORIZATION_F, &h);
+	if (ret < 0) {
+		goto fallback;
+	} else 
+		if (ret > 0) {
+			goto fallback;
+		}
+	
+	if (h) pi=((auth_body_t*)h->parsed)->digest.username.whole;
+
+	goto done;
+		
+fallback:
+	pi = get_public_identity(msg);
+	if (pi.len>4&&strncasecmp(pi.s,"sip:",4)==0) {pi.s+=4;pi.len-=4;}
+	for(i=0;i<pi.len;i++)
+		if (pi.s[i]==';') {
+			pi.len=i;
+			break;
+		}
+done:	
+	return pi;	
+}
+
+/**
+ * Returns the Public Identity extracted from the From header
+ * @param msg - the SIP message
+ * @returns the str containing the public id, no mem dup
+ */
+str get_public_identity(struct sip_msg *msg)
+{
+	str pu={0,0};
+	struct to_body *from;
+	int i;
+	
+	if (parse_headers(msg,HDR_TO_F,0)!=0) {
+		return pu;
+	}
+	
+	if ( get_from(msg) == NULL ) {
+		from = (struct to_body*) pkg_malloc(sizeof(struct to_body));
+		if (!from) {
+			LM_ERR("out of pkg memory\n");
+			return pu;
+		}
+		parse_to( msg->from->body.s, msg->from->body.s + msg->from->body.len, from );
+        msg->from->parsed = from;
+	}
+	else from=(struct to_body *) msg->from->parsed;
+
+	pu = from->uri;
+	
+	/* truncate to sip:username@host or tel:number */
+	for(i=4;i<pu.len;i++)
+		if (pu.s[i]==';' || pu.s[i]=='?' ||pu.s[i]==':'){
+			pu.len = i;
+		}
+	return pu;
+}
+
+/**
+ * Looks for the nonce and response parameters in the Authorization header and returns them
+ * @param msg - the SIP message
+ * @param realm - realm to match the right Authorization header
+ * @param nonce - param to fill with the nonce found
+ * @param response - param to fill with the response
+ * @returns 1 if found, 0 if not
+ */
+int get_nonce_response(struct sip_msg *msg, str realm,str *nonce,str *response,
+	enum qop_type *qop,str *qop_str,str *nc,str *cnonce,str *uri, int is_proxy_auth)
+{
+	struct hdr_field* h = 0;
+	int ret;
+
+
+	ret = parse_headers(msg, is_proxy_auth ? HDR_PROXYAUTH_F : HDR_AUTHORIZATION_F, 0);
+
+	if (ret != 0) {
+		return 0;
+	}
+
+	if ((!is_proxy_auth && !msg->authorization)
+		|| (is_proxy_auth && !msg->proxy_auth)) {
+		return 0;
+	}
+
+	LM_DBG("Calling find_credentials with realm [%.*s]\n", realm.len, realm.s);
+	ret = ims_find_credentials(msg, &realm, is_proxy_auth ? HDR_PROXYAUTH_T : HDR_AUTHORIZATION_T, &h);
+	if (ret < 0) {
+		return 0;
+	} else if (ret > 0) {
+		LM_DBG("ret > 0");
+		return 0;
+	}
+
+	if (h && h->parsed) {
+		if (nonce)
+			*nonce = ((auth_body_t*) h->parsed)->digest.nonce;
+		if (response)
+			*response = ((auth_body_t*) h->parsed)->digest.response;
+		if (qop)
+			*qop = ((auth_body_t*) h->parsed)->digest.qop.qop_parsed;
+		if (qop_str)
+			*qop_str = ((auth_body_t*) h->parsed)->digest.qop.qop_str;
+		if (nc)
+			*nc = ((auth_body_t*) h->parsed)->digest.nc;
+		if (cnonce)
+			*cnonce = ((auth_body_t*) h->parsed)->digest.cnonce;
+		if (uri)
+			*uri = ((auth_body_t*) h->parsed)->digest.uri;
+	}
+	LM_DBG("Found nonce response\n");
+	return 1;
+}
+
+str ims_get_body(struct sip_msg * msg)
+{		
+	str x={0,0};
+	x.s = get_body(msg);	
+	if (x.s==0) return x;
+	if (parse_headers(msg,HDR_CONTENTLENGTH_F,0)!=0) {
+		LM_DBG("Error parsing until header Content-Length: \n");
+		return x;
+	}
+	if  (msg->content_length->parsed==NULL) {
+		LM_ERR(" body <%.*s>\n",msg->content_length->body.len,msg->content_length->body.s);
+		parse_content_length(msg->content_length->body.s,
+			msg->content_length->body.s+msg->content_length->body.len,&(x.len));
+		msg->content_length->parsed=(void*)(long)(x.len);
+	}else 
+		x.len = (long)msg->content_length->parsed;
+	return x;
+}
+
+
+/**
+ * Looks for the auts parameter in the Authorization header and returns its value.
+ * @param msg - the SIP message
+ * @param realm - realm to match the right Authorization header
+ * @returns the auts value or an empty string if not found
+ */
+str ims_get_auts(struct sip_msg *msg, str realm, int is_proxy_auth)
+{
+	str name={"auts=\"",6};
+	struct hdr_field* h=0;
+	int i,ret;
+	str auts={0,0};
+
+	if (parse_headers(msg, is_proxy_auth ? HDR_PROXYAUTH_F : HDR_AUTHORIZATION_F,0)!=0) {
+		LM_ERR("Error parsing until header Authorization: \n");
+		return auts;
+	}
+
+	if ((!is_proxy_auth && !msg->authorization)
+			|| (is_proxy_auth && !msg->proxy_auth)){
+		LM_ERR("Message does not contain Authorization nor Proxy-Authorization header.\n");
+		return auts;
+	}
+
+	ret = find_credentials(msg, &realm, is_proxy_auth ? HDR_PROXYAUTH_F : HDR_AUTHORIZATION_F, &h);
+	if (ret < 0) {
+		LM_ERR("Error while looking for credentials.\n");
+		return auts;
+	} else 
+		if (ret > 0) {
+			LM_ERR("No credentials for this realm found.\n");
+			return auts;
+		}
+	
+	if (h) {
+		for(i=0;i<h->body.len-name.len;i++)
+			if (strncasecmp(h->body.s+i,name.s,name.len)==0){
+				auts.s = h->body.s+i+name.len;
+				while(i+auts.len<h->body.len && auts.s[auts.len]!='\"')
+					auts.len++;
+			}
+	}
+	
+	return auts;	
+}
+
+/**
+ * Looks for the nonce parameter in the Authorization header and returns its value.
+ * @param msg - the SIP message
+ * @param realm - realm to match the right Authorization header
+ * @returns the nonce or an empty string if none found
+ */
+str ims_get_nonce(struct sip_msg *msg, str realm)
+{
+	struct hdr_field* h=0;
+	int ret;
+	str nonce={0,0};
+
+	if (parse_headers(msg,HDR_AUTHORIZATION_F,0)!=0) {
+		LM_ERR("Error parsing until header Authorization: \n");
+		return nonce;
+	}
+
+	if (!msg->authorization){
+		LM_ERR("Message does not contain Authorization header.\n");
+		return nonce;
+	}
+
+	ret = find_credentials(msg, &realm, HDR_AUTHORIZATION_F, &h);
+	if (ret < 0) {
+		LM_ERR("Error while looking for credentials.\n");
+		return nonce;
+	} else 
+		if (ret > 0) {
+			LM_ERR("No credentials for this realm found.\n");
+			return nonce;
+		}
+	
+	if (h&&h->parsed) {
+		nonce = ((auth_body_t*)h->parsed)->digest.nonce;
+	}
+	
+	return nonce;	
+}
+
+/**
+ * Adds a header to the reply message
+ * @param msg - the request to add a header to its reply
+ * @param content - the str containing the new header
+ * @returns 1 on succes, 0 on failure
+ */
+int ims_add_header_rpl(struct sip_msg *msg, str *hdr)
+{
+	if (add_lump_rpl( msg, hdr->s, hdr->len, LUMP_RPL_HDR)==0) {
+		LM_ERR("Can't add header <%.*s>\n",
+			hdr->len,hdr->s);
+ 		return 0;
+ 	}
+ 	return 1;
+}
+
+
