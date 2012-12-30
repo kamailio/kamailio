651dafa718cb96c15781134ed280812c664bd9dc
Jason Penton <jason.penton@smilecoms.com>
2012-12-30 20:19:10 +0200

    modules/dialog2: first version of dialog2

---
+++ modules/dialog2/dlg_req_within.h
@@ -0,0 +1,58 @@
+/*
+ * $Id$
+ *
+ * Copyright (C) 2007 Voice System SRL
+ *
+ * This file is part of Kamailio, a free SIP server.
+ *
+ * Kamailio is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version
+ *
+ * Kamailio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software 
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * History:
+ * --------
+ * 2007-07-10  initial version (ancuta)
+*/
+
+
+
+
+#ifndef DLG_REQUEST_WITHIN_H
+#define DLG_REQUEST_WITHIN_H
+
+#include "dlg_hash.h"
+#include "../../modules/tm/tm_load.h"
+
+#define MAX_FWD			"70"
+#define MAX_SIZE		256
+#define RCV_BYE_REPLY	1
+
+#define MI_DIALOG_NOT_FOUND 		"Requested Dialog not found"
+#define MI_DIALOG_NOT_FOUND_LEN 	(sizeof(MI_DIALOG_NOT_FOUND)-1)
+#define MI_DLG_OPERATION_ERR		"Operation failed"
+#define MI_DLG_OPERATION_ERR_LEN	(sizeof(MI_DLG_OPERATION_ERR)-1)
+
+extern struct tm_binds d_tmb;
+//extern int dlg_enable_stats;
+//extern stat_var * active_dlgs;
+
+struct mi_root * mi_terminate_dlg(struct mi_root *cmd_tree, void *param );
+
+dlg_t* build_dlg_t(struct dlg_cell * cell, int dir);
+int free_tm_dlg(dlg_t *td);
+int dlg_bye(struct dlg_cell *dlg, str *hdrs, int side);
+int dlg_bye_all(struct dlg_cell *dlg, str *hdrs);
+int w_api_terminate_dlg(str *call_id, str *from_tag, str *to_tag, str *hdrs, str* reason);
+int dlg_terminate(struct dlg_cell *dlg, struct sip_msg* msg, str *reason, int side, str *extra_headers);
+
+#endif
---
+++ modules/dialog2/dlg_var.h
@@ -0,0 +1,91 @@
+/**
+ * $Id$
+ *
+ * Copyright (C) 2009 Daniel-Constantin Mierla (asipto.com)
+ * Copyright (C) 2011 Carsten Bock, carsten@ng-voice.com
+ *
+ * This file is part of kamailio, a free SIP server.
+ *
+ * openser is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version
+ *
+ * openser is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+		       
+#ifndef _DLG_VAR_H_
+#define _DLG_VAR_H_
+
+#include "../../pvar.h"
+#include "dlg_hash.h"
+
+#define DLG_TOROUTE_SIZE	32
+/*! dialog context */
+typedef struct _dlg_ctx {
+	int on;
+	unsigned int flags;
+	int to_route;
+	char to_route_name[DLG_TOROUTE_SIZE];
+	int to_bye;
+	int timeout;
+	struct dlg_cell *dlg;
+	int set;
+	unsigned int dir;
+} dlg_ctx_t;
+
+/* A dialog-variable */
+struct dlg_var {
+	str key;
+	str value;
+	unsigned int vflags;		/*!< internal variable flags */
+	struct dlg_var *next;
+};
+
+str * api_get_dlg_variable(str *callid, str *ftag, str *ttag, str *key);
+str * get_dlg_variable(struct dlg_cell *dlg, str *key);
+
+int api_set_dlg_variable(str *callid, str *ftag, str *ttag, str *key, str *val);
+int set_dlg_variable(struct dlg_cell *dlg, str *key, str *val);
+
+int pv_parse_dialog_var_name(pv_spec_p sp, str *in);
+
+int pv_get_dlg_variable(struct sip_msg *msg, pv_param_t *param, pv_value_t *res);
+
+int pv_set_dlg_variable(struct sip_msg* msg, pv_param_t *param, int op, pv_value_t *val);
+
+/*! Retrieve the current var-list */
+struct dlg_var * get_local_varlist_pointer(struct sip_msg *msg, int clear_pointer);
+
+/* Adds, updates and deletes dialog variables */
+int set_dlg_variable_unsafe(struct dlg_cell *dlg, str *key, str *val, int new);
+
+extern dlg_ctx_t _dlg_ctx;
+
+int pv_get_dlg_ctx(struct sip_msg *msg,  pv_param_t *param,
+		pv_value_t *res);
+int pv_set_dlg_ctx(struct sip_msg* msg, pv_param_t *param,
+		int op, pv_value_t *val);
+int pv_parse_dlg_ctx_name(pv_spec_p sp, str *in);
+
+int pv_get_dlg(struct sip_msg *msg,  pv_param_t *param,
+		pv_value_t *res);
+int pv_parse_dlg_name(pv_spec_p sp, str *in);
+
+int dlg_cfg_cb(struct sip_msg *foo, unsigned int flags, void *bar);
+
+void dlg_set_ctx_dialog(struct dlg_cell *dlg);
+struct dlg_cell* dlg_get_ctx_dialog(void);
+
+dlg_ctx_t* dlg_get_dlg_ctx(void);
+
+int spiral_detect_reset(struct sip_msg *foo, unsigned int flags, void *bar);
+
+#endif
