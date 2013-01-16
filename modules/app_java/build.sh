ac2a3acddef3d5dd3a8033859f8a245624944d8e
Daniel-Constantin Mierla <miconda@gmail.com>
2013-01-16 22:28:30 +0100

    Revert "First commit (raw code) of new module: app_java (Java Native Interface support for Kamailio)."
    This reverts commit 3cc85a3915ed69471966cf741d7cf2adee4f3b0b.
    
    - code in master is frozen for v4.0.0

--- modules/app_java/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-INSTPREFIX=/opt/kamailio
-FLAVOUR=FLAVOUR=kamailio
-VERBOSE=Q=verbose
-
-rm -f /opt/kamailio/lib/kamailio/modules/app_java.so
-
-make clean && rm -f makecfg.lst *.d && make ${FLAVOUR} ${VERBOSE} && make ${FLAVOUR} install prefix=${INSTPREFIX} ${VERBOSE}
