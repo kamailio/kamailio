45b2ea3018cc84f8bcf9e687dcc0a03e81597711
Daniel-Constantin Mierla <miconda@gmail.com>
2013-02-24 20:04:51 +0100

    siputils: completed the function to return RURI parameter value
    - closes FS#238, based on a patch by Luis Martin

--- modules/siputils/siputils.c
+++ modules/siputils/siputils.c
@@ -110,6 +110,9 @@ static void mod_destroy(void);
 static int fixup_set_uri(void** param, int param_no);
 static int fixup_free_set_uri(void** param, int param_no);
 static int fixup_tel2sip(void** param, int param_no);
+static int fixup_get_uri_param(void** param, int param_no);
+static int free_fixup_get_uri_param(void** param, int param_no);
+
 
 char *contact_flds_separator = DEFAULT_SEPARATOR;
 
@@ -128,6 +131,8 @@ static cmd_export_t cmds[]={
 		0, REQUEST_ROUTE|LOCAL_ROUTE},
 	{"add_uri_param",      (cmd_function)add_uri_param,     1, fixup_str_null,
 		0, REQUEST_ROUTE},
+	{"get_uri_param",      (cmd_function)get_uri_param,     2, fixup_get_uri_param, 
+		free_fixup_get_uri_param, REQUEST_ROUTE|LOCAL_ROUTE},
 	{"tel2sip", (cmd_function)tel2sip, 3, fixup_tel2sip, 0,
 	 REQUEST_ROUTE|FAILURE_ROUTE|BRANCH_ROUTE|ONREPLY_ROUTE},
 	{"is_e164",            (cmd_function)is_e164,           1, fixup_pvar_null,
@@ -335,4 +340,38 @@ static int fixup_tel2sip(void** param, int param_no)
 
     LM_ERR("invalid parameter number <%d>\n", param_no);
     return -1;
+}
+
+/* */
+static int fixup_get_uri_param(void** param, int param_no) {
+	if (param_no == 1) {
+		return fixup_str_null(param, 1);
+	}
+	if (param_no == 2) {
+		if (fixup_pvar_null(param, 1) != 0) {
+			LM_ERR("failed to fixup result pvar\n");
+			return -1;
+		}
+		if (((pv_spec_t *)(*param))->setf == NULL) {
+			LM_ERR("result pvar is not writeble\n");
+			return -1;
+		}
+		return 0;
+	}
+
+	LM_ERR("invalid parameter number <%d>\n", param_no);
+	return -1;
+}
+
+/* */
+static int free_fixup_get_uri_param(void** param, int param_no) {
+	if (param_no == 1) {
+		LM_WARN("free function has not been defined for spve\n");
+		return 0;
+	}
+	if (param_no == 2) {
+		return fixup_free_pvar_null(param, 1);
+	}
+	LM_ERR("invalid parameter number <%d>\n", param_no);
+	return -1;
 }
