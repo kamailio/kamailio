61c706ef813520ce0ac6767e6be7ef6e52bd6b3a
Daniel-Constantin Mierla <miconda@gmail.com>
2013-03-24 13:07:39 +0100

    db_mysql: new module parameter - insert_delayed
    - if set to 1, then all INSERT API queries will be converted in INSERT
      DELAYED (only DB API insert, not for raw queries)

--- modules/db_mysql/mysql_mod.c
+++ modules/db_mysql/mysql_mod.c
@@ -107,6 +107,7 @@ static param_export_t params[] = {
 
 	{"timeout_interval", INT_PARAM, &db_mysql_timeout_interval},
 	{"auto_reconnect",   INT_PARAM, &db_mysql_auto_reconnect},
+	{"insert_delayed",   INT_PARAM, &db_mysql_insert_all_delayed},
 	{0, 0, 0}
 };
 
