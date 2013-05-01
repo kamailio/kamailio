d16ee1a19c681726b35da78f120a05317b294ce1
Andrew Mortensen <admorten@isc.upenn.edu>
2013-04-30 23:10:10 -0400

    modules/sca: track appearance times.
    Include time of last state chance in sca.all_appearances output.
    (cherry picked from commit da3f7cbd955311309665ab0e6958e009f183f795)

--- modules/sca/sca_rpc.c
+++ modules/sca/sca_rpc.c
@@ -358,10 +358,11 @@ sca_rpc_show_all_appearances( rpc_t *rpc, void *ctx )
 	    app_list = (sca_appearance_list *)ent->value;
 	    for ( app = app_list->appearances; app != NULL; app = app->next ) {
 		sca_appearance_state_to_str( app->state, &state_str );
-		rc = rpc->printf( ctx, "%d: %.*s %d %.*s %.*s %.*s %.*s", i,
+		rc = rpc->printf( ctx, "%.*s %d %.*s %ld %.*s %.*s %.*s",
 				STR_FMT( &app_list->aor ),
 				app->index,
 				STR_FMT( &state_str ),
+				(long)app->times.mtime,
 				STR_FMT( &app->owner ),
 				STR_FMT( &app->callee ),
 				STR_FMT( &app->dialog.id ));
