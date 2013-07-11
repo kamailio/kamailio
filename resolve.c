211ef44012b744f05db1ff17cf23f824727ee6da
Daniel-Constantin Mierla <miconda@gmail.com>
2013-07-12 00:44:51 +0200

    core: avoid doing dns srv again after naptr function that includes srv lookup
    (cherry picked from commit 80935f9e8bbe20e5c320828183999b5d395ec34c)

--- resolve.c
+++ resolve.c
@@ -1515,6 +1515,7 @@ struct hostent* _sip_resolvehost(str* name, unsigned short* port, char* proto)
 #ifdef USE_NAPTR
 	if (cfg_get(core, core_cfg, dns_try_naptr))
 		res = naptr_sip_resolvehost(name, port, proto);
+	else
 #endif
 	res = srv_sip_resolvehost(name, 0, port, proto, 0, 0);
 	if( unlikely(!res) ){
