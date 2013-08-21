3ccf4b43e81bd2654cb306a3c2cc21b97cb51f62
Daniel-Constantin Mierla <miconda@gmail.com>
2013-08-22 00:14:53 +0200

    core: print src address details if initial message parsing fails
    - reported by Juha Heinanen

--- receive.c
+++ receive.c
@@ -143,7 +143,9 @@ int receive_msg(char* buf, unsigned int len, struct receive_info* rcv_info)
 
 	if (parse_msg(buf,len, msg)!=0){
 		LOG(cfg_get(core, core_cfg, corelog),
-				"ERROR: receive_msg: parse_msg failed\n");
+				"core parsing of SIP message failed (%s:%d/%d)\n",
+				ip_addr2a(&msg->rcv.src_ip), (int)msg->rcv.src_port,
+				(int)msg->rcv.proto);
 		goto error02;
 	}
 	DBG("After parse_msg...\n");
