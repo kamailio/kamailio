a722cee262664162f183e00788c2d53c4c98b217
Daniel-Constantin Mierla <miconda@gmail.com>
2013-09-05 10:04:57 +0200

    kamctl: added scripts to define table acc_cdrs

--- utils/kamctl/oracle/acc-create.sql
+++ utils/kamctl/oracle/acc-create.sql
@@ -20,6 +20,24 @@ BEGIN map2users('acc'); END;
 /
 CREATE INDEX acc_callid_idx  ON acc (callid);
 
+INSERT INTO version (table_name, table_version) values ('acc_cdrs','1');
+CREATE TABLE acc_cdrs (
+    id NUMBER(10) PRIMARY KEY,
+    start_time VARCHAR2(32) DEFAULT '',
+    end_time VARCHAR2(32) DEFAULT '',
+    duration VARCHAR2(32) DEFAULT ''
+);
+
+CREATE OR REPLACE TRIGGER acc_cdrs_tr
+before insert on acc_cdrs FOR EACH ROW
+BEGIN
+  auto_id(:NEW.id);
+END acc_cdrs_tr;
+/
+BEGIN map2users('acc_cdrs'); END;
+/
+CREATE INDEX acc_cdrs_start_time_idx  ON acc_cdrs (start_time);
+
 INSERT INTO version (table_name, table_version) values ('missed_calls','3');
 CREATE TABLE missed_calls (
     id NUMBER(10) PRIMARY KEY,
