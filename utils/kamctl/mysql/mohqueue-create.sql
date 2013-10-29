3022220043bcc9c421ac175b2a3290950014a7fc
Daniel-Constantin Mierla <miconda@gmail.com>
2013-10-29 08:34:29 +0100

    kamctl: regenerated db script for mohqueue tables

---
+++ utils/kamctl/mysql/mohqueue-create.sql
@@ -0,0 +1,24 @@
+INSERT INTO version (table_name, table_version) values ('mohqcalls','1');
+CREATE TABLE mohqcalls (
+    id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    mohq_id INT(10) UNSIGNED NOT NULL,
+    call_id VARCHAR(100) NOT NULL,
+    call_status INT UNSIGNED NOT NULL,
+    call_from VARCHAR(100) NOT NULL,
+    call_contact VARCHAR(100),
+    call_time DATETIME DEFAULT 'to_date('','yyyy-mm-dd hh24:mi:ss')' NOT NULL,
+    CONSTRAINT mohqcalls_idx UNIQUE (call_id)
+);
+
+INSERT INTO version (table_name, table_version) values ('mohqueues','1');
+CREATE TABLE mohqueues (
+    id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    name VARCHAR(25) NOT NULL,
+    uri VARCHAR(100) NOT NULL,
+    mohdir VARCHAR(100),
+    mohfile VARCHAR(100) NOT NULL,
+    debug INT NOT NULL,
+    CONSTRAINT mohqueue_uri_idx UNIQUE (uri),
+    CONSTRAINT mohqueue_name_idx UNIQUE (name)
+);
+
