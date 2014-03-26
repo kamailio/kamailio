7526431e8a582e1267667534b59893bacb99a5fd
Richard Fuchs <rfuchs@sipwise.com>
2014-03-26 15:30:18 -0400

    rtpengine: modernize interface, remove legacy interface

--- modules/rtpengine/rtpengine_funcs.c
+++ modules/rtpengine/rtpengine_funcs.c
@@ -31,7 +31,7 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include "rtpproxy_funcs.h"
+#include "rtpengine_funcs.h"
 #include "../../dprint.h"
 #include "../../config.h"
 #include "../../ut.h"
