9467ebbfdeea640923dbb77b4bea97ccda795903
Daniel-Constantin Mierla <miconda@gmail.com>
2014-04-14 14:13:36 +0200

    core: framework for creating asynchronous pool of workers
    - dedicated group of processes that can get tasks from other processes
      via memory pipe
    - react immediately, no time based polling
    - should reduce the need for other components to create extra processes
      for special handling

---
+++ async_task.h
@@ -0,0 +1,36 @@
+/**
+ * Copyright (C) 2014 Daniel-Constantin Mierla (asipto.com)
+ *
+ * This file is part of Extensible SIP Router, a free SIP server.
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+
+#ifndef _ASYNC_TASK_H_
+#define _ASYNC_TASK_H_
+
+typedef void (*async_cbe_t)(void *p);
+
+typedef struct _async_task {
+	async_cbe_t exec;
+	void *param;
+} async_task_t;
+
+int async_task_init(void);
+int async_task_child_init(int rank);
+int async_task_initialized(void);
+int async_task_set_workers(int n);
+int async_task_push(async_task_t *task);
+
+#endif
