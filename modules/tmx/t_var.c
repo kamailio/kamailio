27c383c9224055e3d1b01238ff47785449e4057c
Daniel-Constantin Mierla <miconda@gmail.com>
2014-04-17 16:16:04 +0200

    tmx: properly receive the branch index for $T(...) in the branch_failure_route

--- modules/tmx/t_var.c
+++ modules/tmx/t_var.c
@@ -384,7 +384,8 @@ int pv_get_tm_branch_idx(struct sip_msg *msg, pv_param_t *param,
 			idx = tcx->branch_index;
 	} else switch(route_type) {
 		case BRANCH_ROUTE:
-			/* branches have their index set */
+		case BRANCH_FAILURE_ROUTE:
+			/* branch and branch_failure routes have their index set */
 			tcx = _tmx_tmb.tm_ctx_get();
 			if(tcx != NULL)
 				idx = tcx->branch_index;
