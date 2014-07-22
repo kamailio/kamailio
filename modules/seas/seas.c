62bac5f85662ad42e58f5fb15dd339458af89d30
Alekzander Spiridonov <sipidronov@gmail.com>
2014-07-22 08:43:25 -0700

    seas: replace STR_PARAM with PARAM_STRING

--- modules/seas/seas.c
+++ modules/seas/seas.c
@@ -97,11 +97,11 @@ static cmd_export_t cmds[]=
 
 static param_export_t params[]=
 {
-   {"listen_sockets",STR_PARAM, &seas_listen_socket},
-   {"stats_socket",  STR_PARAM, &seas_stats_socket},
-   {"jain_ping",     STR_PARAM, &jain_ping_config},
-   {"servlet_ping",  STR_PARAM, &servlet_ping_config},
-   {"clusters",      STR_PARAM, &cluster_cfg},
+   {"listen_sockets",PARAM_STRING, &seas_listen_socket},
+   {"stats_socket",  PARAM_STRING, &seas_stats_socket},
+   {"jain_ping",     PARAM_STRING, &jain_ping_config},
+   {"servlet_ping",  PARAM_STRING, &servlet_ping_config},
+   {"clusters",      PARAM_STRING, &cluster_cfg},
    {0,0,0}
 };
 
