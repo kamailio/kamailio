ea6d3205d06b8d9f67429ca43ea97a4dcd6a9f1f
Richard Good <richard.good@smilecoms.com>
2014-10-30 17:23:23 +0200

    modules/cdp: Changed state machine for process unsuccessful answer when pending When in state PENDING and receive ANS_UNSUCCESS correct behaviour is to cleanup and move to IDLE

--- modules/cdp/authstatemachine.c
+++ modules/cdp/authstatemachine.c
@@ -243,10 +243,7 @@ inline int auth_client_statefull_sm_process(cdp_session_t* s, int event, AAAMess
                     //LM_INFO("state machine: i was in pending and i am going to open\n");
                     break;
                 case AUTH_EV_RECV_ANS_UNSUCCESS:
-                    x->state = AUTH_ST_DISCON;
-                    //LM_INFO("state machine: i was in pending and i am going to discon\n");
-                    Send_STR(s, msg);
-                    break;
+		    LM_DBG("In state AUTH_ST_PENDING and received AUTH_EV_RECV_ANS_UNSUCCESS - nothing to do but clean up session\n");
                 case AUTH_EV_SESSION_TIMEOUT:
                 case AUTH_EV_SERVICE_TERMINATED:
                 case AUTH_EV_SESSION_GRACE_TIMEOUT:
