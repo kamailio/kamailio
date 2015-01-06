72a616f074f554dfb177ca8ec2b55f9ff11ad352
Daniel-Constantin Mierla <miconda@gmail.com>
2015-01-06 22:24:35 +0100

    utils/protoshoot: fixed name and ticks
    - version to 0.4

--- utils/protoshoot/protoshoot.c
+++ utils/protoshoot/protoshoot.c
@@ -51,9 +51,9 @@
 
 
 static char *id="$Id$";
-static char *version="protoflood 0.3";
+static char *version="protoshoot 0.4";
 static char* help_msg="\
-Usage: udp_flood -f file -d address -p port -c count [-v]\n\
+Usage: protoshoot -f file -d address -p port -c count [-v]\n\
 Options:\n\
     -f file       file with the content of the udp packet (max 65k)\n\
     -d address    destination address\n\
@@ -200,12 +200,12 @@ int main (int argc, char** argv)
 				break;
 			case '?':
 				if (isprint(optopt))
-					fprintf(stderr, "Unknown option `-%c´\n", optopt);
+					fprintf(stderr, "Unknown option '-%c'\n", optopt);
 				else
-					fprintf(stderr, "Unknown character `\\x%x´\n", optopt);
+					fprintf(stderr, "Unknown character '\\x%x'\n", optopt);
 				goto error;
 			case ':':
-				fprintf(stderr, "Option `-%c´ requires an argument.\n",
+				fprintf(stderr, "Option '-%c' requires an argument.\n",
 						optopt);
 				goto error;
 				break;
