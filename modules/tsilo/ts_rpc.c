046b7c3c40106b662899cbbdc4c7fbf88432109a
Olle E. Johansson <oej@edvina.net>
2015-01-09 21:41:05 +0100

    tsilo REmove SVN ID's, change "SIP-Router" to "Kamailio"

--- modules/tsilo/ts_rpc.c
+++ modules/tsilo/ts_rpc.c
@@ -1,11 +1,9 @@
 /**
- * $Id$
- *
  * Copyright (C) 2014 Federico Cabiddu (federico.cabiddu@gmail.com)
  *
- * This file is part of SIP-Router.org, a free SIP server.
+ * This file is part of Kamailio, a free SIP server.
  *
- * SIP-Router is free software; you can redistribute it and/or modify
+ * Kamailio is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version
