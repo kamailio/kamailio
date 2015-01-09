dd48dce8c526959207f295866b09404f0e5770bf
Olle E. Johansson <oej@edvina.net>
2015-01-09 21:44:36 +0100

    dnssec Remove history

--- modules/dnssec/dnssec_func.c
+++ modules/dnssec/dnssec_func.c
@@ -19,9 +19,6 @@
  * along with this program; if not, write to the Free Software 
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
- * History:
- * --------
- *  2013-03	initial implementation
  */
 /*!
  * \brief DNSsec support
