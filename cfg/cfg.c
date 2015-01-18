1fff03a223fee4674e7e2c5317ccc13bee7c3cf7
Olle E. Johansson <oej@edvina.net>
2015-01-18 20:17:38 +0100

    Cfg Remove SVN ID, history

--- cfg/cfg.c
+++ cfg/cfg.c
@@ -1,16 +1,14 @@
 /*
- * $Id$
- *
  * Copyright (C) 2007 iptelorg GmbH
  *
- * This file is part of SIP-router, a free SIP server.
+ * This file is part of Kamailio, a free SIP server.
  *
- * SIP-router is free software; you can redistribute it and/or modify
+ * Kamailio is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version
  *
- * SIP-router is distributed in the hope that it will be useful,
+ * Kamailio is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
@@ -19,9 +17,6 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
- * History
- * -------
- *  2007-12-03	Initial version (Miklos)
  */
 
 #include <string.h>
