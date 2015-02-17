084e68b337f3afbc841347a6d27398bacfaf191b
Victor Seva <linuxmaniac@torreviejawireless.org>
2015-02-17 10:02:20 +0100

    ctl: use RUN_DIR env to set DEFAULT_CTL_SOCKET

--- modules/ctl/ctl_defaults.h
+++ modules/ctl/ctl_defaults.h
@@ -4,10 +4,10 @@
 /*listen by default on: */
 #ifdef SRNAME
 /* this is used when compiling sercmd tool */
-#define DEFAULT_CTL_SOCKET  "unixs:/tmp/" SRNAME "_ctl"
+#define DEFAULT_CTL_SOCKET  "unixs:" RUN_DIR "/" SRNAME "/" SRNAME "_ctl"
 #else
 /* this is used when compiling sip server */
-#define DEFAULT_CTL_SOCKET  "unixs:/tmp/" NAME "_ctl"
+#define DEFAULT_CTL_SOCKET  "unixs:" RUN_DIR "/" NAME "_ctl"
 #endif
 /* port used by default for tcp/udp if no port is explicitely specified */
 #define DEFAULT_CTL_PORT 2049
