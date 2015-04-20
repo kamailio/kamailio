a19a5feb4c737cab52bdb329d5a880d7f94f048b
Olle E. Johansson <oej@edvina.net>
2015-04-20 21:11:42 +0200

    async: Fix some error messages
    (cherry picked from commit 103b915cffd63b4abab1a3e57b51644eef20e81d)
    (cherry picked from commit 14297b78418cbe963b491de928f8e44510819c4d)

--- modules/async/async_mod.c
+++ modules/async/async_mod.c
@@ -96,7 +96,7 @@ static int mod_init(void)
 {
 	if (load_tm_api( &tmb ) == -1)
 	{
-		LM_ERR("cannot load the TM-functions\n");
+		LM_ERR("cannot load the TM-functions. Missing TM module?\n");
 		return -1;
 	}
 
@@ -153,7 +153,7 @@ static int w_async_sleep(struct sip_msg* msg, char* sec, char* str2)
 
 	if(async_workers<=0)
 	{
-		LM_ERR("no async mod timer wokers\n");
+		LM_ERR("no async mod timer workers (modparam missing?)\n");
 		return -1;
 	}
 
@@ -190,7 +190,7 @@ static int fixup_async_sleep(void** param, int param_no)
 	ap = (async_param_t*)pkg_malloc(sizeof(async_param_t));
 	if(ap==NULL)
 	{
-		LM_ERR("no more pkg\n");
+		LM_ERR("no more pkg memory available\n");
 		return -1;
 	}
 	memset(ap, 0, sizeof(async_param_t));
@@ -217,7 +217,7 @@ static int w_async_route(struct sip_msg* msg, char* rt, char* sec)
 
 	if(async_workers<=0)
 	{
-		LM_ERR("no async mod timer wokers\n");
+		LM_ERR("no async mod timer workers\n");
 		return -1;
 	}
 
