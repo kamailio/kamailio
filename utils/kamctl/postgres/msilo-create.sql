a4a9b6d27ae35ee651840a683a5d70ba52ec232f
Victor Seva <linuxmaniac@torreviejawireless.org>
2015-04-21 16:24:02 +0200

    kamctl: upgrade silo schema

--- utils/kamctl/postgres/msilo-create.sql
+++ utils/kamctl/postgres/msilo-create.sql
@@ -9,8 +9,8 @@ CREATE TABLE silo (
     exp_time INTEGER DEFAULT 0 NOT NULL,
     snd_time INTEGER DEFAULT 0 NOT NULL,
     ctype VARCHAR(32) DEFAULT 'text/plain' NOT NULL,
-    body BYTEA DEFAULT '' NOT NULL,
-    extra_hdrs TEXT DEFAULT '' NOT NULL,
+    body BYTEA,
+    extra_hdrs TEXT,
     callid VARCHAR(128) DEFAULT '' NOT NULL,
     status INTEGER DEFAULT 0 NOT NULL
 );
