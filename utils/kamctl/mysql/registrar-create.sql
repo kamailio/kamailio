a786c3f4a108f7df94cb555ee25444227c7e5c3b
Daniel-Constantin Mierla <miconda@gmail.com>
2015-05-08 09:55:44 +0200

    kamctl: get aliases table db scripts up to date

--- utils/kamctl/mysql/registrar-create.sql
+++ utils/kamctl/mysql/registrar-create.sql
@@ -1,4 +1,4 @@
-INSERT INTO version (table_name, table_version) values ('aliases','6');
+INSERT INTO version (table_name, table_version) values ('aliases','8');
 CREATE TABLE aliases (
     id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
     ruid VARCHAR(64) DEFAULT '' NOT NULL,
@@ -6,7 +6,7 @@ CREATE TABLE aliases (
     domain VARCHAR(64) DEFAULT NULL,
     contact VARCHAR(255) DEFAULT '' NOT NULL,
     received VARCHAR(128) DEFAULT NULL,
-    path VARCHAR(128) DEFAULT NULL,
+    path VARCHAR(512) DEFAULT NULL,
     expires DATETIME DEFAULT '2030-05-28 21:32:15' NOT NULL,
     q FLOAT(10,2) DEFAULT 1.0 NOT NULL,
     callid VARCHAR(255) DEFAULT 'Default-Call-ID' NOT NULL,
@@ -19,8 +19,13 @@ CREATE TABLE aliases (
     methods INT(11) DEFAULT NULL,
     instance VARCHAR(255) DEFAULT NULL,
     reg_id INT(11) DEFAULT 0 NOT NULL,
+    server_id INT(11) DEFAULT 0 NOT NULL,
+    connection_id INT(11) DEFAULT 0 NOT NULL,
+    keepalive INT(11) DEFAULT 0 NOT NULL,
+    partition INT(11) DEFAULT 0 NOT NULL,
     CONSTRAINT ruid_idx UNIQUE (ruid)
 );
 
-CREATE INDEX alias_idx ON aliases (username, domain, contact);
+CREATE INDEX account_contact_idx ON aliases (username, domain, contact);
+CREATE INDEX expires_idx ON aliases (expires);
 
