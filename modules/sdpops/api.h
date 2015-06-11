56b17076be90db99058bec51ad73a5e84d6616ad
Hugh Waite <hugh.waite@acision.com>
2015-06-11 13:27:31 +0100

    sdpops: Export more sdpops functions
    - sdp_with_active_media
    - sdp_with_transport
    - sdp_with_codecs_by_id
    - sdp_with_codecs_by_name
    - sdp_with_ice
    - sdp_keep_codecs_by_id
    - sdp_keep_codecs_by_name
    - sdp_remove_media
    - sdp_remove_transport
    - sdp_remove_line_by_prefix
    - sdp_remove_codecs_by_id
    - sdp_remove_codecs_by_name

--- modules/sdpops/api.h
+++ modules/sdpops/api.h
@@ -3,9 +3,25 @@
 #include "../../str.h"
 
 typedef int (*sdp_with_media_t)(struct sip_msg*, str*);
+typedef int (*sdp_with_transport_t)(struct sip_msg*, str*, int);
+typedef int (*sdp_with_ice_t)(struct sip_msg*);
+typedef int (*sdp_keep_media_t)(struct sip_msg*, str*, str*);
+typedef int (*sdp_remove_media_t)(struct sip_msg*, str*);
 
 typedef struct sdpops_binds {
-	sdp_with_media_t sdp_with_media;
+	sdp_with_media_t     sdp_with_media;
+	sdp_with_media_t     sdp_with_active_media;
+	sdp_with_transport_t sdp_with_transport;
+	sdp_with_media_t     sdp_with_codecs_by_id;
+	sdp_with_media_t     sdp_with_codecs_by_name;
+	sdp_with_ice_t       sdp_with_ice;
+	sdp_keep_media_t     sdp_keep_codecs_by_id;
+	sdp_keep_media_t     sdp_keep_codecs_by_name;
+	sdp_remove_media_t   sdp_remove_media;
+	sdp_remove_media_t   sdp_remove_transport;
+	sdp_remove_media_t   sdp_remove_line_by_prefix;
+	sdp_remove_media_t   sdp_remove_codecs_by_id;
+	sdp_remove_media_t   sdp_remove_codecs_by_name;
 } sdpops_api_t;
 
 typedef int (*bind_sdpops_f)(sdpops_api_t*);
