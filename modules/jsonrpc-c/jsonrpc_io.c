bb0194aa9f0963cb77bb1275b8e0a4cae8f60909
Daniel-Constantin Mierla <miconda@gmail.com>
2015-08-25 15:53:02 +0200

    jsonrpc-c: free pipe cmd for notifications
    - fixes leak reported by GH#296
    
    (cherry picked from commit 79459d3f87731309641794a2b881ee1cacc5e1f2)

--- modules/jsonrpc-c/jsonrpc_io.c
+++ modules/jsonrpc-c/jsonrpc_io.c
@@ -163,7 +163,7 @@ void cmd_pipe_cb(int fd, short event, void *arg)
 
 	if (!payload) {
 		LM_ERR("Failed to build jsonrpc_request_t (method: %s, params: %s)\n", cmd->method, cmd->params);	
-		return;
+		goto error;
 	}
 	char *json = (char*)json_object_get_string(payload);
 
@@ -201,7 +201,7 @@ void cmd_pipe_cb(int fd, short event, void *arg)
 
 		if (timerfd == -1) {
 			LM_ERR("Could not create timerfd.");
-			return;
+			goto error;
 		}
 
 		req->timerfd = timerfd;
@@ -215,7 +215,7 @@ void cmd_pipe_cb(int fd, short event, void *arg)
 		if (timerfd_settime(timerfd, 0, itime, NULL) == -1) 
 		{
 			LM_ERR("Could not set timer.");
-			return;
+			goto error;
 		}
 		pkg_free(itime);
 		struct event *timer_ev = pkg_malloc(sizeof(struct event));
@@ -223,7 +223,7 @@ void cmd_pipe_cb(int fd, short event, void *arg)
 		event_set(timer_ev, timerfd, EV_READ, timeout_cb, req); 
 		if(event_add(timer_ev, NULL) == -1) {
 			LM_ERR("event_add failed while setting request timer (%s).", strerror(errno));
-			return;
+			goto error;
 		}
 		req->timer_ev = timer_ev;
 	} else if (!sent) {
@@ -237,6 +237,10 @@ void cmd_pipe_cb(int fd, short event, void *arg)
 
 	pkg_free(ns);
 	json_object_put(payload);
+	if (cmd->notify_only) free_pipe_cmd(cmd);
+error:
+	if (cmd->notify_only) free_pipe_cmd(cmd);
+	return;
 }
 
 void socket_cb(int fd, short event, void *arg)
