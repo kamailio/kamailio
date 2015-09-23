8cdf9a26e90b1006a6992ad73d1e60fbc1c5178c
Daniel-Constantin Mierla <miconda@gmail.com>
2015-09-23 12:59:52 +0200

    tsilo: ts_append_to() check if transation is active before adding new branches
    (cherry picked from commit 9451625ff8cc7e02abc2c6cf95572bff9c086d20)

--- modules/tsilo/ts_append.c
+++ modules/tsilo/ts_append.c
@@ -77,13 +77,23 @@ int ts_append_to(struct sip_msg* msg, int tindex, int tlabel, char *table) {
 				tindex, tlabel);
 		return -1;
 	}
+	if (t->flags & T_CANCELED) {
+		LM_DBG("trasaction [%u:%u] was cancelled\n",
+				tindex, tlabel);
+		return -2;
+	}
+	if (t->uas.status >= 200) {
+		LM_DBG("trasaction [%u:%u] sent out a final response already - %d\n",
+				tindex, tlabel, t->uas.status);
+		return -3;
+	}
 
 	orig_msg = t->uas.request;
 
 	ret = _regapi.lookup_to_dset(orig_msg, table, NULL);
 	if(ret != 1) {
 		LM_DBG("transaction %u:%u: error updating dset (%d)\n", tindex, tlabel, ret);
-		return -1;
+		return -4;
 	}
 
 	return _tmb.t_append_branches();
