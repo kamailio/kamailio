946e996fed4bed9a62703cdd248777e2c7ab4a89
Daniel-Constantin Mierla <miconda@gmail.com>
2015-11-09 09:21:29 +0100

    tls: check for support of SSLv3 method
    - some versions of libssl removed support for SSLv3
    - reported by Victore Seva, FS#391
    
    (cherry picked from commit b1c6c2af7e96c0bcbdee4ff46f6faf19fdc75d9f)

--- modules/tls/tls_init.c
+++ modules/tls/tls_init.c
@@ -344,9 +344,11 @@ static void init_ssl_methods(void)
 	ssl_methods[TLS_USE_SSLv2 - 1] = SSLv2_method();
 #endif
 
+#ifndef OPENSSL_NO_SSL3_METHOD
 	ssl_methods[TLS_USE_SSLv3_cli - 1] = SSLv3_client_method();
 	ssl_methods[TLS_USE_SSLv3_srv - 1] = SSLv3_server_method();
 	ssl_methods[TLS_USE_SSLv3 - 1] = SSLv3_method();
+#endif
 
 	ssl_methods[TLS_USE_TLSv1_cli - 1] = TLSv1_client_method();
 	ssl_methods[TLS_USE_TLSv1_srv - 1] = TLSv1_server_method();
