65750d3422f97568623036a027ddebcbe636198d
Stefan Mititelu <stefan.mititelu@1and1.ro>
2015-12-04 09:22:41 +0200

    rtpengine: Update kamailio-db-devel db scripts
    Add rtpengine 'utils/kamctl/$dbschema' scripts.

---
+++ utils/kamctl/mysql/rtpengine-create.sql
@@ -0,0 +1,9 @@
+INSERT INTO version (table_name, table_version) values ('rtpengine','1');
+CREATE TABLE `rtpengine` (
+    `setid` INT(10) UNSIGNED DEFAULT 0 NOT NULL,
+    `url` VARCHAR(64) NOT NULL,
+    `weight` INT(10) UNSIGNED DEFAULT 1 NOT NULL,
+    `disabled` INT(1) DEFAULT 0 NOT NULL,
+    CONSTRAINT rtpengine_nodes PRIMARY KEY  (`setid`, `url`)
+);
+
