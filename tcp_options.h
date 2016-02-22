kamailio/kamailio

commit 6627098eb4adad706d20f6b73d1b7e75cc08a21f

tcp: include netinet/tcp.h to make certain macros available
Following are tested: TCP_KEEPINTVL, TCP_KEEPIDLE, TCP_KEEPCNT

tcp_options.h
@@ -23,6 +23,7 @@
 
 #ifdef USE_TCP
 
+#include <netinet/tcp.h>
 #ifndef NO_TCP_ASYNC
 #define TCP_ASYNC /* enabled async mode */
 #endif

commit 46862c7b9a095fb8137e0ef358e0e97599bd444b

tcp: fix tcp_options.o compilation
Commit 6627098eb4adad706d20f6b73d1b7e75cc08a21f broke building on OpenBSD

tcp_options.h
@@ -23,6 +23,7 @@
 
 #ifdef USE_TCP
 
+#include <netinet/in.h>
 #include <netinet/tcp.h>
 #ifndef NO_TCP_ASYNC
 #define TCP_ASYNC /* enabled async mode */