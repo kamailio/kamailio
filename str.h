7772e14fc298956f57429a67ea4524818b22a158
Daniel-Constantin Mierla <miconda@gmail.com>
2016-03-15 12:37:37 +0100

    core: _strnstr() and _strnistr() defined in core
    - alternative to strnstr() which is not in all OSes, pluse the
      insensitive option

--- str.h
+++ str.h
@@ -21,6 +21,8 @@
 #ifndef str_h
 #define str_h
 
+#include <string.h>
+
 /** @defgroup str_string Counted-Length Strings 
  * @{
  * 
@@ -124,5 +126,8 @@ typedef struct _str str;
  * remember to free the dest->s private memory
  */
 int str_append(str *orig, str *suffix, str *dest);
+
+char* _strnstr(const char *s, const char *find, size_t slen);
+char* _strnistr(const char *s, const char *find, size_t slen);
 
 #endif
