9ae73dda743895e221f9e3accc0a35daed8e11dc
Yasin CANER <yasin.caner@netgsm.com.tr>
2016-03-29 12:27:31 +0300

    domain module : fixed response
    fixed xml response as xml specification

--- modules/domain/domain_mod.c
+++ modules/domain/domain_mod.c
@@ -288,7 +288,10 @@ static void domain_rpc_reload(rpc_t* rpc, void* ctx)
 	rpc->fault(ctx, 400, "Reload of domain tables failed");
     }
     lock_release(reload_lock);
-    rpc->fault(ctx, 200, "domain table is reloaded");
+    rpc->add(c, "d", 200);
+    rpc->add(c, "s", "OK");
+    rpc->send(c);
+
 }
 
 
