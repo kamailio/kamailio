3a24b7234bdda477eb3ea88b472c9e8f6fd440a7
Olle E. Johansson <oej@edvina.net>
2016-04-01 17:58:38 +0200

    lcr Change name of software, remove SVN ID and history

--- modules/lcr/hash.c
+++ modules/lcr/hash.c
@@ -3,14 +3,14 @@
  *
  * Copyright (C) 2008-2012 Juha Heinanen
  *
- * This file is part of SIP Router, a free SIP server.
+ * This file is part of Kamailio, a free SIP server.
  *
- * SIP Router is free software; you can redistribute it and/or modify
+ * Kamailio is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version
  *
- * SIP Router is distributed in the hope that it will be useful,
+ * Kamailio is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
@@ -22,7 +22,7 @@
 
 /*!
  * \file
- * \brief SIP Router LCR :: Header file for hash table functions
+ * \brief Kamailio LCR :: Header file for hash table functions
  * \ingroup lcr
  * Module: \ref lcr
  */
