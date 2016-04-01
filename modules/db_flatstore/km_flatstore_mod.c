d5f10966fdf5973b5de21ce71b208ffcffd9c549
Olle E. Johansson <oej@edvina.net>
2016-04-01 18:25:42 +0200

    db_flatstore Edit source code headers - change product name to Kamailio etc

--- modules/db_flatstore/km_flatstore_mod.c
+++ modules/db_flatstore/km_flatstore_mod.c
@@ -1,6 +1,4 @@
 /* 
- * $Id$ 
- *
  * Flatstore module interface
  *
  * Copyright (C) 2004 FhG Fokus
@@ -21,10 +19,6 @@
  * along with this program; if not, write to the Free Software 
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
- * History:
- * --------
- *  2003-03-11  updated to the new module exports interface (andrei)
- *  2003-03-16  flags export parameter added (janakj)
  */
 
 #include "../../sr_module.h"
