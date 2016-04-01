94721323f01f74cc5d454953fe676697e81e0eba
Olle E. Johansson <oej@edvina.net>
2016-04-01 17:47:59 +0200

    auth_radius Remove SVN ID's and history comments

--- modules/auth_radius/authorize.c
+++ modules/auth_radius/authorize.c
@@ -20,10 +20,6 @@
  * along with this program; if not, write to the Free Software 
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
- * History:
- * -------
- * 2003-03-09: Based on authorize.c from radius_auth (janakj)
- * 2006-03-01: pseudo variables support for domain name (bogdan)
  */
 
 
