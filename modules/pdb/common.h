31e70a51ebf0faa97ef9bdef02675254f27a85e1
Olle E. Johansson <oej@edvina.net>
2016-04-03 20:14:14 +0200

    pdb Change sip-router to "Kamailio"

--- modules/pdb/common.h
+++ modules/pdb/common.h
@@ -1,14 +1,14 @@
 /*
  * Copyright (C) 2009 1&1 Internet AG
  *
- * This file is part of sip-router, a free SIP server.
+ * This file is part of Kamailio, a free SIP server.
  *
- * sip-router is free software; you can redistribute it and/or modify
+ * Kamailio is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version
  *
- * sip-router is distributed in the hope that it will be useful,
+ * Kamailio is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
@@ -21,15 +21,9 @@
 #ifndef _COMMON_H_
 #define _COMMON_H_
 
-
-
-
 #include <stdint.h> 
 
-
-
-
-/*
+/*!
  0 no carrier id defined.
  1..999 are regular carrier ids.
  1000 is used as fake carrier id when merging carriers we are not interested in.
@@ -48,8 +42,6 @@
 #define IS_VALID_CARRIERID(id) ((id>=MIN_PDB_CARRIERID) && (id<=MAX_CARRIERID))
 
 #define PDB_VERSION     1
-
-
 
 typedef int16_t carrier_t;
 
