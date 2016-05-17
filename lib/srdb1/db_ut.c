1cfef090e524faa4704acccd0e52ecc1bf7549b3
Mikko Lehto <mslehto@iki.fi>
2016-05-18 02:18:52 +0300

    lib/srdb1: fix compiler warning
    > CC (gcc) [L libsrdb1.so.1.0]            db_ut.o
    > db_ut.c:57:32: warning: extra tokens at end of #undef directive
    >   #undef _XOPEN_SOURCE_EXTENDED 1  /* solaris */
    >                                 ^
    
    (cherry picked from commit dee2478e521f2f829c66d025fb6ca17326138910)
    (cherry picked from commit 01698f47ab0be22f0d80cb0a66bbeb92f8177821)
    (cherry picked from commit 6218646c0a0d14ac10d7e6010550c08dc736debb)

--- lib/srdb1/db_ut.c
+++ lib/srdb1/db_ut.c
@@ -54,8 +54,8 @@
 #ifndef __OS_solaris
 	#undef _XOPEN_SOURCE
 	#undef _XOPEN_SOURCE_EXTENDED
-#else
-	#undef _XOPEN_SOURCE_EXTENDED 1   /* solaris */
+#else  /* solaris */
+	#undef _XOPEN_SOURCE_EXTENDED
 #endif
 
 #include <limits.h>
