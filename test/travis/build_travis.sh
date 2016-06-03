e4e5ad4c674d1b27f0c68b7932b679b36e50c08d
Victor Seva <linuxmaniac@torreviejawireless.org>
2016-06-03 11:17:07 +0200

    travis: remove rebuild twice for MEMDBG test
    (cherry picked from commit eb0d271af95ba0eb409716335d86351ea9e94343)

--- test/travis/build_travis.sh
+++ test/travis/build_travis.sh
@@ -17,11 +17,24 @@ else
 	rm -rf /code/debian
 	ln -s /code/pkg/kamailio/deb/"${DIST}" /code/debian
 fi
-function build {
+
+function _clean {
 	echo "make clean"
 	make -f debian/rules clean
+}
+
+function _build {
 	echo "make build"
 	make -f debian/rules build
+}
+
+function _install {
+	if [[ "$CC" =~ gcc ]] ; then
+		echo "make install"
+		make install
+	else
+		echo "skip make install step"
+	fi
 }
 
 if [[ "${CC}" =~ clang ]] ; then
@@ -33,18 +46,6 @@ fi
 echo "environment DIST=$DIST CC=$CC"
 ${CC} --version
 
-# build flags
-export MEMDBG=0
-echo "build with MEMDBG=0"
-build
-
-export MEMDBG=1
-echo "build with MEMDBG=1"
-build
-
-if [[ "$CC" =~ gcc ]] ; then
-	echo "make install"
-	make install
-else
-	echo "skip make install step"
-fi
+_clean
+_build
+_install
