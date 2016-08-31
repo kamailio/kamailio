a8fae373c1b197012a33bb0b8114fc22b6c86835
Kevin Scott Adams <kevin.adams@ksatechnologies.com>
2016-08-31 15:32:49 -0400

    sca: terminate all error and debug messages with a new line
    - issue reported by oej, FH #580
      Commmit also includes whitespace cleanup..

--- modules/sca/sca_reply.c
+++ modules/sca/sca_reply.c
@@ -10,12 +10,12 @@
  *
  * The sca module is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA. 02110-1301 USA
  */
 #include "sca_common.h"
 
@@ -25,32 +25,31 @@
 #include "sca_event.h"
 #include "sca_reply.h"
 
-    int
-sca_reply( sca_mod *scam, int status_code, char *status_msg,
-	str *extra_headers, sip_msg_t *msg )
+int sca_reply(sca_mod *scam, int status_code, char *status_msg,
+		str *extra_headers, sip_msg_t *msg)
 {
-    str		status_str = STR_NULL;
+	str status_str = STR_NULL;
 
-    assert( scam != NULL && scam->sl_api != NULL );
-    assert( msg != NULL );
+	assert(scam != NULL && scam->sl_api != NULL);
+	assert(msg != NULL);
 
-    status_str.s = status_msg;
-    status_str.len = strlen( status_msg );
+	status_str.s = status_msg;
+	status_str.len = strlen(status_msg);
 
-    if ( extra_headers && extra_headers->len ) {
-        if ( add_lump_rpl( msg, extra_headers->s, extra_headers->len,
-                        LUMP_RPL_HDR ) == NULL ) {
-            LM_ERR("sca_subscription_reply: failed to add Retry-After header");
-            return( -1 );
-        }
-    }
+	if (extra_headers && extra_headers->len) {
+		if (add_lump_rpl(msg, extra_headers->s, extra_headers->len,
+		LUMP_RPL_HDR) == NULL) {
+			LM_ERR("sca_subscription_reply: failed to add Retry-After header\n");
+			return (-1);
+		}
+	}
 
-    if ( scam->sl_api->freply( msg, status_code, &status_str ) < 0 ) {
-	LM_ERR( "Failed to send \"%d %s\" reply to %.*s",
-		status_code, status_msg,
-		get_from( msg )->body.len, get_from( msg )->body.s );
-	return( -1 );
-    }
+	if (scam->sl_api->freply(msg, status_code, &status_str) < 0) {
+		LM_ERR("Failed to send \"%d %s\" reply to %.*s",
+				status_code, status_msg,
+				get_from(msg)->body.len, get_from(msg)->body.s);
+		return (-1);
+	}
 
-    return( 0 );
+	return (0);
 }
