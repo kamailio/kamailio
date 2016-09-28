0965c90471932936a8e66c1ebd9299ca7be02721
Daniel-Constantin Mierla <miconda@gmail.com>
2016-09-28 15:31:33 +0200

    core: from/to/callid/cseq headers are parsed before logging prefix is used

--- receive.c
+++ receive.c
@@ -181,6 +181,8 @@ int receive_msg(char* buf, unsigned int len, struct receive_info* rcv_info)
 		}
 		else if(ret == NONSIP_MSG_DROP) goto error02;
 	}
+
+	parse_headers(msg, HDR_FROM_F|HDR_TO_F|HDR_CALLID_F|HDR_CSEQ_F, 0);
 	LM_DBG("--- received sip message - %s - call-id: [%.*s] - cseq: [%.*s]\n",
 			(msg->first_line.type==SIP_REQUEST)?"request":"reply",
 			(msg->callid && msg->callid->body.s)?msg->callid->body.len:0,
