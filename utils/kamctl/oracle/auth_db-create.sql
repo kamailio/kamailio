57c1f4037a36f9b40845c3f29684b5b4ecdfd479
Daniel-Constantin Mierla <miconda@gmail.com>
2016-10-06 14:52:19 +0200

    utils/kamctl: db creation scripts refreshed after schema updates

--- utils/kamctl/oracle/auth_db-create.sql
+++ utils/kamctl/oracle/auth_db-create.sql
@@ -2,11 +2,11 @@ CREATE TABLE subscriber (
     id NUMBER(10) PRIMARY KEY,
     username VARCHAR2(64) DEFAULT '',
     domain VARCHAR2(64) DEFAULT '',
-    password VARCHAR2(25) DEFAULT '',
-    email_address VARCHAR2(64) DEFAULT '',
-    ha1 VARCHAR2(64) DEFAULT '',
-    ha1b VARCHAR2(64) DEFAULT '',
-    rpid VARCHAR2(64) DEFAULT NULL,
+    password VARCHAR2(64) DEFAULT '',
+    ha1 VARCHAR2(128) DEFAULT '',
+    ha1b VARCHAR2(128) DEFAULT '',
+    email_address VARCHAR2(128) DEFAULT '',
+    rpid VARCHAR2(128) DEFAULT NULL,
     CONSTRAINT subscriber_account_idx  UNIQUE (username, domain)
 );
 
@@ -20,5 +20,5 @@ BEGIN map2users('subscriber'); END;
 /
 CREATE INDEX subscriber_username_idx  ON subscriber (username);
 
-INSERT INTO version (table_name, table_version) values ('subscriber','6');
+INSERT INTO version (table_name, table_version) values ('subscriber','7');
 
