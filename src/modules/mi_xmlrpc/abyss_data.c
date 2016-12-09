1bba5faf5ced9fdd95446e4ae8913a0cf2426b85
Daniel-Constantin Mierla <miconda@gmail.com>
2016-12-09 09:52:27 +0100

    mi_xmlrpc: define TRUE/FALSE if not defined
    - reported by GH #885

--- src/modules/mi_xmlrpc/abyss_data.c
+++ src/modules/mi_xmlrpc/abyss_data.c
@@ -45,6 +45,14 @@
 
 #include "abyss_data.h"
 
+#ifndef TRUE
+#define TRUE	1
+#endif	/* TRUE */
+
+#ifndef FALSE
+#define FALSE    0
+#endif  /* FALSE */
+
 /*********************************************************************
 ** List
 *********************************************************************/
