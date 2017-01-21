54aac8b91375e5a5ca61bf97a67afd4476ef5882
Daniel-Constantin Mierla <miconda@gmail.com>
2017-01-21 19:24:57 +0100

    tm: select code migrated to new log macros
    (cherry picked from commit 3e88ff5662ccf8204206f4e6b770efaf509ad3f7)

--- modules/tm/select.h
+++ modules/tm/select.h
@@ -13,8 +13,8 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
  */
@@ -22,8 +22,6 @@
 #ifndef _T_SELECT_H
 #define _T_SELECT_H
 
-
 int tm_init_selects(void);
-
 
 #endif
