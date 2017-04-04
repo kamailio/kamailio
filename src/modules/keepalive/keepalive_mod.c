cf07d3feab3494c934b27a8e52724be3d94fe00a
Guillaume Bour <guillaume@bour.cc>
2017-04-04 16:00:15 +0200

    keepalive: new module to monitor remote destinations

---
+++ src/modules/keepalive/keepalive_mod.c
@@ -0,0 +1,190 @@
+/**
+ * keepalive module - remote destinations probing
+ *
+ * Copyright (C) 2017 Guillaume Bour <guillaume@bour.cc>
+ *
+ * This file is part of Kamailio, a free SIP server.
+ *
+ * Kamailio is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version
+ *
+ * Kamailio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+/*! \file
+ * \ingroup keepalive
+ * \brief Keepalive :: Send keepalives
+ */
+
+/*! \defgroup keepalive Keepalive :: Probing remote gateways by sending keepalives
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "../tm/tm_load.h"
+#include "../dispatcher/api.h"
+
+#include "keepalive.h"
+#include "api.h"
+
+MODULE_VERSION
+
+
+static int  mod_init(void);
+static void mod_destroy(void);
+static int  ka_mod_add_destination(modparam_t type, void *val);
+int  ka_init_rpc(void);
+int         ka_alloc_destinations_list();
+extern void ka_check_timer(unsigned int ticks, void* param);
+
+static int cmd_is_alive(struct sip_msg* msg, char *str1, char *str2);
+
+extern struct tm_binds tmb;
+
+int ka_ping_interval = 30;
+ka_destinations_list_t *ka_destinations_list = NULL;
+
+
+static cmd_export_t cmds[]={
+	{"is_alive",       (cmd_function)cmd_is_alive, 1, 0, 0,
+		REQUEST_ROUTE|FAILURE_ROUTE|ONREPLY_ROUTE},
+	// internal API
+	{"bind_keepalive", (cmd_function)bind_keepalive, 0, 0, 0, 0},
+	{0,0,0,0,0,0}
+};
+
+
+static param_export_t params[]={
+	{"ping_interval", PARAM_INT                  , &ka_ping_interval},
+	{"destination"  , PARAM_STRING|USE_FUNC_PARAM, (void *)ka_mod_add_destination},
+	{0,0,0}
+};
+
+
+/** module exports */
+struct module_exports exports= {
+	"keepalive",
+	DEFAULT_DLFLAGS, /* dlopen flags */
+	cmds,
+	params,
+	0,          /* exported statistics */
+	0,          /* exported MI functions - no available anymore since 5.0 */
+	0,          /* exported pseudo-variables */
+	0,          /* extra processes */
+	mod_init,   /* module initialization function */
+	0,
+	(destroy_function) mod_destroy,
+	0           /* per-child init function */
+};
+
+
+/**
+ * Module initialization
+ */
+static int mod_init(void)
+{
+	LM_INFO("Initializing keepalive module\n");
+
+	if (load_tm_api( &tmb ) == -1)
+	{
+		LM_ERR("could not load the TM-functions - please load tm module\n");
+		return -1;
+	}
+
+	if(ka_init_rpc() < 0) {
+		LM_ERR("failed to register RPC commands\n");
+		return -1;
+	}
+
+	if (ka_alloc_destinations_list() < 0)
+		return -1;
+
+	if(register_timer(ka_check_timer, NULL, ka_ping_interval) < 0) {
+		LM_ERR("failed registering timer\n");
+		return -1;
+	}
+
+	return 0;
+}
+
+/*! \brief
+ * destroy function
+ */
+static void mod_destroy(void)
+{
+}
+
+
+/*! \brief
+ * parses string to dispatcher dst flags set
+ * returns <0 on failure or int with flag on success.
+ */
+int ka_parse_flags( char* flag_str, int flag_len )
+{
+	return 0;
+}
+
+
+/*
+ * Function callback executer per module param "destination".
+ * Is just a wrapper to ka_add_dest() api function
+ */
+static int ka_mod_add_destination(modparam_t type, void *val) 
+{
+	if (ka_alloc_destinations_list() < 0)
+		return -1;
+
+	str dest = {val, strlen(val)};
+	str owner = str_init("_params");
+	LM_DBG("adding destination %.*s\n", dest.len, dest.s);
+
+	return ka_add_dest(dest, owner, 0, 0, 0);
+}
+
+/*
+ * Allocate global variable *ka_destination_list* if not already done
+ * WHY:  when specifying static destinations as module param, ka_mod_add_destination() is 
+ *       executed BEFORE mod_init()
+ */
+int ka_alloc_destinations_list()
+{
+	if (ka_destinations_list != NULL) {
+		LM_DBG("ka_destinations_list already allocated\n");
+		return 1;
+	}
+
+	ka_destinations_list = (ka_destinations_list_t *) shm_malloc(sizeof(ka_destinations_list_t));
+	if(ka_destinations_list == NULL) {
+		LM_ERR("no more memory.\n");
+		return -1;
+	}
+
+	return 0;
+}
+
+
+static int cmd_is_alive(struct sip_msg* msg, char *str1, char *str2)
+{
+	str dest = {str1, strlen(str1)};
+
+	ka_state state = ka_destination_state(dest);
+	// must not return 0, as it stops dialplan execution
+	if (state == KA_STATE_UNKNOWN) {
+		return KA_STATE_UP;
+	}
+
+	return state;
+}
---
+++ src/modules/keepalive/keepalive_rpc.c
@@ -0,0 +1,101 @@
+/**
+ * keepalive module - remote destinations probing
+ *
+ * Copyright (C) 2017 Guillaume Bour <guillaume@bour.cc>
+ *
+ * This file is part of Kamailio, a free SIP server.
+ *
+ * Kamailio is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version
+ *
+ * Kamailio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+/*! \file
+ * \ingroup keepalive
+ * \brief Keepalive :: Send keepalives
+ */
+
+/*! \defgroup keepalive Keepalive :: Probing remote gateways by sending keepalives
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <time.h>
+
+#include "../../core/rpc.h"
+#include "../../core/rpc_lookup.h"
+
+#include "keepalive.h"
+#include "api.h"
+
+static const char *keepalive_rpc_list_doc[2];
+static void keepalive_rpc_list(rpc_t *rpc, void *ctx);
+
+rpc_export_t keepalive_rpc_cmds[] = {
+	{"keepalive.list", keepalive_rpc_list, keepalive_rpc_list_doc, 0},
+	{0, 0, 0, 0}
+};
+
+int ka_init_rpc(void) {
+	if (rpc_register_array(keepalive_rpc_cmds) != 0) {
+		LM_ERR("failed to register RPC commands\n");
+	}
+
+	return 0;
+}
+
+static const char *keepalive_rpc_list_doc[2] = {
+		"Return the content of dispatcher sets", 0};
+
+static void keepalive_rpc_list(rpc_t *rpc, void *ctx) {
+	str text = str_init("foobar");
+	if (rpc->add(ctx, "Sd", &text, 42) < 0) 
+		LM_ERR("%s: error creating RPC struct\n", __FUNCTION__);
+	if (rpc->add(ctx, "Sd", &text, 42) < 0) 
+		LM_ERR("%s: error creating RPC struct\n", __FUNCTION__);
+
+	void *foo;
+	rpc->add(ctx, "{", &foo);
+	rpc->struct_add(foo, "Sd", "text", &text, "number", 42);
+
+	void *bar, *baz;
+	rpc->add(ctx, "{", &bar);
+	rpc->struct_add(bar, "[", "list", &baz);
+	rpc->struct_add(baz, "d", "nn", 17);
+	rpc->struct_add(baz, "d", "nn", 22);
+	
+
+	for(ka_dest_t *dest = ka_destinations_list->first; dest != NULL; dest = dest->next) {
+		void *sub;
+		rpc->add(ctx, "{", &sub);
+
+		rpc->struct_add(sub, "SS", 
+			"uri"  , &dest->uri,
+			"owner", &dest->owner);
+
+		char *_ctime = ctime(&dest->last_checked);
+		_ctime[strlen(_ctime) - 1] = '\0';
+		rpc->struct_add(sub, "s", "last checked", _ctime);
+		char *_utime = ctime(&dest->last_up);
+		_utime[strlen(_utime) - 1] = '\0';
+		rpc->struct_add(sub, "s", "last up", _utime);
+		char *_dtime = ctime(&dest->last_down);
+		_dtime[strlen(_dtime) - 1] = '\0';
+		rpc->struct_add(sub, "s", "last down", _dtime);
+	}
+
+	return;
+}
