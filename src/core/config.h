fad30d41ba822d1062e039dcf58f3af1b399e442
Mikko Lehto <mslehto@iki.fi>
2017-04-18 20:36:54 +0300

    core: remove dead code
    (cherry picked from commit 0df66ff94a9ccef4fe072f8704dc47f4f790cda4)

--- src/core/config.h
+++ src/core/config.h
@@ -35,8 +35,6 @@
 #ifndef config_h
 #define config_h
 
-#include "types.h"
-
 #define SIP_PORT  5060 /*!< default SIP port if none specified */
 #define SIPS_PORT 5061 /*!< default SIP port for TLS if none specified */
 
--- src/core/globals.h
+++ src/core/globals.h
@@ -32,7 +32,6 @@
 #ifndef globals_h
 #define globals_h
 
-#include "types.h"
 #include "ip_addr.h"
 #include "str.h"
 #include "poll_types.h"
@@ -113,7 +112,6 @@ extern int dont_daemonize;
 extern int check_via;
 extern int phone2tel;
 extern int received_dns;
-/* extern int process_no; */
 extern int child_rank;
 extern int sip_warning;
 extern int server_signature;
@@ -144,12 +142,6 @@ extern int auto_bind_ipv6;
 extern int tos;
 extern int pmtu_discovery;
 
-/*
- * debug & log_stderr moved to dprint.h*/
-
-/* extern process_bm_t process_bit; */
-/* extern int *pids; -moved to pt.h */
-
 extern int cfg_errors;
 extern int cfg_warnings;
 extern unsigned int msg_no;
@@ -159,11 +151,6 @@ extern unsigned long pkg_mem_size;
 
 /* AVP configuration */
 extern char *avp_db_url;  /* db url used by user preferences (AVPs) */
-
-/* moved to pt.h
-extern int *pids;
-extern int process_no;
-*/
 
 extern int reply_to_via;
 
