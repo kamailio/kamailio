a6e72190208fb81a197a30e4935a4ac61d8f2cde
Daniel-Constantin Mierla <miconda@gmail.com>
2017-04-25 22:08:48 +0200

    core: added helper function to free spve fixup parameters
    (cherry picked from commit 5a87defebe64c04c3276adb964c31031f64f0717)
    (cherry picked from commit f0efe7007f64ebef367534cf66dd76e9f6b0cc38)

--- mod_fix.h
+++ mod_fix.h
@@ -136,6 +136,7 @@ int fixup_spve_str(void** param, int param_no);
 int fixup_free_spve_str(void** param, int param_no);
 
 int fixup_spve_all(void** param, int param_no);
+int fixup_free_spve_all(void** param, int param_no);
 int fixup_igp_all(void** param, int param_no);
 
 int fixup_spve_igp(void** param, int param_no);
