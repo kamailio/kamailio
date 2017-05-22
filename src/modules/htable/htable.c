0c69e7bc4486ff6b10da6502b72c1805ac6e5f83
Daniel-Constantin Mierla <miconda@gmail.com>
2017-05-22 13:04:15 +0200

    htable: added functions to remove items using variables

--- src/modules/htable/htable.c
+++ src/modules/htable/htable.c
@@ -67,6 +67,8 @@ static void destroy(void);
 static int fixup_ht_key(void** param, int param_no);
 static int ht_rm_name_re(struct sip_msg* msg, char* key, char* foo);
 static int ht_rm_value_re(struct sip_msg* msg, char* key, char* foo);
+static int w_ht_rm_name(struct sip_msg* msg, char* hname, char* op, char *val);
+static int w_ht_rm_value(struct sip_msg* msg, char* hname, char* op, char *val);
 static int w_ht_slot_lock(struct sip_msg* msg, char* key, char* foo);
 static int w_ht_slot_unlock(struct sip_msg* msg, char* key, char* foo);
 static int ht_reset(struct sip_msg* msg, char* htname, char* foo);
@@ -106,6 +108,10 @@ static cmd_export_t cmds[]={
 	{"sht_rm_name_re",  (cmd_function)ht_rm_name_re,   1, fixup_ht_key, 0,
 		ANY_ROUTE},
 	{"sht_rm_value_re", (cmd_function)ht_rm_value_re,  1, fixup_ht_key, 0,
+		ANY_ROUTE},
+	{"sht_rm_name",  (cmd_function)w_ht_rm_name,   3, fixup_spve_all, 0,
+		ANY_ROUTE},
+	{"sht_rm_value", (cmd_function)w_ht_rm_value,  3, fixup_spve_all, 0,
 		ANY_ROUTE},
 	{"sht_lock",        (cmd_function)w_ht_slot_lock,    1, fixup_ht_key, 0,
 		ANY_ROUTE},
@@ -405,6 +411,72 @@ static int ht_rm_value_re(struct sip_msg* msg, char* key, char* foo)
 	if(ht_rm_cell_re(&sre, hpv->ht, 1)<0)
 		return -1;
 	return 1;
+}
+
+static int ht_rm_items(sip_msg_t* msg, str* hname, str* op, str *val,
+		int mkey)
+{
+	ht_t *ht;
+	int_str isval;
+
+	ht = ht_get_table(hname);
+	if(ht==NULL) {
+		LM_ERR("cannot get hash table [%.*s]\n", hname->len, hname->s);
+		return -1;
+	}
+
+	switch(op->len) {
+		case 2:
+			if(strncmp(op->s, "re", 2)==0) {
+				isval.s = *val;
+				if (ht_dmq_replicate_action(HT_DMQ_RM_CELL_RE, &ht->name, NULL,
+							AVP_VAL_STR, &isval, mkey)!=0) {
+					LM_ERR("dmq relication failed (op %d)\n", mkey);
+				}
+				if(ht_rm_cell_re(val, ht, mkey)<0) {
+					return -1;
+				}
+				return 1;
+			}
+			LM_WARN("unsupported match operator: %.*s\n", op->len, op->s);
+			break;
+		default:
+			LM_WARN("unsupported match operator: %.*s\n", op->len, op->s);
+	}
+	return -1;
+}
+
+static int w_ht_rm_items(sip_msg_t* msg, char* hname, char* op, char *val,
+		int mkey)
+{
+	str sname;
+	str sop;
+	str sval;
+
+	if(fixup_get_svalue(msg, (gparam_t*)hname, &sname)<0 || sname.len<=0) {
+		LM_ERR("cannot get the hash table name\n");
+		return -1;
+	}
+	if(fixup_get_svalue(msg, (gparam_t*)op, &sop)<0 || sop.len<=0) {
+		LM_ERR("cannot get the match operation\n");
+		return -1;
+	}
+	if(fixup_get_svalue(msg, (gparam_t*)val, &sval)<0 || sval.len<=0) {
+		LM_ERR("cannot the get match value\n");
+		return -1;
+	}
+
+	return ht_rm_items(msg, &sname, &sop, &sval, mkey);
+}
+
+static int w_ht_rm_name(sip_msg_t* msg, char* hname, char* op, char *val)
+{
+	return w_ht_rm_items(msg, hname, op, val, 0);
+}
+
+static int w_ht_rm_value(sip_msg_t* msg, char* hname, char* op, char *val)
+{
+	return w_ht_rm_items(msg, hname, op, val, 1);
 }
 
 static int ht_reset_by_name(str *hname)
