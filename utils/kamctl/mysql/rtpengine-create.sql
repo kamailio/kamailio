de953dfb93cfc3a14295f4167af01e522444b066
Daniel-Constantin Mierla <miconda@gmail.com>
2017-07-11 12:36:07 +0200

    kamctl: refreshed db creation scripts for sql databases

--- utils/kamctl/mysql/rtpengine-create.sql
+++ utils/kamctl/mysql/rtpengine-create.sql
@@ -5,7 +5,7 @@ CREATE TABLE `rtpengine` (
     `weight` INT(10) UNSIGNED DEFAULT 1 NOT NULL,
     `disabled` INT(1) DEFAULT 0 NOT NULL,
     `stamp` DATETIME DEFAULT '1900-01-01 00:00:01' NOT NULL,
-    CONSTRAINT rtpengine_nodes PRIMARY KEY  (`setid`, `url`)
+    CONSTRAINT rtpengine_nodes UNIQUE (`setid`, `url`)
 );
 
 INSERT INTO version (table_name, table_version) values ('rtpengine','1');
