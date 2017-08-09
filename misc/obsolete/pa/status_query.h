eff1e2c6a2ccceae1ddfc81298cbdcc789fcf6dd
Daniel-Constantin Mierla <miconda@gmail.com>
2017-08-09 12:34:46 +0200

    misc: removed obsolete folder
    - content relocated to its own github repo: kamailio-obsolete

--- misc/obsolete/pa/status_query.h
+++
@@ -1,7 +0,0 @@
-#ifndef __STATUS_QUERY_H
-#define __STATUS_QUERY_H
-
-int target_online(struct sip_msg* _m, char* _domain, char* _s2);
-/* add other query functions: get_online_contact, ... */
-
-#endif
--- misc/obsolete/rls/rpc.h
+++
@@ -1,8 +0,0 @@
-#ifndef __RLS_RPC_H
-#define __RLS_RPC_H
-
-#include "../../rpc.h"
-
-extern rpc_export_t rls_rpc_methods[];
-
-#endif
