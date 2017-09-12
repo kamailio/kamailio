0dd00c1aedc14e2b5509b84468c277e69056493b
Daniel-Constantin Mierla <miconda@gmail.com>
2017-09-12 16:10:10 +0200

    seas: added guards to several include files
    - bits of formating and removal of inline history
    
    (cherry picked from commit e04716f4f9325fa5290555823132455ea599daac)

--- src/modules/seas/encode_content_type.h
+++ src/modules/seas/encode_content_type.h
@@ -1,5 +1,4 @@
-/* $Id$
- *
+/*
  * Copyright (C) 2006-2007 VozTelecom Sistemas S.L
  *
  * This file is part of Kamailio, a free SIP server.
@@ -14,15 +13,17 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-
+#ifndef __ENCODE_CONTENT_TYPE_H__
+#define __ENCODE_CONTENT_TYPE_H__
 int encode_content_type(char *hdrstart,int hdrlen,unsigned int bodi,char *where);
 int encode_accept(char *hdrstart,int hdrlen,unsigned int *bodi,char *where);
 int encode_mime_type(char *hdrstart,int hdrlen,unsigned int bodi,char *where);
 int print_encoded_mime_type(FILE* fd,char *hdr,int hdrlen,unsigned int* payload,int paylen,char *prefix);
 int print_encoded_content_type(FILE* fd,char *hdr,int hdrlen,unsigned char* payload,int paylen,char *prefix);
 int print_encoded_accept(FILE* fd,char *hdr,int hdrlen,unsigned char* payload,int paylen,char *prefix);
+#endif
