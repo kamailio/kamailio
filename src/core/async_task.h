60db119bf8388f047cb7f7e2ce9f2e740b5abb8d
Daniel-Constantin Mierla <miconda@gmail.com>
2017-11-09 06:42:49 +0100

    core: warning when a task is pushed but no workers
    - helper functions to get the number of workers

--- src/core/async_task.h
+++ src/core/async_task.h
@@ -37,5 +37,7 @@ int async_task_initialized(void);
 int async_task_set_workers(int n);
 int async_task_push(async_task_t *task);
 int async_task_set_usleep(int n);
+int async_task_workers_get(void);
+int async_task_workers_active(void);
 
 #endif
