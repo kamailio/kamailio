2dd28a6285b688606b69691d33000d30f877c9a4
Daniel-Constantin Mierla <miconda@gmail.com>
2017-11-16 14:33:28 +0100

    drouting: removed svn ids and history

--- src/modules/drouting/drouting.c
+++ src/modules/drouting/drouting.c
@@ -1,6 +1,4 @@
 /*
- * $Id$
- *
  * Copyright (C) 2005-2009 Voice Sistem SRL
  *
  * This file is part of Kamailio, a free SIP server.
@@ -18,12 +16,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-
-
- * History:
- * ---------
- *  2005-02-20  first version (cristian)
- *  2005-02-27  ported to 0.9.0 (bogdan)
  */
 
 #include "stdlib.h"
