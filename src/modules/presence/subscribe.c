85b92f51b173bd76d288feeb69d576964ef1e9d4
lazedo <luis.azedo@factorlusitano.com>
2017-11-29 04:14:07 +0000

    presence: update contact in update_subs_db (cherry picked from commit 49272f23b22445415721766709bf5841865e8609)

--- src/modules/presence/subscribe.c
+++ src/modules/presence/subscribe.c
@@ -420,6 +420,13 @@ int update_subs_db(subs_t* subs, int type)
 		update_vals[n_update_cols].nul = 0;
 		update_vals[n_update_cols].val.int_val = subs->updated_winfo;
 		n_update_cols++;
+
+		update_keys[n_update_cols] = &str_contact_col;
+		update_vals[n_update_cols].type = DB1_STR;
+		update_vals[n_update_cols].nul = 0;
+		update_vals[n_update_cols].val.str_val = subs->contact;
+		n_update_cols++;
+
 	}
 	if(type & LOCAL_TYPE)
 	{
