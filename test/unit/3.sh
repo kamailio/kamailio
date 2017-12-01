f5ea35bc424f34c64ee71a745db93326f597c4a1
Daniel-Constantin Mierla <miconda@gmail.com>
2017-12-01 11:07:07 +0100

    test/unit: 3.sh - updates for using db root password with kamdbctl

--- test/unit/3.sh
+++ test/unit/3.sh
@@ -20,10 +20,10 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 # Needs a mysql database, the root user password must be assigned to
-# the 'PW' variable in the file 'dbrootpw' in the test directory, e.g.:
-# PW=sql_root_passwd
+# the 'DBROOTPW' variable in the file 'dbrootpw' in the test directory, e.g.:
+# DBROOTPW=sql_root_passwd
 # If MySQL root password is empty, add in the file the line:
-# PWSKIP=yes
+# DBROOTPWSKIP=yes
 
 . include/common
 
@@ -50,13 +50,13 @@ sed -i '' -e "s/TEST=\"false\"/TEST=\"true\"/g" $DBCTL
 
 # set the mysql root password
 cp $DBCTL.mysql $DBCTL.mysql.bak
-sed -i '' -e "s/#PW=\"\"/PW=\"$PW\"/g" $DBCTL.mysql
+sed -i '' -e "s/#DBROOTPW=\"\"/DBROOTPW=\"$DBROOTPW\"/g" $DBCTL.mysql
 
-PWSKIP="$PWSKIP" CHARSET="latin1" ./$DBCTL create $tmp_name > /dev/null
+DBROOTPWSKIP="$DBROOTPWSKIP" CHARSET="latin1" ./$DBCTL create $tmp_name > /dev/null
 ret=$?
 
 if [ "$ret" -eq 0 ] ; then
-	PWSKIP="$PWSKIP" ./$DBCTL drop $tmp_name > /dev/null
+	DBROOTPWSKIP="$DBROOTPWSKIP" ./$DBCTL drop $tmp_name > /dev/null
 	ret=$?
 fi ;
 
