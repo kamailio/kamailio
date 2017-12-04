782b76b40672075756348297bea61d2d38f96022
Daniel-Constantin Mierla <miconda@gmail.com>
2017-12-04 12:49:41 +0100

    jansson: fix the include guard in header file
    (cherry picked from commit d17a48c186b0b8066518db2106037fe925846d2e)
    (cherry picked from commit 90b1b0e1f00f72dcac7f7ef1c734ea43afd1cd83)

--- src/modules/jansson/jansson_path.h
+++ src/modules/jansson/jansson_path.h
@@ -2,12 +2,12 @@
  * Copyright (c) 2012 Rogerz Zhang <rogerz.zhang@gmail.com>
  *
  * Jansson is free software; you can redistribute it and/or modify
- * it under the terms of the MIT license. 
+ * it under the terms of the MIT license.
  */
 
 
-#ifndef _JANSSON_FUNCS_H_
-#define _JANSSON_FUNCS_H_
+#ifndef _JANSSON_PATH_H_
+#define _JANSSON_PATH_H_
 #include <jansson.h>
 
 json_t *json_path_get(const json_t *json, const char *path);
