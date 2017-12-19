2673911a9866953661e95de7f6d63bc9c0524cb4
Daniel-Constantin Mierla <miconda@gmail.com>
2017-12-19 10:02:42 +0100

    app_java: clang format the code
    (cherry picked from commit fddc58b97796904a17bb2d01f4480e095a5c4cda)

--- src/modules/app_java/java_native_methods.h
+++ src/modules/app_java/java_native_methods.h
@@ -21,64 +21,93 @@
  */
 
 #ifndef __JAVA_NATIVE_METHODS_H__
-#define	__JAVA_NATIVE_METHODS_H__
+#define __JAVA_NATIVE_METHODS_H__
 
 #include "../../core/str.h"
 #include "../../core/sr_module.h"
 
 #include <jni.h>
 
-JNIEXPORT void JNICALL Java_org_siprouter_NativeMethods_LM_1ERR(JNIEnv *, jobject, jstring);
-JNIEXPORT void JNICALL Java_org_siprouter_NativeMethods_LM_1WARN(JNIEnv *, jobject, jstring);
-JNIEXPORT void JNICALL Java_org_siprouter_NativeMethods_LM_1NOTICE(JNIEnv *, jobject, jstring);
-JNIEXPORT void JNICALL Java_org_siprouter_NativeMethods_LM_1INFO(JNIEnv *, jobject, jstring);
-JNIEXPORT void JNICALL Java_org_siprouter_NativeMethods_LM_1DBG(JNIEnv *, jobject, jstring);
-JNIEXPORT void JNICALL Java_org_siprouter_NativeMethods_LM_1CRIT(JNIEnv *, jobject, jstring);
-JNIEXPORT void JNICALL Java_org_siprouter_NativeMethods_LM_1ALERT(JNIEnv *, jobject, jstring);
-JNIEXPORT void JNICALL Java_org_siprouter_NativeMethods_LM_1GEN1(JNIEnv *, jobject, jint, jstring);
-JNIEXPORT void JNICALL Java_org_siprouter_NativeMethods_LM_1GEN2(JNIEnv *, jobject, jint, jint, jstring);
+JNIEXPORT void JNICALL Java_org_siprouter_NativeMethods_LM_1ERR(
+		JNIEnv *, jobject, jstring);
+JNIEXPORT void JNICALL Java_org_siprouter_NativeMethods_LM_1WARN(
+		JNIEnv *, jobject, jstring);
+JNIEXPORT void JNICALL Java_org_siprouter_NativeMethods_LM_1NOTICE(
+		JNIEnv *, jobject, jstring);
+JNIEXPORT void JNICALL Java_org_siprouter_NativeMethods_LM_1INFO(
+		JNIEnv *, jobject, jstring);
+JNIEXPORT void JNICALL Java_org_siprouter_NativeMethods_LM_1DBG(
+		JNIEnv *, jobject, jstring);
+JNIEXPORT void JNICALL Java_org_siprouter_NativeMethods_LM_1CRIT(
+		JNIEnv *, jobject, jstring);
+JNIEXPORT void JNICALL Java_org_siprouter_NativeMethods_LM_1ALERT(
+		JNIEnv *, jobject, jstring);
+JNIEXPORT void JNICALL Java_org_siprouter_NativeMethods_LM_1GEN1(
+		JNIEnv *, jobject, jint, jstring);
+JNIEXPORT void JNICALL Java_org_siprouter_NativeMethods_LM_1GEN2(
+		JNIEnv *, jobject, jint, jint, jstring);
 
-JNIEXPORT jint JNICALL Java_org_siprouter_NativeMethods_KamExec(JNIEnv *, jobject, jstring, jobjectArray);
+JNIEXPORT jint JNICALL Java_org_siprouter_NativeMethods_KamExec(
+		JNIEnv *, jobject, jstring, jobjectArray);
 int KamExec(JNIEnv *, char *, int, char **);
 
 
-JNIEXPORT jobject JNICALL Java_org_siprouter_SipMsg_ParseSipMsg(JNIEnv *, jobject);
+JNIEXPORT jobject JNICALL Java_org_siprouter_SipMsg_ParseSipMsg(
+		JNIEnv *, jobject);
 
-JNIEXPORT jstring JNICALL Java_org_siprouter_SipMsg_getMsgType(JNIEnv *, jobject);
-JNIEXPORT jstring JNICALL Java_org_siprouter_SipMsg_getStatus(JNIEnv *, jobject);
+JNIEXPORT jstring JNICALL Java_org_siprouter_SipMsg_getMsgType(
+		JNIEnv *, jobject);
+JNIEXPORT jstring JNICALL Java_org_siprouter_SipMsg_getStatus(
+		JNIEnv *, jobject);
 JNIEXPORT jstring JNICALL Java_org_siprouter_SipMsg_getRURI(JNIEnv *, jobject);
-JNIEXPORT jobject JNICALL Java_org_siprouter_SipMsg_getSrcAddress(JNIEnv *, jobject);
-JNIEXPORT jobject JNICALL Java_org_siprouter_SipMsg_getDstAddress(JNIEnv *, jobject);
-JNIEXPORT jstring JNICALL Java_org_siprouter_SipMsg_getBuffer(JNIEnv *, jobject);
+JNIEXPORT jobject JNICALL Java_org_siprouter_SipMsg_getSrcAddress(
+		JNIEnv *, jobject);
+JNIEXPORT jobject JNICALL Java_org_siprouter_SipMsg_getDstAddress(
+		JNIEnv *, jobject);
+JNIEXPORT jstring JNICALL Java_org_siprouter_SipMsg_getBuffer(
+		JNIEnv *, jobject);
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-JNIEXPORT jint JNICALL Java_org_siprouter_CoreMethods_seturi(JNIEnv *, jobject, jstring);
-JNIEXPORT jint JNICALL Java_org_siprouter_CoreMethods_rewriteuri(JNIEnv *, jobject, jstring);
+JNIEXPORT jint JNICALL Java_org_siprouter_CoreMethods_seturi(
+		JNIEnv *, jobject, jstring);
+JNIEXPORT jint JNICALL Java_org_siprouter_CoreMethods_rewriteuri(
+		JNIEnv *, jobject, jstring);
 jint cf_seturi(JNIEnv *, jobject, jstring, char *);
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-JNIEXPORT jint JNICALL Java_org_siprouter_CoreMethods_add_1local_1rport(JNIEnv *, jobject);
-JNIEXPORT jint JNICALL Java_org_siprouter_CoreMethods_append_1branch(JNIEnv *, jobject, jstring);
+JNIEXPORT jint JNICALL Java_org_siprouter_CoreMethods_add_1local_1rport(
+		JNIEnv *, jobject);
+JNIEXPORT jint JNICALL Java_org_siprouter_CoreMethods_append_1branch(
+		JNIEnv *, jobject, jstring);
 JNIEXPORT jint JNICALL Java_org_siprouter_CoreMethods_drop(JNIEnv *, jobject);
 
-JNIEXPORT jint JNICALL Java_org_siprouter_CoreMethods_force_1rport(JNIEnv *, jobject);
-JNIEXPORT jint JNICALL Java_org_siprouter_CoreMethods_add_1rport(JNIEnv *, jobject);
+JNIEXPORT jint JNICALL Java_org_siprouter_CoreMethods_force_1rport(
+		JNIEnv *, jobject);
+JNIEXPORT jint JNICALL Java_org_siprouter_CoreMethods_add_1rport(
+		JNIEnv *, jobject);
 jint cf_force_rport(JNIEnv *, jobject, char *);
 
 // not confirmed as working
-JNIEXPORT jint JNICALL Java_org_siprouter_CoreMethods_force_1send_1socket(JNIEnv *, jobject, jstring, jint);
+JNIEXPORT jint JNICALL Java_org_siprouter_CoreMethods_force_1send_1socket(
+		JNIEnv *, jobject, jstring, jint);
 
 // not confirmed as working
-JNIEXPORT jint JNICALL Java_org_siprouter_CoreMethods_forward(JNIEnv *, jobject, jstring, jint);
+JNIEXPORT jint JNICALL Java_org_siprouter_CoreMethods_forward(
+		JNIEnv *, jobject, jstring, jint);
 
-JNIEXPORT jboolean JNICALL Java_org_siprouter_CoreMethods_isflagset(JNIEnv *, jobject, jint);
-JNIEXPORT void JNICALL Java_org_siprouter_CoreMethods_setflag(JNIEnv *, jobject, jint);
-JNIEXPORT void JNICALL Java_org_siprouter_CoreMethods_resetflag(JNIEnv *, jobject, jint);
-JNIEXPORT jint JNICALL Java_org_siprouter_CoreMethods_revert_1uri(JNIEnv *, jobject);
-JNIEXPORT jint JNICALL Java_org_siprouter_CoreMethods_route(JNIEnv *, jobject, jobject);
+JNIEXPORT jboolean JNICALL Java_org_siprouter_CoreMethods_isflagset(
+		JNIEnv *, jobject, jint);
+JNIEXPORT void JNICALL Java_org_siprouter_CoreMethods_setflag(
+		JNIEnv *, jobject, jint);
+JNIEXPORT void JNICALL Java_org_siprouter_CoreMethods_resetflag(
+		JNIEnv *, jobject, jint);
+JNIEXPORT jint JNICALL Java_org_siprouter_CoreMethods_revert_1uri(
+		JNIEnv *, jobject);
+JNIEXPORT jint JNICALL Java_org_siprouter_CoreMethods_route(
+		JNIEnv *, jobject, jobject);
 
 #endif
