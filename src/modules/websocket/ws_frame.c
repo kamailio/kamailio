a6ad018a1d7d796272bc0cb07f7540c711990e37
Daniel-Constantin Mierla <miconda@gmail.com>
2017-12-31 10:39:16 +0100

    websocket: early check for frame size to fit max buf size
    - avoid decoding a large buffer and then fail
    - allocate BUF_SIZE+1 for fragment buffer, coherent with other recv
    buffers
    
    (cherry picked from commit 3302687e2b995ee9faab1655e6bb5e5d4a0dbc87)

--- src/modules/websocket/ws_frame.c
+++ src/modules/websocket/ws_frame.c
@@ -406,7 +406,7 @@ static int decode_and_validate_ws_frame(ws_frame_t *frame,
                                         short *err_code, str *err_text)
 {
 	unsigned int i, len = tcpinfo->len;
-	int mask_start, j;
+	unsigned int mask_start, j;
 	char *buf = tcpinfo->buf;
 
 	LM_DBG("decoding WebSocket frame\n");
@@ -533,6 +533,13 @@ static int decode_and_validate_ws_frame(ws_frame_t *frame,
 			frame->payload_len + mask_start + 4, len);
 		*err_code = 1002;
 		*err_text = str_status_protocol_error;
+		return -1;
+	}
+	if(frame->payload_len >= BUF_SIZE) {
+		LM_WARN("message is too long for our buffer size (%d / %d)\n",
+				BUF_SIZE, frame->payload_len);
+		*err_code = 1009;
+		*err_text = str_status_message_too_big;
 		return -1;
 	}
 	frame->payload_data = &buf[mask_start + 4];
