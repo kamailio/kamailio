f622be60abe31bc4d0618b92caddced4fb5a0287
Daniel-Constantin Mierla <miconda@gmail.com>
2018-01-17 22:18:20 +0100

    exec: clang format the code
    (cherry picked from commit 34ec1fc381916ee2ce7b08afce413bf2aa427a55)

--- src/modules/exec/exec.h
+++ src/modules/exec/exec.h
@@ -13,14 +13,14 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 /*!
  * \file
  * \brief Exec module:: Module interface
- * \ingroup exec 
+ * \ingroup exec
  * Module: \ref exec
  */
 
@@ -30,8 +30,7 @@
 #include "../../core/pvar.h"
 
 int exec_str(struct sip_msg *msg, char *cmd, char *param, int param_len);
-int exec_msg(struct sip_msg *msg, char *cmd );
+int exec_msg(struct sip_msg *msg, char *cmd);
 int exec_avp(struct sip_msg *msg, char *cmd, pvname_list_p avpl);
 
 #endif
-
