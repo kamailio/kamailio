4244ccd34f525a2dbabfd7734747aaac2caa9318
Emmanuel Schmidbauer <eschmidbauer@gmail.com>
2018-02-08 09:29:47 -0500

    pua_json: remove contributors from source

--- src/modules/pua_json/pua_json_publish.c
+++ src/modules/pua_json/pua_json_publish.c
@@ -19,9 +19,6 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * Contributor(s):
- * 2600Hz Team
- * Emmanuel Schmidbauer <eschmidbauer@gmail.com>
  *
  */
 
