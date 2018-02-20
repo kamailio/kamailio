6568170a68a8850df164707bda87bb67eb7d6523
AnthonyA <ascanio.alba7@gmail.com>
2018-02-20 21:14:48 +0800

    app_python3: update documentation
    - update documentation and python_examples/ to reflect the new module name
    - fix typo arg->args in python_examples/handler.py

--- src/modules/app_python3/python_examples/Loggers.py
+++ src/modules/app_python3/python_examples/Loggers.py
@@ -46,61 +46,62 @@ import Router.Ranks as Ranks
         PROC_TCP_MAIN
         PROC_UNIXSOCK
         PROC_ATTENDANT
-	PROC_INIT
-	PROC_NOCHLDINIT
-	PROC_SIPINIT
-	PROC_SIPRPC
-	PROC_MIN
+        PROC_INIT
+        PROC_NOCHLDINIT
+        PROC_SIPINIT
+        PROC_SIPRPC
+        PROC_MIN
 
 """
+
 
 class Loggers:
 
     def __init__(self):
-	pass
+        pass
 
     def __del__(self):
-	pass
+        pass
 
     def child_init(self, rank):
-	if rank == Ranks.PROC_MAIN:
-	    Logger.LM_ERR("rank is PROC_MAIN")
-	elif rank == Ranks.PROC_TIMER:
-	    Logger.LM_ERR("rank is PROC_TIMER")
-	elif rank == Ranks.PROC_RPC:
-	    Logger.LM_ERR("rank is PROC_RPC")
-	elif rank == Ranks.PROC_FIFO:
-	    Logger.LM_ERR("rank is PROC_FIFO")
-	elif rank == Ranks.PROC_TCP_MAIN:
-	    Logger.LM_ERR("rank is PROC_TCP_MAIN")
-	elif rank == Ranks.PROC_UNIXSOCK:
-	    Logger.LM_ERR("rank is PROC_UNIXSOCK")
-	elif rank == Ranks.PROC_ATTENDANT:
-	    Logger.LM_ERR("rank is PROC_ATTENDANT")
-	elif rank == Ranks.PROC_INIT:
-	    Logger.LM_ERR("rank is PROC_INIT")
-	elif rank == Ranks.PROC_NOCHLDINIT:
-	    Logger.LM_ERR("rank is PROC_NOCHLDINIT")
-	elif rank == Ranks.PROC_SIPINIT:
-	    Logger.LM_ERR("rank is PROC_SIPINIT")
-	elif rank == Ranks.PROC_SIPRPC:
-	    Logger.LM_ERR("rank is PROC_SIPRPC")
-	elif rank == Ranks.PROC_MIN:
-	    Logger.LM_ERR("rank is PROC_MIN")
+        if rank == Ranks.PROC_MAIN:
+            Logger.LM_ERR("rank is PROC_MAIN")
+        elif rank == Ranks.PROC_TIMER:
+            Logger.LM_ERR("rank is PROC_TIMER")
+        elif rank == Ranks.PROC_RPC:
+            Logger.LM_ERR("rank is PROC_RPC")
+        elif rank == Ranks.PROC_FIFO:
+            Logger.LM_ERR("rank is PROC_FIFO")
+        elif rank == Ranks.PROC_TCP_MAIN:
+            Logger.LM_ERR("rank is PROC_TCP_MAIN")
+        elif rank == Ranks.PROC_UNIXSOCK:
+            Logger.LM_ERR("rank is PROC_UNIXSOCK")
+        elif rank == Ranks.PROC_ATTENDANT:
+            Logger.LM_ERR("rank is PROC_ATTENDANT")
+        elif rank == Ranks.PROC_INIT:
+            Logger.LM_ERR("rank is PROC_INIT")
+        elif rank == Ranks.PROC_NOCHLDINIT:
+            Logger.LM_ERR("rank is PROC_NOCHLDINIT")
+        elif rank == Ranks.PROC_SIPINIT:
+            Logger.LM_ERR("rank is PROC_SIPINIT")
+        elif rank == Ranks.PROC_SIPRPC:
+            Logger.LM_ERR("rank is PROC_SIPRPC")
+        elif rank == Ranks.PROC_MIN:
+            Logger.LM_ERR("rank is PROC_MIN")
 
-	return 0
+        return 0
 
     def TestLoggers(self, msg, args):
-	Logger.LM_GEN1(Logger.L_INFO,                           	"Loggers.py:     LM_GEN1: msg: %s" % str(args))
-	Logger.LM_GEN2(Logger.L_INFO, Logger.DEFAULT_FACILITY,	        "Loggers.py:     LM_GEN2: msg: %s" % str(args))
-	Logger.LM_ALERT(                                        	"Loggers.py:    LM_ALERT: msg: %s" % str(args))
-	Logger.LM_CRIT(                                         	"Loggers.py:     LM_CRIT: msg: %s" % str(args))
-	Logger.LM_ERR(                                          	"Loggers.py:      LM_ERR: msg: %s" % str(args))
-	Logger.LM_WARN(                                         	"Loggers.py:     LM_WARN: msg: %s" % str(args))
-	Logger.LM_NOTICE(                                       	"Loggers.py:   LM_NOTICE: msg: %s" % str(args))
-	Logger.LM_INFO(                                         	"Loggers.py:     LM_INFO: msg: %s" % str(args))
-	Logger.LM_DBG(                                          	"Loggers.py:      LM_DBG: msg: %s" % str(args))
-	return 1
+        Logger.LM_GEN1(Logger.L_INFO,                                   "Loggers.py:     LM_GEN1: msg: %s" % str(args))
+        Logger.LM_GEN2(Logger.L_INFO, Logger.DEFAULT_FACILITY,                "Loggers.py:     LM_GEN2: msg: %s" % str(args))
+        Logger.LM_ALERT(                                                "Loggers.py:    LM_ALERT: msg: %s" % str(args))
+        Logger.LM_CRIT(                                                 "Loggers.py:     LM_CRIT: msg: %s" % str(args))
+        Logger.LM_ERR(                                                  "Loggers.py:      LM_ERR: msg: %s" % str(args))
+        Logger.LM_WARN(                                                 "Loggers.py:     LM_WARN: msg: %s" % str(args))
+        Logger.LM_NOTICE(                                               "Loggers.py:   LM_NOTICE: msg: %s" % str(args))
+        Logger.LM_INFO(                                                 "Loggers.py:     LM_INFO: msg: %s" % str(args))
+        Logger.LM_DBG(                                                  "Loggers.py:      LM_DBG: msg: %s" % str(args))
+        return 1
 
 def mod_init():
     return Loggers()
