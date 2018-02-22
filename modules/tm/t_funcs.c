6eb530626184641c9fbb10bd819d253be2e39947
Daniel-Constantin Mierla <miconda@gmail.com>
2018-02-22 10:45:29 +0100

    tm: handle success return code by forward_request() for e2e ACK
    - forward_request() returns 0 on success, propagate 1 to config wrapper
    - backport of 171e3b9f197ffebaa2b1a1959ba109bb442764a2
    
    (cherry picked from commit ef84d989228836c28dd5ec5acea5c4239cab58db)

--- modules/tm/t_funcs.c
+++ modules/tm/t_funcs.c
@@ -282,7 +282,7 @@ int t_relay_to( struct sip_msg  *p_msg , struct proxy_l *proxy, int proto,
 	/* at this point if the msg is an ACK it is an e2e ACK and
 	   e2e ACKs do not establish a transaction and are fwd-ed statelessly */
 	if ( p_msg->REQ_METHOD==METHOD_ACK) {
-		DBG( "SER: forwarding ACK  statelessly \n");
+		DBG("forwarding ACK statelessly\n");
 		if (proxy==0) {
 			init_dest_info(&dst);
 			dst.proto=proto;
@@ -303,7 +303,11 @@ int t_relay_to( struct sip_msg  *p_msg , struct proxy_l *proxy, int proto,
 			proxy2su(&dst.to, proxy);
 			/* dst->send_sock not set, but forward_request will take care
 			 * of it */
-			ret=forward_request( p_msg , 0, 0, &dst) ;
+			ret=forward_request(p_msg, 0, 0, &dst);
+		}
+		if (ret>=0) {
+			/* convert return code for cfg script */
+			ret=1;
 		}
 		goto done;
 	}
