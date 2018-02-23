6d0f8994611b50faa7ef7d1299acf0c390a2eed1
Claudiu Boriga <paul.boriga@1and1.ro>
2018-02-23 16:12:42 +0200

    presence: test if event is dialog to avoid unnecessary xml parsing

--- src/modules/presence/presentity.c
+++ src/modules/presence/presentity.c
@@ -734,17 +734,18 @@ int update_presentity(struct sip_msg* msg, presentity_t* presentity, str* body,
 					goto error;
 				}
 			}
+			if (presentity->event->evp->type == EVENT_DIALOG) {
+				check_if_dialog(*body, &is_dialog, &dialog_id);
+				if (dialog_id) {
+					if (delete_presentity_if_dialog_id_exists(presentity, dialog_id) < 0) {
+						free(dialog_id);
+						dialog_id = NULL;
+						goto error;
+					}
 
-			check_if_dialog(*body, &is_dialog, &dialog_id);
-			if ( dialog_id ) {
-				if (delete_presentity_if_dialog_id_exists(presentity, dialog_id) < 0) {
 					free(dialog_id);
 					dialog_id = NULL;
-					goto error;
 				}
-
-				free(dialog_id);
-				dialog_id = NULL;
 			}
 			LM_DBG("inserting %d cols into table\n",n_query_cols);
 
