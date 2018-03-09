31aee7380ba1aa0cb6eddeaae709829087504aae
Daniel-Constantin Mierla <miconda@gmail.com>
2018-03-09 09:44:10 +0100

    app_jsdt: check if kemi onsend_route callback name is set

--- src/modules/app_jsdt/app_jsdt_mod.c
+++ src/modules/app_jsdt/app_jsdt_mod.c
@@ -165,7 +165,11 @@ int sr_kemi_config_engine_jsdt(sip_msg_t *msg, int rtype, str *rname,
 			ret = app_jsdt_run_ex(msg, rname->s, NULL, NULL, NULL, 0);
 		}
 	} else if(rtype==ONSEND_ROUTE) {
-		ret = app_jsdt_run_ex(msg, "ksr_onsend_route", NULL, NULL, NULL, 0);
+		if(kemi_onsend_route_callback.len>0) {
+			ret = app_jsdt_run_ex(msg, kemi_onsend_route_callback.s,
+					NULL, NULL, NULL, 0);
+		}
+		return 1;
 	} else if(rtype==EVENT_ROUTE) {
 		if(rname!=NULL && rname->s!=NULL) {
 			ret = app_jsdt_run_ex(msg, rname->s,
