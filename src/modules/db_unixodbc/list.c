7ec9dd32fcd5454b7d9d584ec790a9a10af9d20a
Henning Westerholt <hw@kamailio.org>
2018-03-14 21:55:20 +0100

    db_unixodbc: doxygen fixes

--- src/modules/db_unixodbc/list.c
+++ src/modules/db_unixodbc/list.c
@@ -98,7 +98,7 @@ int db_unixodbc_list_insert(list** start, list** link, int n, strn* value)
 
 /*!
  * \brief Destroy a list
- * \param link list element(s)
+ * \param start start of the list element(s)
  */
 void db_unixodbc_list_destroy(list *start)
 {
