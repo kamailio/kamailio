2f63caf7ac87c1eca9c6f4150dc7418fa9f4fc35
Henning Westerholt <hw@kamailio.org>
2018-06-10 22:39:35 +0200

    core: dns name to IP conversion - actually stop processing in error case
    (cherry picked from commit 55d115e255b1d87121922f689b93870b84e1b2b4)

--- src/core/resolve.h
+++ src/core/resolve.h
@@ -232,6 +232,7 @@ static inline struct ip_addr* str2ip(str* st)
 	/* just in case that e.g. the VIA parser get confused */
 	if(unlikely(!st->s || st->len <= 0)) {
 		LM_ERR("invalid name, no conversion to IP address possible\n");
+		return 0;
 	}
 	s=(unsigned char*)st->s;
 
@@ -288,6 +289,7 @@ static inline struct ip_addr* str2ip6(str* st)
 	/* just in case that e.g. the VIA parser get confused */
 	if(unlikely(!st->s || st->len <= 0)) {
 		LM_ERR("invalid name, no conversion to IP address possible\n");
+		return 0;
 	}
 	/* init */
 	if ((st->len) && (st->s[0]=='[')){
