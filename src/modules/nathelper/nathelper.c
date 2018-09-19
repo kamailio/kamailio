514b2eb25b36f0a59c0897a462b276e8f45d73cd
Daniel-Constantin Mierla <miconda@gmail.com>
2018-09-19 09:03:10 +0200

    nathelper: exported fix_nated_sdp() variants to KEMI framework
    (cherry picked from commit 0d14ce8bf45ada579f7ec85793d358a5fa32e4b7)

--- src/modules/nathelper/nathelper.c
+++ src/modules/nathelper/nathelper.c
@@ -1548,24 +1548,14 @@ static inline int replace_sdp_ip(
 	return count;
 }
 
-static int fix_nated_sdp_f(struct sip_msg *msg, char *str1, char *str2)
+static int ki_fix_nated_sdp_ip(sip_msg_t *msg, int level, str *ip)
 {
 	str body;
-	str ip;
-	int level, rest_len;
+	int rest_len;
 	char *buf, *m_start, *m_end;
 	struct lump *anchor;
 	int ret;
 	int count = 0;
-
-	if(fixup_get_ivalue(msg, (gparam_t *)str1, &level) != 0) {
-		LM_ERR("failed to get value for first parameter\n");
-		return -1;
-	}
-	if(str2 && fixup_get_svalue(msg, (gparam_t *)str2, &ip) != 0) {
-		LM_ERR("failed to get value for second parameter\n");
-		return -1;
-	}
 
 	if(extract_body(msg, &body) == -1) {
 		LM_ERR("cannot extract body from msg!\n");
@@ -1631,7 +1621,7 @@ static int fix_nated_sdp_f(struct sip_msg *msg, char *str1, char *str2)
 
 	if(level & FIX_MEDIP) {
 		/* Iterate all c= and replace ips in them. */
-		ret = replace_sdp_ip(msg, &body, "c=", str2 ? &ip : 0);
+		ret = replace_sdp_ip(msg, &body, "c=", (ip && ip->len>0) ? ip : 0);
 		if(ret == -1)
 			return -1;
 		count += ret;
@@ -1639,13 +1629,35 @@ static int fix_nated_sdp_f(struct sip_msg *msg, char *str1, char *str2)
 
 	if(level & FIX_ORGIP) {
 		/* Iterate all o= and replace ips in them. */
-		ret = replace_sdp_ip(msg, &body, "o=", str2 ? &ip : 0);
+		ret = replace_sdp_ip(msg, &body, "o=",  (ip && ip->len>0) ? ip : 0);
 		if(ret == -1)
 			return -1;
 		count += ret;
 	}
 
 	return count > 0 ? 1 : 2;
+}
+
+static int ki_fix_nated_sdp(sip_msg_t *msg, int level)
+{
+	return ki_fix_nated_sdp_ip(msg, level, NULL);
+}
+
+static int fix_nated_sdp_f(struct sip_msg *msg, char *str1, char *str2)
+{
+	int level;
+	str ip;
+
+	if(fixup_get_ivalue(msg, (gparam_t *)str1, &level) != 0) {
+		LM_ERR("failed to get value for first parameter\n");
+		return -1;
+	}
+	if(str2 && fixup_get_svalue(msg, (gparam_t *)str2, &ip) != 0) {
+		LM_ERR("failed to get value for second parameter\n");
+		return -1;
+	}
+
+	return ki_fix_nated_sdp_ip(msg, level, &ip);
 }
 
 static int extract_mediaip(str *body, str *mediaip, int *pf, char *line)
@@ -2423,6 +2435,16 @@ static sr_kemi_t sr_kemi_nathelper_exports[] = {
 	{ str_init("nathelper"), str_init("add_rcv_param"),
 		SR_KEMIP_INT, ki_add_rcv_param,
 		{ SR_KEMIP_INT, SR_KEMIP_NONE, SR_KEMIP_NONE,
+			SR_KEMIP_NONE, SR_KEMIP_NONE, SR_KEMIP_NONE }
+	},
+	{ str_init("nathelper"), str_init("fix_nated_sdp"),
+		SR_KEMIP_INT, ki_fix_nated_sdp,
+		{ SR_KEMIP_INT, SR_KEMIP_NONE, SR_KEMIP_NONE,
+			SR_KEMIP_NONE, SR_KEMIP_NONE, SR_KEMIP_NONE }
+	},
+	{ str_init("nathelper"), str_init("fix_nated_sdp_ip"),
+		SR_KEMIP_INT, ki_fix_nated_sdp_ip,
+		{ SR_KEMIP_INT, SR_KEMIP_STR, SR_KEMIP_NONE,
 			SR_KEMIP_NONE, SR_KEMIP_NONE, SR_KEMIP_NONE }
 	},
 
