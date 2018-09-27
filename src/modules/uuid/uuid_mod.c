2087b9360fb407edb6ee2d5961cd8cf179f798a2
Daniel-Constantin Mierla <miconda@gmail.com>
2018-09-27 23:10:07 +0200

    uuid: updated to the new mod interface

--- src/modules/uuid/uuid_mod.c
+++ src/modules/uuid/uuid_mod.c
@@ -61,18 +61,16 @@ static param_export_t params[]={
 };
 
 struct module_exports exports = {
-	"uuid",
+	"uuid",          /* module name */
 	DEFAULT_DLFLAGS, /* dlopen flags */
-	cmds,
-	params,
-	0,
-	0,              /* exported MI functions */
-	mod_pvs,        /* exported pseudo-variables */
-	0,              /* extra processes */
-	mod_init,       /* module initialization function */
-	0,              /* response function */
-	mod_destroy,    /* destroy function */
-	child_init      /* per child init function */
+	cmds,            /* exported functions */
+	params,          /* exported parameters */
+	0,               /* exported rpc functions */
+	mod_pvs,         /* exported pseudo-variables */
+	0,               /* response handling function */
+	mod_init,        /* module init function */
+	child_init,      /* per child init function */
+	mod_destroy      /* destroy function */
 };
 
 
