7678d2921186a1b6ce37cdb160f390edb1d84636
Daniel-Constantin Mierla <miconda@gmail.com>
2018-09-27 23:34:01 +0200

    tmrec: updated to the new mod interface

--- src/modules/tmrec/tmrec_mod.c
+++ src/modules/tmrec/tmrec_mod.c
@@ -85,16 +85,14 @@ static param_export_t params[]={
 struct module_exports exports = {
 	"tmrec",
 	DEFAULT_DLFLAGS, /* dlopen flags */
-	cmds,
-	params,
-	0,
-	0,              /* exported MI functions */
-	0,              /* exported pseudo-variables */
-	0,              /* extra processes */
-	mod_init,       /* module initialization function */
-	0,              /* response function */
-	mod_destroy,    /* destroy function */
-	child_init      /* per child init function */
+	cmds,            /* exported functions */
+	params,          /* exported parameters */
+	0,               /* exported rpc functions */
+	0,               /* exported pseudo-variables */
+	0,               /* response function */
+	mod_init,        /* module init function */
+	child_init,      /* per child init function */
+	mod_destroy      /* destroy function */
 };
 
 
