1be9a9f1ddcedadf94b810964b08d79e9036f3f4
Charles Chance <charles.chance@sipcentric.com>
2018-09-27 18:07:27 +0200

    group: updated to new module interface format

--- src/modules/group/group_mod.c
+++ src/modules/group/group_mod.c
@@ -140,18 +140,16 @@ static param_export_t params[] = {
  * Module interface
  */
 struct module_exports exports = {
-	"group", 
-	DEFAULT_DLFLAGS, /* dlopen flags */
-	cmds,       /* Exported functions */
-	params,     /* Exported parameters */
-	0,          /* exported statistics */
-	0,          /* exported MI functions */
-	0,          /* exported pseudo-variables */
-	0,          /* extra processes */
-	mod_init,   /* module initialization function */
-	0,          /* response function */
-	destroy,    /* destroy function */
-	child_init  /* child initialization function */
+	"group",			/* module name */
+	DEFAULT_DLFLAGS,	/* dlopen flags */
+	cmds,				/* exported functions */
+	params,				/* exported parameters */
+	0,					/* RPC method exports */
+	0,					/* exported pseudo-variables */
+	0,					/* response handling function */
+	mod_init,			/* module initialization function */
+	child_init,			/* per-child init function */
+	destroy				/* module destroy function */
 };
 
 
