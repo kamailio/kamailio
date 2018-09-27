5859131b07dfbbeba1c3d769c656063f60d21197
Charles Chance <charles.chance@sipcentric.com>
2018-09-27 17:16:07 +0200

    domain: updated to new module interface format

--- src/modules/domain/domain_mod.c
+++ src/modules/domain/domain_mod.c
@@ -133,18 +133,16 @@ static param_export_t params[] = {
  */
 /* clang-format off */
 struct module_exports exports = {
-	"domain",
-	DEFAULT_DLFLAGS, /* dlopen flags */
-	cmds,      /* Exported functions */
-	params,    /* Exported parameters */
-	0,         /* exported statistics */
-	0,         /* exported MI functions */
-	0,         /* exported pseudo-variables */
-	0,         /* extra processes */
-	mod_init,  /* module initialization function */
-	0,         /* response function*/
-	destroy,   /* destroy function */
-	child_init /* per-child init function */
+	"domain",			/* module name */
+	DEFAULT_DLFLAGS,	/* dlopen flags */
+	cmds,				/* exported functions */
+	params,				/* exported parameters */
+	0,					/* RPC method exports */
+	0,					/* exported pseudo-variables */
+	0,					/* response handling function */
+	mod_init,			/* module initialization function */
+	child_init,			/* per-child init function */
+	destroy				/* module destroy function */
 };
 /* clang-format on */
 
