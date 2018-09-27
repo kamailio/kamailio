00cafe2124681c2b95b3e5fd38a89d9f4b5320c1
Federico Cabiddu <fcabiddu@libon.com>
2018-09-27 17:44:17 +0200

    benchmark: converted to the new module interface

--- src/modules/benchmark/benchmark.c
+++ src/modules/benchmark/benchmark.c
@@ -154,14 +154,12 @@ struct module_exports exports = {
 	DEFAULT_DLFLAGS,
 	cmds,       /* Exported functions */
 	params,     /* Exported parameters */
-	0,          /* exported statistics */
-	0,          /* exported MI functions */
+	0,          /* exported RPC methods */
 	mod_items,  /* exported pseudo-variables */
-	0,          /* extra processes */
-	mod_init,   /* module initialization function */
 	0,          /* response function */
-	destroy,    /* destroy function */
-	0           /* child initialization function */
+	mod_init,   /* module initialization function */
+	0,          /* child initialization function */
+	destroy     /* destroy function */
 };
 
 
