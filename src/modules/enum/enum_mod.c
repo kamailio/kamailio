b2dca77ea1dc131cfc93e0bed0d8c9c288ccd779
Charles Chance <charles.chance@sipcentric.com>
2018-09-27 17:28:02 +0200

    enum: updated to new module interface format

--- src/modules/enum/enum_mod.c
+++ src/modules/enum/enum_mod.c
@@ -102,18 +102,16 @@ static param_export_t params[] = {
  */
 /* clang-format off */
 struct module_exports exports = {
-	"enum", 
-	DEFAULT_DLFLAGS, /* dlopen flags */
-	cmds,     /* Exported functions */
-	params,   /* Exported parameters */
-	0,        /* exported statistics */
-	0,        /* exported MI functions */
-	0,        /* exported pseudo-variables */
-	0,        /* extra processes */
-	0,        /* module initialization function */
-	0,        /* response function*/
-	0,        /* destroy function */
-	0         /* per-child init function */
+	"enum",				/* module name */
+	DEFAULT_DLFLAGS,	/* dlopen flags */
+	cmds,				/* exported functions */
+	params,				/* exported parameters */
+	0,					/* RPC method exports */
+	0,					/* exported pseudo-variables */
+	0,					/* response handling function */
+	0,					/* module initialization function */
+	0,					/* per-child init function */
+	0					/* module destroy function */
 };
 /* clang-format on */
 
