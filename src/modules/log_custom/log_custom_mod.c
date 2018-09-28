886079dd6bfb2410678705624b4ec7ea8893c15a
Federico Cabiddu <fcabiddu@libon.com>
2018-09-28 10:20:47 +0200

    log_custom: converted to the new module interface

--- src/modules/log_custom/log_custom_mod.c
+++ src/modules/log_custom/log_custom_mod.c
@@ -62,14 +62,12 @@ struct module_exports exports = {
 	DEFAULT_DLFLAGS, /* dlopen flags */
 	cmds,
 	params,
-	0,
-	0,              /* exported MI functions */
+	0,              /* exported RPC functions */
 	0,              /* exported pseudo-variables */
-	0,              /* extra processes */
-	mod_init,       /* module initialization function */
 	0,              /* response function */
-	mod_destroy,    /* destroy function */
-	child_init      /* per child init function */
+	mod_init,       /* module initialization function */
+	child_init,     /* per child init function */
+	mod_destro,    	/* destroy function */
 };
 
 /**
