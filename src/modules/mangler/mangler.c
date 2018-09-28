b9346ca70386cc25d733c8eb19857fd5ecb97008
Victor Seva <linuxmaniac@torreviejawireless.org>
2018-09-28 10:24:12 +0200

    mangler: updated to the new mod interface

--- src/modules/mangler/mangler.c
+++ src/modules/mangler/mangler.c
@@ -85,12 +85,12 @@ static param_export_t params[] = {
  */
 static cmd_export_t cmds[] =
 {
-	{"sdp_mangle_ip", sdp_mangle_ip, 2,0, REQUEST_ROUTE|ONREPLY_ROUTE}, // fixup_char2str?
-	{"sdp_mangle_port",sdp_mangle_port, 1,0, REQUEST_ROUTE|ONREPLY_ROUTE},// fixup_char2int if I use an int as offset
-	{"encode_contact",encode_contact,2,0,REQUEST_ROUTE|ONREPLY_ROUTE},//fixup_char2str
-	{"decode_contact",decode_contact,0,0,REQUEST_ROUTE},
-	{"decode_contact_header",decode_contact_header,0,0,REQUEST_ROUTE|ONREPLY_ROUTE},
-	{0, 0, 0, 0, 0}
+	{"sdp_mangle_ip", sdp_mangle_ip, 2,0,0, REQUEST_ROUTE|ONREPLY_ROUTE}, // fixup_char2str?
+	{"sdp_mangle_port",sdp_mangle_port, 1,0,0, REQUEST_ROUTE|ONREPLY_ROUTE},// fixup_char2int if I use an int as offset
+	{"encode_contact",encode_contact,2,0,0,REQUEST_ROUTE|ONREPLY_ROUTE},//fixup_char2str
+	{"decode_contact",decode_contact,0,0,0,REQUEST_ROUTE},
+	{"decode_contact_header",decode_contact_header,0,0,0,REQUEST_ROUTE|ONREPLY_ROUTE},
+	{0, 0, 0, 0, 0, 0}
 };
 
 
@@ -98,15 +98,16 @@ static cmd_export_t cmds[] =
  * Module interface
  */
 struct module_exports exports = {
-	"mangler",
-	cmds,			/* Exported functions */
-	0,                      /* RPC methods */
-	params,			/* Exported parameters */
-	mod_init,		/* module initialization function */
-	0,			/* response function */
-	destroy,		/* destroy function */
-	0,			/* oncancel function */
-	0			/* child initialization function */
+	"mangler",       /* module name */
+	DEFAULT_DLFLAGS, /* dlopen flags */
+	cmds,            /* cmd (cfg function) exports */
+	params,          /* param exports */
+	0,               /* RPC method exports */
+	0,               /* pseudo-variables exports */
+	0,               /* response handling function */
+	mod_init,        /* module init function */
+	0,               /* per-child init function */
+	destroy          /* module destroy function */
 };
 
 
