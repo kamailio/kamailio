02abcb1c456717d78f71c05abea9853cf6d6b913
Victor Seva <linuxmaniac@torreviejawireless.org>
2018-09-28 10:16:38 +0200

    keepalive: updated to the new mod interface

--- src/modules/keepalive/keepalive_mod.c
+++ src/modules/keepalive/keepalive_mod.c
@@ -80,18 +80,16 @@ static param_export_t params[] = {
 
 /** module exports */
 struct module_exports exports = {
-	"keepalive",
+	"keepalive",     /* module name */
 	DEFAULT_DLFLAGS, /* dlopen flags */
-	cmds,
-	params,
-	0,		  /* exported statistics */
-	0,		  /* exported MI functions - no available anymore since 5.0 */
-	0,		  /* exported pseudo-variables */
-	0,		  /* extra processes */
-	mod_init, /* module initialization function */
-	0,
-	(destroy_function)mod_destroy,
-	0 /* per-child init function */
+	cmds,            /* cmd (cfg function) exports */
+	params,          /* param exports */
+	0,               /* RPC method exports */
+	0,               /* pseudo-variables exports */
+	0,               /* response handling function */
+	mod_init,        /* module init function */
+	0,               /* per-child init function */
+	mod_destroy      /* module destroy function */
 };
 
 
