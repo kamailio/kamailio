b5f27fe25247e3605aeeb600de5a6b31081d6ac6
Daniel-Constantin Mierla <miconda@gmail.com>
2019-01-24 15:53:55 +0100

    core: export pp_define_get() via ppcfg.h

--- src/core/ppcfg.h
+++ src/core/ppcfg.h
@@ -41,6 +41,7 @@ int pp_subst_run(char **data);
 int  pp_define(int len, const char *text);
 int  pp_define_set(int len, char *text);
 int  pp_define_set_type(int type);
+str *pp_define_get(int len, const char * text);
 
 void pp_ifdef_level_update(int val);
 void pp_ifdef_level_check(void);
