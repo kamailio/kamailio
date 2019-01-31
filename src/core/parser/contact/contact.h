5af1a114d879f36fbe1982c32e27b497926487c0
Alex Hermann <alex@hexla.nl>
2019-01-31 17:28:33 +0100

    core/parser: Add parser for 'flags' param in Contact header

--- src/core/parser/contact/contact.h
+++ src/core/parser/contact/contact.h
@@ -50,6 +50,7 @@ typedef struct contact {
 	param_t* received;      /* received parameter hook */
 	param_t* instance;      /* sip.instance parameter hook */
 	param_t* reg_id;        /* reg-id parameter hook */
+	param_t* flags;         /* flags parameter hook */
 	param_t* params;        /* List of all parameters */
 	int len;                /* Total length of the element */
         struct contact* next; /* Next contact in the list */
