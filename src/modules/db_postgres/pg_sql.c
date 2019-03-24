a92f257f6aa439c772fb1c8db4b909a234fae908
Henning Westerholt <hw@kamailio.org>
2019-03-24 19:19:37 +0100

    db_postgres: small spelling fix: lenght -> length

--- src/modules/db_postgres/pg_sql.c
+++ src/modules/db_postgres/pg_sql.c
@@ -138,7 +138,7 @@ static inline int sb_add(struct string_buffer *sb, str *nstr)
 
 /** Creates str string from zero terminated string without copying.
  * This function initializes members of a temporary str structure
- * with the pointer and lenght of the string from s parameter.
+ * with the pointer and length of the string from s parameter.
  *
  * @param str A pointer to temporary str structure.
  * @param s   A zero terminated string.
