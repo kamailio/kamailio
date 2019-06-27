4761c2708a6b315d4cc0b1d8c88fd9d90e1400ee
Daniel-Constantin Mierla <miconda@gmail.com>
2019-06-27 16:25:27 +0200

    sdpops: exported to kemi the functions to detect sdp with codecs
    - sdp_with_codecs_by_id() and sdp_with_codecs_by_name()
    
    (cherry picked from commit 3716541db01c09bd00998bdbd0c35b3a842cdaf5)

--- src/modules/sdpops/sdpops_mod.c
+++ src/modules/sdpops/sdpops_mod.c
@@ -2187,6 +2187,16 @@ static sr_kemi_t sr_kemi_sdpops_exports[] = {
 		{ SR_KEMIP_STR, SR_KEMIP_NONE, SR_KEMIP_NONE,
 			SR_KEMIP_NONE, SR_KEMIP_NONE, SR_KEMIP_NONE }
 	},
+	{ str_init("sdpops"), str_init("sdp_with_codecs_by_id"),
+		SR_KEMIP_INT, sdp_with_codecs_by_id,
+		{ SR_KEMIP_STR, SR_KEMIP_NONE, SR_KEMIP_NONE,
+			SR_KEMIP_NONE, SR_KEMIP_NONE, SR_KEMIP_NONE }
+	},
+	{ str_init("sdpops"), str_init("sdp_with_codecs_by_name"),
+		SR_KEMIP_INT, sdp_with_codecs_by_name,
+		{ SR_KEMIP_STR, SR_KEMIP_NONE, SR_KEMIP_NONE,
+			SR_KEMIP_NONE, SR_KEMIP_NONE, SR_KEMIP_NONE }
+	},
 	{ str_init("sdpops"), str_init("sdp_get"),
 		SR_KEMIP_INT, ki_sdp_get,
 		{ SR_KEMIP_STR, SR_KEMIP_NONE, SR_KEMIP_NONE,
