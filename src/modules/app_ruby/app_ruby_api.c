ea526a8de9c961a57858ecc4c47ce4ba24e03908
Daniel-Constantin Mierla <miconda@gmail.com>
2019-08-05 18:11:00 +0200

    app_ruby: print pointers in error log message

--- src/modules/app_ruby/app_ruby_api.c
+++ src/modules/app_ruby/app_ruby_api.c
@@ -462,7 +462,8 @@ VALUE sr_kemi_ruby_exec_func_ex(ksr_ruby_context_t *R, sr_kemi_t *ket, int argc,
 
 	env_R = app_ruby_sr_env_get();
 	if(env_R==NULL || env_R->msg==NULL || ket==NULL) {
-		LM_ERR("invalid ruby environment attributes or parameters\n");
+		LM_ERR("invalid ruby environment attributes or parameters (%p/%p/%p)\n",
+				env_R, env_R->msg, ket);
 		return Qfalse;
 	}
 
