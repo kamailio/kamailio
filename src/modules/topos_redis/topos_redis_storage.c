kamailio/kamailio

commit cfc89db53c087382c29d809dc0bc56f7351af2d1

topos_redis: check the branch value to avoid using uninitialized fields
- GH #1784

src/modules/topos_redis/topos_redis_storage.c
@@ -737,7 +737,12 @@ int tps_redis_load_branch(sip_msg_t *msg, tps_data_t *md, tps_data_t *sd,
 			LM_ERR("failed to load the INVITE branch value\n");
 			return -1;
 		}
+		memset(&id, 0, sizeof(tps_data_t));
 		xvbranch1 = &id.x_vbranch1;
+	}
+	if(xvbranch1->len<=0 || xvbranch1->s==NULL) {
+		LM_DBG("branch value not found (mode: %u)\n", mode);
+		return 1;
 	}
 	rp = _tps_redis_cbuf;
 	memcpy(rp, _tps_redis_bprefix.s, _tps_redis_bprefix.len);

commit a263f5b62c03851a5d8a61f09e69745f90370703

topos_redis: fix regression in PRACK routing (commit cfc89db53c087, GH #1784)
- fix regression in PRACK routing (introduced in cfc89db53c, GH #1784)
- result set were set to 0 before usage by a memset

src/modules/topos_redis/topos_redis_storage.c
@@ -727,6 +727,7 @@ int tps_redis_load_branch(sip_msg_t *msg, tps_data_t *md, tps_data_t *sd,
 	memset(argv, 0, TPS_REDIS_NR_KEYS * sizeof(char*));
 	memset(argvlen, 0, TPS_REDIS_NR_KEYS * sizeof(size_t));
 	argc = 0;
+	memset(&id, 0, sizeof(tps_data_t));
 
 	if(mode==0) {
 		/* load same transaction using Via branch */
@@ -737,7 +738,6 @@ int tps_redis_load_branch(sip_msg_t *msg, tps_data_t *md, tps_data_t *sd,
 			LM_ERR("failed to load the INVITE branch value\n");
 			return -1;
 		}
-		memset(&id, 0, sizeof(tps_data_t));
 		xvbranch1 = &id.x_vbranch1;
 	}
 	if(xvbranch1->len<=0 || xvbranch1->s==NULL) {