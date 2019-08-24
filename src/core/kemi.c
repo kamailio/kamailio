b21a75dad1575999549f9a84de522dfb8d7fb50f
Daniel-Constantin Mierla <miconda@gmail.com>
2019-08-24 16:09:45 +0200

    core: kemi - fix matching update for is_method_in("U")
    (cherry picked from commit e876770e36c83ce47d1b0f95c14fe269b92a4e70)

--- src/core/kemi.c
+++ src/core/kemi.c
@@ -839,6 +839,12 @@ static int sr_kemi_core_is_method_in(sip_msg_t *msg, str *vmethod)
 					return SR_KEMI_TRUE;
 				}
 			break;
+			case 'U':
+			case 'u':
+				if(imethod==METHOD_UPDATE) {
+					return SR_KEMI_TRUE;
+				}
+			break;
 			case 'K':
 			case 'k':
 				if(imethod==METHOD_KDMQ) {
