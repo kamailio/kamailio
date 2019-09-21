56c6f787a0e968d3ed05c21cffe8bdeb8e286d84
Henning Westerholt <hw@skalatan.de>
2019-09-22 00:31:08 +0200

    cdp: several own processes are forked, we need to properly initialize the PRNG for them
    - cdp: several own processes are forked, properly initialize the PRNG for them
    - related to commit df25b16635e

--- src/modules/cdp/diameter_peer.c
+++ src/modules/cdp/diameter_peer.c
@@ -226,8 +226,10 @@ int diameter_peer_start(int blocking)
 {
 	int pid;
 	int k=0;
+	int seed;
 	peer *p;
 
+	seed = random();
 	/* fork workers */
 	for(k=0;k<config->workers;k++){
 		pid = fork_process(1001+k,"cdp_worker",1);
@@ -236,6 +238,7 @@ int diameter_peer_start(int blocking)
 			return 0;
 		}
 		if (pid==0) {
+			srandom(seed*k);
 			snprintf(pt[process_no].desc, MAX_PT_DESC,"cdp worker child=%d", k );
 			if (cfg_child_init()) return 0;
 			worker_process(k);
@@ -253,7 +256,7 @@ int diameter_peer_start(int blocking)
 
 
 	/* fork receiver for unknown peers */
-
+	seed = random();
 	pid = fork_process(1001+k,"cdp_receiver_peer_unkown",1);
 
 	if (pid==-1){
@@ -261,6 +264,7 @@ int diameter_peer_start(int blocking)
 		return 0;
 	}
 	if (pid==0) {
+		srandom(seed*k);
 		snprintf(pt[process_no].desc, MAX_PT_DESC,
 				"cdp receiver peer unknown");
 		if (cfg_child_init()) return 0;
@@ -272,6 +276,7 @@ int diameter_peer_start(int blocking)
 	}
 
 	/* fork receivers for each pre-configured peers */
+	seed = random();
 	lock_get(peer_list_lock);
 	for(p = peer_list->head,k=-1;p;p = p->next,k--){
 		pid = fork_process(1001+k,"cdp_receiver_peer",1);
@@ -280,6 +285,7 @@ int diameter_peer_start(int blocking)
 			return 0;
 		}
 		if (pid==0) {
+			srandom(seed*k);
 				snprintf(pt[process_no].desc, MAX_PT_DESC,
 					"cdp_receiver_peer=%.*s", p->fqdn.len,p->fqdn.s );
 			if (cfg_child_init()) return 0;
