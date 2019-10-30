95dd5245ce2499f97ade9cf3f24b2ee52690880c
Vicente Hernando <vhernando@systemonenoc.com>
2019-10-31 00:00:24 +0100

    kafka: get log levels from syslog.h header

--- src/modules/kafka/kfk.c
+++ src/modules/kafka/kfk.c
@@ -27,6 +27,7 @@
  * - Module: \ref kfk
  */
 
+#include <syslog.h> /* For log levels. */
 #include <librdkafka/rdkafka.h>
 
 #include "../../core/dprint.h"
@@ -106,20 +107,6 @@ static int kfk_topic_exist(str *topic_name);
 static rd_kafka_topic_t* kfk_topic_get(str *topic_name);
 static int kfk_stats_add(const char *topic, rd_kafka_resp_err_t err);
 static void kfk_stats_topic_free(kfk_stats_t *st_topic);
-
-/**
- * \name Logging_macros from /usr/include/sys/syslog.h
- */
-/*@{*/
-#define	LOG_EMERG	0	/* system is unusable */
-#define	LOG_ALERT	1	/* action must be taken immediately */
-#define	LOG_CRIT	2	/* critical conditions */
-#define	LOG_ERR		3	/* error conditions */
-#define	LOG_WARNING	4	/* warning conditions */
-#define	LOG_NOTICE	5	/* normal but significant condition */
-#define	LOG_INFO	6	/* informational */
-#define	LOG_DEBUG	7	/* debug-level messages */
-/*@}*/
 
 /**
  * \brief Kafka logger callback
