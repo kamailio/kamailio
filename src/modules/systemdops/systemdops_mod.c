fffd892b7d18734c8f60d2eebec5d4a15a0fdc26
Daniel-Constantin Mierla <miconda@gmail.com>
2019-11-22 09:42:08 +0100

    systemdops: proper check for null value

--- src/modules/systemdops/systemdops_mod.c
+++ src/modules/systemdops/systemdops_mod.c
@@ -69,7 +69,7 @@ void ksr_sd_app_shutdown(void)
 int mod_register(char *path, int *dlflags, void *p1, void *p2)
 {
 	sr_corecb_t *cbp = sr_corecb_get();
-	if(cbp) {
+	if(cbp==NULL) {
 		return -1;
 	}
 	cbp->app_ready = ksr_sd_app_ready;
