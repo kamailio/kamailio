18059dcc87f371f353fb585a89aefd8d7cadce8c
Victor Seva <linuxmaniac@torreviejawireless.org>
2019-12-09 18:44:18 +0100

    tmx: add clang-format directives to skip format on kemi_exports
    format is important for generating docs by our python tool

--- src/modules/tmx/tmx_mod.c
+++ src/modules/tmx/tmx_mod.c
@@ -1082,6 +1082,7 @@ unsigned long tmx_stats_rld_rcv_rpls(void)
 /**
  *
  */
+/* clang-format off */
 static sr_kemi_t sr_kemi_tmx_exports[] = {
 	{ str_init("tmx"), str_init("t_precheck_trans"),
 		SR_KEMIP_INT, t_precheck_trans,
@@ -1166,6 +1167,7 @@ static sr_kemi_t sr_kemi_tmx_exports[] = {
 
 	{ {0, 0}, {0, 0}, 0, NULL, { 0, 0, 0, 0, 0, 0 } }
 };
+/* clang-format on */
 
 /**
  *
