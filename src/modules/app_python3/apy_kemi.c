731056f0e7ae752273e16bc7bc1a48ba16638235
Victor Seva <linuxmaniac@torreviejawireless.org>
2019-12-09 18:47:00 +0100

    app_python3: add clang-format directives to skip format on kemi_exports
    format is important for generating docs by our python tool

--- src/modules/app_python3/apy_kemi.c
+++ src/modules/app_python3/apy_kemi.c
@@ -1846,6 +1846,7 @@ static int sr_apy_kemi_f_ktest(sip_msg_t *msg, str *txt)
 /**
  *
  */
+/* clang-format off */
 static sr_kemi_t _sr_apy_kemi_test[] = {
 	{ str_init(""), str_init("ktest"),
 		SR_KEMIP_NONE, sr_apy_kemi_f_ktest,
@@ -1855,7 +1856,7 @@ static sr_kemi_t _sr_apy_kemi_test[] = {
 
 	{ {0, 0}, {0, 0}, 0, NULL, { 0, 0, 0, 0, 0, 0 } }
 };
-
+/* clang-format on */
 
 static struct PyModuleDef KSR_moduledef = {
         PyModuleDef_HEAD_INIT,
