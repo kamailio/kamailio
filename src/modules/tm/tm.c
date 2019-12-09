6e7a441a9f27cb22ff22919f2e7b911045b7a7c0
Victor Seva <linuxmaniac@torreviejawireless.org>
2019-12-09 18:43:29 +0100

    tm: add clang-format directives to skip format on kemi_exports
    format is important for generating docs by our python tool

--- src/modules/tm/tm.c
+++ src/modules/tm/tm.c
@@ -2908,6 +2908,7 @@ static int ki_t_relay_to_flags(sip_msg_t *msg, int rflags)
 /**
  *
  */
+/* clang-format off */
 static sr_kemi_t tm_kemi_exports[] = {
 	{ str_init("tm"), str_init("t_relay"),
 		SR_KEMIP_INT, ki_t_relay,
@@ -3157,7 +3158,7 @@ static sr_kemi_t tm_kemi_exports[] = {
 
 	{ {0, 0}, {0, 0}, 0, NULL, { 0, 0, 0, 0, 0, 0 } }
 };
-
+/* clang-format on */
 
 int mod_register(char *path, int *dlflags, void *p1, void *p2)
 {
