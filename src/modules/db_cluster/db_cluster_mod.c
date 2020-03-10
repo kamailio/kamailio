40128763d8a987e4c64d51584f1d72872b53dff5
Daniel-Constantin Mierla <miconda@gmail.com>
2020-03-10 16:11:14 +0100

    db_cluster: trailing spaces removed

--- src/modules/db_cluster/db_cluster_mod.c
+++ src/modules/db_cluster/db_cluster_mod.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * Generic db cluster module interface
  *
  * Copyright (C) 2012 Daniel-Constantin Mierla (asipto.com)
@@ -15,8 +15,8 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
@@ -56,7 +56,7 @@ static param_export_t params[] = {
 	{0, 0, 0}
 };
 
-struct module_exports exports = {	
+struct module_exports exports = {
 	"db_cluster",		/* module name */
 	DEFAULT_DLFLAGS,	/* dlopen flags */
 	cmds,				/* exported functions */
