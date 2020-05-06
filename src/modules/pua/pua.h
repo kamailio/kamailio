076649793bfdbdc4f03f96961f23997c87ce4268
Daniel-Constantin Mierla <miconda@gmail.com>
2020-05-06 17:15:36 +0200

    pua: use file name specific include guard

--- src/modules/pua/pua.h
+++ src/modules/pua/pua.h
@@ -15,13 +15,13 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-#ifndef PUBLISH_H
-#define PUBLISH_H
+#ifndef _PUA_H_
+#define _PUA_H_
 
 #include "../../core/str.h"
 #include "../../lib/srdb1/db.h"
