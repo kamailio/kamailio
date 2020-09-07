9fbed998e03935193c05186325c188271a9f686e
ovoshlook <ovoshlook@gmail.com>
2020-09-07 11:06:14 +0200

    updated vie make dbschema

--- utils/kamctl/mysql/uac-create.sql
+++ utils/kamctl/mysql/uac-create.sql
@@ -18,5 +18,5 @@ CREATE TABLE `uacreg` (
     CONSTRAINT l_uuid_idx UNIQUE (`l_uuid`)
 );
 
-INSERT INTO version (table_name, table_version) values ('uacreg','5');
+INSERT INTO version (table_name, table_version) values ('uacreg','4');
 
