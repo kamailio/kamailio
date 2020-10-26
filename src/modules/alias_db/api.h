3460b6b0a35919b01a6371aa678c642730b7352a
Daniel-Constantin Mierla <miconda@gmail.com>
2020-10-26 07:52:49 +0100

    alias_db: removed history from top comments
    - missing copyright header
    
    (cherry picked from commit 7ae0caaffd3d6e523fd32501401213521a1881e5)
    (cherry picked from commit 5114cc77cb61744843ed8b535d9804fcdf61e635)

--- src/modules/alias_db/api.h
+++ src/modules/alias_db/api.h
@@ -1,5 +1,29 @@
+/*
+ * ALIAS_DB Module
+ *
+ * Copyright (C) 2011 Crocodile RCS
+ *
+ * This file is part of a module for Kamailio, a free SIP server.
+ *
+ * Kamailio is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version
+ *
+ * Kamailio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
 #ifndef ALIASDB_API_H
 #define ALIASDB_API_H
+
 #include "../../core/str.h"
 
 typedef int (*alias_db_lookup_t)(sip_msg_t*, str table);
