0f0a56ad5c97a611fab64fc9b9eba304643dcec8
Daniel-Constantin Mierla <miconda@gmail.com>
2021-03-29 15:04:38 +0200

    path: removed svn id header

--- src/modules/path/path_mod.c
+++ src/modules/path/path_mod.c
@@ -1,6 +1,4 @@
 /*
- * $Id$ 
- *
  * Path handling for intermediate proxies
  *
  * Copyright (C) 2006 Inode GmbH (Andreas Granig <andreas.granig@inode.info>)
@@ -17,8 +15,8 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
  */
