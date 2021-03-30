bb2fd8c0cd9909f87df7df6fa300a06c38259a10
Daniel-Constantin Mierla <miconda@gmail.com>
2021-03-30 11:29:26 +0200

    pv: new transformation class - urialias
    - encode/decode uri alias values (addr~port~proto)

--- src/modules/pv/pv_trans.h
+++ src/modules/pv/pv_trans.h
@@ -31,7 +31,7 @@
 
 
 enum _tr_type { TR_NONE=0, TR_STRING, TR_URI, TR_PARAMLIST, TR_NAMEADDR,
-				TR_TOBODY, TR_LINE };
+				TR_TOBODY, TR_LINE, TR_URIALIAS};
 enum _tr_s_subtype {
 	TR_S_NONE=0, TR_S_LEN, TR_S_INT, TR_S_MD5, TR_S_SHA256, TR_S_SHA384,
 	TR_S_SHA512, TR_S_SUBSTR, TR_S_SELECT, TR_S_ENCODEHEXA, TR_S_DECODEHEXA,
@@ -67,6 +67,9 @@ enum _tr_tobody_subtype {
 enum _tr_line_subtype {
 	TR_LINE_NONE=0, TR_LINE_COUNT, TR_LINE_AT, TR_LINE_SW
 };
+enum _tr_urialias_subtype {
+	TR_URIALIAS_NONE=0, TR_URIALIAS_ENCODE, TR_URIALIAS_DECODE
+};
 
 
 char* tr_parse_string(str *in, trans_t *tr);
@@ -75,6 +78,7 @@ char* tr_parse_paramlist(str *in, trans_t *tr);
 char* tr_parse_nameaddr(str *in, trans_t *tr);
 char* tr_parse_tobody(str* in, trans_t *t);
 char* tr_parse_line(str* in, trans_t *t);
+char* tr_parse_urialias(str* in, trans_t *t);
 
 int tr_init_buffers(void);
 
