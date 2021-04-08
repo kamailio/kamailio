ebe9db5f8888f74376c7841815d6b3582d5736c1
Daniel-Constantin Mierla <miconda@gmail.com>
2021-04-08 12:05:07 +0200

    nat_traversal: check parameter for send_keepalive()
    (cherry picked from commit f33abd5e2d5978ba9e6e8a48066b0daff70c0b3b)
    (cherry picked from commit e0948071566a8d978dc2c57a21a7448b953a11e1)

--- src/modules/nat_traversal/nat_traversal.c
+++ src/modules/nat_traversal/nat_traversal.c
@@ -1596,6 +1596,11 @@ static void send_keepalive(NAT_Contact *contact)
 	unsigned short lport;
 	char lproto;
 
+	if(contact==NULL || contact->socket==NULL) {
+		LM_ERR("invalid parameters\n");
+		return;
+	}
+
 	if(keepalive_params.from == NULL) {
 		if(contact->socket != last_socket) {
 			memcpy(from_ip, contact->socket->address_str.s,
