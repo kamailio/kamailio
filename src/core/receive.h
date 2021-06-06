c336eed393f2fedae16dbcf004e143fdd38c46e2
Sergey Safarov <s.safarov@gmail.com>
2021-06-06 18:50:05 +0300

    core: published sip_check_fline function in .h file

--- src/core/receive.h
+++ src/core/receive.h
@@ -32,6 +32,7 @@
 #include "ip_addr.h"
 
 int receive_msg(char* buf, unsigned int len, struct receive_info *ri);
+int sip_check_fline(char *buf, unsigned int len);
 unsigned int inc_msg_no(void);
 void ksr_msg_env_reset(void);
 
