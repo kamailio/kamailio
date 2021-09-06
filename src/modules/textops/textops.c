kamailio/kamailio

commit cbcc27ef8b1674448bd8b1c45f03388e36f65f51

textops: check session sendrecv_mode in is_audio_on_hold
- if sendrecv_mode is not set in media, check if its set in session

src/modules/textops/textops.c
@@ -4517,8 +4517,15 @@ static int ki_is_audio_on_hold(sip_msg_t *msg)
 				if(!sdp_stream) break;
 				if(sdp_stream->media.len==AUDIO_STR_LEN &&
 					strncmp(sdp_stream->media.s,AUDIO_STR,AUDIO_STR_LEN)==0 &&
+					sdp_stream->sendrecv_mode.len &&
 					sdp_stream->is_on_hold)
 					return sdp_stream->is_on_hold;
+				if(sdp_stream->media.len==AUDIO_STR_LEN &&
+					strncmp(sdp_stream->media.s,AUDIO_STR,AUDIO_STR_LEN)==0 &&
+					!sdp_stream->sendrecv_mode.len &&
+					sdp_session->sendrecv_mode.len &&
+					sdp_session->is_on_hold)
+					return sdp_session->is_on_hold;
 				sdp_stream_num++;
 			}
 			sdp_session_num++;

commit c552775a11cde3ac637a0bfcf81eaeb2eb326f58

textops: RFC2543 is broken
commit cbcc27ef8b1674448bd8b1c45f03388e36f65f51 broke RFC2543 hold

there's no need to check for sendrecv_mode, is_on_hold is either set or not in sdp_stream and/or sdp_session

src/modules/textops/textops.c
@@ -4517,13 +4517,10 @@ static int ki_is_audio_on_hold(sip_msg_t *msg)
 				if(!sdp_stream) break;
 				if(sdp_stream->media.len==AUDIO_STR_LEN &&
 					strncmp(sdp_stream->media.s,AUDIO_STR,AUDIO_STR_LEN)==0 &&
-					sdp_stream->sendrecv_mode.len &&
 					sdp_stream->is_on_hold)
 					return sdp_stream->is_on_hold;
 				if(sdp_stream->media.len==AUDIO_STR_LEN &&
 					strncmp(sdp_stream->media.s,AUDIO_STR,AUDIO_STR_LEN)==0 &&
-					!sdp_stream->sendrecv_mode.len &&
-					sdp_session->sendrecv_mode.len &&
 					sdp_session->is_on_hold)
 					return sdp_session->is_on_hold;
 				sdp_stream_num++;