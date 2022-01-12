016febcb8c4a2de4d21201d0f2f74ba268b8bbde
Daniel-Constantin Mierla <miconda@gmail.com>
2022-01-12 14:12:42 +0100

    core: parser - print ascii code and position of invalid char for identity info
    (cherry picked from commit 40e59d9c3b8ec531cb9e26093c43647a43108dfd)
    (cherry picked from commit ef9b353910d10d91fde88193b7a3dd9c27bf18ad)

--- src/core/parser/parse_identityinfo.c
+++ src/core/parser/parse_identityinfo.c
@@ -310,8 +310,8 @@ void parse_identityinfo(char *buffer, char *end, struct identityinfo_body *ii_b)
 
 parseerror:
 	if(p < end) {
-		LM_ERR("unexpected char [%c] in status %d: [%.*s]\n", *p, status,
-				(int)(p - buffer), buffer);
+		LM_ERR("unexpected char [%c/%d] in status %d - pos %d: [%.*s]\n", *p, *p,
+				status, (int)(p - buffer), (int)(p - buffer), buffer);
 	} else {
 		LM_ERR("unexpected end of buffer - status %d\n", status);
 	}
