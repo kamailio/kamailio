d475ee121cb0f6ff71146f8ce108a466ce792fcf
Daniel-Constantin Mierla <miconda@gmail.com>
2022-02-25 09:13:46 +0100

    core: fixed comment typo
    (cherry picked from commit c37ccdc6e3bad0af4644941a872f510c51936c58)
    (cherry picked from commit 42b80a2cf0fa1b2e7192ade5844df670a49480ff)

--- src/core/sr_module.h
+++ src/core/sr_module.h
@@ -153,7 +153,7 @@ typedef int (*param_func_t)( modparam_t type, void* val);
 							  @warning child_init(PROC_MAIN) is again called
 							 in the same process (main), but latter
 							 (before tcp), so make sure you don't init things
-							 twice, bot in PROC_MAIN and PROC_INT */
+							 twice, both in PROC_MAIN and PROC_INT */
 #define PROC_NOCHLDINIT -128 /**< no child init functions will be called
                                 if this rank is used in fork_process() */
 
