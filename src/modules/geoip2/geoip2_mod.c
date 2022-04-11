7f03faa01ee4d832f5079b9344fc895ff425ac6c
Henning Westerholt <hw@gilawa.com>
2022-04-11 09:38:23 +0000

    geoip2: logging library version at startup

--- src/modules/geoip2/geoip2_mod.c
+++ src/modules/geoip2/geoip2_mod.c
@@ -80,6 +80,7 @@ struct module_exports exports = {
  */
 static int mod_init(void)
 {
+	LM_INFO("using GeoIP library version %s\n", MMDB_lib_version());
 
 	if(geoip2_path==NULL || strlen(geoip2_path)==0)
 	{
