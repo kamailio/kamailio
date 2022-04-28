ec3f39f610120fe8d64c0c1ee1c5453cd78b38f5
Daniel-Constantin Mierla <miconda@gmail.com>
2022-04-28 14:03:55 +0200

    presence_xml: free uri for http xcap auth
    (cherry picked from commit eba6c7d78088cde411f5cd1b7730ba458e62a69e)
    (cherry picked from commit 1d39dc57cda8fbb169a3848c5e4dba3e7a91e16b)

--- src/modules/presence_xml/xcap_auth.c
+++ src/modules/presence_xml/xcap_auth.c
@@ -521,7 +521,7 @@ error:
 
 int http_get_rules_doc(str user, str domain, str *rules_doc)
 {
-	str uri;
+	str uri = STR_NULL;
 	xcap_doc_sel_t doc_sel;
 	char *doc = NULL;
 	xcap_serv_t *xs;
@@ -558,6 +558,8 @@ int http_get_rules_doc(str user, str domain, str *rules_doc)
 
 	rules_doc->s = doc;
 	rules_doc->len = doc ? strlen(doc) : 0;
+
+	pkg_free(uri.s);
 
 	return 0;
 
