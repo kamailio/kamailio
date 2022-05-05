5dd8f0a6ff30fd3aa917a98b006106bef61506e5
Bastian Triller <bastian.triller@gmail.com>
2022-05-05 18:21:23 +0200

    core: Fix typos

--- src/core/resolve.c
+++ src/core/resolve.c
@@ -1388,9 +1388,9 @@ end:
  *         tried      - bitmap used to keep track of the already tried records
  *                      (no more then sizeof(tried)*8 valid records are
  *                      ever walked
- *         srv_name   - if succesfull, it will be set to the selected record
+ *         srv_name   - if successful, it will be set to the selected record
  *                      srv name (naptr repl.)
- *         proto      - if succesfull it will be set to the selected record
+ *         proto      - if successful it will be set to the selected record
  *                      protocol
  * returns  0 if no more records found or a pointer to the selected record
  *  and sets  protocol and srv_name
