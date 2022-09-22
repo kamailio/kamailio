023af516f4ede3742f2e1836474978b596a4a3a8
Daniel-Constantin Mierla <miconda@gmail.com>
2022-09-22 20:30:52 +0200

    acc_json: moved global vars declaration to .c file
    - made them static

--- src/modules/acc_json/acc_json_mod.h
+++ src/modules/acc_json/acc_json_mod.h
@@ -24,25 +24,6 @@
 #ifndef _ACC_JSON_MOD_H_
 #define _ACC_JSON_MOD_H_
 
-str acc_method_key = str_init("method");
-str acc_fromtag_key = str_init("from_tag");
-str acc_totag_key = str_init("to_tag");
-str acc_callid_key = str_init("callid");
-str acc_sipcode_key = str_init("sip_code");
-str acc_sipreason_key = str_init("sip_reason");
-str acc_time_key = str_init("time");
-
-str cdr_start_str = str_init("start_time");
-str cdr_end_str = str_init("end_time");
-str cdr_duration_str = str_init("duration");
-
 #define ACC_TIME_FORMAT_SIZE 128
-static char acc_time_format_buf[ACC_TIME_FORMAT_SIZE];
-char *acc_time_format = "%Y-%m-%d %H:%M:%S";
-
-int acc_log_level = L_NOTICE;
-int acc_log_facility = LOG_DAEMON;
-int cdr_log_level = L_NOTICE;
-int cdr_log_facility = LOG_DAEMON;
 
 #endif
