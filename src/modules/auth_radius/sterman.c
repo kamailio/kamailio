4a6b6e3f14318794405ba88a50f3d78971d60197
Daniel-Constantin Mierla <miconda@gmail.com>
2022-11-22 10:19:06 +0100

    auth_radius: updated to use xavp long value field

--- src/modules/auth_radius/sterman.c
+++ src/modules/auth_radius/sterman.c
@@ -160,7 +160,7 @@ static int generate_avps(VALUE_PAIR *received)
 		if(add_avp(flags, name, val) < 0) {
 			LM_ERR("unable to create a new AVP\n");
 		} else {
-			LM_DBG("AVP '%.*s'/%d='%.*s'/%d has been added\n",
+			LM_DBG("AVP '%.*s'/%ld='%.*s'/%ld has been added\n",
 					(flags & AVP_NAME_STR) ? name.s.len : 4,
 					(flags & AVP_NAME_STR) ? name.s.s : "null",
 					(flags & AVP_NAME_STR) ? 0 : name.n,
