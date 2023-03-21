76e5317cca8337d5de78b4506a91cd5cca789f7c
Seven Du <dujinfang@x-y-t.cn>
2023-03-21 19:08:00 +0800

    nats: add a reply param to nats_publish and expose as nats_publish_request in KEMI

--- src/modules/nats/nats_pub.h
+++ src/modules/nats/nats_pub.h
@@ -33,12 +33,13 @@ typedef struct _nats_pub_delivery
 {
 	char *subject;
 	char *payload;
+	char *reply;
 } nats_pub_delivery, *nats_pub_delivery_ptr;
 
-nats_pub_delivery_ptr _nats_pub_delivery_new(str subject, str payload);
+nats_pub_delivery_ptr _nats_pub_delivery_new(str subject, str payload, str reply);
 void nats_pub_free_delivery_ptr(nats_pub_delivery_ptr ptr);
-int w_nats_publish_f(sip_msg_t *msg, char *subj, char *payload);
-int w_nats_publish(sip_msg_t *msg, str subj_s, str payload_s);
+int w_nats_publish_f(sip_msg_t *msg, char *subj, char *payload, char *reply);
+int w_nats_publish(sip_msg_t *msg, str subj_s, str payload_s, str reply_s);
 int fixup_publish_get_value(void **param, int param_no);
 int fixup_publish_get_value_free(void **param, int param_no);
 
