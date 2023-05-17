55ed36e45baf11abd4c67f17be8d3eaf313cd133
Victor Seva <linuxmaniac@torreviejawireless.org>
2023-05-17 16:36:58 +0200

    nats: clang-format for coherent indentation and coding style

--- src/modules/nats/nats_mod.c
+++ src/modules/nats/nats_mod.c
@@ -121,8 +121,7 @@ static void closedCB(natsConnection *nc, void *closure)
 	*closed = true;
 }
 
-void nats_consumer_worker_proc(
-		nats_consumer_worker_t *worker)
+void nats_consumer_worker_proc(nats_consumer_worker_t *worker)
 {
 	natsStatus s = NATS_OK;
 
@@ -263,8 +262,7 @@ int init_worker(
 	return 0;
 }
 
-int init_pub_worker(
-		nats_pub_worker_t *worker)
+int init_pub_worker(nats_pub_worker_t *worker)
 {
 	nats_connection_ptr nc = NULL;
 	nc = _init_nats_connection();
@@ -286,8 +284,7 @@ void worker_loop(int id)
 	}
 }
 
-int _nats_pub_worker_proc(
-		nats_pub_worker_t *worker, int fd)
+int _nats_pub_worker_proc(nats_pub_worker_t *worker, int fd)
 {
 	natsStatus s = NATS_OK;
 
@@ -324,8 +321,10 @@ int _nats_pub_worker_proc(
 		LM_ERR("uv_poll_init failed\n");
 		return 0;
 	}
-	uv_handle_set_data((uv_handle_t *)&worker->poll, (nats_pub_worker_t *)worker);
-	if(uv_poll_start(&worker->poll, UV_READABLE | UV_DISCONNECT, _nats_pub_worker_cb)
+	uv_handle_set_data(
+			(uv_handle_t *)&worker->poll, (nats_pub_worker_t *)worker);
+	if(uv_poll_start(
+			   &worker->poll, UV_READABLE | UV_DISCONNECT, _nats_pub_worker_cb)
 			< 0) {
 		LM_ERR("uv_poll_start failed\n");
 		return 0;
@@ -388,7 +387,8 @@ static int mod_child_init(int rank)
 					return -1;
 				close(nats_pub_worker_pipes_fds[i * 2 + 1]);
 				cfg_update();
-				return (_nats_pub_worker_proc(&nats_pub_workers[i], nats_pub_worker_pipes_fds[i * 2]));
+				return (_nats_pub_worker_proc(&nats_pub_workers[i],
+						nats_pub_worker_pipes_fds[i * 2]));
 			} else {
 				nats_pub_workers[i].pid = newpid;
 			}
@@ -578,7 +578,7 @@ int nats_destroy_workers()
 					}
 				}
 				if(worker->on_message != NULL) {
-					if (worker->on_message->_evname) {
+					if(worker->on_message->_evname) {
 						free(worker->on_message->_evname);
 					}
 					shm_free(worker->on_message);
@@ -597,7 +597,7 @@ int nats_destroy_workers()
 						LM_ERR("could not cleanup worker connection\n");
 					}
 				}
-				if(uv_is_active((uv_handle_t*)&pub_worker->poll)) {
+				if(uv_is_active((uv_handle_t *)&pub_worker->poll)) {
 					uv_poll_stop(&pub_worker->poll);
 				}
 				shm_free(pub_worker);
@@ -689,7 +689,8 @@ int nats_run_cfg_route(int rt, str *evname)
 
 	// check for valid route pointer
 	if(rt < 0 || !event_rt.rlist[rt]) {
-		if (keng == NULL) return 0;
+		if(keng == NULL)
+			return 0;
 	}
 
 	fmsg = faked_msg_next();
@@ -697,9 +698,9 @@ int nats_run_cfg_route(int rt, str *evname)
 	fmsg = &tmsg;
 	set_route_type(EVENT_ROUTE);
 	init_run_actions_ctx(&ctx);
-	if (rt < 0 && keng) {
-		if (sr_kemi_route(keng, fmsg, EVENT_ROUTE,
-			&nats_event_callback, evname) < 0) {
+	if(rt < 0 && keng) {
+		if(sr_kemi_route(keng, fmsg, EVENT_ROUTE, &nats_event_callback, evname)
+				< 0) {
 			LM_ERR("error running event route kemi callback\n");
 		}
 		return 0;
@@ -835,7 +836,8 @@ int ki_nats_publish(sip_msg_t *msg, str *subject, str *payload)
 /**
  *
  */
-int ki_nats_publish_request(sip_msg_t *msg, str *subject, str *payload, str *reply)
+int ki_nats_publish_request(
+		sip_msg_t *msg, str *subject, str *payload, str *reply)
 {
 	return w_nats_publish(msg, *subject, *payload, *reply);
 }
