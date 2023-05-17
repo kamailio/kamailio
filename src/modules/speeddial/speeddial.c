1b5ab42c21c9b248f35d956ca3af53e193c81824
Victor Seva <linuxmaniac@torreviejawireless.org>
2023-05-17 16:37:14 +0200

    speeddial: clang-format for coherent indentation and coding style

--- src/modules/speeddial/speeddial.c
+++ src/modules/speeddial/speeddial.c
@@ -48,58 +48,53 @@ static int child_init(int rank);
 static int mod_init(void);
 
 /* Module parameter variables */
-static str db_url    = str_init(DEFAULT_RODB_URL);
-str user_column      = str_init("username");
-str domain_column    = str_init("domain");
-str sd_user_column   = str_init("sd_username");
+static str db_url = str_init(DEFAULT_RODB_URL);
+str user_column = str_init("username");
+str domain_column = str_init("domain");
+str sd_user_column = str_init("sd_username");
 str sd_domain_column = str_init("sd_domain");
-str new_uri_column   = str_init("new_uri");
-int use_domain       = 0;
-static str domain_prefix    = {NULL, 0};
+str new_uri_column = str_init("new_uri");
+int use_domain = 0;
+static str domain_prefix = {NULL, 0};
 
 str dstrip_s = {NULL, 0};
 
 
-db_func_t db_funcs;      /* Database functions */
-db1_con_t* db_handle=0;   /* Database connection handle */
+db_func_t db_funcs;		  /* Database functions */
+db1_con_t *db_handle = 0; /* Database connection handle */
 
 
 /* Exported functions */
-static cmd_export_t cmds[] = {
-	{"sd_lookup", (cmd_function)w_sd_lookup, 1, fixup_spve_null, 0,
-		REQUEST_ROUTE},
-	{"sd_lookup", (cmd_function)w_sd_lookup, 2, fixup_spve_spve, 0,
-		REQUEST_ROUTE},
-	{0, 0, 0, 0, 0, 0}
-};
+static cmd_export_t cmds[] = {{"sd_lookup", (cmd_function)w_sd_lookup, 1,
+									  fixup_spve_null, 0, REQUEST_ROUTE},
+		{"sd_lookup", (cmd_function)w_sd_lookup, 2, fixup_spve_spve, 0,
+				REQUEST_ROUTE},
+		{0, 0, 0, 0, 0, 0}};
 
 
 /* Exported parameters */
-static param_export_t params[] = {
-	{"db_url",           PARAM_STR, &db_url             },
-	{"user_column",      PARAM_STR, &user_column        },
-	{"domain_column",    PARAM_STR, &domain_column      },
-	{"sd_user_column",   PARAM_STR, &sd_user_column     },
-	{"sd_domain_column", PARAM_STR, &sd_domain_column   },
-	{"new_uri_column",   PARAM_STR, &new_uri_column     },
-	{"use_domain",       INT_PARAM, &use_domain           },
-	{"domain_prefix",    PARAM_STR, &domain_prefix      },
-	{0, 0, 0}
-};
+static param_export_t params[] = {{"db_url", PARAM_STR, &db_url},
+		{"user_column", PARAM_STR, &user_column},
+		{"domain_column", PARAM_STR, &domain_column},
+		{"sd_user_column", PARAM_STR, &sd_user_column},
+		{"sd_domain_column", PARAM_STR, &sd_domain_column},
+		{"new_uri_column", PARAM_STR, &new_uri_column},
+		{"use_domain", INT_PARAM, &use_domain},
+		{"domain_prefix", PARAM_STR, &domain_prefix}, {0, 0, 0}};
 
 
 /* Module interface */
 struct module_exports exports = {
-	"speeddial",     /* module name */
-	DEFAULT_DLFLAGS, /* dlopen flags */
-	cmds,            /* exported functions */
-	params,          /* exported parameters */
-	0,               /* exported RPC functions */
-	0,               /* exported pseudo-variables */
-	0,               /* response function */
-	mod_init,        /* module initialization function */
-	child_init,      /* child initialization function */
-	destroy          /* destroy function */
+		"speeddial",	 /* module name */
+		DEFAULT_DLFLAGS, /* dlopen flags */
+		cmds,			 /* exported functions */
+		params,			 /* exported parameters */
+		0,				 /* exported RPC functions */
+		0,				 /* exported pseudo-variables */
+		0,				 /* response function */
+		mod_init,		 /* module initialization function */
+		child_init,		 /* child initialization function */
+		destroy			 /* destroy function */
 };
 
 
@@ -108,17 +103,15 @@ struct module_exports exports = {
  */
 static int child_init(int rank)
 {
-	if (rank==PROC_INIT || rank==PROC_MAIN || rank==PROC_TCP_MAIN)
+	if(rank == PROC_INIT || rank == PROC_MAIN || rank == PROC_TCP_MAIN)
 		return 0; /* do nothing for the main process */
 
 	db_handle = db_funcs.init(&db_url);
-	if (!db_handle)
-	{
+	if(!db_handle) {
 		LM_ERR("failed to connect database\n");
 		return -1;
 	}
 	return 0;
-
 }
 
 
@@ -128,18 +121,16 @@ static int child_init(int rank)
 static int mod_init(void)
 {
 	/* Find a database module */
-	if (db_bind_mod(&db_url, &db_funcs))
-	{
+	if(db_bind_mod(&db_url, &db_funcs)) {
 		LM_ERR("failed to bind database module\n");
 		return -1;
 	}
-	if (!DB_CAPABILITY(db_funcs, DB_CAP_QUERY))
-	{
+	if(!DB_CAPABILITY(db_funcs, DB_CAP_QUERY)) {
 		LM_ERR("Database modules does not "
-			"provide all functions needed by SPEEDDIAL module\n");
+			   "provide all functions needed by SPEEDDIAL module\n");
 		return -1;
 	}
-	if (domain_prefix.s && domain_prefix.len > 0) {
+	if(domain_prefix.s && domain_prefix.len > 0) {
 		dstrip_s.s = domain_prefix.s;
 		dstrip_s.len = domain_prefix.len;
 	}
@@ -153,7 +144,7 @@ static int mod_init(void)
  */
 static void destroy(void)
 {
-	if (db_handle)
+	if(db_handle)
 		db_funcs.close(db_handle);
 }
 
