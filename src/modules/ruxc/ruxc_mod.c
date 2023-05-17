79a79011305a03c7fdec13991772f39c1f9f4d18
Victor Seva <linuxmaniac@torreviejawireless.org>
2023-05-17 16:37:11 +0200

    ruxc: clang-format for coherent indentation and coding style

--- src/modules/ruxc/ruxc_mod.c
+++ src/modules/ruxc/ruxc_mod.c
@@ -54,18 +54,19 @@ static void mod_destroy(void);
 
 static int fixup_ruxc_http_get(void **param, int param_no);
 static int fixup_free_ruxc_http_get(void **param, int param_no);
-static int w_ruxc_http_get(struct sip_msg *_msg, char *_url,
-		char *_hdrs, char *_result);
+static int w_ruxc_http_get(
+		struct sip_msg *_msg, char *_url, char *_hdrs, char *_result);
 
 static int fixup_ruxc_http_post(void **param, int param_no);
 static int fixup_free_ruxc_http_post(void **param, int param_no);
-static int w_ruxc_http_post(struct sip_msg *_msg, char *_url,
-		char *_body, char *_hdrs, char *_result);
+static int w_ruxc_http_post(struct sip_msg *_msg, char *_url, char *_body,
+		char *_hdrs, char *_result);
 
-static int w_ruxc_http_delete(struct sip_msg *_msg, char *_url,
-		char *_body, char *_hdrs, char *_result);
+static int w_ruxc_http_delete(struct sip_msg *_msg, char *_url, char *_body,
+		char *_hdrs, char *_result);
 
-typedef struct ruxc_data {
+typedef struct ruxc_data
+{
 	str value;
 	int ret;
 } ruxc_data_t;
@@ -116,10 +117,10 @@ struct module_exports exports = {
  */
 static int mod_init(void)
 {
-	if(_ruxc_http_logtype==1 && log_stderr==1) {
+	if(_ruxc_http_logtype == 1 && log_stderr == 1) {
 		LM_INFO("setting http logtype to 0\n");
 		_ruxc_http_logtype = 0;
-	} else if(_ruxc_http_logtype==0 && log_stderr==0) {
+	} else if(_ruxc_http_logtype == 0 && log_stderr == 0) {
 		LM_INFO("setting http logtype to 1\n");
 		_ruxc_http_logtype = 1;
 	}
@@ -161,8 +162,8 @@ static void ruxc_request_params_init(RuxcHTTPRequest *v_http_request)
 /**
  *
  */
-static int ki_ruxc_http_get_helper(sip_msg_t *_msg, str *url, str *hdrs,
-		pv_spec_t *dst)
+static int ki_ruxc_http_get_helper(
+		sip_msg_t *_msg, str *url, str *hdrs, pv_spec_t *dst)
 {
 	RuxcHTTPRequest v_http_request = {0};
 	RuxcHTTPResponse v_http_response = {0};
@@ -174,7 +175,7 @@ static int ki_ruxc_http_get_helper(sip_msg_t *_msg, str *url, str *hdrs,
 	v_http_request.url = url->s;
 	v_http_request.url_len = url->len;
 
-	if(hdrs!=NULL && hdrs->s!=NULL && hdrs->len>0) {
+	if(hdrs != NULL && hdrs->s != NULL && hdrs->len > 0) {
 		v_http_request.headers = hdrs->s;
 		v_http_request.headers_len = hdrs->len;
 	}
@@ -182,10 +183,11 @@ static int ki_ruxc_http_get_helper(sip_msg_t *_msg, str *url, str *hdrs,
 	ruxc_http_get(&v_http_request, &v_http_response);
 
 	if(v_http_response.retcode < 0) {
-		LM_ERR("failed to perform http get - retcode: %d\n", v_http_response.retcode);
+		LM_ERR("failed to perform http get - retcode: %d\n",
+				v_http_response.retcode);
 		ret = v_http_response.retcode;
 	} else {
-		if(v_http_response.resdata != NULL &&  v_http_response.resdata_len>0) {
+		if(v_http_response.resdata != NULL && v_http_response.resdata_len > 0) {
 			LM_DBG("response code: %d - data len: %d - data: [%.*s]\n",
 					v_http_response.rescode, v_http_response.resdata_len,
 					v_http_response.resdata_len, v_http_response.resdata);
@@ -202,7 +204,7 @@ static int ki_ruxc_http_get_helper(sip_msg_t *_msg, str *url, str *hdrs,
 	}
 	ruxc_http_response_release(&v_http_response);
 
-	return (ret!=0)?ret:-2;
+	return (ret != 0) ? ret : -2;
 }
 
 /**
@@ -213,11 +215,11 @@ static int ki_ruxc_http_get(sip_msg_t *_msg, str *url, str *hdrs, str *dpv)
 	pv_spec_t *dst;
 
 	dst = pv_cache_get(dpv);
-	if(dst==NULL) {
+	if(dst == NULL) {
 		LM_ERR("failed to get pv spec for: %.*s\n", dpv->len, dpv->s);
 		return -1;
 	}
-	if(dst->setf==NULL) {
+	if(dst->setf == NULL) {
 		LM_ERR("target pv is not writable: %.*s\n", dpv->len, dpv->s);
 		return -1;
 	}
@@ -227,8 +229,8 @@ static int ki_ruxc_http_get(sip_msg_t *_msg, str *url, str *hdrs, str *dpv)
 /**
  *
  */
-static int w_ruxc_http_get(struct sip_msg *_msg, char *_url,
-		char *_hdrs, char *_result)
+static int w_ruxc_http_get(
+		struct sip_msg *_msg, char *_url, char *_hdrs, char *_result)
 {
 	str url = {NULL, 0};
 	str hdrs = {NULL, 0};
@@ -254,8 +256,8 @@ static int w_ruxc_http_get(struct sip_msg *_msg, char *_url,
 /**
  *
  */
-static int ki_ruxc_http_post_helper(sip_msg_t *_msg, str *url, str *body, str *hdrs,
-		pv_spec_t *dst)
+static int ki_ruxc_http_post_helper(
+		sip_msg_t *_msg, str *url, str *body, str *hdrs, pv_spec_t *dst)
 {
 	RuxcHTTPRequest v_http_request = {0};
 	RuxcHTTPResponse v_http_response = {0};
@@ -267,12 +269,12 @@ static int ki_ruxc_http_post_helper(sip_msg_t *_msg, str *url, str *body, str *h
 	v_http_request.url = url->s;
 	v_http_request.url_len = url->len;
 
-	if(body!=NULL && body->s!=NULL && body->len>0) {
+	if(body != NULL && body->s != NULL && body->len > 0) {
 		v_http_request.data = body->s;
 		v_http_request.data_len = body->len;
 	}
 
-	if(hdrs!=NULL && hdrs->s!=NULL && hdrs->len>0) {
+	if(hdrs != NULL && hdrs->s != NULL && hdrs->len > 0) {
 		v_http_request.headers = hdrs->s;
 		v_http_request.headers_len = hdrs->len;
 	}
@@ -280,10 +282,11 @@ static int ki_ruxc_http_post_helper(sip_msg_t *_msg, str *url, str *body, str *h
 	ruxc_http_post(&v_http_request, &v_http_response);
 
 	if(v_http_response.retcode < 0) {
-		LM_ERR("failed to perform http get - retcode: %d\n", v_http_response.retcode);
+		LM_ERR("failed to perform http get - retcode: %d\n",
+				v_http_response.retcode);
 		ret = v_http_response.retcode;
 	} else {
-		if(v_http_response.resdata != NULL &&  v_http_response.resdata_len>0) {
+		if(v_http_response.resdata != NULL && v_http_response.resdata_len > 0) {
 			LM_DBG("response code: %d - data len: %d - data: [%.*s]\n",
 					v_http_response.rescode, v_http_response.resdata_len,
 					v_http_response.resdata_len, v_http_response.resdata);
@@ -300,22 +303,23 @@ static int ki_ruxc_http_post_helper(sip_msg_t *_msg, str *url, str *body, str *h
 	}
 	ruxc_http_response_release(&v_http_response);
 
-	return (ret!=0)?ret:-2;
+	return (ret != 0) ? ret : -2;
 }
 
 /**
  *
  */
-static int ki_ruxc_http_post(sip_msg_t *_msg, str *url, str *body, str *hdrs, str *dpv)
+static int ki_ruxc_http_post(
+		sip_msg_t *_msg, str *url, str *body, str *hdrs, str *dpv)
 {
 	pv_spec_t *dst;
 
 	dst = pv_cache_get(dpv);
-	if(dst==NULL) {
+	if(dst == NULL) {
 		LM_ERR("failed to get pv spec for: %.*s\n", dpv->len, dpv->s);
 		return -1;
 	}
-	if(dst->setf==NULL) {
+	if(dst->setf == NULL) {
 		LM_ERR("target pv is not writable: %.*s\n", dpv->len, dpv->s);
 		return -1;
 	}
@@ -325,8 +329,8 @@ static int ki_ruxc_http_post(sip_msg_t *_msg, str *url, str *body, str *hdrs, st
 /**
  *
  */
-static int w_ruxc_http_post(struct sip_msg *_msg, char *_url,
-		char *_body, char *_hdrs, char *_result)
+static int w_ruxc_http_post(struct sip_msg *_msg, char *_url, char *_body,
+		char *_hdrs, char *_result)
 {
 	str url = {NULL, 0};
 	str body = {NULL, 0};
@@ -361,8 +365,8 @@ static int w_ruxc_http_post(struct sip_msg *_msg, char *_url,
 /**
  *
  */
-static int ki_ruxc_http_delete_helper(sip_msg_t *_msg, str *url, str *body, str *hdrs,
-		pv_spec_t *dst)
+static int ki_ruxc_http_delete_helper(
+		sip_msg_t *_msg, str *url, str *body, str *hdrs, pv_spec_t *dst)
 {
 	RuxcHTTPRequest v_http_request = {0};
 	RuxcHTTPResponse v_http_response = {0};
@@ -374,12 +378,12 @@ static int ki_ruxc_http_delete_helper(sip_msg_t *_msg, str *url, str *body, str 
 	v_http_request.url = url->s;
 	v_http_request.url_len = url->len;
 
-	if(body!=NULL && body->s!=NULL && body->len>0) {
+	if(body != NULL && body->s != NULL && body->len > 0) {
 		v_http_request.data = body->s;
 		v_http_request.data_len = body->len;
 	}
 
-	if(hdrs!=NULL && hdrs->s!=NULL && hdrs->len>0) {
+	if(hdrs != NULL && hdrs->s != NULL && hdrs->len > 0) {
 		v_http_request.headers = hdrs->s;
 		v_http_request.headers_len = hdrs->len;
 	}
@@ -387,10 +391,11 @@ static int ki_ruxc_http_delete_helper(sip_msg_t *_msg, str *url, str *body, str 
 	ruxc_http_delete(&v_http_request, &v_http_response);
 
 	if(v_http_response.retcode < 0) {
-		LM_ERR("failed to perform http get - retcode: %d\n", v_http_response.retcode);
+		LM_ERR("failed to perform http get - retcode: %d\n",
+				v_http_response.retcode);
 		ret = v_http_response.retcode;
 	} else {
-		if(v_http_response.resdata != NULL &&  v_http_response.resdata_len>0) {
+		if(v_http_response.resdata != NULL && v_http_response.resdata_len > 0) {
 			LM_DBG("response code: %d - data len: %d - data: [%.*s]\n",
 					v_http_response.rescode, v_http_response.resdata_len,
 					v_http_response.resdata_len, v_http_response.resdata);
@@ -407,22 +412,23 @@ static int ki_ruxc_http_delete_helper(sip_msg_t *_msg, str *url, str *body, str 
 	}
 	ruxc_http_response_release(&v_http_response);
 
-	return (ret!=0)?ret:-2;
+	return (ret != 0) ? ret : -2;
 }
 
 /**
  *
  */
-static int ki_ruxc_http_delete(sip_msg_t *_msg, str *url, str *body, str *hdrs, str *dpv)
+static int ki_ruxc_http_delete(
+		sip_msg_t *_msg, str *url, str *body, str *hdrs, str *dpv)
 {
 	pv_spec_t *dst;
 
 	dst = pv_cache_get(dpv);
-	if(dst==NULL) {
+	if(dst == NULL) {
 		LM_ERR("failed to get pv spec for: %.*s\n", dpv->len, dpv->s);
 		return -1;
 	}
-	if(dst->setf==NULL) {
+	if(dst->setf == NULL) {
 		LM_ERR("target pv is not writable: %.*s\n", dpv->len, dpv->s);
 		return -1;
 	}
@@ -432,8 +438,8 @@ static int ki_ruxc_http_delete(sip_msg_t *_msg, str *url, str *body, str *hdrs, 
 /**
  *
  */
-static int w_ruxc_http_delete(struct sip_msg *_msg, char *_url,
-		char *_body, char *_hdrs, char *_result)
+static int w_ruxc_http_delete(struct sip_msg *_msg, char *_url, char *_body,
+		char *_hdrs, char *_result)
 {
 	str url = {NULL, 0};
 	str body = {NULL, 0};
