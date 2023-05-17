1e567d5adf631bd96a09aeb315cf2cbe374f051d
Victor Seva <linuxmaniac@torreviejawireless.org>
2023-05-17 16:37:08 +0200

    pua_dialoginfo: clang-format for coherent indentation and coding style

--- src/modules/pua_dialoginfo/dialog_publish.c
+++ src/modules/pua_dialoginfo/dialog_publish.c
@@ -46,7 +46,7 @@ extern int include_tags;
 extern int puadinfo_attribute_display;
 
 /* for debug purpose only */
-void print_publ(publ_info_t* p)
+void print_publ(publ_info_t *p)
 {
 	LM_DBG("publ:\n");
 	LM_DBG("uri= %.*s\n", p->pres_uri->len, p->pres_uri->s);
@@ -54,42 +54,42 @@ void print_publ(publ_info_t* p)
 	LM_DBG("expires= %d\n", p->expires);
 }
 
-static xmlNodePtr puadi_xmlNewChildStr(xmlNodePtr node, xmlNsPtr ns, char *name,
-		str *sval)
+static xmlNodePtr puadi_xmlNewChildStr(
+		xmlNodePtr node, xmlNsPtr ns, char *name, str *sval)
 {
-	char buf[MAX_URI_SIZE+1];
+	char buf[MAX_URI_SIZE + 1];
 
-	if (sval->len > MAX_URI_SIZE) {
+	if(sval->len > MAX_URI_SIZE) {
 		LM_ERR("value '%.*s' too long for '%s' - maximum=%d\n", sval->len,
 				sval->s, name, MAX_URI_SIZE);
 		return NULL;
 	}
 	memcpy(buf, sval->s, sval->len);
-	buf[sval->len]= '\0';
+	buf[sval->len] = '\0';
 
-	return xmlNewChild(node, ns, BAD_CAST name, BAD_CAST buf) ;
+	return xmlNewChild(node, ns, BAD_CAST name, BAD_CAST buf);
 }
 
 static xmlAttrPtr puadi_xmlNewPropStr(xmlNodePtr node, char *name, str *sval)
 {
-	char buf[MAX_URI_SIZE+1];
+	char buf[MAX_URI_SIZE + 1];
 
-	if (sval->len > MAX_URI_SIZE) {
+	if(sval->len > MAX_URI_SIZE) {
 		LM_ERR("value '%.*s' too long for '%s' - maximum=%d\n", sval->len,
 				sval->s, name, MAX_URI_SIZE);
 		return NULL;
 	}
 	memcpy(buf, sval->s, sval->len);
-	buf[sval->len]= '\0';
+	buf[sval->len] = '\0';
 
 	return xmlNewProp(node, BAD_CAST name, BAD_CAST buf);
 }
 
-str* build_dialoginfo(char *state, str *entity, str *peer, str *callid,
-		unsigned int initiator, str *localtag, str *remotetag,
-		str *localtarget, str *remotetarget, str *uuid)
+str *build_dialoginfo(char *state, str *entity, str *peer, str *callid,
+		unsigned int initiator, str *localtag, str *remotetag, str *localtarget,
+		str *remotetarget, str *uuid)
 {
-	xmlDocPtr  doc = NULL;
+	xmlDocPtr doc = NULL;
 	xmlNodePtr root_node = NULL;
 	xmlNodePtr dialog_node = NULL;
 	xmlNodePtr state_node = NULL;
@@ -102,12 +102,12 @@ str* build_dialoginfo(char *state, str *entity, str *peer, str *callid,
 
 	/* create the Publish body */
 	doc = xmlNewDoc(BAD_CAST "1.0");
-	if(doc==0) {
+	if(doc == 0) {
 		return NULL;
 	}
 
 	root_node = xmlNewNode(NULL, BAD_CAST "dialog-info");
-	if(root_node==0) {
+	if(root_node == 0) {
 		goto error;
 	}
 
@@ -117,11 +117,9 @@ str* build_dialoginfo(char *state, str *entity, str *peer, str *callid,
 			BAD_CAST "urn:ietf:params:xml:ns:dialog-info");
 	/* we set the version to 0 but it should be set to the correct value
 	 * in the pua module */
-	xmlNewProp(root_node, BAD_CAST "version",
-			BAD_CAST "0");
-	xmlNewProp(root_node, BAD_CAST  "state",
-			BAD_CAST "full" );
-	if(puadi_xmlNewPropStr(root_node, "entity", entity)==NULL) {
+	xmlNewProp(root_node, BAD_CAST "version", BAD_CAST "0");
+	xmlNewProp(root_node, BAD_CAST "state", BAD_CAST "full");
+	if(puadi_xmlNewPropStr(root_node, "entity", entity) == NULL) {
 		goto error;
 	}
 
@@ -135,58 +133,60 @@ str* build_dialoginfo(char *state, str *entity, str *peer, str *callid,
 	 * Thus, id and call-id will be populated identically */
 
 	/* dialog tag */
-	dialog_node =xmlNewChild(root_node, NULL, BAD_CAST "dialog", NULL) ;
+	dialog_node = xmlNewChild(root_node, NULL, BAD_CAST "dialog", NULL);
 	if(dialog_node == NULL) {
 		LM_ERR("while adding child\n");
 		goto error;
 	}
 
-	if(puadi_xmlNewPropStr(dialog_node, "id", uuid)==NULL) {
+	if(puadi_xmlNewPropStr(dialog_node, "id", uuid) == NULL) {
 		goto error;
 	}
 
-	if (include_callid) {
-		if(puadi_xmlNewPropStr(dialog_node, "call-id", callid)==NULL) {
+	if(include_callid) {
+		if(puadi_xmlNewPropStr(dialog_node, "call-id", callid) == NULL) {
 			goto error;
 		}
 	}
-	if (include_tags) {
-		if (localtag && localtag->s) {
-			if(puadi_xmlNewPropStr(dialog_node, "local-tag", localtag)==NULL) {
+	if(include_tags) {
+		if(localtag && localtag->s) {
+			if(puadi_xmlNewPropStr(dialog_node, "local-tag", localtag)
+					== NULL) {
 				goto error;
 			}
 		}
-		if (remotetag && remotetag->s) {
-			if(puadi_xmlNewPropStr(dialog_node, "remote-tag", remotetag)==NULL) {
+		if(remotetag && remotetag->s) {
+			if(puadi_xmlNewPropStr(dialog_node, "remote-tag", remotetag)
+					== NULL) {
 				goto error;
 			}
 		}
 	}
 
-	if (initiator) {
+	if(initiator) {
 		xmlNewProp(dialog_node, BAD_CAST "direction", BAD_CAST "initiator");
-	}else {
+	} else {
 		xmlNewProp(dialog_node, BAD_CAST "direction", BAD_CAST "recipient");
 	}
 
 	/* state tag */
-	state_node = xmlNewChild(dialog_node, NULL, BAD_CAST "state",
-			BAD_CAST state) ;
+	state_node =
+			xmlNewChild(dialog_node, NULL, BAD_CAST "state", BAD_CAST state);
 	if(state_node == NULL) {
 		LM_ERR("while adding child\n");
 		goto error;
 	}
 
-	if (include_localremote) {
+	if(include_localremote) {
 		/* remote tag */
-		remote_node = xmlNewChild(dialog_node, NULL, BAD_CAST "remote", NULL) ;
-		if( remote_node == NULL) {
+		remote_node = xmlNewChild(dialog_node, NULL, BAD_CAST "remote", NULL);
+		if(remote_node == NULL) {
 			LM_ERR("while adding child\n");
 			goto error;
 		}
 
-		tag_node = puadi_xmlNewChildStr(remote_node, NULL, "identity", peer) ;
-		if(tag_node ==NULL) {
+		tag_node = puadi_xmlNewChildStr(remote_node, NULL, "identity", peer);
+		if(tag_node == NULL) {
 			LM_ERR("while adding child\n");
 			goto error;
 		}
@@ -196,17 +196,17 @@ str* build_dialoginfo(char *state, str *entity, str *peer, str *callid,
 			LM_ERR("while adding child\n");
 			goto error;
 		}
-		if (remotetarget && remotetarget->s) {
+		if(remotetarget && remotetarget->s) {
 			suri = remotetarget;
 		} else {
 			suri = peer;
 		}
 		if(puadinfo_attribute_display) {
-			if(parse_uri(suri->s, suri->len, &puri)<0) {
+			if(parse_uri(suri->s, suri->len, &puri) < 0) {
 				LM_ERR("failed to parse uri [%.*s]\n", suri->len, suri->s);
 				goto error;
 			}
-			if(puri.user.s!=NULL && puri.user.len>0) {
+			if(puri.user.s != NULL && puri.user.len > 0) {
 				puadi_xmlNewPropStr(tag_node, "display", &puri.user);
 			}
 		}
@@ -219,7 +219,7 @@ str* build_dialoginfo(char *state, str *entity, str *peer, str *callid,
 			goto error;
 		}
 
-		tag_node = puadi_xmlNewChildStr(local_node, NULL, "identity", entity) ;
+		tag_node = puadi_xmlNewChildStr(local_node, NULL, "identity", entity);
 		if(tag_node == NULL) {
 			LM_ERR("while adding child\n");
 			goto error;
@@ -230,17 +230,17 @@ str* build_dialoginfo(char *state, str *entity, str *peer, str *callid,
 			LM_ERR("while adding child\n");
 			goto error;
 		}
-		if (localtarget && localtarget->s) {
+		if(localtarget && localtarget->s) {
 			suri = localtarget;
 		} else {
 			suri = entity;
 		}
 		if(puadinfo_attribute_display) {
-			if(parse_uri(suri->s, suri->len, &puri)<0) {
+			if(parse_uri(suri->s, suri->len, &puri) < 0) {
 				LM_ERR("failed to parse uri [%.*s]\n", suri->len, suri->s);
 				goto error;
 			}
-			if(puri.user.s!=NULL && puri.user.len>0) {
+			if(puri.user.s != NULL && puri.user.len > 0) {
 				puadi_xmlNewPropStr(tag_node, "display", &puri.user);
 			}
 		}
@@ -248,17 +248,17 @@ str* build_dialoginfo(char *state, str *entity, str *peer, str *callid,
 	}
 
 	/* create the body */
-	body = (str*)pkg_malloc(sizeof(str));
-	if(body == NULL)
-	{
+	body = (str *)pkg_malloc(sizeof(str));
+	if(body == NULL) {
 		PKG_MEM_ERROR;
 		goto error;
 	}
 	memset(body, 0, sizeof(str));
 
-	xmlDocDumpFormatMemory(doc,(unsigned char**)(void*)&body->s,&body->len,1);
+	xmlDocDumpFormatMemory(
+			doc, (unsigned char **)(void *)&body->s, &body->len, 1);
 
-	if(body->s==NULL || body->len==0) {
+	if(body->s == NULL || body->len == 0) {
 		LM_ERR("failure formatting xml doc from memory or empty doc\n");
 		goto error;
 	}
@@ -286,20 +286,20 @@ error:
 	return NULL;
 }
 
-void dialog_publish(char *state, str* ruri, str *entity, str *peer, str *callid,
+void dialog_publish(char *state, str *ruri, str *entity, str *peer, str *callid,
 		unsigned int initiator, unsigned int lifetime, str *localtag,
 		str *remotetag, str *localtarget, str *remotetarget,
-		unsigned short do_pubruri_localcheck, str* uuid)
+		unsigned short do_pubruri_localcheck, str *uuid)
 {
-	str* body= NULL;
-	str uri= {NULL, 0};
-	publ_info_t* publ= NULL;
-	int size= 0;
+	str *body = NULL;
+	str uri = {NULL, 0};
+	publ_info_t *publ = NULL;
+	int size = 0;
 	str content_type;
 	struct sip_uri ruri_uri;
 
 
-	if (parse_uri(ruri->s, ruri->len, &ruri_uri) < 0) {
+	if(parse_uri(ruri->s, ruri->len, &ruri_uri) < 0) {
 		LM_ERR("failed to parse the PUBLISH R-URI\n");
 		return;
 	}
@@ -307,83 +307,79 @@ void dialog_publish(char *state, str* ruri, str *entity, str *peer, str *callid,
 	if(do_pubruri_localcheck) {
 
 		/* send PUBLISH only if the receiver PUBLISH R-URI is local*/
-		if (!check_self(&(ruri_uri.host), 0, 0)) {
-			LM_DBG("do not send PUBLISH to external URI %.*s\n",
-					ruri->len, ruri->s);
+		if(!check_self(&(ruri_uri.host), 0, 0)) {
+			LM_DBG("do not send PUBLISH to external URI %.*s\n", ruri->len,
+					ruri->s);
 			return;
 		}
-
 	}
 
-	content_type.s= "application/dialog-info+xml";
-	content_type.len= 27;
+	content_type.s = "application/dialog-info+xml";
+	content_type.len = 27;
 
-	body= build_dialoginfo(state, entity, peer, callid, initiator, localtag,
+	body = build_dialoginfo(state, entity, peer, callid, initiator, localtag,
 			remotetag, localtarget, remotetarget, uuid);
 	if(body == NULL || body->s == NULL)
 		goto error;
 
 	LM_DBG("publish uri= %.*s\n", ruri->len, ruri->s);
 
-	size= sizeof(publ_info_t)
-		+ sizeof(str) 			/* *pres_uri */
-		+ ( ruri->len 		/* pres_uri->s */
-				+ uuid->len + 16	/* id.s */
-				+ content_type.len	/* content_type.s */
-			)*sizeof(char);
+	size = sizeof(publ_info_t) + sizeof(str) /* *pres_uri */
+		   + (ruri->len						 /* pres_uri->s */
+					 + uuid->len + 16		 /* id.s */
+					 + content_type.len		 /* content_type.s */
+					 )
+					 * sizeof(char);
 
 	if(body)
-		size+= sizeof(str)+ body->len* sizeof(char);
+		size += sizeof(str) + body->len * sizeof(char);
 
-	publ= (publ_info_t*)pkg_malloc(size);
-	if(publ== NULL)
-	{
+	publ = (publ_info_t *)pkg_malloc(size);
+	if(publ == NULL) {
 		PKG_MEM_ERROR;
 		goto error;
 	}
 	memset(publ, 0, size);
-	size= sizeof(publ_info_t);
+	size = sizeof(publ_info_t);
 
-	publ->pres_uri= (str*)((char*)publ + size);
-	size+= sizeof(str);
-	publ->pres_uri->s= (char*)publ+ size;
+	publ->pres_uri = (str *)((char *)publ + size);
+	size += sizeof(str);
+	publ->pres_uri->s = (char *)publ + size;
 	memcpy(publ->pres_uri->s, ruri->s, ruri->len);
-	publ->pres_uri->len= ruri->len;
-	size+= ruri->len;
+	publ->pres_uri->len = ruri->len;
+	size += ruri->len;
 
-	if(body)
-	{
-		publ->body= (str*)( (char*)publ + size);
-		size+= sizeof(str);
+	if(body) {
+		publ->body = (str *)((char *)publ + size);
+		size += sizeof(str);
 
-		publ->body->s= (char*)publ + size;
+		publ->body->s = (char *)publ + size;
 		memcpy(publ->body->s, body->s, body->len);
-		publ->body->len= body->len;
-		size+= body->len;
+		publ->body->len = body->len;
+		size += body->len;
 	}
-	publ->id.s= (char*)publ+ size;
+	publ->id.s = (char *)publ + size;
 	memcpy(publ->id.s, "DIALOG_PUBLISH.", 15);
-	memcpy(publ->id.s+15, uuid->s, uuid->len);
-	publ->id.len= 15+ uuid->len;
-	size+= publ->id.len;
+	memcpy(publ->id.s + 15, uuid->s, uuid->len);
+	publ->id.len = 15 + uuid->len;
+	size += publ->id.len;
 
-	publ->content_type.s= (char*)publ+ size;
+	publ->content_type.s = (char *)publ + size;
 	memcpy(publ->content_type.s, content_type.s, content_type.len);
-	publ->content_type.len= content_type.len;
-	size+= content_type.len;
+	publ->content_type.len = content_type.len;
+	size += content_type.len;
 
-	publ->expires= lifetime;
+	publ->expires = lifetime;
 
 	/* make UPDATE_TYPE, as if this "publish dialog" is not found
 	 * by pua it will fallback to INSERT_TYPE anyway */
-	publ->flag|= UPDATE_TYPE;
+	publ->flag |= UPDATE_TYPE;
 
-	publ->source_flag|= DIALOG_PUBLISH;
-	publ->event|= DIALOG_EVENT;
-	publ->extra_headers= NULL;
+	publ->source_flag |= DIALOG_PUBLISH;
+	publ->event |= DIALOG_EVENT;
+	publ->extra_headers = NULL;
 	print_publ(publ);
-	if(pua_send_publish(publ)< 0)
-	{
+	if(pua_send_publish(publ) < 0) {
 		LM_ERR("while sending publish\n");
 	}
 
@@ -392,8 +388,7 @@ error:
 	if(publ)
 		pkg_free(publ);
 
-	if(body)
-	{
+	if(body) {
 		if(body->s)
 			xmlFree(body->s);
 		pkg_free(body);
@@ -406,19 +401,17 @@ error:
 }
 
 
-
-void dialog_publish_multi(char *state, struct str_list* ruris, str *entity,
+void dialog_publish_multi(char *state, struct str_list *ruris, str *entity,
 		str *peer, str *callid, unsigned int initiator, unsigned int lifetime,
-		str *localtag, str *remotetag,
-		str *localtarget, str *remotetarget, unsigned short
-		do_pubruri_localcheck, str *uuid)
+		str *localtag, str *remotetag, str *localtarget, str *remotetarget,
+		unsigned short do_pubruri_localcheck, str *uuid)
 {
 	while(ruris) {
-		LM_DBG("CALLING dialog_publish for URI %.*s\n",
-				ruris->s.len, ruris->s.s);
-		dialog_publish(state,&(ruris->s),entity,peer,callid,initiator,
-				lifetime,localtag,remotetag,localtarget,remotetarget,
-				do_pubruri_localcheck,uuid);
-		ruris=ruris->next;
+		LM_DBG("CALLING dialog_publish for URI %.*s\n", ruris->s.len,
+				ruris->s.s);
+		dialog_publish(state, &(ruris->s), entity, peer, callid, initiator,
+				lifetime, localtag, remotetag, localtarget, remotetarget,
+				do_pubruri_localcheck, uuid);
+		ruris = ruris->next;
 	}
 }
