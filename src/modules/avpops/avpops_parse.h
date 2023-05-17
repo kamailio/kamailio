0ded8b19819ad9eccb6114589b90d857e8d86ca9
Victor Seva <linuxmaniac@torreviejawireless.org>
2023-05-17 16:35:58 +0200

    avpops: clang-format for coherent indentation and coding style

--- src/modules/avpops/avpops_parse.h
+++ src/modules/avpops/avpops_parse.h
@@ -31,17 +31,16 @@
 
 struct fis_param *avpops_parse_pvar(char *s);
 
-int   parse_avp_db(char *s, struct db_param *dbp, int allow_scheme);
+int parse_avp_db(char *s, struct db_param *dbp, int allow_scheme);
 
-int   parse_avp_aliases(char *s, char c1, char c2);
+int parse_avp_aliases(char *s, char c1, char c2);
 
-struct fis_param* parse_intstr_value(char *p, int len);
+struct fis_param *parse_intstr_value(char *p, int len);
 
 int parse_avp_db_scheme(char *s, struct db_scheme *scheme);
 
-struct fis_param*  parse_check_value(char *s);
+struct fis_param *parse_check_value(char *s);
 
-struct fis_param*  parse_op_value(char *s);
+struct fis_param *parse_op_value(char *s);
 
 #endif
-
