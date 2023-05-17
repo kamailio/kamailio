6d9f98c7ccfeaf40a9881784ba0dd5bb0065124b
Victor Seva <linuxmaniac@torreviejawireless.org>
2023-05-17 16:36:02 +0200

    db_mongodb: clang-format for coherent indentation and coding style

--- src/modules/db_mongodb/mongodb_connection.h
+++ src/modules/db_mongodb/mongodb_connection.h
@@ -29,21 +29,22 @@
 #include "../../lib/srdb1/db_pool.h"
 #include "../../lib/srdb1/db_id.h"
 
-typedef struct km_mongodb_con {
-	struct db_id* id;        /*!< Connection identifier */
-	unsigned int ref;        /*!< Reference count */
-	struct pool_con* next;   /*!< Next connection in the pool */
+typedef struct km_mongodb_con
+{
+	struct db_id *id;	   /*!< Connection identifier */
+	unsigned int ref;	   /*!< Reference count */
+	struct pool_con *next; /*!< Next connection in the pool */
 
-	mongoc_client_t *con;              /*!< Connection representation */
-	mongoc_collection_t *collection;   /*!< Collection link */
-	mongoc_cursor_t *cursor;           /*!< Cursor link */
+	mongoc_client_t *con;			 /*!< Connection representation */
+	mongoc_collection_t *collection; /*!< Collection link */
+	mongoc_cursor_t *cursor;		 /*!< Cursor link */
 
 	bson_t *colsdoc; /*!< Names of columns */
-	int nrcols;  /*!< Number of columns */
+	int nrcols;		 /*!< Number of columns */
 } km_mongodb_con_t;
 
-#define MONGODB_CON(db_con)  ((km_mongodb_con_t*)((db_con)->tail))
+#define MONGODB_CON(db_con) ((km_mongodb_con_t *)((db_con)->tail))
 
-km_mongodb_con_t* db_mongodb_new_connection(const struct db_id* id);
-void db_mongodb_free_connection(struct pool_con* con);
+km_mongodb_con_t *db_mongodb_new_connection(const struct db_id *id);
+void db_mongodb_free_connection(struct pool_con *con);
 #endif
