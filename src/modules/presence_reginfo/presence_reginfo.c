bffc6c4f69dac25e70505775bd017f6d4d3526ca
Victor Seva <linuxmaniac@torreviejawireless.org>
2023-05-17 16:37:07 +0200

    presence_reginfo: clang-format for coherent indentation and coding style

--- src/modules/presence_reginfo/presence_reginfo.c
+++ src/modules/presence_reginfo/presence_reginfo.c
@@ -52,10 +52,10 @@ unsigned int pres_reginfo_default_expires = 3600;
 
 /* module exported parameters */
 static param_export_t params[] = {
-	{ "default_expires", INT_PARAM, &pres_reginfo_default_expires },
-	{ "aggregate_presentities", INT_PARAM, &pres_reginfo_aggregate_presentities },
-	{0, 0, 0}
-};
+		{"default_expires", INT_PARAM, &pres_reginfo_default_expires},
+		{"aggregate_presentities", INT_PARAM,
+				&pres_reginfo_aggregate_presentities},
+		{0, 0, 0}};
 
 /* module exports */
 /* clang-format off */
@@ -81,24 +81,25 @@ static int mod_init(void)
 	presence_api_t pres;
 	bind_presence_t bind_presence;
 
-	bind_presence= (bind_presence_t)find_export("bind_presence", 1,0);
-	if (!bind_presence) {
+	bind_presence = (bind_presence_t)find_export("bind_presence", 1, 0);
+	if(!bind_presence) {
 		LM_ERR("can't bind presence\n");
 		return -1;
 	}
-	if (bind_presence(&pres) < 0) {
+	if(bind_presence(&pres) < 0) {
 		LM_ERR("can't bind presence\n");
 		return -1;
 	}
 
-	if (pres_reginfo_aggregate_presentities != 0
+	if(pres_reginfo_aggregate_presentities != 0
 			&& pres_reginfo_aggregate_presentities != 1) {
-		LM_ERR("invalid aggregate_presentities param value, should be 0 or 1\n");
+		LM_ERR("invalid aggregate_presentities param value, should be 0 or "
+			   "1\n");
 		return -1;
 	}
 
 	pres_add_event = pres.add_event;
-	if (pres_add_event == NULL) {
+	if(pres_add_event == NULL) {
 		LM_ERR("could not import add_event\n");
 		return -1;
 	}
