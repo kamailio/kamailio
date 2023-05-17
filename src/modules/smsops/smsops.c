2007b8bd50246db5da3116d34049d70371c980a9
Victor Seva <linuxmaniac@torreviejawireless.org>
2023-05-17 16:37:14 +0200

    smsops: clang-format for coherent indentation and coding style

--- src/modules/smsops/smsops.c
+++ src/modules/smsops/smsops.c
@@ -30,34 +30,31 @@
 
 MODULE_VERSION
 
-static pv_export_t mod_pvs[] = {
-	{ {"smsack", sizeof("smsack")-1}, PVT_OTHER, pv_sms_ack, 0, 0, 0, 0, 0 },
-	{ {"rpdata", sizeof("rpdata")-1}, PVT_OTHER, pv_get_sms, pv_set_sms,
-		pv_parse_rpdata_name, 0, 0, 0 },
-	{ {"tpdu", sizeof("tpdu")-1}, PVT_OTHER, pv_get_sms, pv_set_sms,
-		pv_parse_tpdu_name, 0, 0, 0 },
-	{ {"smsbody", sizeof("smsbody")-1}, PVT_OTHER, pv_sms_body, 0, 0, 0, 0, 0 },
-	{ {0, 0}, 0, 0, 0, 0, 0, 0, 0 }
-};
+static pv_export_t mod_pvs[] = {{{"smsack", sizeof("smsack") - 1}, PVT_OTHER,
+										pv_sms_ack, 0, 0, 0, 0, 0},
+		{{"rpdata", sizeof("rpdata") - 1}, PVT_OTHER, pv_get_sms, pv_set_sms,
+				pv_parse_rpdata_name, 0, 0, 0},
+		{{"tpdu", sizeof("tpdu") - 1}, PVT_OTHER, pv_get_sms, pv_set_sms,
+				pv_parse_tpdu_name, 0, 0, 0},
+		{{"smsbody", sizeof("smsbody") - 1}, PVT_OTHER, pv_sms_body, 0, 0, 0, 0,
+				0},
+		{{0, 0}, 0, 0, 0, 0, 0, 0, 0}};
 
-static cmd_export_t cmds[]={
-	{"smsdump",   (cmd_function)smsdump, 0, 0, 0, REQUEST_ROUTE},
-	{"isRPDATA",  (cmd_function)isRPDATA, 0, 0, 0, REQUEST_ROUTE},
-	{0,0,0,0,0,0}
-};
+static cmd_export_t cmds[] = {
+		{"smsdump", (cmd_function)smsdump, 0, 0, 0, REQUEST_ROUTE},
+		{"isRPDATA", (cmd_function)isRPDATA, 0, 0, 0, REQUEST_ROUTE},
+		{0, 0, 0, 0, 0, 0}};
 
 /** module exports */
-struct module_exports exports= {
-	"smsops",    /* module name */
-	DEFAULT_DLFLAGS, /* dlopen flags */
-	cmds,        /* exported functions */
-	0,           /* exported parameters */
-	0,           /* exported rpc functions */
-	mod_pvs,     /* exported pseudo-variables */
-	0,           /* response handling function*/
-	0,           /* module init function */
-	0,           /* per-child init function */
-	0            /* module destroy function */
+struct module_exports exports = {
+		"smsops",		 /* module name */
+		DEFAULT_DLFLAGS, /* dlopen flags */
+		cmds,			 /* exported functions */
+		0,				 /* exported parameters */
+		0,				 /* exported rpc functions */
+		mod_pvs,		 /* exported pseudo-variables */
+		0,				 /* response handling function*/
+		0,				 /* module init function */
+		0,				 /* per-child init function */
+		0				 /* module destroy function */
 };
-
-
