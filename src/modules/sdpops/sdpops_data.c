689400aba46ede6c1bae39e9b8d6c04302e96822
Victor Seva <linuxmaniac@torreviejawireless.org>
2023-05-17 16:37:12 +0200

    sdpops: clang-format for coherent indentation and coding style

--- src/modules/sdpops/sdpops_data.c
+++ src/modules/sdpops/sdpops_data.c
@@ -91,27 +91,19 @@ Registration Procedures: Standards Action Process or expert approval
 
 */
 
-typedef struct _codecsmap {
+typedef struct _codecsmap
+{
 	str name;
 	str ids;
 } codecsmap_t;
 
-codecsmap_t sdpops_codecsmap_table[] = {
-	{ {"PCMU",  4},  {"0",         1} },
-	{ {"GSM",   3},  {"3",         1} },
-	{ {"G723",  4},  {"4",         1} },
-	{ {"DVI4",  4},  {"5,6,16,17", 9} },
-	{ {"LPC",   3},  {"7",         1} },
-	{ {"PCMA",  4},  {"8",         1} },
-	{ {"G722",  4},  {"9",         1} },
-	{ {"L16",   3},  {"10,11",     5} },
-	{ {"QCELP", 5},  {"12",        2} },
-	{ {"CN",    2},  {"13",        5} },
-	{ {"MPA",   3},  {"14",        2} },
-	{ {"G728",  4},  {"15",        2} },
-	{ {"G729",  4},  {"18",        2} },
-	{ {0, 0}, {0, 0} }
-};
+codecsmap_t sdpops_codecsmap_table[] = {{{"PCMU", 4}, {"0", 1}},
+		{{"GSM", 3}, {"3", 1}}, {{"G723", 4}, {"4", 1}},
+		{{"DVI4", 4}, {"5,6,16,17", 9}}, {{"LPC", 3}, {"7", 1}},
+		{{"PCMA", 4}, {"8", 1}}, {{"G722", 4}, {"9", 1}},
+		{{"L16", 3}, {"10,11", 5}}, {{"QCELP", 5}, {"12", 2}},
+		{{"CN", 2}, {"13", 5}}, {{"MPA", 3}, {"14", 2}},
+		{{"G728", 4}, {"15", 2}}, {{"G729", 4}, {"18", 2}}, {{0, 0}, {0, 0}}};
 
 /**
  * set the string with the IDs mapped to codec name
@@ -120,12 +112,11 @@ codecsmap_t sdpops_codecsmap_table[] = {
 int sdpops_get_ids_by_name(str *name, str *ids)
 {
 	int i;
-	for(i=0; sdpops_codecsmap_table[i].name.s!=0; i++)
-	{
-		if(name->len==sdpops_codecsmap_table[i].name.len
-				&& strncasecmp(sdpops_codecsmap_table[i].name.s, name->s,
-					name->len)==0)
-		{
+	for(i = 0; sdpops_codecsmap_table[i].name.s != 0; i++) {
+		if(name->len == sdpops_codecsmap_table[i].name.len
+				&& strncasecmp(
+						   sdpops_codecsmap_table[i].name.s, name->s, name->len)
+						   == 0) {
 			*ids = sdpops_codecsmap_table[i].ids;
 			return 0;
 		}
@@ -149,38 +140,38 @@ int sdpops_sdp_get_ids_by_name(sdp_info_t *sdp, str *cname, str *cids, int n)
 
 	sdp_session_num = 0;
 	i = 0;
-	for(;;)
-	{
+	for(;;) {
 		sdp_session = get_sdp_session_sdp(sdp, sdp_session_num);
-		if(!sdp_session) break;
+		if(!sdp_session)
+			break;
 		sdp_stream_num = 0;
-		for(;;)
-		{
-			sdp_stream = get_sdp_stream_sdp(sdp, sdp_session_num,
-							sdp_stream_num);
-			if(!sdp_stream) break;
+		for(;;) {
+			sdp_stream =
+					get_sdp_stream_sdp(sdp, sdp_session_num, sdp_stream_num);
+			if(!sdp_stream)
+				break;
 			sdp_payload = sdp_stream->payload_attr;
-			while (sdp_payload) {
-				if(sdp_payload->rtp_enc.len==cname->len
-						&& strncasecmp(cname->s, sdp_payload->rtp_enc.s,
-								cname->len)==0)
-				{
-					if(i==n)
+			while(sdp_payload) {
+				if(sdp_payload->rtp_enc.len == cname->len
+						&& strncasecmp(
+								   cname->s, sdp_payload->rtp_enc.s, cname->len)
+								   == 0) {
+					if(i == n)
 						goto notfound;
 					cids[i] = sdp_payload->rtp_payload;
 					i++;
 				}
 
-				sdp_payload=sdp_payload->next;
+				sdp_payload = sdp_payload->next;
 			}
 			sdp_stream_num++;
 		}
 		sdp_session_num++;
 	}
 
-	if(i==0)
+	if(i == 0)
 		goto notfound;
-	if(i<n)
+	if(i < n)
 		cids[i].s = NULL;
 	return 0;
 
@@ -195,43 +186,40 @@ notfound:
  */
 int sdpops_build_ids_list(sdp_info_t *sdp, str *names, str *ids)
 {
-#define SDPOPS_MAX_LIST_SIZE	128
+#define SDPOPS_MAX_LIST_SIZE 128
 	static char _local_idslist[SDPOPS_MAX_LIST_SIZE];
 	str tmp;
 	str codec;
-#define SDPOPS_CIDS_SIZE	16
+#define SDPOPS_CIDS_SIZE 16
 	str cids[SDPOPS_CIDS_SIZE];
 	char *p;
 	int i;
 
 	tmp = *names;
 	ids->len = 0;
-	ids->s   = 0;
+	ids->s = 0;
 	p = _local_idslist;
-	while(str_find_token(&tmp, &codec, ',')==0
-		&& codec.len>0)
-	{
-		tmp.len -= (int)(&codec.s[codec.len]-codec.s);
+	while(str_find_token(&tmp, &codec, ',') == 0 && codec.len > 0) {
+		tmp.len -= (int)(&codec.s[codec.len] - codec.s);
 		tmp.s = codec.s + codec.len;
 
 		cids[0].s = NULL;
-		if(sdpops_get_ids_by_name(&codec, &cids[0])==0) {
+		if(sdpops_get_ids_by_name(&codec, &cids[0]) == 0) {
 			LM_DBG("codecs list [%.*s] - at name [%.*s] with list ids [%.*s]\n",
-				names->len, names->s,
-				codec.len, codec.s,
-				cids[0].len,  cids[0].s);
+					names->len, names->s, codec.len, codec.s, cids[0].len,
+					cids[0].s);
 			cids[1].s = NULL;
 		} else {
-			if(sdpops_sdp_get_ids_by_name(sdp, &codec, cids, SDPOPS_CIDS_SIZE)==0) {
-				LM_DBG("codecs list [%.*s] - at name [%.*s] with first sdp id [%.*s]\n",
-					names->len, names->s,
-					codec.len, codec.s,
-					cids[0].len,  cids[0].s);
+			if(sdpops_sdp_get_ids_by_name(sdp, &codec, cids, SDPOPS_CIDS_SIZE)
+					== 0) {
+				LM_DBG("codecs list [%.*s] - at name [%.*s] with first sdp id "
+					   "[%.*s]\n",
+						names->len, names->s, codec.len, codec.s, cids[0].len,
+						cids[0].s);
 			}
 		}
-		for(i=0; i<SDPOPS_CIDS_SIZE && cids[i].s!=NULL; i++) {
-			if(ids->len + cids[i].len>=SDPOPS_MAX_LIST_SIZE)
-			{
+		for(i = 0; i < SDPOPS_CIDS_SIZE && cids[i].s != NULL; i++) {
+			if(ids->len + cids[i].len >= SDPOPS_MAX_LIST_SIZE) {
 				LM_ERR("the list with codecs ids is too big\n");
 				ids->len = 0;
 				ids->s = 0;
@@ -244,14 +232,12 @@ int sdpops_build_ids_list(sdp_info_t *sdp, str *names, str *ids)
 			ids->len += cids[i].len + 1;
 		}
 	}
-	if(ids->len>0)
-	{
+	if(ids->len > 0) {
 		p--;
 		ids->len--;
 		*p = '\0';
 		ids->s = _local_idslist;
-		LM_DBG("codecs list [%.*s] - ids list [%.*s]\n",
-				names->len, names->s,
+		LM_DBG("codecs list [%.*s] - ids list [%.*s]\n", names->len, names->s,
 				ids->len, ids->s);
 		return 0;
 	}
@@ -265,20 +251,18 @@ int sdpops_build_ids_list(sdp_info_t *sdp, str *names, str *ids)
 int str_find_token(str *text, str *result, char delim)
 {
 	int i;
-	if(text==NULL || result==NULL)
+	if(text == NULL || result == NULL)
 		return -1;
-	if(text->s[0] == delim)
-	{
+	if(text->s[0] == delim) {
 		text->s += 1;
 		text->len -= 1;
 	}
 	trim_leading(text);
 	result->s = text->s;
 	result->len = 0;
-	for (i=0; i<text->len; i++)
-	{
-		if(result->s[i]==delim || result->s[i]=='\0'
-				|| result->s[i]=='\r' || result->s[i]=='\n')
+	for(i = 0; i < text->len; i++) {
+		if(result->s[i] == delim || result->s[i] == '\0' || result->s[i] == '\r'
+				|| result->s[i] == '\n')
 			return 0;
 		result->len++;
 	}
