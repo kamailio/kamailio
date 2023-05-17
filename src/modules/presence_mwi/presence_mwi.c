5152e2790722257ca75771a000140c96b5afc981
Victor Seva <linuxmaniac@torreviejawireless.org>
2023-05-17 16:37:07 +0200

    presence_mwi: clang-format for coherent indentation and coding style

--- src/modules/presence_mwi/presence_mwi.c
+++ src/modules/presence_mwi/presence_mwi.c
@@ -63,29 +63,24 @@ add_event_t pres_add_event;
 unsigned int pres_mwi_default_expires = 3600;
 
 /* module exported commands */
-static cmd_export_t cmds[] =
-{
-	{0,	0, 0, 0, 0, 0}
-};
+static cmd_export_t cmds[] = {{0, 0, 0, 0, 0, 0}};
 
 /* module exported parameters */
 static param_export_t params[] = {
-	{ "default_expires", INT_PARAM, &pres_mwi_default_expires },
-	{0, 0, 0}
-};
+		{"default_expires", INT_PARAM, &pres_mwi_default_expires}, {0, 0, 0}};
 
 /* module exports */
-struct module_exports exports= {
-	"presence_mwi",		/* module name */
-	DEFAULT_DLFLAGS,	/* dlopen flags */
-	cmds,				/* exported functions */
-	params,				/* exported parameters */
-	0,					/* RPC method exports */
-	0,					/* exported pseudo-variables */
-	0,					/* response handling function */
-	mod_init,			/* module initialization function */
-	0,					/* per-child init function */
-	0					/* module destroy function */
+struct module_exports exports = {
+		"presence_mwi",	 /* module name */
+		DEFAULT_DLFLAGS, /* dlopen flags */
+		cmds,			 /* exported functions */
+		params,			 /* exported parameters */
+		0,				 /* RPC method exports */
+		0,				 /* exported pseudo-variables */
+		0,				 /* response handling function */
+		mod_init,		 /* module initialization function */
+		0,				 /* per-child init function */
+		0				 /* module destroy function */
 };
 
 /*
@@ -96,18 +91,18 @@ static int mod_init(void)
 	presence_api_t pres;
 	bind_presence_t bind_presence;
 
-	bind_presence= (bind_presence_t)find_export("bind_presence", 1,0);
-	if (!bind_presence) {
+	bind_presence = (bind_presence_t)find_export("bind_presence", 1, 0);
+	if(!bind_presence) {
 		LM_ERR("can't bind presence\n");
 		return -1;
 	}
-	if (bind_presence(&pres) < 0) {
+	if(bind_presence(&pres) < 0) {
 		LM_ERR("can't bind pua\n");
 		return -1;
 	}
 
 	pres_add_event = pres.add_event;
-	if (pres_add_event == NULL) {
+	if(pres_add_event == NULL) {
 		LM_ERR("could not import add_event\n");
 		return -1;
 	}
