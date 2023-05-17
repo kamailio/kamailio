a1a2f82e0cf1c30c044a951750f0fc6553d5223a
Victor Seva <linuxmaniac@torreviejawireless.org>
2023-05-17 16:36:55 +0200

    log_custom: clang-format for coherent indentation and coding style

--- src/modules/log_custom/log_custom_mod.c
+++ src/modules/log_custom/log_custom_mod.c
@@ -39,35 +39,31 @@ MODULE_VERSION
 static int _lc_log_udp = 0;
 static struct dest_info _lc_udp_dst = {0};
 
-static int  mod_init(void);
-static int  child_init(int);
+static int mod_init(void);
+static int child_init(int);
 static void mod_destroy(void);
 
-static int w_log_udp(struct sip_msg* msg, char* txt, char* p2);
+static int w_log_udp(struct sip_msg *msg, char *txt, char *p2);
 
 void _lc_core_log_udp(int lpriority, const char *format, ...);
 
-static cmd_export_t cmds[]={
-	{"log_udp", (cmd_function)w_log_udp, 1, fixup_spve_null,
-		0, ANY_ROUTE},
-	{0, 0, 0, 0, 0, 0}
-};
+static cmd_export_t cmds[] = {
+		{"log_udp", (cmd_function)w_log_udp, 1, fixup_spve_null, 0, ANY_ROUTE},
+		{0, 0, 0, 0, 0, 0}};
 
-static param_export_t params[]={
-	{0, 0, 0}
-};
+static param_export_t params[] = {{0, 0, 0}};
 
 struct module_exports exports = {
-	"log_custom",   /* module name */
-	DEFAULT_DLFLAGS, /* dlopen flags */
-	cmds,           /* exported functions */
-	params,         /* exported parameters */
-	0,              /* exported RPC functions */
-	0,              /* exported pseudo-variables */
-	0,              /* response function */
-	mod_init,       /* module initialization function */
-	child_init,     /* per child init function */
-	mod_destroy    	/* destroy function */
+		"log_custom",	 /* module name */
+		DEFAULT_DLFLAGS, /* dlopen flags */
+		cmds,			 /* exported functions */
+		params,			 /* exported parameters */
+		0,				 /* exported RPC functions */
+		0,				 /* exported pseudo-variables */
+		0,				 /* response function */
+		mod_init,		 /* module initialization function */
+		child_init,		 /* per child init function */
+		mod_destroy		 /* destroy function */
 };
 
 /**
@@ -83,14 +79,14 @@ static int mod_init(void)
  */
 static int child_init(int rank)
 {
-	if(rank!=PROC_INIT)
+	if(rank != PROC_INIT)
 		return 0;
 
 	_lc_udp_dst.proto = PROTO_UDP;
-	_lc_udp_dst.send_sock=get_send_socket(NULL, &_lc_udp_dst.to, PROTO_UDP);
-	if (_lc_udp_dst.send_sock==0) {
+	_lc_udp_dst.send_sock = get_send_socket(NULL, &_lc_udp_dst.to, PROTO_UDP);
+	if(_lc_udp_dst.send_sock == 0) {
 		_lc_udp_dst.send_sock = get_out_socket(&_lc_udp_dst.to, PROTO_UDP);
-		if (_lc_udp_dst.send_sock==0) {
+		if(_lc_udp_dst.send_sock == 0) {
 			LM_ERR("failed to get send socket\n");
 			return -1;
 		}
@@ -98,7 +94,6 @@ static int child_init(int rank)
 	LM_DBG("setting udp-send custom logging function\n");
 	km_log_func_set(&_lc_core_log_udp);
 	_lc_log_udp = 1;
-
 
 
 	return 0;
@@ -113,27 +108,28 @@ static void mod_destroy(void)
 /**
  *
  */
-static int w_log_udp(struct sip_msg* msg, char* txt, char* p2)
+static int w_log_udp(struct sip_msg *msg, char *txt, char *p2)
 {
 	str stxt;
 	int ret;
 
-	if(_lc_log_udp==0)
+	if(_lc_log_udp == 0)
 		return 1;
 
-	if(fixup_get_svalue(msg, (gparam_t*)txt, &stxt)!=0) {
+	if(fixup_get_svalue(msg, (gparam_t *)txt, &stxt) != 0) {
 		udp_send(&_lc_udp_dst, "error: unable to get text parameter\n", 36);
 		return -1;
 	}
 
-	ret=udp_send(&_lc_udp_dst, stxt.s, stxt.len);
+	ret = udp_send(&_lc_udp_dst, stxt.s, stxt.len);
 
-	if(ret==0) return 1;
+	if(ret == 0)
+		return 1;
 
 	return ret;
 }
 
-#define LC_LOG_MSG_MAX_SIZE	16384
+#define LC_LOG_MSG_MAX_SIZE 16384
 void _lc_core_log_udp(int lpriority, const char *format, ...)
 {
 	va_list arglist;
@@ -148,10 +144,11 @@ void _lc_core_log_udp(int lpriority, const char *format, ...)
 	n += vsnprintf(obuf + n, LC_LOG_MSG_MAX_SIZE - n, format, arglist);
 	va_end(arglist);
 	r = udp_send(&_lc_udp_dst, obuf, n);
-	if(r<0) {
+	if(r < 0) {
 		/* sending log message failed - print to stderror to help debugging */
-		fprintf(stderr, "error: previous udp send returned failure (%d:%d:%s)\n",
-				r, errno, strerror(errno));
+		fprintf(stderr,
+				"error: previous udp send returned failure (%d:%d:%s)\n", r,
+				errno, strerror(errno));
 	}
 }
 
@@ -159,15 +156,15 @@ int ki_log_udp(sip_msg_t *msg, str *txt)
 {
 	int ret;
 
-	if(_lc_log_udp==0)
+	if(_lc_log_udp == 0)
 		return 1;
 
-	ret=udp_send(&_lc_udp_dst, txt->s, txt->len);
+	ret = udp_send(&_lc_udp_dst, txt->s, txt->len);
 
-	if(ret>0) return 1;
+	if(ret > 0)
+		return 1;
 
-	return (ret==0)?-1:ret;
-
+	return (ret == 0) ? -1 : ret;
 }
 
 /**
@@ -192,11 +189,11 @@ int mod_register(char *path, int *dlflags, void *p1, void *p2)
 	struct sip_uri next_hop, *u;
 	char *p;
 
-	if(_km_log_engine_type==0 || _km_log_engine_data==0)
+	if(_km_log_engine_type == 0 || _km_log_engine_data == 0)
 		return 0;
 
 
-	if(strcasecmp(_km_log_engine_type, "udp")!=0)
+	if(strcasecmp(_km_log_engine_type, "udp") != 0)
 		return 0;
 
 	dest.s = _km_log_engine_data;
@@ -210,20 +207,21 @@ int mod_register(char *path, int *dlflags, void *p1, void *p2)
 	p = dest.s;
 	/* detect ipv6 */
 	p = memchr(p, ']', dest.len);
-	if (p) p++;
-	else p = dest.s;
+	if(p)
+		p++;
+	else
+		p = dest.s;
 	p = memchr(p, ':', dest.len - (p - dest.s));
-	if (p) {
+	if(p) {
 		u->host.len = p - dest.s;
 		p++;
 		u->port_no = str2s(p, dest.len - (p - dest.s), NULL);
 	}
 
-	ret = sip_hostport2su(&_lc_udp_dst.to, &u->host, u->port_no,
-			&_lc_udp_dst.proto);
-	if(ret!=0) {
-		LM_ERR("failed to resolve [%.*s]\n", u->host.len,
-				ZSW(u->host.s));
+	ret = sip_hostport2su(
+			&_lc_udp_dst.to, &u->host, u->port_no, &_lc_udp_dst.proto);
+	if(ret != 0) {
+		LM_ERR("failed to resolve [%.*s]\n", u->host.len, ZSW(u->host.s));
 		return -1;
 	}
 
