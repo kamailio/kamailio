cbc6a0fe229435f0345828dbb6c8e78ae0c5ca94
Victor Seva <linuxmaniac@torreviejawireless.org>
2023-05-18 15:22:08 +0200

    core: [mem] clang-format for coherent indentation and coding style

--- src/core/mem/memapi.h
+++ src/core/mem/memapi.h
@@ -28,40 +28,41 @@
 
 #ifdef DBG_SR_MEMORY
 
-typedef void* (*sr_malloc_f)(void* mbp, size_t size,
-					const char* file, const char* func, unsigned int line, const char* mname);
-typedef void  (*sr_free_f)(void* mbp, void* p, const char* file, const char* func,
-					unsigned int line, const char* mname);
-typedef void* (*sr_realloc_f)(void* mbp, void* p, size_t size,
-					const char* file, const char* func, unsigned int line, const char* mname);
-typedef void* (*sr_resize_f)(void* mbp, void* p, size_t size,
-					const char* file, const char* func, unsigned int line, const char* mname);
+typedef void *(*sr_malloc_f)(void *mbp, size_t size, const char *file,
+		const char *func, unsigned int line, const char *mname);
+typedef void (*sr_free_f)(void *mbp, void *p, const char *file,
+		const char *func, unsigned int line, const char *mname);
+typedef void *(*sr_realloc_f)(void *mbp, void *p, size_t size, const char *file,
+		const char *func, unsigned int line, const char *mname);
+typedef void *(*sr_resize_f)(void *mbp, void *p, size_t size, const char *file,
+		const char *func, unsigned int line, const char *mname);
 
 #else /*DBG_SR_MEMORY*/
 
-typedef void* (*sr_malloc_f)(void* mbp, size_t size);
-typedef void  (*sr_free_f)(void* mbp, void* p);
-typedef void* (*sr_realloc_f)(void* mbp, void* p, size_t size);
-typedef void* (*sr_resize_f)(void* mbp, void* p, size_t size);
+typedef void *(*sr_malloc_f)(void *mbp, size_t size);
+typedef void (*sr_free_f)(void *mbp, void *p);
+typedef void *(*sr_realloc_f)(void *mbp, void *p, size_t size);
+typedef void *(*sr_resize_f)(void *mbp, void *p, size_t size);
 
 #endif /*DBG_SR_MEMORY*/
 
-typedef void  (*sr_shm_glock_f)(void* mbp);
-typedef void  (*sr_shm_gunlock_f)(void* mbp);
+typedef void (*sr_shm_glock_f)(void *mbp);
+typedef void (*sr_shm_gunlock_f)(void *mbp);
 
-typedef void  (*sr_mem_status_f)(void* mbp);
-typedef void  (*sr_mem_info_f)(void* mbp, struct mem_info* info);
-typedef void  (*sr_mem_report_f)(void* mbp, mem_report_t* mrep);
-typedef unsigned long (*sr_mem_available_f)(void* mbp);
-typedef void  (*sr_mem_sums_f)(void* mbp);
+typedef void (*sr_mem_status_f)(void *mbp);
+typedef void (*sr_mem_info_f)(void *mbp, struct mem_info *info);
+typedef void (*sr_mem_report_f)(void *mbp, mem_report_t *mrep);
+typedef unsigned long (*sr_mem_available_f)(void *mbp);
+typedef void (*sr_mem_sums_f)(void *mbp);
 
-typedef void  (*sr_mem_destroy_f)(void);
+typedef void (*sr_mem_destroy_f)(void);
 
-typedef void (*sr_mem_mod_get_stats_f)(void* mbp, void **p);
-typedef void (*sr_mem_mod_free_stats_f)(void* mbp);
+typedef void (*sr_mem_mod_get_stats_f)(void *mbp, void **p);
+typedef void (*sr_mem_mod_free_stats_f)(void *mbp);
 
 /*private memory api*/
-typedef struct sr_pkg_api {
+typedef struct sr_pkg_api
+{
 	/*memory manager name - soft copy*/
 	char *mname;
 	/*entire private memory zone - soft copy*/
@@ -69,35 +70,36 @@ typedef struct sr_pkg_api {
 	/*memory manager block - soft copy*/
 	void *mem_block;
 	/*memory chunk allocation*/
-	sr_malloc_f        xmalloc;
+	sr_malloc_f xmalloc;
 	/*memory chunk allocation with 0 filling */
-	sr_malloc_f        xmallocxz;
+	sr_malloc_f xmallocxz;
 	/*memory chunk reallocation*/
-	sr_realloc_f       xrealloc;
+	sr_realloc_f xrealloc;
 	/*memory chunk reallocation with always free of old buffer*/
-	sr_realloc_f       xreallocxf;
+	sr_realloc_f xreallocxf;
 	/*memory chunk free*/
-	sr_free_f          xfree;
+	sr_free_f xfree;
 	/*memory status*/
-	sr_mem_status_f    xstatus;
+	sr_mem_status_f xstatus;
 	/*memory info - internal metrics*/
-	sr_mem_info_f      xinfo;
+	sr_mem_info_f xinfo;
 	/*memory report - internal report*/
-	sr_mem_report_f    xreport;
+	sr_mem_report_f xreport;
 	/*memory available size*/
 	sr_mem_available_f xavailable;
 	/*memory summary*/
-	sr_mem_sums_f      xsums;
+	sr_mem_sums_f xsums;
 	/*memory destroy manager*/
-	sr_mem_destroy_f   xdestroy;
+	sr_mem_destroy_f xdestroy;
 	/*memory stats per module*/
-	sr_mem_mod_get_stats_f  xmodstats;
+	sr_mem_mod_get_stats_f xmodstats;
 	/*memory stats free per module*/
 	sr_mem_mod_free_stats_f xfmodstats;
 } sr_pkg_api_t;
 
 /*shared memory api*/
-typedef struct sr_shm_api {
+typedef struct sr_shm_api
+{
 	/*memory manager name - soft copy*/
 	char *mname;
 	/*entire private memory zone - soft copy*/
@@ -105,41 +107,41 @@ typedef struct sr_shm_api {
 	/*memory manager block - soft copy*/
 	void *mem_block;
 	/*memory chunk allocation*/
-	sr_malloc_f        xmalloc;
+	sr_malloc_f xmalloc;
 	/*memory chunk allocation with 0 filling */
-	sr_malloc_f        xmallocxz;
+	sr_malloc_f xmallocxz;
 	/*memory chunk allocation without locking shm*/
-	sr_malloc_f        xmalloc_unsafe;
+	sr_malloc_f xmalloc_unsafe;
 	/*memory chunk reallocation*/
-	sr_realloc_f       xrealloc;
+	sr_realloc_f xrealloc;
 	/*memory chunk reallocation with always free of old buffer*/
-	sr_realloc_f       xreallocxf;
+	sr_realloc_f xreallocxf;
 	/*memory chunk resizing - free+malloc in same locking*/
-	sr_resize_f        xresize;
+	sr_resize_f xresize;
 	/*memory chunk free*/
-	sr_free_f          xfree;
+	sr_free_f xfree;
 	/*memory chunk free without locking shm*/
-	sr_free_f          xfree_unsafe;
+	sr_free_f xfree_unsafe;
 	/*memory status*/
-	sr_mem_status_f    xstatus;
+	sr_mem_status_f xstatus;
 	/*memory info - internal metrics*/
-	sr_mem_info_f      xinfo;
+	sr_mem_info_f xinfo;
 	/*memory report - internal report*/
-	sr_mem_report_f    xreport;
+	sr_mem_report_f xreport;
 	/*memory available size*/
 	sr_mem_available_f xavailable;
 	/*memory summary*/
-	sr_mem_sums_f      xsums;
+	sr_mem_sums_f xsums;
 	/*memory destroy manager*/
-	sr_mem_destroy_f   xdestroy;
+	sr_mem_destroy_f xdestroy;
 	/*memory stats per module*/
-	sr_mem_mod_get_stats_f  xmodstats;
+	sr_mem_mod_get_stats_f xmodstats;
 	/*memory stats free per module*/
 	sr_mem_mod_free_stats_f xfmodstats;
 	/*memory managing global lock*/
-	sr_shm_glock_f          xglock;
+	sr_shm_glock_f xglock;
 	/*memory managing global unlock*/
-	sr_shm_gunlock_f        xgunlock;
+	sr_shm_gunlock_f xgunlock;
 } sr_shm_api_t;
 
 #endif
--- src/core/mem/shm.h
+++ src/core/mem/shm.h
@@ -47,46 +47,54 @@ extern sr_shm_api_t _shm_root;
 
 #ifdef DBG_SR_MEMORY
 
-#	define shm_malloc(s)         _shm_root.xmalloc(_shm_root.mem_block, (s), _SRC_LOC_, \
-									_SRC_FUNCTION_, _SRC_LINE_, _SRC_MODULE_)
-#	define shm_mallocxz(s)       _shm_root.xmallocxz(_shm_root.mem_block, (s), _SRC_LOC_, \
-									_SRC_FUNCTION_, _SRC_LINE_, _SRC_MODULE_)
-#	define shm_malloc_unsafe(s)  _shm_root.xmalloc_unsafe(_shm_root.mem_block, (s), _SRC_LOC_, \
-									_SRC_FUNCTION_, _SRC_LINE_, _SRC_MODULE_)
-#	define shm_realloc(p, s)     _shm_root.xrealloc(_shm_root.mem_block, (p), (s), \
-									_SRC_LOC_, _SRC_FUNCTION_, _SRC_LINE_, _SRC_MODULE_)
-#	define shm_reallocxf(p, s)   _shm_root.xreallocxf(_shm_root.mem_block, (p), (s), \
-									_SRC_LOC_, _SRC_FUNCTION_, _SRC_LINE_, _SRC_MODULE_)
-#	define shm_resize(p, s)      _shm_root.xresize(_shm_root.mem_block, (p), (s), \
-									_SRC_LOC_, _SRC_FUNCTION_, _SRC_LINE_, _SRC_MODULE_)
-#	define shm_free(p)           _shm_root.xfree(_shm_root.mem_block, (p), _SRC_LOC_, \
-									_SRC_FUNCTION_, _SRC_LINE_, _SRC_MODULE_)
-#	define shm_free_unsafe(p)    _shm_root.xfree_unsafe(_shm_root.mem_block, (p), _SRC_LOC_, \
-									_SRC_FUNCTION_, _SRC_LINE_, _SRC_MODULE_)
+#define shm_malloc(s)                                                      \
+	_shm_root.xmalloc(_shm_root.mem_block, (s), _SRC_LOC_, _SRC_FUNCTION_, \
+			_SRC_LINE_, _SRC_MODULE_)
+#define shm_mallocxz(s)                                                      \
+	_shm_root.xmallocxz(_shm_root.mem_block, (s), _SRC_LOC_, _SRC_FUNCTION_, \
+			_SRC_LINE_, _SRC_MODULE_)
+#define shm_malloc_unsafe(s)                                      \
+	_shm_root.xmalloc_unsafe(_shm_root.mem_block, (s), _SRC_LOC_, \
+			_SRC_FUNCTION_, _SRC_LINE_, _SRC_MODULE_)
+#define shm_realloc(p, s)                                        \
+	_shm_root.xrealloc(_shm_root.mem_block, (p), (s), _SRC_LOC_, \
+			_SRC_FUNCTION_, _SRC_LINE_, _SRC_MODULE_)
+#define shm_reallocxf(p, s)                                        \
+	_shm_root.xreallocxf(_shm_root.mem_block, (p), (s), _SRC_LOC_, \
+			_SRC_FUNCTION_, _SRC_LINE_, _SRC_MODULE_)
+#define shm_resize(p, s)                                        \
+	_shm_root.xresize(_shm_root.mem_block, (p), (s), _SRC_LOC_, \
+			_SRC_FUNCTION_, _SRC_LINE_, _SRC_MODULE_)
+#define shm_free(p)                                                      \
+	_shm_root.xfree(_shm_root.mem_block, (p), _SRC_LOC_, _SRC_FUNCTION_, \
+			_SRC_LINE_, _SRC_MODULE_)
+#define shm_free_unsafe(p)                                      \
+	_shm_root.xfree_unsafe(_shm_root.mem_block, (p), _SRC_LOC_, \
+			_SRC_FUNCTION_, _SRC_LINE_, _SRC_MODULE_)
 #else
-#	define shm_malloc(s)         _shm_root.xmalloc(_shm_root.mem_block, (s))
-#	define shm_mallocxz(s)       _shm_root.xmallocxz(_shm_root.mem_block, (s))
-#	define shm_malloc_unsafe(s)  _shm_root.xmalloc_unsafe(_shm_root.mem_block, (s))
-#	define shm_realloc(p, s)     _shm_root.xrealloc(_shm_root.mem_block, (p), (s))
-#	define shm_reallocxf(p, s)   _shm_root.xreallocxf(_shm_root.mem_block, (p), (s))
-#	define shm_resize(p, s)      _shm_root.xresize(_shm_root.mem_block, (p), (s))
-#	define shm_free(p)           _shm_root.xfree(_shm_root.mem_block, (p))
-#	define shm_free_unsafe(p)    _shm_root.xfree_unsafe(_shm_root.mem_block, (p))
+#define shm_malloc(s) _shm_root.xmalloc(_shm_root.mem_block, (s))
+#define shm_mallocxz(s) _shm_root.xmallocxz(_shm_root.mem_block, (s))
+#define shm_malloc_unsafe(s) _shm_root.xmalloc_unsafe(_shm_root.mem_block, (s))
+#define shm_realloc(p, s) _shm_root.xrealloc(_shm_root.mem_block, (p), (s))
+#define shm_reallocxf(p, s) _shm_root.xreallocxf(_shm_root.mem_block, (p), (s))
+#define shm_resize(p, s) _shm_root.xresize(_shm_root.mem_block, (p), (s))
+#define shm_free(p) _shm_root.xfree(_shm_root.mem_block, (p))
+#define shm_free_unsafe(p) _shm_root.xfree_unsafe(_shm_root.mem_block, (p))
 #endif
 
-#	define shm_status()    _shm_root.xstatus(_shm_root.mem_block)
-#	define shm_info(mi)    _shm_root.xinfo(_shm_root.mem_block, mi)
-#	define shm_report(mr)  _shm_root.xreport(_shm_root.mem_block, mr)
-#	define shm_available() _shm_root.xavailable(_shm_root.mem_block)
-#	define shm_sums()      _shm_root.xsums(_shm_root.mem_block)
-#	define shm_mod_get_stats(x)     _shm_root.xmodstats(_shm_root.mem_block, x)
-#	define shm_mod_free_stats(x)    _shm_root.xfmodstats(x)
+#define shm_status() _shm_root.xstatus(_shm_root.mem_block)
+#define shm_info(mi) _shm_root.xinfo(_shm_root.mem_block, mi)
+#define shm_report(mr) _shm_root.xreport(_shm_root.mem_block, mr)
+#define shm_available() _shm_root.xavailable(_shm_root.mem_block)
+#define shm_sums() _shm_root.xsums(_shm_root.mem_block)
+#define shm_mod_get_stats(x) _shm_root.xmodstats(_shm_root.mem_block, x)
+#define shm_mod_free_stats(x) _shm_root.xfmodstats(x)
 
-#	define shm_global_lock() _shm_root.xglock(_shm_root.mem_block)
-#	define shm_global_unlock() _shm_root.xgunlock(_shm_root.mem_block)
+#define shm_global_lock() _shm_root.xglock(_shm_root.mem_block)
+#define shm_global_unlock() _shm_root.xgunlock(_shm_root.mem_block)
 
 
-void* shm_core_get_pool(void);
+void *shm_core_get_pool(void);
 int shm_init_api(sr_shm_api_t *ap);
 int shm_init_manager(char *name);
 void shm_destroy_manager(void);
@@ -95,18 +103,25 @@ void shm_print_manager(void);
 int shm_address_in(void *p);
 
 #define shm_available_safe() shm_available()
-#define shm_malloc_on_fork() do{}while(0)
+#define shm_malloc_on_fork() \
+	do {                     \
+	} while(0)
 
 /* generic logging helper for allocation errors in shared memory pool */
 #define SHM_MEM_ERROR LM_ERR("could not allocate shared memory from shm pool\n")
-#define SHM_MEM_CRITICAL LM_CRIT("could not allocate shared memory from shm pool\n")
+#define SHM_MEM_CRITICAL \
+	LM_CRIT("could not allocate shared memory from shm pool\n")
 
 #ifdef __SUNPRO_C
-#define SHM_MEM_ERROR_FMT(...) LM_ERR("could not allocate shared memory from shm pool" __VA_ARGS__)
-#define SHM_MEM_CRITICAL_FMT(...) LM_CRIT("could not allocate shared memory from shm pool" __VA_ARGS__)
+#define SHM_MEM_ERROR_FMT(...) \
+	LM_ERR("could not allocate shared memory from shm pool" __VA_ARGS__)
+#define SHM_MEM_CRITICAL_FMT(...) \
+	LM_CRIT("could not allocate shared memory from shm pool" __VA_ARGS__)
 #else
-#define SHM_MEM_ERROR_FMT(fmt, args...) LM_ERR("could not allocate shared memory from shm pool - " fmt , ## args)
-#define SHM_MEM_CRITICAL_FMT(fmt, args...) LM_CRIT("could not allocate shared memory from shm pool - " fmt , ## args)
+#define SHM_MEM_ERROR_FMT(fmt, args...) \
+	LM_ERR("could not allocate shared memory from shm pool - " fmt, ##args)
+#define SHM_MEM_CRITICAL_FMT(fmt, args...) \
+	LM_CRIT("could not allocate shared memory from shm pool - " fmt, ##args)
 #endif
 
 #endif /* _sr_shm_h_ */
