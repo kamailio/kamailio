39262e50389d41c50d0e6ff2ae2b0a2521f91640
Daniel-Constantin Mierla <miconda@gmail.com>
2023-06-12 15:53:38 +0200

    dialplan: restructured condition on input and output params
    (cherry picked from commit f99b116e876170709ad364ac2b8093969a939d4a)

--- src/modules/dialplan/dialplan.c
+++ src/modules/dialplan/dialplan.c
@@ -881,11 +881,14 @@ static int ki_dp_translate(sip_msg_t* msg, int id, str *input_spv, str *output_s
 		return -1;
 	}
 
-	if (input_spv == NULL && output_spv == NULL) {
+	if(input_spv == NULL || input_spv->len <= 0) {
 		pvs_i = pv_cache_get(&dp_default_param_s);
-		pvs_o = pv_cache_get(&dp_default_param_s);
 	} else {
 		pvs_i = pv_cache_get(input_spv);
+	}
+	if(output_spv == NULL || output_spv->len <= 0) {
+		pvs_o = pv_cache_get(&dp_default_param_s);
+	} else {
 		pvs_o = pv_cache_get(output_spv);
 	}
 
