bc6326ab747c35f689410b1f51aa956141ffe7e5
Victor Seva <linuxmaniac@torreviejawireless.org>
2023-06-19 20:12:17 +0200

    tls: disable tls_rand for openssl >= 3.0
    From https://www.openssl.org/docs/man3.0/man3/RAND_set_rand_method.html
    
    > All of the functions described on this page are deprecated.
    > Applications should instead use RAND_set_DRBG_type(3), EVP_RAND(3) and
    > EVP_RAND(7).

--- src/modules/tls/tls_mod.c
+++ src/modules/tls/tls_mod.c
@@ -510,7 +510,8 @@ static void mod_destroy(void)
 
 int ksr_rand_engine_param(modparam_t type, void *val)
 {
-#if OPENSSL_VERSION_NUMBER >= 0x10100000L
+#if OPENSSL_VERSION_NUMBER >= 0x10100000L \
+		&& OPENSSL_VERSION_NUMBER < 0x030000000L
 	str *reng;
 
 	if(val == NULL) {
@@ -678,7 +679,8 @@ int mod_register(char *path, int *dlflags, void *p1, void *p2)
 
 	register_tls_hooks(&tls_h);
 
-#if OPENSSL_VERSION_NUMBER >= 0x10100000L
+#if OPENSSL_VERSION_NUMBER >= 0x10100000L \
+		&& OPENSSL_VERSION_NUMBER < 0x030000000L
 	LM_DBG("setting cryptorand random engine\n");
 	RAND_set_rand_method(RAND_ksr_cryptorand_method());
 #endif
