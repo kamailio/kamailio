d8a35b3b6c837b36779e232b65fce61c3aa93387
Daniel-Constantin Mierla <miconda@gmail.com>
2023-09-26 20:20:56 +0200

    core: dns cache - warn messages on safety checks for items

--- src/core/dns_cache.c
+++ src/core/dns_cache.c
@@ -1690,6 +1690,9 @@ inline static struct dns_hash_entry *dns_get_related(
 	l = e;
 	LM_DBG("(%p (%.*s, %d), %d, *%p) (%d)\n", e, e->name_len, e->name, e->type,
 			type, *records, cname_chain_len);
+	if(l->prev != NULL || l->next != NULL) {
+		LM_WARN("record not alone: %p - type: %d\n", l, (int)l->type);
+	}
 	clist_init(l, next, prev);
 	if(type == e->type) {
 		ret = e;
@@ -2034,6 +2037,9 @@ inline static struct dns_hash_entry *dns_cache_do_request(str *name, int type)
 	}
 #endif
 end:
+	if(e != NULL && e->prev == NULL && e->next == NULL) {
+		LM_WARN("record not linked: %p - type: %d\n", e, (int)e->type);
+	}
 	return e;
 }
 
