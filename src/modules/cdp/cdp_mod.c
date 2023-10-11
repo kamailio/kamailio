6bd5c508bb536efb516a608ffbcbd11f8e6dba89
Morten Tryfoss <morten@tryfoss.no>
2023-10-11 12:32:09 +0200

    cdp: Disable TLS support for openssl versions older than 1.1.0

--- src/modules/cdp/cdp_mod.c
+++ src/modules/cdp/cdp_mod.c
@@ -239,6 +239,7 @@ static int cdp_init(void)
 		return 1;
 	}
 
+	#if OPENSSL_VERSION_NUMBER >= 0x10100000L
 	if(enable_tls) {
 		init_ssl_methods();
 		method = tls_parse_method(&tls_method);
@@ -247,6 +248,12 @@ static int cdp_init(void)
 			return -1;
 		}
 	}
+	#else
+	if(enable_tls) {
+		LM_ERR("TLS requires openssl 1.1.0 or newer\n");
+		return -1;
+	}
+	#endif
 
 	register_procs(2 + config->workers + 2 * config->peers_cnt);
 	cfg_register_child(2 + config->workers + 2 * config->peers_cnt);
