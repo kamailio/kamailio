34dbf1f6e9f0fa4594358e86d08e43b3fbef6cda
Victor Seva <linuxmaniac@torreviejawireless.org>
2023-11-11 00:29:16 +0100

    db_cassandra: clang-format for coherent indentation and coding style

--- src/modules/db_cassandra/cassandra_constants.h
+++ src/modules/db_cassandra/cassandra_constants.h
@@ -8,17 +8,22 @@
 
 #include "cassandra_types.h"
 
-namespace org { namespace apache { namespace cassandra {
+namespace org {
+namespace apache {
+namespace cassandra {
 
-class cassandraConstants {
- public:
-  cassandraConstants();
+class cassandraConstants
+{
+public:
+	cassandraConstants();
 
-  std::string CASS_VERSION;
+	std::string CASS_VERSION;
 };
 
 extern const cassandraConstants g_cassandra_constants;
 
-}}} // namespace
+} // namespace cassandra
+} // namespace apache
+} // namespace org
 
 #endif
