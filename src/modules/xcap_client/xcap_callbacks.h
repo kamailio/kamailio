42f35537daf954a30308d9310658f9b083daa2b7
Victor Seva <linuxmaniac@torreviejawireless.org>
2023-11-11 00:30:05 +0100

    xcap_client: clang-format for coherent indentation and coding style

--- src/modules/xcap_client/xcap_callbacks.h
+++ src/modules/xcap_client/xcap_callbacks.h
@@ -26,36 +26,37 @@
 
 #include "../../core/str.h"
 
-#define PRES_RULES         1<<1
-#define RESOURCE_LIST      1<<2
-#define RLS_SERVICE        1<<3
-#define PIDF_MANIPULATION  1<<4
-#define XCAP_CAPS          1<<5
-#define USER_PROFILE       1<<6
-#define PRES_CONTENT       1<<7
-#define SEARCH             1<<8
-#define DIRECTORY          1<<9
+#define PRES_RULES 1 << 1
+#define RESOURCE_LIST 1 << 2
+#define RLS_SERVICE 1 << 3
+#define PIDF_MANIPULATION 1 << 4
+#define XCAP_CAPS 1 << 5
+#define USER_PROFILE 1 << 6
+#define PRES_CONTENT 1 << 7
+#define SEARCH 1 << 8
+#define DIRECTORY 1 << 9
 
 /* callback function prototype */
-typedef int (xcap_cb)(int doc_type, str xid, char* doc);
+typedef int(xcap_cb)(int doc_type, str xid, char *doc);
 
 /* register callback function prototype */
 typedef int (*register_xcapcb_t)(int types, xcap_cb f);
 
 
-typedef struct xcap_callback {
-	int types;                   /* types of events that trigger the callback*/
-	xcap_cb* callback;            /* callback function */
-	struct xcap_callback* next;
-}xcap_callback_t;
+typedef struct xcap_callback
+{
+	int types;		   /* types of events that trigger the callback*/
+	xcap_cb *callback; /* callback function */
+	struct xcap_callback *next;
+} xcap_callback_t;
 
 /* destroy registered callback list */
 void destroy_xcapcb_list(void);
 
 /* register a callback for several types of events */
-int register_xcapcb( int types, xcap_cb f);
+int register_xcapcb(int types, xcap_cb f);
 
 /* run all transaction callbacks for a composed type */
-void run_xcap_update_cb(int type, str xid, char* stream);
+void run_xcap_update_cb(int type, str xid, char *stream);
 
 #endif
