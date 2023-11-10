eb906d05f635bdd5a906c94a9566eb5571f59cbe
Victor Seva <linuxmaniac@torreviejawireless.org>
2023-11-11 00:29:34 +0100

    kex: clang-format for coherent indentation and coding style

--- src/modules/kex/km_core.c
+++ src/modules/kex/km_core.c
@@ -35,23 +35,22 @@ int w_setdsturi(struct sip_msg *msg, char *uri, str *s2)
 {
 	str suri;
 
-	if(fixup_get_svalue(msg, (gparam_t*)uri, &suri)!=0) {
+	if(fixup_get_svalue(msg, (gparam_t *)uri, &suri) != 0) {
 		LM_ERR("cannot get the URI parameter\n");
 		return -1;
 	}
 
-	if(set_dst_uri(msg, &suri)!=0)
+	if(set_dst_uri(msg, &suri) != 0)
 		return -1;
 	/* dst_uri changed, so it makes sense to re-use the current uri
 	 * for forking */
 	ruri_mark_new(); /* re-use uri for serial forking */
 	return 1;
-
 }
 
 int w_resetdsturi(struct sip_msg *msg, char *uri, str *s2)
 {
-	if(msg->dst_uri.s!=0)
+	if(msg->dst_uri.s != 0)
 		pkg_free(msg->dst_uri.s);
 	msg->dst_uri.s = 0;
 	msg->dst_uri.len = 0;
@@ -60,47 +59,42 @@ int w_resetdsturi(struct sip_msg *msg, char *uri, str *s2)
 
 int w_isdsturiset(struct sip_msg *msg, char *uri, str *s2)
 {
-	if(msg->dst_uri.s==0 || msg->dst_uri.len<=0)
+	if(msg->dst_uri.s == 0 || msg->dst_uri.len <= 0)
 		return -1;
 	return 1;
 }
 
-int pv_printf_fixup(void** param, int param_no)
+int pv_printf_fixup(void **param, int param_no)
 {
-	pv_spec_t *spec=NULL;
-	pv_elem_t *pvmodel=NULL;
+	pv_spec_t *spec = NULL;
+	pv_elem_t *pvmodel = NULL;
 	str tstr;
 
-	if(param_no==1)
-	{
-		spec = (pv_spec_t*)pkg_malloc(sizeof(pv_spec_t));
-		if(spec==NULL)
-		{
+	if(param_no == 1) {
+		spec = (pv_spec_t *)pkg_malloc(sizeof(pv_spec_t));
+		if(spec == NULL) {
 			PKG_MEM_ERROR;
 			return -1;
 		}
 		memset(spec, 0, sizeof(pv_spec_t));
-		tstr.s = (char*)(*param);
+		tstr.s = (char *)(*param);
 		tstr.len = strlen(tstr.s);
-		if(pv_parse_spec(&tstr, spec)==NULL)
-		{
+		if(pv_parse_spec(&tstr, spec) == NULL) {
 			LM_ERR("unknown script variable in first parameter");
 			pkg_free(spec);
 			return -1;
 		}
-		if(!pv_is_w(spec))
-		{
+		if(!pv_is_w(spec)) {
 			LM_ERR("read-only script variable in first parameter");
 			pkg_free(spec);
 			return -1;
 		}
 		*param = spec;
-	} else if(param_no==2) {
+	} else if(param_no == 2) {
 		pvmodel = 0;
-		tstr.s = (char*)(*param);
+		tstr.s = (char *)(*param);
 		tstr.len = strlen(tstr.s);
-		if(pv_parse_format(&tstr, &pvmodel)<0)
-		{
+		if(pv_parse_format(&tstr, &pvmodel) < 0) {
 			LM_ERR("error in second parameter");
 			return -1;
 		}
@@ -111,23 +105,21 @@ int pv_printf_fixup(void** param, int param_no)
 
 int w_pv_printf(struct sip_msg *msg, char *s1, str *s2)
 {
-	pv_spec_t *spec=NULL;
-	pv_elem_t *model=NULL;
+	pv_spec_t *spec = NULL;
+	pv_elem_t *model = NULL;
 	pv_value_t val;
 
-	spec = (pv_spec_t*)s1;
+	spec = (pv_spec_t *)s1;
 
-	model = (pv_elem_t*)s2;
+	model = (pv_elem_t *)s2;
 
 	memset(&val, 0, sizeof(pv_value_t));
-	if(pv_printf_s(msg, model, &val.rs)!=0)
-	{
+	if(pv_printf_s(msg, model, &val.rs) != 0) {
 		LM_ERR("cannot eval second parameter\n");
 		goto error;
 	}
 	val.flags = PV_VAL_STR;
-	if(spec->setf(msg, &spec->pvp, EQ_T, &val)<0)
-	{
+	if(spec->setf(msg, &spec->pvp, EQ_T, &val) < 0) {
 		LM_ERR("setting PV failed\n");
 		goto error;
 	}
@@ -136,4 +128,3 @@ int w_pv_printf(struct sip_msg *msg, char *s1, str *s2)
 error:
 	return -1;
 }
-
