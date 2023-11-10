4aaed849caa4102e1e6dacaf87cb59e3442ca87b
Victor Seva <linuxmaniac@torreviejawireless.org>
2023-11-11 00:29:15 +0100

    crypto: clang-format for coherent indentation and coding style

--- src/modules/crypto/crypto_aes.h
+++ src/modules/crypto/crypto_aes.h
@@ -34,9 +34,9 @@ int crypto_aes_init(unsigned char *key_data, int key_data_len,
 		unsigned char *salt, unsigned char *custom_iv, EVP_CIPHER_CTX *e_ctx,
 		EVP_CIPHER_CTX *d_ctx);
 
-unsigned char *crypto_aes_encrypt(EVP_CIPHER_CTX *e, unsigned char *plaintext,
-		int *len);
+unsigned char *crypto_aes_encrypt(
+		EVP_CIPHER_CTX *e, unsigned char *plaintext, int *len);
 
-unsigned char *crypto_aes_decrypt(EVP_CIPHER_CTX *e, unsigned char *ciphertext,
-		int *len);
+unsigned char *crypto_aes_decrypt(
+		EVP_CIPHER_CTX *e, unsigned char *ciphertext, int *len);
 #endif
