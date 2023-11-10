61889d2fb6281fdbb11e273f89775de1a54a8c24
Victor Seva <linuxmaniac@torreviejawireless.org>
2023-11-11 00:29:33 +0100

    jsonrpcs: clang-format for coherent indentation and coding style

--- src/modules/jsonrpcs/jsonrpcs_mod.h
+++ src/modules/jsonrpcs/jsonrpcs_mod.h
@@ -29,7 +29,7 @@
 #include "../../core/parser/msg_parser.h"
 #include "../../core/utils/srjson.h"
 
-#define JSONRPC_ID_SIZE	64
+#define JSONRPC_ID_SIZE 64
 
 /** The context of the jsonrpc request being processed.
  *
@@ -39,50 +39,55 @@
  *
  * There is always one context per jsonrpc request.
  */
-typedef struct jsonrpc_ctx {
-	sip_msg_t* msg;        /**< The SIP/HTTP received message. */
+typedef struct jsonrpc_ctx
+{
+	sip_msg_t *msg;			/**< The SIP/HTTP received message. */
 	int msg_shm_block_size; /**< non-zero for delayed reply contexts with
 							  shm cloned msgs */
-	char* method;          /**< Name of the management function to be called */
-	unsigned int flags;    /**< Various flags, such as return value type */
-	srjson_doc_t *jreq;    /**< JSON request document */
-	srjson_t *req_node;    /**< Pointer to crt node in json req parameter list */
-	srjson_doc_t *jrpl;    /**< JSON reply document */
-	srjson_t *rpl_node;    /**< Pointer to crt node in json reply doc */
-	int reply_sent;        /**< Flag set if the json reply was sent */
-	int error_code;        /**< Json error code */
-	str error_text;        /**< Json error text */
-	int http_code;         /**< http reply code */
-	str http_text;         /**< http reply reason text */
-	int transport;         /**< RPC transport */
-	int jsrid_type;        /**< type for Json RPC id value */
+	char *method;			/**< Name of the management function to be called */
+	unsigned int flags;		/**< Various flags, such as return value type */
+	srjson_doc_t *jreq;		/**< JSON request document */
+	srjson_t *req_node; /**< Pointer to crt node in json req parameter list */
+	srjson_doc_t *jrpl; /**< JSON reply document */
+	srjson_t *rpl_node; /**< Pointer to crt node in json reply doc */
+	int reply_sent;		/**< Flag set if the json reply was sent */
+	int error_code;		/**< Json error code */
+	str error_text;		/**< Json error text */
+	int http_code;		/**< http reply code */
+	str http_text;		/**< http reply reason text */
+	int transport;		/**< RPC transport */
+	int jsrid_type;		/**< type for Json RPC id value */
 	char jsrid_val[JSONRPC_ID_SIZE]; /**< value for Json RPC id */
 } jsonrpc_ctx_t;
 
 /* extra rpc_ctx_t flags */
 /* first 8 bits reserved for rpc flags (e.g. RET_ARRAY) */
-#define JSONRPC_DELAYED_CTX_F	256
-#define JSONRPC_DELAYED_REPLY_F	512
+#define JSONRPC_DELAYED_CTX_F 256
+#define JSONRPC_DELAYED_REPLY_F 512
 
-#define JSONRPC_TRANS_NONE	0
-#define JSONRPC_TRANS_HTTP	1
-#define JSONRPC_TRANS_FIFO	2
-#define JSONRPC_TRANS_DGRAM	3
+#define JSONRPC_TRANS_NONE 0
+#define JSONRPC_TRANS_HTTP 1
+#define JSONRPC_TRANS_FIFO 2
+#define JSONRPC_TRANS_DGRAM 3
 
-typedef struct jsonrpc_plain_reply {
-	int rcode;         /**< reply code */
-	str rtext;         /**< reply reason text */
-	str rbody;         /**< reply body */
+typedef struct jsonrpc_plain_reply
+{
+	int rcode; /**< reply code */
+	str rtext; /**< reply reason text */
+	str rbody; /**< reply body */
 } jsonrpc_plain_reply_t;
 
-jsonrpc_plain_reply_t* jsonrpc_plain_reply_get(void);
+jsonrpc_plain_reply_t *jsonrpc_plain_reply_get(void);
 
 int jsonrpc_exec_ex(str *cmd, str *rpath, str *spath);
 char *jsonrpcs_stored_id_get(void);
 
-#define JSONRPC_RESPONSE_STORING_DONE "{\n\"jsonrpc\": \"2.0\",\n\t\"result\": \"Stored\",\n\t\"id\": %s\n}"
-#define JSONRPC_RESPONSE_STORING_FAILED "{\n\"jsonrpc\": \"2.0\",\n\t\"error\": {\n\t\t\"code\": 500,\n\t\t\"message\": \"Storing failed\"\n\t},\n\t\"id\": %s\n}"
-#define JSONRPC_RESPONSE_STORING_BUFSIZE (sizeof(JSONRPC_RESPONSE_STORING_FAILED)+64)
+#define JSONRPC_RESPONSE_STORING_DONE \
+	"{\n\"jsonrpc\": \"2.0\",\n\t\"result\": \"Stored\",\n\t\"id\": %s\n}"
+#define JSONRPC_RESPONSE_STORING_FAILED                        \
+	"{\n\"jsonrpc\": \"2.0\",\n\t\"error\": {\n\t\t\"code\": " \
+	"500,\n\t\t\"message\": \"Storing failed\"\n\t},\n\t\"id\": %s\n}"
+#define JSONRPC_RESPONSE_STORING_BUFSIZE \
+	(sizeof(JSONRPC_RESPONSE_STORING_FAILED) + 64)
 
 #endif
-
