c22c60afa4db0d9e4e853815cbbca95fe13c73ab
Victor Seva <linuxmaniac@torreviejawireless.org>
2023-11-11 12:19:22 +0100

    sqlops: clang-format for coherent indentation and coding style

--- src/modules/sqlops/sql_api.h
+++ src/modules/sqlops/sql_api.h
@@ -35,7 +35,7 @@
 typedef struct _sql_col
 {
 	str name;
-    unsigned int colid;
+	unsigned int colid;
 } sql_col_t;
 
 typedef struct _sql_val
@@ -60,7 +60,7 @@ typedef struct _sql_con
 	str name;
 	unsigned int conid;
 	str db_url;
-	db1_con_t  *dbh;
+	db1_con_t *dbh;
 	db_func_t dbf;
 	struct _sql_con *next;
 } sql_con_t;
@@ -72,16 +72,15 @@ int sql_reconnect(sql_con_t *sc);
 
 int sql_do_query(sql_con_t *con, str *query, sql_result_t *res);
 int sql_do_query_async(sql_con_t *con, str *query);
-int sql_do_xquery(sip_msg_t *msg, sql_con_t *con, pv_elem_t *query,
-		pv_elem_t *res);
-int sql_do_pvquery(sip_msg_t *msg, sql_con_t *con, pv_elem_t *query,
-		pvname_list_t *res);
-int pv_get_sqlrows(sip_msg_t *msg,  pv_param_t *param,
-		pv_value_t *res);
+int sql_do_xquery(
+		sip_msg_t *msg, sql_con_t *con, pv_elem_t *query, pv_elem_t *res);
+int sql_do_pvquery(
+		sip_msg_t *msg, sql_con_t *con, pv_elem_t *query, pvname_list_t *res);
+int pv_get_sqlrows(sip_msg_t *msg, pv_param_t *param, pv_value_t *res);
 int pv_parse_con_name(pv_spec_p sp, str *in);
 
-sql_con_t* sql_get_connection(str *name);
-sql_result_t* sql_get_result(str *name);
+sql_con_t *sql_get_connection(str *name);
+sql_result_t *sql_get_result(str *name);
 
 void sql_reset_result(sql_result_t *res);
 
@@ -106,10 +105,12 @@ int sqlops_num_rows(str *sres);
 typedef void (*sqlops_reset_result_f)(str *sres);
 void sqlops_reset_result(str *sres);
 
-typedef int (*sqlops_do_xquery_f)(sip_msg_t *msg, str *scon, str *squery, str *sxavp);
+typedef int (*sqlops_do_xquery_f)(
+		sip_msg_t *msg, str *scon, str *squery, str *sxavp);
 int sqlops_do_xquery(sip_msg_t *msg, str *scon, str *squery, str *sxavp);
 
-typedef struct sqlops_api {
+typedef struct sqlops_api
+{
 	sqlops_do_query_f query;
 	sqlops_get_value_f value;
 	sqlops_is_null_f is_null;
@@ -120,7 +121,7 @@ typedef struct sqlops_api {
 	sqlops_do_xquery_f xquery;
 } sqlops_api_t;
 
-typedef int (*bind_sqlops_f)(sqlops_api_t* api);
+typedef int (*bind_sqlops_f)(sqlops_api_t *api);
 
 /**
  * @brief Load the SQLOps API
@@ -130,12 +131,11 @@ static inline int sqlops_load_api(sqlops_api_t *sqb)
 	bind_sqlops_f bindsqlops;
 
 	bindsqlops = (bind_sqlops_f)find_export("bind_sqlops", 0, 0);
-	if ( bindsqlops == 0) {
+	if(bindsqlops == 0) {
 		LM_ERR("cannot find bind_sqlops\n");
 		return -1;
 	}
-	if (bindsqlops(sqb)==-1)
-	{
+	if(bindsqlops(sqb) == -1) {
 		LM_ERR("cannot bind sqlops api\n");
 		return -1;
 	}
