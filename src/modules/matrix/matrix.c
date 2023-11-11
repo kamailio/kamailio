bf6c243515f4b2906dfb0472625fdb172d7b759c
Victor Seva <linuxmaniac@torreviejawireless.org>
2023-11-11 12:19:06 +0100

    matrix: clang-format for coherent indentation and coding style

--- src/modules/matrix/matrix.c
+++ src/modules/matrix/matrix.c
@@ -36,33 +36,31 @@
 MODULE_VERSION
 
 
-
-
 #define MAXCOLS 1000
 
 
-
-
 str matrix_db_url = str_init(DEFAULT_RODB_URL);
-
-
 
 
 /**
  * Generic parameter that holds a string, an int or a pseudo-variable
  * @todo replace this with gparam_t
  */
-struct multiparam_t {
-	enum {
+struct multiparam_t
+{
+	enum
+	{
 		MP_INT,
 		MP_STR,
 		MP_AVP,
 		MP_PVE,
 	} type;
-	union {
+	union
+	{
 		int n;
 		str s;
-		struct {
+		struct
+		{
 			unsigned short flags;
 			int_str name;
 		} a;
@@ -71,14 +69,14 @@ struct multiparam_t {
 };
 
 
-
 int matrix_rpc_init(void);
 
 /* ---- fixup functions: */
-static int matrix_fixup(void** param, int param_no);
+static int matrix_fixup(void **param, int param_no);
 
 /* ---- exported commands: */
-static int lookup_matrix(struct sip_msg *msg, struct multiparam_t *_first, struct multiparam_t *_second, struct multiparam_t *_dstavp);
+static int lookup_matrix(struct sip_msg *msg, struct multiparam_t *_first,
+		struct multiparam_t *_second, struct multiparam_t *_dstavp);
 
 /* ---- module init functions: */
 static int mod_init(void);
@@ -86,60 +84,46 @@ static int child_init(int rank);
 static void mod_destroy(void);
 
 
-static cmd_export_t cmds[]={
-	{ "matrix", (cmd_function)lookup_matrix, 3, matrix_fixup, 0, REQUEST_ROUTE | FAILURE_ROUTE },
-	{ 0, 0, 0, 0, 0, 0}
-};
-
-
+static cmd_export_t cmds[] = {
+		{"matrix", (cmd_function)lookup_matrix, 3, matrix_fixup, 0,
+				REQUEST_ROUTE | FAILURE_ROUTE},
+		{0, 0, 0, 0, 0, 0}};
 
 
 static param_export_t params[] = {
-	matrix_DB_URL
-		matrix_DB_TABLE
-		matrix_DB_COLS
-		{ 0, 0, 0}
-};
-
-
+		matrix_DB_URL matrix_DB_TABLE matrix_DB_COLS{0, 0, 0}};
 
 
 struct module_exports exports = {
-	"matrix",        /* module name */
-	DEFAULT_DLFLAGS, /* dlopen flags */
-	cmds,            /* cmd (cfg function) exports */
-	params,          /* param exports */
-	0,               /* RPC method exports */
-	0,               /* pseudo-variables exports */
-	0,               /* response handling function */
-	mod_init,        /* module init function */
-	child_init,      /* per-child init function */
-	mod_destroy      /* module destroy function */
+		"matrix",		 /* module name */
+		DEFAULT_DLFLAGS, /* dlopen flags */
+		cmds,			 /* cmd (cfg function) exports */
+		params,			 /* param exports */
+		0,				 /* RPC method exports */
+		0,				 /* pseudo-variables exports */
+		0,				 /* response handling function */
+		mod_init,		 /* module init function */
+		child_init,		 /* per-child init function */
+		mod_destroy		 /* module destroy function */
 };
 
 
-
-
-struct first_t {
+struct first_t
+{
 	struct first_t *next;
 	int id;
-	short int second_list[MAXCOLS+1];
+	short int second_list[MAXCOLS + 1];
 };
 
 
-
-
-struct matrix_t {
+struct matrix_t
+{
 	struct first_t *head;
 };
 
 
-
-
 static gen_lock_t *lock = NULL;
 static struct matrix_t *matrix = NULL;
-
-
 
 
 /**
@@ -150,13 +134,14 @@ static struct matrix_t *matrix = NULL;
  *
  * @return 0 on success, -1 on failure
  */
-static int mp_fixup(void ** param) {
+static int mp_fixup(void **param)
+{
 	pv_spec_t avp_spec;
 	struct multiparam_t *mp;
 	str s;
 
 	mp = (struct multiparam_t *)pkg_malloc(sizeof(struct multiparam_t));
-	if (mp == NULL) {
+	if(mp == NULL) {
 		LM_ERR("out of pkg memory\n");
 		return -1;
 	}
@@ -165,41 +150,40 @@ static int mp_fixup(void ** param) {
 	s.s = (char *)(*param);
 	s.len = strlen(s.s);
 
-	if (s.s[0]!='$') {
+	if(s.s[0] != '$') {
 		/* This is string */
-		mp->type=MP_STR;
-		mp->u.s=s;
-	}
-	else {
+		mp->type = MP_STR;
+		mp->u.s = s;
+	} else {
 		/* This is a pseudo-variable */
-		if (pv_parse_spec(&s, &avp_spec)==0) {
+		if(pv_parse_spec(&s, &avp_spec) == 0) {
 			LM_ERR("pv_parse_spec failed for '%s'\n", (char *)(*param));
 			pkg_free(mp);
 			return -1;
 		}
-		if (avp_spec.type==PVT_AVP) {
+		if(avp_spec.type == PVT_AVP) {
 			/* This is an AVP - could be an id or name */
-			mp->type=MP_AVP;
-			if(pv_get_avp_name(0, &(avp_spec.pvp), &(mp->u.a.name), &(mp->u.a.flags))!=0) {
+			mp->type = MP_AVP;
+			if(pv_get_avp_name(
+					   0, &(avp_spec.pvp), &(mp->u.a.name), &(mp->u.a.flags))
+					!= 0) {
 				LM_ERR("Invalid AVP definition <%s>\n", (char *)(*param));
 				pkg_free(mp);
 				return -1;
 			}
 		} else {
-			mp->type=MP_PVE;
-			if(pv_parse_format(&s, &(mp->u.p))<0) {
+			mp->type = MP_PVE;
+			if(pv_parse_format(&s, &(mp->u.p)) < 0) {
 				LM_ERR("pv_parse_format failed for '%s'\n", (char *)(*param));
 				pkg_free(mp);
 				return -1;
 			}
 		}
 	}
-	*param = (void*)mp;
+	*param = (void *)mp;
 
 	return 0;
 }
-
-
 
 
 /**
@@ -209,60 +193,59 @@ static int mp_fixup(void ** param) {
  *
  * @return 0 on success, -1 on failure
  */
-static int avp_name_fixup(void ** param) {
+static int avp_name_fixup(void **param)
+{
 	pv_spec_t avp_spec;
 	struct multiparam_t *mp;
 	str s;
 
 	s.s = (char *)(*param);
 	s.len = strlen(s.s);
-	if (s.len <= 0) return -1;
-	if (pv_parse_spec(&s, &avp_spec)==0 || avp_spec.type!=PVT_AVP) {
+	if(s.len <= 0)
+		return -1;
+	if(pv_parse_spec(&s, &avp_spec) == 0 || avp_spec.type != PVT_AVP) {
 		LM_ERR("Malformed or non AVP definition <%s>\n", (char *)(*param));
 		return -1;
 	}
 
 	mp = (struct multiparam_t *)pkg_malloc(sizeof(struct multiparam_t));
-	if (mp == NULL) {
+	if(mp == NULL) {
 		LM_ERR("out of pkg memory\n");
 		return -1;
 	}
 	memset(mp, 0, sizeof(struct multiparam_t));
 
-	mp->type=MP_AVP;
-	if(pv_get_avp_name(0, &(avp_spec.pvp), &(mp->u.a.name), &(mp->u.a.flags))!=0) {
+	mp->type = MP_AVP;
+	if(pv_get_avp_name(0, &(avp_spec.pvp), &(mp->u.a.name), &(mp->u.a.flags))
+			!= 0) {
 		LM_ERR("Invalid AVP definition <%s>\n", (char *)(*param));
 		pkg_free(mp);
 		return -1;
 	}
 
-	*param = (void*)mp;
+	*param = (void *)mp;
 
 	return 0;
 }
 
 
-
-
-static int matrix_fixup(void** param, int param_no)
+static int matrix_fixup(void **param, int param_no)
 {
-	if (param_no == 1) {
+	if(param_no == 1) {
 		/* source id */
-		if (mp_fixup(param) < 0) {
+		if(mp_fixup(param) < 0) {
 			LM_ERR("cannot fixup parameter %d\n", param_no);
 			return -1;
 		}
-	}
-	else if (param_no == 2) {
+	} else if(param_no == 2) {
 		/* destination id */
-		if (mp_fixup(param) < 0) {
+		if(mp_fixup(param) < 0) {
 			LM_ERR("cannot fixup parameter %d\n", param_no);
 			return -1;
 		}
-	}
-	else if (param_no == 3) {
+	} else if(param_no == 3) {
 		/* destination avp name */
-		if (avp_name_fixup(param) < 0) {
+		if(avp_name_fixup(param) < 0) {
 			LM_ERR("cannot fixup parameter %d\n", param_no);
 			return -1;
 		}
@@ -270,15 +253,13 @@ static int matrix_fixup(void** param, int param_no)
 
 	return 0;
 }
-
-
 
 
 static void matrix_clear(void)
 {
 	struct first_t *srcitem;
-	if (matrix) {
-		while (matrix->head) {
+	if(matrix) {
+		while(matrix->head) {
 			srcitem = matrix->head;
 			matrix->head = srcitem->next;
 			shm_free(srcitem);
@@ -287,22 +268,20 @@ static void matrix_clear(void)
 }
 
 
-
-
 static int matrix_insert(int first, short int second, int res)
 {
 	struct first_t *srcitem;
 	int i;
 
-	if ((second<0) || (second>MAXCOLS)) {
+	if((second < 0) || (second > MAXCOLS)) {
 		LM_ERR("invalid second value %d\n", second);
 		return -1;
 	}
 	LM_DBG("searching for %d, %d\n", first, second);
-	if (matrix) {
+	if(matrix) {
 		srcitem = matrix->head;
-		while (srcitem) {
-			if (srcitem->id == first) {
+		while(srcitem) {
+			if(srcitem->id == first) {
 				srcitem->second_list[second] = res;
 				LM_DBG("inserted (%d, %d, %d)\n", first, second, res);
 				return 0;
@@ -311,14 +290,15 @@ static int matrix_insert(int first, short int second, int res)
 		}
 		/* not found */
 		srcitem = shm_malloc(sizeof(struct first_t));
-		if (srcitem == NULL) {
+		if(srcitem == NULL) {
 			LM_ERR("out of shared memory.\n");
 			return -1;
 		}
 		memset(srcitem, 0, sizeof(struct first_t));
 
 		/* Mark all new cells as empty */
-		for (i=0; i<=MAXCOLS; i++) srcitem->second_list[i] = -1;
+		for(i = 0; i <= MAXCOLS; i++)
+			srcitem->second_list[i] = -1;
 
 		srcitem->next = matrix->head;
 		srcitem->id = first;
@@ -331,23 +311,21 @@ static int matrix_insert(int first, short int second, int res)
 }
 
 
-
-
 /* Returns the res id if the matrix contains an entry for the given indices, -1 otherwise.
 */
 static int internal_lookup(int first, short int second)
 {
 	struct first_t *item;
 
-	if ((second<0) || (second>MAXCOLS)) {
+	if((second < 0) || (second > MAXCOLS)) {
 		LM_ERR("invalid second value %d\n", second);
 		return -1;
 	}
 
-	if (matrix) {
+	if(matrix) {
 		item = matrix->head;
-		while (item) {
-			if (item->id == first) {
+		while(item) {
+			if(item->id == first) {
 				return item->second_list[second];
 			}
 			item = item->next;
@@ -358,51 +336,52 @@ static int internal_lookup(int first, short int second)
 }
 
 
-
-
-static int lookup_matrix(struct sip_msg *msg, struct multiparam_t *_srctree, struct multiparam_t *_second, struct multiparam_t *_dstavp)
+static int lookup_matrix(struct sip_msg *msg, struct multiparam_t *_srctree,
+		struct multiparam_t *_second, struct multiparam_t *_dstavp)
 {
 	int first;
 	int second;
 	struct usr_avp *avp;
 	int_str avp_val;
 
-	switch (_srctree->type) {
+	switch(_srctree->type) {
 		case MP_INT:
 			first = _srctree->u.n;
 			break;
 		case MP_AVP:
-			avp = search_first_avp(_srctree->u.a.flags, _srctree->u.a.name, &avp_val, 0);
-			if (!avp) {
+			avp = search_first_avp(
+					_srctree->u.a.flags, _srctree->u.a.name, &avp_val, 0);
+			if(!avp) {
 				LM_ERR("cannot find srctree AVP\n");
 				return -1;
 			}
-			if ((avp->flags&AVP_VAL_STR)) {
+			if((avp->flags & AVP_VAL_STR)) {
 				LM_ERR("cannot process string value in srctree AVP\n");
 				return -1;
-			}
-			else first = avp_val.n;
+			} else
+				first = avp_val.n;
 			break;
 		default:
 			LM_ERR("invalid srctree type\n");
 			return -1;
 	}
 
-	switch (_second->type) {
+	switch(_second->type) {
 		case MP_INT:
 			second = _second->u.n;
 			break;
 		case MP_AVP:
-			avp = search_first_avp(_second->u.a.flags, _second->u.a.name, &avp_val, 0);
-			if (!avp) {
+			avp = search_first_avp(
+					_second->u.a.flags, _second->u.a.name, &avp_val, 0);
+			if(!avp) {
 				LM_ERR("cannot find second_value AVP\n");
 				return -1;
 			}
-			if ((avp->flags&AVP_VAL_STR)) {
+			if((avp->flags & AVP_VAL_STR)) {
 				LM_ERR("cannot process string value in second_value AVP\n");
 				return -1;
-			}
-			else second = avp_val.n;
+			} else
+				second = avp_val.n;
 			break;
 		default:
 			LM_ERR("invalid second_value type\n");
@@ -413,18 +392,18 @@ static int lookup_matrix(struct sip_msg *msg, struct multiparam_t *_srctree, str
 	/* critical section start: avoids dirty reads when updating d-tree */
 	lock_get(lock);
 
-	avp_val.n=internal_lookup(first, second);
+	avp_val.n = internal_lookup(first, second);
 
 	/* critical section end */
 	lock_release(lock);
 
-	if (avp_val.n<0) {
+	if(avp_val.n < 0) {
 		LM_INFO("lookup failed\n");
 		return -1;
 	}
 
 	/* set avp ! */
-	if (add_avp(_dstavp->u.a.flags, _dstavp->u.a.name, avp_val)<0) {
+	if(add_avp(_dstavp->u.a.flags, _dstavp->u.a.name, avp_val) < 0) {
 		LM_ERR("add AVP failed\n");
 		return -1;
 	}
@@ -433,24 +412,24 @@ static int lookup_matrix(struct sip_msg *msg, struct multiparam_t *_srctree, str
 }
 
 
-
-
 /**
  * Rebuild matrix using database entries
  * \return negative on failure, positive on success, indicating the number of matrix entries
  */
 static int db_reload_matrix(void)
 {
-	db_key_t columns[3] = { &matrix_first_col, &matrix_second_col, &matrix_res_col };
+	db_key_t columns[3] = {
+			&matrix_first_col, &matrix_second_col, &matrix_res_col};
 	db1_res_t *res;
 	int i;
 	int n = 0;
 
-	if (matrix_dbf.use_table(matrix_dbh, &matrix_table) < 0) {
+	if(matrix_dbf.use_table(matrix_dbh, &matrix_table) < 0) {
 		LM_ERR("cannot use table '%.*s'.\n", matrix_table.len, matrix_table.s);
 		return -1;
 	}
-	if (matrix_dbf.query(matrix_dbh, NULL, NULL, NULL, columns, 0, 3, NULL, &res) < 0) {
+	if(matrix_dbf.query(matrix_dbh, NULL, NULL, NULL, columns, 0, 3, NULL, &res)
+			< 0) {
 		LM_ERR("error while executing query.\n");
 		return -1;
 	}
@@ -460,16 +439,18 @@ static int db_reload_matrix(void)
 
 	matrix_clear();
 
-	if (RES_COL_N(res) > 2) {
+	if(RES_COL_N(res) > 2) {
 		for(i = 0; i < RES_ROW_N(res); i++) {
-			if ((!RES_ROWS(res)[i].values[0].nul) && (!RES_ROWS(res)[i].values[1].nul)) {
-				if ((RES_ROWS(res)[i].values[0].type == DB1_INT) &&
-						(RES_ROWS(res)[i].values[1].type == DB1_INT) &&
-						(RES_ROWS(res)[i].values[2].type == DB1_INT)) {
-					matrix_insert(RES_ROWS(res)[i].values[0].val.int_val, RES_ROWS(res)[i].values[1].val.int_val, RES_ROWS(res)[i].values[2].val.int_val);
+			if((!RES_ROWS(res)[i].values[0].nul)
+					&& (!RES_ROWS(res)[i].values[1].nul)) {
+				if((RES_ROWS(res)[i].values[0].type == DB1_INT)
+						&& (RES_ROWS(res)[i].values[1].type == DB1_INT)
+						&& (RES_ROWS(res)[i].values[2].type == DB1_INT)) {
+					matrix_insert(RES_ROWS(res)[i].values[0].val.int_val,
+							RES_ROWS(res)[i].values[1].val.int_val,
+							RES_ROWS(res)[i].values[2].val.int_val);
 					n++;
-				}
-				else {
+				} else {
 					LM_ERR("got invalid result type from query.\n");
 				}
 			}
@@ -486,16 +467,14 @@ static int db_reload_matrix(void)
 }
 
 
-
-
 static int init_shmlock(void)
 {
 	lock = lock_alloc();
-	if (!lock) {
+	if(!lock) {
 		LM_CRIT("cannot allocate memory for lock.\n");
 		return -1;
 	}
-	if (lock_init(lock) == 0) {
+	if(lock_init(lock) == 0) {
 		LM_CRIT("cannot initialize lock.\n");
 		return -1;
 	}
@@ -504,11 +483,9 @@ static int init_shmlock(void)
 }
 
 
-
-
 static void destroy_shmlock(void)
 {
-	if (lock) {
+	if(lock) {
 		lock_destroy(lock);
 		lock_dealloc((void *)lock);
 		lock = NULL;
@@ -516,10 +493,9 @@ static void destroy_shmlock(void)
 }
 
 
-
-static void matrix_rpc_reload(rpc_t* rpc, void* c)
+static void matrix_rpc_reload(rpc_t *rpc, void *c)
 {
-	if (matrix_db_open() != 0) {
+	if(matrix_db_open() != 0) {
 		rpc->fault(c, 500, "Failed to connect to db");
 		return;
 	}
@@ -531,36 +507,31 @@ static void matrix_rpc_reload(rpc_t* rpc, void* c)
 }
 
 static const char *matrix_rpc_reload_doc[2] = {
-	"Reload matrix records from database",
-	0
-};
+		"Reload matrix records from database", 0};
 
 
 rpc_export_t matrix_rpc_cmds[] = {
-	{"matrix.reload", matrix_rpc_reload, matrix_rpc_reload_doc, 0},
-	{0, 0, 0, 0}
-};
+		{"matrix.reload", matrix_rpc_reload, matrix_rpc_reload_doc, 0},
+		{0, 0, 0, 0}};
 
 int matrix_rpc_init(void)
 {
-	if (rpc_register_array(matrix_rpc_cmds)!=0)
-	{
+	if(rpc_register_array(matrix_rpc_cmds) != 0) {
 		LM_ERR("failed to register RPC commands\n");
 		return -1;
 	}
 	return 0;
-
 }
 
 static int init_matrix(void)
 {
 	matrix = shm_malloc(sizeof(struct matrix_t));
-	if (!matrix) {
+	if(!matrix) {
 		LM_ERR("out of shared memory\n");
 		return -1;
 	}
 	memset(matrix, 0, sizeof(struct matrix_t));
-	if (db_reload_matrix() < 0) {
+	if(db_reload_matrix() < 0) {
 		LM_ERR("cannot populate matrix\n");
 		return -1;
 	}
@@ -569,44 +540,43 @@ static int init_matrix(void)
 }
 
 
-
-
 static void destroy_matrix(void)
 {
-	if (matrix) {
+	if(matrix) {
 		matrix_clear();
 		shm_free(matrix);
 	}
 }
 
 
-
-
 static int mod_init(void)
 {
-	if(matrix_rpc_init()<0) {
+	if(matrix_rpc_init() < 0) {
 		LM_ERR("failed to init RPC commands");
 		return -1;
 	}
 
-	if (init_shmlock() != 0) return -1;
-	if (matrix_db_init() != 0) return -1;
-	if (matrix_db_open() != 0) return -1;
-	if (init_matrix() != 0) return -1;
+	if(init_shmlock() != 0)
+		return -1;
+	if(matrix_db_init() != 0)
+		return -1;
+	if(matrix_db_open() != 0)
+		return -1;
+	if(init_matrix() != 0)
+		return -1;
 	matrix_db_close();
 	return 0;
 }
 
 
-
 static int child_init(int rank)
 {
-	if(rank==PROC_INIT || rank==PROC_TCP_MAIN)
+	if(rank == PROC_INIT || rank == PROC_TCP_MAIN)
 		return 0;
-	if (matrix_db_open() != 0) return -1;
+	if(matrix_db_open() != 0)
+		return -1;
 	return 0;
 }
-
 
 
 static void mod_destroy(void)
