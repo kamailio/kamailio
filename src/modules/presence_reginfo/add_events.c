655be0a86cdc7eaeb24248fd87894d1865e44f0a
Victor Seva <linuxmaniac@torreviejawireless.org>
2023-11-11 12:19:15 +0100

    presence_reginfo: clang-format for coherent indentation and coding style

--- src/modules/presence_reginfo/add_events.c
+++ src/modules/presence_reginfo/add_events.c
@@ -39,12 +39,12 @@ int reginfo_add_events(void)
 
 	event.content_type.s = "application/reginfo+xml";
 	event.content_type.len = 23;
-	event.default_expires= pres_reginfo_default_expires;
+	event.default_expires = pres_reginfo_default_expires;
 	event.type = PUBL_TYPE;
 	event.req_auth = 0;
 	event.evs_publ_handl = 0;
 
-	if (pres_add_event(&event) < 0) {
+	if(pres_add_event(&event) < 0) {
 		LM_ERR("failed to add event \"reginfo\"\n");
 		return -1;
 	}
--- src/modules/presence_reginfo/presence_reginfo.c
+++ src/modules/presence_reginfo/presence_reginfo.c
@@ -51,9 +51,8 @@ unsigned int pres_reginfo_default_expires = 3600;
 
 /* module exported paramaters */
 static param_export_t params[] = {
-	{ "default_expires", INT_PARAM, &pres_reginfo_default_expires },
-	{0, 0, 0}
-};
+		{"default_expires", INT_PARAM, &pres_reginfo_default_expires},
+		{0, 0, 0}};
 
 /* module exports */
 /* clang-format off */
@@ -79,18 +78,18 @@ static int mod_init(void)
 	presence_api_t pres;
 	bind_presence_t bind_presence;
 
-	bind_presence= (bind_presence_t)find_export("bind_presence", 1,0);
-	if (!bind_presence) {
+	bind_presence = (bind_presence_t)find_export("bind_presence", 1, 0);
+	if(!bind_presence) {
 		LM_ERR("can't bind presence\n");
 		return -1;
 	}
-	if (bind_presence(&pres) < 0) {
+	if(bind_presence(&pres) < 0) {
 		LM_ERR("can't bind presence\n");
 		return -1;
 	}
 
 	pres_add_event = pres.add_event;
-	if (pres_add_event == NULL) {
+	if(pres_add_event == NULL) {
 		LM_ERR("could not import add_event\n");
 		return -1;
 	}
