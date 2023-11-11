ce40a3d2e0c1ea024931fd4c0022d3be8e1879cd
Victor Seva <linuxmaniac@torreviejawireless.org>
2023-11-11 12:18:58 +0100

    geoip: clang-format for coherent indentation and coding style

--- src/modules/geoip/geoip_mod.c
+++ src/modules/geoip/geoip_mod.c
@@ -39,41 +39,36 @@ MODULE_VERSION
 
 static char *geoip_path = NULL;
 
-static int  mod_init(void);
+static int mod_init(void);
 static void mod_destroy(void);
 
-static int w_geoip_match(struct sip_msg* msg, char* str1, char* str2);
+static int w_geoip_match(struct sip_msg *msg, char *str1, char *str2);
 static int geoip_match(sip_msg_t *msg, str *tomatch, str *pvclass);
 
 static pv_export_t mod_pvs[] = {
-	{ {"gip", sizeof("gip")-1}, PVT_OTHER, pv_get_geoip, 0,
-		pv_parse_geoip_name, 0, 0, 0 },
-	{ {0, 0}, 0, 0, 0, 0, 0, 0, 0 }
-};
+		{{"gip", sizeof("gip") - 1}, PVT_OTHER, pv_get_geoip, 0,
+				pv_parse_geoip_name, 0, 0, 0},
+		{{0, 0}, 0, 0, 0, 0, 0, 0, 0}};
 
 
-static cmd_export_t cmds[]={
-	{"geoip_match", (cmd_function)w_geoip_match, 2, fixup_spve_spve,
-		0, ANY_ROUTE},
-	{0, 0, 0, 0, 0, 0}
-};
+static cmd_export_t cmds[] = {{"geoip_match", (cmd_function)w_geoip_match, 2,
+									  fixup_spve_spve, 0, ANY_ROUTE},
+		{0, 0, 0, 0, 0, 0}};
 
-static param_export_t params[]={
-	{"path",     PARAM_STRING, &geoip_path},
-	{0, 0, 0}
-};
+static param_export_t params[] = {
+		{"path", PARAM_STRING, &geoip_path}, {0, 0, 0}};
 
 struct module_exports exports = {
-	"geoip",			/* module name */
-	DEFAULT_DLFLAGS,	/* dlopen flags */
-	cmds,				/* exported functions */
-	params,				/* exported parameters */
-	0,					/* RPC method exports */
-	mod_pvs,			/* exported pseudo-variables */
-	0,					/* response handling function */
-	mod_init,			/* module initialization function */
-	0,					/* per-child init function */
-	mod_destroy			/* module destroy function */
+		"geoip",		 /* module name */
+		DEFAULT_DLFLAGS, /* dlopen flags */
+		cmds,			 /* exported functions */
+		params,			 /* exported parameters */
+		0,				 /* RPC method exports */
+		mod_pvs,		 /* exported pseudo-variables */
+		0,				 /* response handling function */
+		mod_init,		 /* module initialization function */
+		0,				 /* per-child init function */
+		mod_destroy		 /* module destroy function */
 };
 
 
@@ -83,14 +78,12 @@ struct module_exports exports = {
 static int mod_init(void)
 {
 
-	if(geoip_path==NULL || strlen(geoip_path)==0)
-	{
+	if(geoip_path == NULL || strlen(geoip_path) == 0) {
 		LM_ERR("path to GeoIP database file not set\n");
 		return -1;
 	}
 
-	if(geoip_init_pv(geoip_path)!=0)
-	{
+	if(geoip_init_pv(geoip_path) != 0) {
 		LM_ERR("cannot init for database file at: %s\n", geoip_path);
 		return -1;
 	}
@@ -113,21 +106,21 @@ static int geoip_match(sip_msg_t *msg, str *tomatch, str *pvclass)
 	return geoip_update_pv(tomatch, pvclass);
 }
 
-static int w_geoip_match(sip_msg_t* msg, char* target, char* pvname)
+static int w_geoip_match(sip_msg_t *msg, char *target, char *pvname)
 {
 	str tomatch = STR_NULL;
 	str pvclass = STR_NULL;
 
-	if(msg==NULL) {
+	if(msg == NULL) {
 		LM_ERR("received null msg\n");
 		return -1;
 	}
 
-	if(fixup_get_svalue(msg, (gparam_t*)target, &tomatch)<0) {
+	if(fixup_get_svalue(msg, (gparam_t *)target, &tomatch) < 0) {
 		LM_ERR("cannot get the address\n");
 		return -1;
 	}
-	if(fixup_get_svalue(msg, (gparam_t*)pvname, &pvclass)<0) {
+	if(fixup_get_svalue(msg, (gparam_t *)pvname, &pvclass) < 0) {
 		LM_ERR("cannot get the pv class\n");
 		return -1;
 	}
@@ -150,7 +143,8 @@ static sr_kemi_t sr_kemi_geoip_exports[] = {
 };
 /* clang-format on */
 
-int mod_register(char *path, int *dlflags, void *p1, void *p2) {
-    sr_kemi_modules_add(sr_kemi_geoip_exports);
-    return 0;
+int mod_register(char *path, int *dlflags, void *p1, void *p2)
+{
+	sr_kemi_modules_add(sr_kemi_geoip_exports);
+	return 0;
 }
