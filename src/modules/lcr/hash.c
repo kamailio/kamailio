136e9714c66e7c2f9824b768f05f6afeeba9cd9c
Daniel-Constantin Mierla <miconda@gmail.com>
2023-11-13 16:22:22 +0100

    lcr:  removed trailing spaces

--- src/modules/lcr/hash.c
+++ src/modules/lcr/hash.c
@@ -15,8 +15,8 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
@@ -180,7 +180,7 @@ int rule_hash_table_insert_target(struct rule_info **hash_table,
 }
 
 
-/* 
+/*
  * Return pointer to lcr hash table entry to which given prefix hashes to.
  */
 struct rule_info *rule_hash_table_lookup(
