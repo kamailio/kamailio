865ed137878923133137e78bd1ee9b6f21a996ca
Daniel-Constantin Mierla <miconda@gmail.com>
2023-11-13 15:23:55 +0100

    sctp: removed trailing spaces

--- src/modules/sctp/sctp_options.h
+++ src/modules/sctp/sctp_options.h
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright (C) 2008 iptelorg GmbH
  *
  * Permission to use, copy, modify, and distribute this software for any
@@ -13,7 +13,7 @@
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
-/* 
+/*
  * sctp options
  */
 
