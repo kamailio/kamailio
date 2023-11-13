f9c78be0e014ae5c5416c06883f4d33cabee2d4f
Daniel-Constantin Mierla <miconda@gmail.com>
2023-11-13 16:30:31 +0100

    db_berkeley: removed trailing spaces

--- utils/db_berkeley/kambdb_recover.h
+++ utils/db_berkeley/kambdb_recover.h
@@ -2,7 +2,7 @@
  * $Id$
  *
  * recovery for berkeley_db module
- * 
+ *
  * Copyright (C) 2007 Cisco Systems
  *
  * This file is part of Kamailio, a free SIP server.
@@ -17,10 +17,10 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- * 
+ *
  * History:
  * --------
  * 2007-09-19  genesis (wiquan)
