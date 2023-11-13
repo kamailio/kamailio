52fb5f703aab3abace5f31be85d8a92733a0648b
Daniel-Constantin Mierla <miconda@gmail.com>
2023-11-13 16:44:23 +0100

    xlog: removed trailing spaces

--- src/modules/xlog/xl_lib.c
+++ src/modules/xlog/xl_lib.c
@@ -2,7 +2,7 @@
  * XLOG module
  *
  * Copyright (C) 2001-2003 FhG Fokus
- * 
+ *
  * This file is part of Kamailio, a free SIP server.
  *
  * Kamailio is free software; you can redistribute it and/or modify
@@ -15,12 +15,12 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
  * History:
- * 
+ *
  */
 
 /*!
