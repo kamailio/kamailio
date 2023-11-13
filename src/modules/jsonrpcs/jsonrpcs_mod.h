b462e4efb5e200ad898a944ba47b3bc5acb4c5b7
Daniel-Constantin Mierla <miconda@gmail.com>
2023-11-13 16:44:21 +0100

    jsonrpcs: removed trailing spaces

--- src/modules/jsonrpcs/jsonrpcs_mod.h
+++ src/modules/jsonrpcs/jsonrpcs_mod.h
@@ -1,6 +1,6 @@
 /**
  *
- * Copyright (C) 2014 Daniel-Constantin Mierla (asipto.com) 
+ * Copyright (C) 2014 Daniel-Constantin Mierla (asipto.com)
  *
  * This file is part of Kamailio, a free SIP server.
  *
