37e43241d23bea9e158e9d71e79350fbaf911343
Daniel-Constantin Mierla <miconda@gmail.com>
2023-11-13 15:23:53 +0100

    ims_isc: removed trailing spaces

--- src/modules/ims_isc/mark.c
+++ src/modules/ims_isc/mark.c
@@ -3,23 +3,23 @@
  *
  * Copyright (C) 2012 Smile Communications, jason.penton@smilecoms.com
  * Copyright (C) 2012 Smile Communications, richard.good@smilecoms.com
- * 
+ *
  * The initial version of this code was written by Dragos Vingarzan
  * (dragos(dot)vingarzan(at)fokus(dot)fraunhofer(dot)de and the
  * Fruanhofer Institute. It was and still is maintained in a separate
  * branch of the original SER. We are therefore migrating it to
  * Kamailio/SR and look forward to maintaining it from here on out.
  * 2011/2012 Smile Communications, Pty. Ltd.
- * ported/maintained/improved by 
+ * ported/maintained/improved by
  * Jason Penton (jason(dot)penton(at)smilecoms.com and
- * Richard Good (richard(dot)good(at)smilecoms.com) as part of an 
+ * Richard Good (richard(dot)good(at)smilecoms.com) as part of an
  * effort to add full IMS support to Kamailio/SR using a new and
  * improved architecture
- * 
+ *
  * NB: Alot of this code was originally part of OpenIMSCore,
- * FhG Fokus. 
+ * FhG Fokus.
  * Copyright (C) 2004-2006 FhG Fokus
- * Thanks for great work! This is an effort to 
+ * Thanks for great work! This is an effort to
  * break apart the various CSCF functions into logically separate
  * components. We hope this will drive wider use. We also feel
  * that in this way the architecture is more complete and thereby easier
@@ -37,10 +37,10 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- * 
+ *
  */
 
 #include "mark.h"
@@ -62,8 +62,8 @@ char *hexchars = "0123456789abcdef";
  * Converts a binary encoded value to its base16 representation.
  * @param from - buffer containing the input data
  * @param len - the size of from
- * @param to - the output buffer  !!! must have at least len*2 allocated memory 
- * @returns the written length 
+ * @param to - the output buffer  !!! must have at least len*2 allocated memory
+ * @returns the written length
  */
 int bin_to_base16(char *from, int len, char *to)
 {
@@ -195,8 +195,8 @@ void isc_mark_get(str x, isc_mark *mark)
  * Converts a hex encoded value to its binary value
  * @param from - buffer containing the input data
  * @param len - the size of from
- * @param to - the output buffer  !!! must have at least len/2 allocated memory 
- * @returns the written length 
+ * @param to - the output buffer  !!! must have at least len/2 allocated memory
+ * @returns the written length
  */
 int base16_to_bin(char *from, int len, char *to)
 {
@@ -286,7 +286,7 @@ int isc_mark_set(struct sip_msg *msg, isc_match *match, isc_mark *mark)
  * - the marking will be in a header like below
  * - if the "as" parameter is empty: \code Route: <[iscmark]> \endcode
  * - else: \code Route: <sip:as@asdomain.net;lr>, <[iscmark]> \endcode
- * 			 
+ *
  *
  *	@param msg - SIP message to mark
  *	@param as - SIP address of the application server to forward to
