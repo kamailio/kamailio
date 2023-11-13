097d49f47130481d415147c8d000cc7bfaf916bd
Daniel-Constantin Mierla <miconda@gmail.com>
2023-11-13 15:23:53 +0100

    dispatcher: removed trailing spaces

--- src/modules/dispatcher/api.h
+++ src/modules/dispatcher/api.h
@@ -1,6 +1,6 @@
 /**
  * dispatcher module
- * 
+ *
  * Copyright (C) 2004-2006 FhG Fokus
  *
  * This file is part of Kamailio, a free SIP server.
@@ -15,8 +15,8 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
  */
