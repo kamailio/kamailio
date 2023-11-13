5ff9fa2612eb4725f5d0228d13e70562f1a88f9d
Daniel-Constantin Mierla <miconda@gmail.com>
2023-11-13 16:44:21 +0100

    ims_registrar_pcscf: removed trailing spaces

--- src/modules/ims_registrar_pcscf/subscribe.h
+++ src/modules/ims_registrar_pcscf/subscribe.h
@@ -16,15 +16,15 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- * 
+ *
  * * History:
  * ========
- * 
+ *
  * Nov 2013 Richard Good migrated pua_reginfo funtionality to ims_registrar_pcscf
- * 
+ *
  */
 
 #ifndef SUBSCRIBE_H
