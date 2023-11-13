d93e07532ddb2e4bd47ea60f529374071fd977e4
Daniel-Constantin Mierla <miconda@gmail.com>
2023-11-13 16:44:22 +0100

    sms: removed trailing spaces

--- src/modules/sms/libsms_charset.c
+++ src/modules/sms/libsms_charset.c
@@ -3,7 +3,7 @@ SMS Server Tools
 Copyright (C) 2000 Stefan Frings
 
 This program is free software unless you got it under another license directly
-from the author. You can redistribute it and/or modify it under the terms of 
+from the author. You can redistribute it and/or modify it under the terms of
 the GNU General Public License as published by the Free Software Foundation.
 Either version 2 of the License, or (at your option) any later version.
 
