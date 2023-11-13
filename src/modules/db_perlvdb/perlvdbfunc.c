816d8264219ac2a2cb1fe4248cf7e8f5f6ff9986
Daniel-Constantin Mierla <miconda@gmail.com>
2023-11-13 15:23:52 +0100

    db_perlvdb: removed trailing spaces

--- src/modules/db_perlvdb/perlvdbfunc.c
+++ src/modules/db_perlvdb/perlvdbfunc.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * $Id: perlvdbfunc.c 816 2007-02-13 18:33:22Z bastian $
  *
  * Perl virtual database module interface
@@ -18,8 +18,8 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
  */
--- src/modules/db_perlvdb/perlvdbfunc.h
+++ src/modules/db_perlvdb/perlvdbfunc.h
@@ -1,4 +1,4 @@
-/* 
+/*
  * $Id: perlvdbfunc.h 770 2007-01-22 10:16:34Z bastian $
  *
  * Perl virtual database module interface
@@ -18,8 +18,8 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
