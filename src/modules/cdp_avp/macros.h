a22d2aa019f1ecd13c902106b3fed7288dc4aa4b
Daniel-Constantin Mierla <miconda@gmail.com>
2023-11-13 16:44:19 +0100

    cdp_avp: removed trailing spaces

--- src/modules/cdp_avp/macros.h
+++ src/modules/cdp_avp/macros.h
@@ -7,14 +7,14 @@
  * branch of the original SER. We are therefore migrating it to
  * Kamailio/SR and look forward to maintaining it from here on out.
  * 2011/2012 Smile Communications, Pty. Ltd.
- * ported/maintained/improved by 
+ * ported/maintained/improved by
  * Jason Penton (jason(dot)penton(at)smilecoms.com and
- * Richard Good (richard(dot)good(at)smilecoms.com) as part of an 
+ * Richard Good (richard(dot)good(at)smilecoms.com) as part of an
  * effort to add full IMS support to Kamailio/SR using a new and
  * improved architecture
- * 
+ *
  * NB: Alot of this code was originally part of OpenIMSCore,
- * FhG Focus. Thanks for great work! This is an effort to 
+ * FhG Focus. Thanks for great work! This is an effort to
  * break apart the various CSCF functions into logically separate
  * components. We hope this will drive wider use. We also feel
  * that in this way the architecture is more complete and thereby easier
@@ -32,10 +32,10 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- * 
+ *
  */
 
 #ifndef CDP_AVP_MODULE
@@ -392,7 +392,7 @@
 #elif defined(CDP_AVP_EMPTY_MACROS)
 
 /*
-	 * This is for ensuring that the templates won't actually output anything 
+	 * This is for ensuring that the templates won't actually output anything
 	 */
 
 #define cdp_avp_add(avp_name, vendor_id, flags, avp_type, data_type)
@@ -419,7 +419,7 @@
 #else
 
 /*
-	 * This is the declaration of the functions, so this needs to generate the normal .h header file 
+	 * This is the declaration of the functions, so this needs to generate the normal .h header file
 	 */
 
 #define cdp_avp_add(avp_name, vendor_id, flags, avp_type, data_type) \
