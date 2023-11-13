161e96ab4b17989b0f5dfabe98297d52117cb1f9
Daniel-Constantin Mierla <miconda@gmail.com>
2023-11-13 16:44:19 +0100

    auth_ephemeral: removed trailing spaces

--- src/modules/auth_ephemeral/checks.h
+++ src/modules/auth_ephemeral/checks.h
@@ -15,8 +15,8 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
  * Exception: permission to copy, modify, propagate, and distribute a work
