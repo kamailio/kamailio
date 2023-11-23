d4c0e1bbcb0e423a545650aad4fbb4b2da8bb488
Daniel-Constantin Mierla <miconda@gmail.com>
2023-11-23 12:35:47 +0100

    tmx: use literal module name for stats group
    - prevent conflicts with global exports

--- src/modules/tmx/tmx_mod.c
+++ src/modules/tmx/tmx_mod.c
@@ -226,7 +226,7 @@ static int mod_init(void)
 
 #ifdef STATISTICS
 	/* register statistics */
-	if(register_module_stats(exports.name, mod_stats) != 0) {
+	if(register_module_stats("tmx", mod_stats) != 0) {
 		LM_ERR("failed to register statistics\n");
 		return -1;
 	}
