59c32aef1e40edcfb081a18728b4e6029d2d2be8
Daniel-Constantin Mierla <miconda@gmail.com>
2023-11-23 12:35:47 +0100

    p_usrloc: use literal module name for stats group
    - prevent conflicts with global exports
    
    (cherry picked from commit a951cb44ecbcd0f9cba937176ea5117c6a1d15b6)

--- src/modules/p_usrloc/p_usrloc_mod.c
+++ src/modules/p_usrloc/p_usrloc_mod.c
@@ -311,7 +311,7 @@ static int mod_init(void)
 
 #ifdef STATISTICS
 	/* register statistics */
-	if(register_module_stats(exports.name, mod_stats) != 0) {
+	if(register_module_stats("p_usrloc", mod_stats) != 0) {
 		LM_ERR("failed to register core statistics\n");
 		return -1;
 	}
