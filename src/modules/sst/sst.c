21abe7ab16d450f1b085a97a4c0f416d45ac2daf
Daniel-Constantin Mierla <miconda@gmail.com>
2023-11-23 12:35:47 +0100

    sst: use literal module name for stats group
    - prevent conflicts with global exports
    
    (cherry picked from commit 96bdd69945af9f08e6c89fb725248b8b268ee71a)

--- src/modules/sst/sst.c
+++ src/modules/sst/sst.c
@@ -152,7 +152,7 @@ static int mod_init(void)
 #ifdef STATISTICS
 	/* register statistics */
 	if(sst_enable_stats != 0) {
-		if(register_module_stats(exports.name, mod_stats) != 0) {
+		if(register_module_stats("sst", mod_stats) != 0) {
 			LM_ERR("failed to register core statistics\n");
 			return -1;
 		}
