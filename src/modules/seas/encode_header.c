68940cc09c999621b1f9a765e190b3207728106e
Daniel-Constantin Mierla <miconda@gmail.com>
2023-12-15 12:19:25 +0100

    seas: use same type iterator to avoid risk of overflow

--- src/modules/seas/encode_header.c
+++ src/modules/seas/encode_header.c
@@ -366,6 +366,7 @@ int print_encoded_header(FILE *fd, char *msg, int msglen,
 {
 	char *hdr_start_ptr;
 	short int start_idx, i;
+	int j;
 
 	memcpy(&start_idx, payload, 2);
 	start_idx = ntohs(start_idx);
@@ -378,9 +379,9 @@ int print_encoded_header(FILE *fd, char *msg, int msglen,
 			hdr_start_ptr);
 	fprintf(fd, "%sHEADER:[%.*s]\n", prefix, i - 2, hdr_start_ptr);
 	fprintf(fd, "%sHEADER CODE=", prefix);
-	for(i = 0; i < len; i++)
-		fprintf(fd, "%s%d%s", i == 0 ? "[" : ":", payload[i],
-				i == len - 1 ? "]\n" : "");
+	for(j = 0; j < len; j++)
+		fprintf(fd, "%s%d%s", j == 0 ? "[" : ":", payload[j],
+				j == len - 1 ? "]\n" : "");
 	if(len == 4)
 		return 1;
 	switch(type) {
