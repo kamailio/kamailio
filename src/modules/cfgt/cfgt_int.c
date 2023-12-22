8bfd6580533b6d6005cd257632a5c0e575f75e39
Victor Seva <linuxmaniac@torreviejawireless.org>
2023-12-22 09:57:25 +0100

    cfgt: fix build warning
    > CC (gcc) [M cfgt.so]		cfgt_mod.o
    > cfgt_int.c:22: warning: "_GNU_SOURCE" redefined
    >    22 | #define _GNU_SOURCE
    >       |
    > <command-line>: note: this is the location of the previous definition

--- src/modules/cfgt/cfgt_int.c
+++ src/modules/cfgt/cfgt_int.c
@@ -19,7 +19,9 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
  */
+#ifndef _GNU_SOURCE
 #define _GNU_SOURCE
+#endif
 #include <stdio.h>
 #include <sys/stat.h>
 #include <sys/time.h>
