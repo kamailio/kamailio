86c57ef77848513e3224c73a6d2889e8a2bd6fc3
Victor Seva <linuxmaniac@torreviejawireless.org>
2023-12-22 09:58:39 +0100

    db_text: fix build warning
    > CC (gcc) [M db_text.so]		dbt_raw_query.o
    > dbt_raw_query.c:23: warning: "_GNU_SOURCE" redefined
    >    23 | #define _GNU_SOURCE
    >       |
    > <command-line>: note: this is the location of the previous definition

--- src/modules/db_text/dbt_raw_query.c
+++ src/modules/db_text/dbt_raw_query.c
@@ -19,8 +19,9 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
-
+#ifndef _GNU_SOURCE
 #define _GNU_SOURCE
+#endif
 #include <string.h>
 
 #include "../../core/str.h"
