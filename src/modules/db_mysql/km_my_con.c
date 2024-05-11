aa9b7cd96dec96c68f9801230121665667d027f8
Sergey Safarov <s.safarov@gmail.com>
2024-05-11 17:45:08 +0300

    db_mysql: fixed build on CentOS 7

--- src/modules/db_mysql/km_my_con.c
+++ src/modules/db_mysql/km_my_con.c
@@ -178,9 +178,16 @@ struct my_con *db_mysql_new_connection(const struct db_id *id)
 	}
 #endif /* MYSQL_VERSION_ID */
 #endif /* MARIADB_BASE_VERSION */
+
+#ifdef MYSQL_OPT_SSL_CA
 	if(db_mysql_opt_ssl_ca)
 		mysql_options(
 				ptr->con, MYSQL_OPT_SSL_CA, (const void *)db_mysql_opt_ssl_ca);
+#else
+	LM_WARN("opt_ssl_ca option not supported by mysql version (value %s) - "
+			"ignoring\n",
+			db_mysql_opt_ssl_ca);
+#endif /* MYSQL_OPT_SSL_CA */
 
 #ifdef KSR_MYSQL_OPT_RECONNECT
 	/* set reconnect flag if enabled */
