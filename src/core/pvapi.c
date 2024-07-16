fa792054a58126b57797675aadc4f0e862316dfd
Daniel-Constantin Mierla <miconda@gmail.com>
2024-07-16 20:27:27 +0200

    core: pvapi - use defined types for pv_get_avp_name() name and flags

--- src/core/pvapi.c
+++ src/core/pvapi.c
@@ -1254,8 +1254,8 @@ int pv_parse_avp_name(pv_spec_p sp, str *in)
  * fill avp name details (id and type)
  * @return 0 on success, -1 on error
  */
-int pv_get_avp_name(struct sip_msg *msg, pv_param_p ip, int_str *avp_name,
-		unsigned short *name_type)
+int pv_get_avp_name(struct sip_msg *msg, pv_param_p ip, avp_name_t *avp_name,
+		avp_flags_t *name_type)
 {
 	pv_value_t tv;
 	if(ip == NULL || avp_name == NULL || name_type == NULL)
