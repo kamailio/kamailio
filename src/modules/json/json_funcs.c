e67ad97d3da4e3b5149ea0de31f0930b015a1dd9
Daniel-Constantin Mierla <miconda@gmail.com>
2024-07-17 08:54:37 +0200

    json: coherent use of defined types for avp flags, name and value

--- src/modules/json/json_funcs.c
+++ src/modules/json/json_funcs.c
@@ -321,8 +321,8 @@ int tr_json_get_keys(struct sip_msg *msg, char *json, char *field, char *dst)
 {
 	str json_s;
 	str field_s;
-	int_str keys_avp_name;
-	unsigned short keys_avp_type;
+	avp_name_t keys_avp_name;
+	avp_flags_t keys_avp_type;
 	pv_spec_t *avp_spec;
 
 	if(fixup_get_svalue(msg, (gparam_p)json, &json_s) != 0) {
