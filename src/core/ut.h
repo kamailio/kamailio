57144a263680e86616dea49e9f2ca9f84a48d777
Henning Westerholt <hw@gilawa.com>
2024-08-23 09:26:06 +0000

    core: add new function pkg_char_dup, similar as shm_char_dup but for PKG memory

--- src/core/ut.h
+++ src/core/ut.h
@@ -1018,6 +1018,34 @@ static inline int pkg_str_dup(str *dst, const str *src)
 }
 
 /**
+ * \brief Make a copy of a char pointer to a char pointer using pkg_malloc
+ * \param src source
+ * \return a pointer to the new allocated char on success, 0 on failure
+ */
+static inline char *pkg_char_dup(const char *src)
+{
+	char *rval;
+	int len;
+
+	if(!src) {
+		LM_ERR("NULL src or dst\n");
+		return NULL;
+	}
+
+	len = strlen(src) + 1;
+	rval = (char *)pkg_malloc(len);
+	if(!rval) {
+		PKG_MEM_ERROR;
+		return NULL;
+	}
+
+	memcpy(rval, src, len);
+
+	return rval;
+}
+
+
+/**
  * \brief Compare two str's case sensitive
  * \param str1 first str
  * \param str2 second str
