8a13a1d8301aa22baa9b3a52b891fd04a04c3768
Daniel-Constantin Mierla <miconda@gmail.com>
2024-09-06 10:16:50 +0200

    utils/kamctl: regenerate db table definitions

--- utils/kamctl/db_sqlite/htable-create.sql
+++ utils/kamctl/db_sqlite/htable-create.sql
@@ -1,11 +1,10 @@
 CREATE TABLE htable (
     id INTEGER PRIMARY KEY NOT NULL,
-    key_name VARCHAR(64) DEFAULT '' NOT NULL,
+    key_name VARCHAR(256) DEFAULT '' NOT NULL,
     key_type INTEGER DEFAULT 0 NOT NULL,
     value_type INTEGER DEFAULT 0 NOT NULL,
-    key_value VARCHAR(128) DEFAULT '' NOT NULL,
+    key_value VARCHAR(512) DEFAULT '' NOT NULL,
     expires INTEGER DEFAULT 0 NOT NULL
 );
 
 INSERT INTO version (table_name, table_version) values ('htable','2');
-
--- utils/kamctl/oracle/htable-create.sql
+++ utils/kamctl/oracle/htable-create.sql
@@ -1,9 +1,9 @@
 CREATE TABLE htable (
     id NUMBER(10) PRIMARY KEY,
-    key_name VARCHAR2(64) DEFAULT '',
+    key_name VARCHAR2(256) DEFAULT '',
     key_type NUMBER(10) DEFAULT 0 NOT NULL,
     value_type NUMBER(10) DEFAULT 0 NOT NULL,
-    key_value VARCHAR2(128) DEFAULT '',
+    key_value VARCHAR2(512) DEFAULT '',
     expires NUMBER(10) DEFAULT 0 NOT NULL
 );
 
@@ -16,4 +16,3 @@ END htable_tr;
 BEGIN map2users('htable'); END;
 /
 INSERT INTO version (table_name, table_version) values ('htable','2');
-
