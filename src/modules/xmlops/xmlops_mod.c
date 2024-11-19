1629292f68f05d16843d5d813ad4c05b3a97c76f
Victor Seva <linuxmaniac@torreviejawireless.org>
2024-11-19 17:23:57 +0100

    xmlops: replace USE_FUNC_PARAM => PARAM_USE_FUNC

--- src/modules/xmlops/xmlops_mod.c
+++ src/modules/xmlops/xmlops_mod.c
@@ -52,7 +52,7 @@ static pv_export_t mod_pvs[] = {
 
 static param_export_t params[]={
 	{ "buf_size",  PARAM_INT, &pv_xml_buf_size },
-	{ "xml_ns",    PARAM_STRING|USE_FUNC_PARAM, (void*)pv_xml_ns_param },
+	{ "xml_ns",    PARAM_STRING|PARAM_USE_FUNC, (void*)pv_xml_ns_param },
 	{ 0, 0, 0}
 };
 
