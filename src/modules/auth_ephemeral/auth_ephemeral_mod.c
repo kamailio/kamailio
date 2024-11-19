ffc491647a41f36ca668bcf5edddc18e9965bdd6
Victor Seva <linuxmaniac@torreviejawireless.org>
2024-11-19 17:11:17 +0100

    auth_ephemeral: replace USE_FUNC_PARAM => PARAM_USE_FUNC

--- src/modules/auth_ephemeral/auth_ephemeral_mod.c
+++ src/modules/auth_ephemeral/auth_ephemeral_mod.c
@@ -78,7 +78,7 @@ static cmd_export_t cmds[] = {{"autheph_check", (cmd_function)autheph_check, 1,
 		{0, 0, 0, 0, 0, 0}};
 
 static param_export_t params[] = {
-		{"secret", PARAM_STRING | USE_FUNC_PARAM, (void *)secret_param},
+		{"secret", PARAM_STRING | PARAM_USE_FUNC, (void *)secret_param},
 		{"username_format", PARAM_INT, &autheph_username_format},
 		{"sha_algorithm", PARAM_INT, &autheph_sha_alg}, {0, 0, 0}};
 
