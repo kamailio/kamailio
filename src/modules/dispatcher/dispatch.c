cf72416567d915c1024471e917b933d057452ed9
Daniel-Constantin Mierla <miconda@gmail.com>
2024-11-26 10:36:07 +0100

    dispatcher: ds_oc_set_attrs() return -2 when no oc attributes are updated

--- src/modules/dispatcher/dispatch.c
+++ src/modules/dispatcher/dispatch.c
@@ -483,6 +483,7 @@ int ds_oc_set_attrs(
 	tdiff.tv_sec = (((itval > 0) ? itval : 500) * 1000) / 1000000;
 	tdiff.tv_usec = (((itval > 0) ? itval : 500) * 1000) % 1000000;
 
+	ret = -2;
 	for(i = 0; i < idx->nr; i++) {
 		if(idx->dlist[i].uri.len == duri->len
 				&& strncasecmp(idx->dlist[i].uri.s, duri->s, duri->len) == 0) {
