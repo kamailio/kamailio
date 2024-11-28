78f272da156ff7796dd84697bbf04522b7a08009
Daniel-Constantin Mierla <miconda@gmail.com>
2024-11-28 08:33:14 +0100

    async: fixed typo in log message

--- src/modules/async/async_mod.c
+++ src/modules/async/async_mod.c
@@ -130,7 +130,7 @@ static int mod_init(void)
 {
 	/* init faked sip msg */
 	if(faked_msg_init() < 0) {
-		LM_ERR("failed to iit local sip msg\n");
+		LM_ERR("failed to init local sip msg\n");
 		return -1;
 	}
 
