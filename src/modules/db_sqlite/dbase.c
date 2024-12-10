06cc2560e5358468178ef3e5c795a8a0ec67b693
Victor Seva <linuxmaniac@torreviejawireless.org>
2024-12-10 23:58:06 +0100

    db_sqlite: fix incompatible-pointer-types warning
    fixes #4064

--- src/modules/db_sqlite/dbase.c
+++ src/modules/db_sqlite/dbase.c
@@ -142,14 +142,17 @@ db1_con_t *db_sqlite_init(const str *_url)
  * No function should be called after this
  */
 
-static void db_sqlite_free_connection(struct sqlite_connection *con)
+static void db_sqlite_free_connection(struct pool_con *con)
 {
+	struct sqlite_connection *_c;
+
 	if(!con)
 		return;
+	_c = (struct sqlite_connection *)con;
 
-	sqlite3_close(con->conn);
-	free_db_id(con->hdr.id);
-	pkg_free(con);
+	sqlite3_close(_c->conn);
+	free_db_id(_c->hdr.id);
+	pkg_free(_c);
 }
 
 void db_sqlite_close(db1_con_t *_h)
