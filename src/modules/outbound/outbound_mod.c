793a494f5b23f2ef5e82664924c3bed3893105bb
Victor Seva <linuxmaniac@torreviejawireless.org>
2024-12-12 14:50:42 +0100

    outbound: reformat exported structures

--- src/modules/outbound/outbound_mod.c
+++ src/modules/outbound/outbound_mod.c
@@ -61,25 +61,32 @@ static unsigned int ob_force_no_flag = (unsigned int)-1;
 static str ob_key = {0, 0};
 static str flow_token_secret = {0, 0};
 
+/* clang-format off */
 static cmd_export_t cmds[] = {
-		{"bind_ob", (cmd_function)bind_ob, 1, 0, 0, 0}, {0, 0, 0, 0, 0, 0}};
+	{"bind_ob", (cmd_function)bind_ob, 1, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0}
+};
 
 static param_export_t params[] = {
-		{"force_outbound_flag", PARAM_INT, &ob_force_flag},
-		{"force_no_outbound_flag", PARAM_INT, &ob_force_no_flag},
-		{"flow_token_secret", PARAM_STRING, &flow_token_secret}, {0, 0, 0}};
+	{"force_outbound_flag", PARAM_INT, &ob_force_flag},
+	{"force_no_outbound_flag", PARAM_INT, &ob_force_no_flag},
+	{"flow_token_secret", PARAM_STRING, &flow_token_secret},
+	{0, 0, 0}
+};
 
 struct module_exports exports = {
-		"outbound", DEFAULT_DLFLAGS, /* dlopen flags */
-		cmds,						 /* exported functions */
-		params,						 /* exported parameters */
-		0,							 /* exported·RPC·methods */
-		0,							 /* exported pseudo-variables */
-		0,							 /* response·function */
-		mod_init,					 /* module initialization function */
-		0,							 /* per-child initialization function */
-		destroy						 /* destroy function */
+	"outbound",
+	DEFAULT_DLFLAGS,    /* dlopen flags */
+	cmds,               /* exported functions */
+	params,             /* exported parameters */
+	0,                  /* RPC method exports */
+	0,                  /* exported pseudo-variables */
+	0,                  /* response handling function */
+	mod_init,           /* module initialization function */
+	0,                  /* per-child init function */
+	destroy             /* module destroy function */
 };
+/* clang-format on */
 
 static void mod_init_openssl(void)
 {
