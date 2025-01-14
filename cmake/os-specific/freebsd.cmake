0498446fd24462a7de72f73aae37a76ceabc4276
drTr0jan <drtr0jan@yandex.ru>
2025-01-14 15:25:25 +0300

    cmake: add FreeBSD support

---
+++ cmake/os-specific/freebsd.cmake
@@ -0,0 +1,37 @@
+message(
+  STATUS
+    "Configuring for FreeBSD"
+)
+
+target_compile_definitions(
+  common
+  INTERFACE HAVE_SOCKADDR_SA_LEN
+            HAVE_GETHOSTBYNAME2
+            HAVE_UNION_SEMUN
+            HAVE_SCHED_YIELD
+            HAVE_MSGHDR_MSG_CONTROL
+            HAVE_CONNECT_ECONNRESET_BUG
+            HAVE_TIMEGM
+            HAVE_IP_MREQN
+)
+
+if(${RAW_SOCKS})
+  target_compile_definitions(common INTERFACE USE_RAW_SOCKS)
+endif()
+
+message(STATUS "USE_FAST_LOCK = ${USE_FAST_LOCK}")
+if(NOT ${USE_FAST_LOCK})
+  target_compile_definitions(common INTERFACE USE_PTHREAD_MUTEX)
+endif()
+
+if(NOT DEFINED ${NO_SELECT})
+  target_compile_definitions(common INTERFACE HAVE_SELECT)
+endif()
+
+if(NOT DEFINED ${NO_KQUEUE})
+  target_compile_definitions(common INTERFACE HAVE_KQUEUE)
+endif()
+
+if(NOT DEFINED RUN_PREFIX)
+  set(RUN_PREFIX "/var")
+endif()
