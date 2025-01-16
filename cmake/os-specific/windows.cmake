b363991b641ea3979fb6e3d5b8da2fdbab431f4c
soroshsabz <soorosh_abi@hotmail.com>
2025-01-16 04:53:26 +0330

    Exclude some subdirectory

--- cmake/os-specific/windows.cmake
+++ cmake/os-specific/windows.cmake
@@ -1,5 +1,7 @@
 # Windows specific flags
 message(STATUS "Configuring for Windows")
 
-set(FLEX_ROOT "C:/Program Files (x86)/GnuWin32")
-set(BISON_ROOT "C:/Program Files (x86)/GnuWin32")
+set(FLEX_ROOT "C:/GnuWin32")
+set(BISON_ROOT "C:/GnuWin32")
+set(ENV PATH "C:/GnuWin32/bin;$ENV{PATH}" CACHE INTERNAL "")
+set(ENV PATH "C:/GnuWin32/bin;$ENV{PATH}")
