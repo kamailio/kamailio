024c649c36bbe1d5ba4b306de53102cec4096734
Henning Westerholt <hw@gilawa.com>
2025-01-20 19:36:11 +0000

    htable: close parentheses on error description

--- src/modules/htable/htable.c
+++ src/modules/htable/htable.c
@@ -1561,7 +1561,7 @@ static void htable_rpc_delete(rpc_t *rpc, void *c)
 	int res;
 
 	if(rpc->scan(c, "SS", &htname, &keyname) < 2) {
-		rpc->fault(c, 500, "Not enough parameters (htable name & key name");
+		rpc->fault(c, 500, "Not enough parameters (htable name & key name)");
 		return;
 	}
 	ht = ht_get_table(&htname);
