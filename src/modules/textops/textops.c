a88946a84133660d5b2a466a231dff226c9db995
Daniel-Constantin Mierla <miconda@gmail.com>
2025-02-05 13:30:17 +0100

    textops: catch return 0 when removing via param
    (cherry picked from commit 65645c761e94cedde04f100764d5c506590cb200)

--- src/modules/textops/textops.c
+++ src/modules/textops/textops.c
@@ -5026,11 +5026,7 @@ static int ki_via_param_rm(sip_msg_t *msg, str *name, int idx)
 								LM_ERR("no memory for delete operation\n");
 								return -1;
 							}
-							if(ret < 0) {
-								ret = 1;
-							} else {
-								ret++;
-							}
+							ret++;
 						}
 					}
 					if(ret > 0) {
@@ -5041,7 +5037,7 @@ static int ki_via_param_rm(sip_msg_t *msg, str *name, int idx)
 			}
 		}
 	}
-	return ret;
+	return (ret == 0) ? -1 : ret;
 }
 
 /**
