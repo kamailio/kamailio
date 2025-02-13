ff712539566c903312ebba2f8e2585f9c80ffdcf
Daniel-Constantin Mierla <miconda@gmail.com>
2025-02-13 09:40:00 +0100

    xmlrpc: clean allocated buffer

--- src/modules/xmlrpc/xmlrpc.c
+++ src/modules/xmlrpc/xmlrpc.c
@@ -2024,7 +2024,7 @@ static int rpc_struct_printf(
 		struct rpc_struct *s, char *member_name, char *fmt, ...)
 {
 	int n, buf_size;
-	char *buf;
+	char *buf = NULL;
 	va_list ap;
 	str st, name;
 	struct xmlrpc_reply *reply;
@@ -2076,6 +2076,7 @@ static int rpc_struct_printf(
 			if(add_xmlrpc_reply(out, &member_suffix) < 0)
 				goto err;
 
+			mxr_free(buf);
 			return 0;
 		}
 		/* Else try again with more space. */
@@ -2090,6 +2091,8 @@ static int rpc_struct_printf(
 			goto err;
 		}
 	}
+	if(buf)
+		mxr_free(buf);
 	return 0;
 err:
 	if(buf)
@@ -2745,7 +2748,7 @@ static int ki_xmlrpc_reply(sip_msg_t *msg, int rcode, str *reason)
 		if(add_xmlrpc_reply(&reply, &int_suffix) < 0)
 			goto error;
 		if(add_xmlrpc_reply(&reply, &success_suffix) < 0)
-			return -1;
+			goto error;
 	}
 	if(send_reply(msg, &reply.body) < 0)
 		goto error;
