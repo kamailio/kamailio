edbb5b11e9453be3519e6e9a945937307c95709e
Daniel-Constantin Mierla <miconda@gmail.com>
2025-02-14 08:37:23 +0100

    ctl: refactored error handling and removed unnecessary code

--- src/modules/ctl/fifo_server.c
+++ src/modules/ctl/fifo_server.c
@@ -1290,7 +1290,7 @@ static int rpc_struct_printf(struct text_chunk *c, char *name, char *fmt, ...)
 					&nm, 1); /* Escape all characters, including : and , */
 			if(!m) {
 				rpc_fault(ctx, 500, "Internal Server Error");
-				goto err;
+				goto error;
 			}
 
 			s.s = buf;
@@ -1300,7 +1300,7 @@ static int rpc_struct_printf(struct text_chunk *c, char *name, char *fmt, ...)
 				rpc_fault(ctx, 500, "Internal Server Error");
 				free_chunk(m);
 				ERR("Error while creating text_chunk structure");
-				goto err;
+				goto error;
 			}
 
 			l->flags |= CHUNK_MEMBER_VALUE;
@@ -1326,13 +1326,12 @@ static int rpc_struct_printf(struct text_chunk *c, char *name, char *fmt, ...)
 		if((buf0 = ctl_realloc(buf, buf_size)) == 0) {
 			rpc_fault(ctx, 500, "Internal Server Error (No memory left)");
 			ERR("No memory left\n");
-			goto err;
+			goto error;
 		}
 		buf = buf0;
 	}
-	ctl_free(buf);
-	return 0;
-err:
+
+error:
 	if(buf)
 		ctl_free(buf);
 	return -1;
