039c877b8846043db9cef6a8024e68d25369cbc7
Daniel-Constantin Mierla <miconda@gmail.com>
2025-04-16 08:31:10 +0200

    microhttpd: warning on failing to add content-type
    (cherry picked from commit 2b331d95ae62eb0000a289e1bdb668ee7bc572cb)

--- src/modules/microhttpd/microhttpd_mod.c
+++ src/modules/microhttpd/microhttpd_mod.c
@@ -373,7 +373,10 @@ static int ksr_mhttpd_send_reply(
 		return -1;
 	}
 	if(sctype->len > 0) {
-		MHD_add_response_header(response, "Content-Type", sctype->s);
+		if(MHD_add_response_header(response, "Content-Type", sctype->s)
+				== MHD_NO) {
+			LM_WARN("failed to add Content-Type header\n");
+		}
 	}
 	ret = MHD_queue_response(
 			_ksr_mhttpd_ctx.connection, (unsigned int)rcode, response);
