621a2d33d509baa3f11801b0464ca7f32b092a75
Michael Furmur <furmur@pm.me>
2025-04-24 13:08:37 +0300

    tls: implement match_domain,match_connections_domain hooks
    * save tls_domain_str() to the tls_extra_data and use it for connections matching
    * rpc tls.list: add 'dom' attribute

--- src/modules/tls/tls_rpc.c
+++ src/modules/tls/tls_rpc.c
@@ -113,7 +113,7 @@ static void tls_list(rpc_t *rpc, void *c)
 	int i, len, timeout;
 	struct tm timestamp;
 	char timestamp_s[128];
-	const char *sni;
+	const char *sni, *dom;
 
 	TCPCONN_LOCK;
 	for(i = 0; i < TCP_ID_HASH_SIZE; i++) {
@@ -146,17 +146,19 @@ static void tls_list(rpc_t *rpc, void *c)
 
 			if(tls_d) {
 				sni = SSL_get_servername(tls_d->ssl, TLSEXT_NAMETYPE_host_name);
+				dom = tls_d->dom.s;
 				if(sni == NULL) {
 					sni = "N/A";
 				}
 			} else {
 				sni = "N/A";
+				dom = "N/A";
 			}
 
-			rpc->struct_add(handle, "dssdsdsd", "id", con->id, "sni", sni,
-					"timestamp", timestamp_s, "timeout", timeout, "src_ip",
-					src_ip, "src_port", con->rcv.src_port, "dst_ip", dst_ip,
-					"dst_port", con->rcv.dst_port);
+			rpc->struct_add(handle, "dsssdsdsd", "id", con->id, "dom", dom,
+					"sni", sni, "timestamp", timestamp_s, "timeout", timeout,
+					"src_ip", src_ip, "src_port", con->rcv.src_port, "dst_ip",
+					dst_ip, "dst_port", con->rcv.dst_port);
 			if(tls_d) {
 				if(SSL_get_current_cipher(tls_d->ssl)) {
 					tls_info = SSL_CIPHER_description(
