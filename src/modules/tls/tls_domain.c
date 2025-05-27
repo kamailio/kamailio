544a6665e9ceb930f3a50d256258830598d0c45c
Nicolas Chapleau <nicchap@outlook.com>
2025-05-27 10:26:07 -0400

    Removed extra return variable sslrtn in set_cipher_list() in favor of logging each SSL_CTX_cipher_() function call errors more accurately

--- src/modules/tls/tls_domain.c
+++ src/modules/tls/tls_domain.c
@@ -708,7 +708,6 @@ static int load_crl(tls_domain_t *d)
 static int set_cipher_list(tls_domain_t *d)
 {
 	int i;
-	int sslrtn;	/* used for return value from SSL_CTX_cipher... */
 	int procs_no;
 	char *cipher_list;
 
@@ -746,15 +745,18 @@ static int set_cipher_list(tls_domain_t *d)
 		}
 #else
 		if(d->method == TLS_USE_TLSv1_3 || d->method == TLS_USE_TLSv1_3_PLUS) {
-			sslrtn = SSL_CTX_set_ciphersuites(d->ctx[i], cipher_list);
-		} else {
-			sslrtn = SSL_CTX_set_cipher_list(d->ctx[i], cipher_list);
-		}
-		if(sslrtn == 0) {
-			ERR("%s: Failure to set SSL context cipher list \"%s\"\n",
+			if(SSL_CTX_set_ciphersuites(d->ctx[i], cipher_list) == 0) {
+				ERR("%s: Failure to set SSL context cipher suites \"%s\"\n",
 					tls_domain_str(d), cipher_list);
-			return -1;
-		}
+				return -1;
+			}
+		} else {
+			if(SSL_CTX_set_cipher_list(d->ctx[i], cipher_list) == 0) {
+				ERR("%s: Failure to set SSL context cipher list \"%s\"\n",
+					tls_domain_str(d), cipher_list);
+				return -1;
+			}
+		}		
 #endif
 #if !defined(OPENSSL_NO_ECDH) && OPENSSL_VERSION_NUMBER < 0x10100000L
 		setup_ecdh(d->ctx[i]);
