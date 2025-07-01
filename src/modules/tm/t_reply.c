8de5ff7ece75a08f30087edd21ff0ced909e784d
ovoshlook <ovoshlook@users.noreply.github.com>
2025-07-01 16:10:54 +0200

    tm: allow pending forked REGISTER auths being handled the same way as forked INVITEs auths
    Forked REGISTER negative replies will be treated in the same way as forked INVITE negative replies are treated in case of late authentication on multiple REGISTRARs:
    If a negative reply from forked REGISTER comes after 200 reply from another fork already received, it won't be discarded.

--- src/modules/tm/t_reply.c
+++ src/modules/tm/t_reply.c
@@ -1386,7 +1386,7 @@ static enum rps t_should_relay_response(struct cell *Trans, int new_code,
 	LM_DBG("->>>>>>>>> T_code=%d, new_code=%d\n", Trans->uas.status,new_code);
 	inv_through=new_code>=200 && new_code<300 && is_invite(Trans);
 	/* if final response sent out, allow only INVITE 2xx && REGISTER > 299 */
-	if ( Trans->uas.status >= 200 ) {
+	if(Trans->uas.status >= 200) {
 		if (inv_through) {
 			LM_DBG("200 INV after final sent\n");
 			*should_store = 0;
@@ -1400,7 +1400,7 @@ static enum rps t_should_relay_response(struct cell *Trans, int new_code,
 		 * some of registrars may reply later than others being authenticated.
 		 * This lests late forked transactions being finalised correctly
 		*/
-		if (!(strncmp(Trans->method.s, "REGISTER", 8) == 0 && new_code >= 300)) {
+		if(!(strncmp(Trans->method.s, "REGISTER", 8) == 0 && new_code >= 300)) {
 			/*Except the exceptions above, too late  messages will be discarded */
 			LM_DBG("final reply already sent\n");
 			goto discard;
