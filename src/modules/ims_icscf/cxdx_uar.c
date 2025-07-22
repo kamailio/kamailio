c9712fb06bb5cfa037aaea41828e132bdd5f82d4
Victor Seva <linuxmaniac@torreviejawireless.org>
2025-07-22 11:09:02 +0200

    ims_icscf: cxdx_send_uar() return error on fail send message
    (cherry picked from commit 8a30b015e5d8da263cd6f91cc659813cc6c9946a)
    (cherry picked from commit 9bc482d9e859473621b5a4bebb3bf7e11e9bdd96)

--- src/modules/ims_icscf/cxdx_uar.c
+++ src/modules/ims_icscf/cxdx_uar.c
@@ -300,6 +300,7 @@ int cxdx_send_uar(struct sip_msg *msg, str private_identity,
 {
 	AAAMessage *uar = 0;
 	AAASession *session = 0;
+	int ret = -1;
 
 	session = cdpb.AAACreateSession(0);
 
@@ -332,12 +333,16 @@ int cxdx_send_uar(struct sip_msg *msg, str private_identity,
 			goto error1;
 
 	if(cxdx_forced_peer.len)
-		cdpb.AAASendMessageToPeer(uar, &cxdx_forced_peer,
+		ret = cdpb.AAASendMessageToPeer(uar, &cxdx_forced_peer,
 				(void *)async_cdp_uar_callback, (void *)transaction_data);
 	else
-		cdpb.AAASendMessage(
+		ret = cdpb.AAASendMessage(
 				uar, (void *)async_cdp_uar_callback, (void *)transaction_data);
 
+	if(ret != 1) {
+		LM_DBG("Error sending async diameter (%d)\n", ret);
+		return -1;
+	}
 	LM_DBG("Successfully sent async diameter\n");
 
 	return 0;
