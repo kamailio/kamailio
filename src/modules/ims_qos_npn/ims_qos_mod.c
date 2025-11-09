ec41c439899304b635c2bf702e1a5f607ddbe1d0
a.shirmast <amir.sheykhshirmast@gmail.com>
2025-11-09 17:38:53 +0330

    ims_qos_npn: fix getaddrinfo input and update Specific-Action handling in AAR-RX

--- src/modules/ims_qos_npn/ims_qos_mod.c
+++ src/modules/ims_qos_npn/ims_qos_mod.c
@@ -124,6 +124,10 @@ int trust_bottom_via = 0;
 int cdp_event_list_size_threshold =
 		0; /**Threshold for size of cdp event list after which a warning is logged */
 
+int rx_include_alloc_result_indications =
+		0; /* When set to 1, adds Specific-Action AVPs 8 (successful) and 9 (failed) to AAR so the PCRF
+			reports resource-allocation results to the AF.*/
+
 stat_var *aars;
 stat_var *strs;
 stat_var *asrs;
@@ -297,6 +301,8 @@ static param_export_t params[] = {
 		{"af_signaling_ip6", PARAM_STR, &af_signaling_ip6},
 		{"af_signaling_port_pc", PARAM_STR, &af_signaling_port_pc},
 		{"af_signaling_port_ps", PARAM_STR, &af_signaling_port_ps},
+		{"rx_include_alloc_result_indications", PARAM_INT,
+				&rx_include_alloc_result_indications},
 		{0, 0, 0},
 };
 
@@ -1258,6 +1264,8 @@ static int w_rx_aar(struct sip_msg *msg, char *route, char *dir, char *c_id,
 					LM_ERR("Reply SDP IP information could not be retrieved");
 					goto error;
 				}
+				LM_DBG("IP retrieved from Reply sdp_stream: [%.*s]", ip.len,
+						ip.s);
 				ip_version = check_ip_version(ip);
 				if(!ip_version) {
 					LM_ERR("check_ip_version returned 0 \n");
