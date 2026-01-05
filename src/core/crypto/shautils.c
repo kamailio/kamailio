d48a29cf0c4936953e7a002bedf3ba538c41ff20
Xenofon Karamanos <xk@gilawa.com>
2026-01-05 13:16:55 +0000

    core/crypto: Add utils for sha3 hex to bytes and vice versa

--- src/core/crypto/shautils.c
+++ src/core/crypto/shautils.c
@@ -31,6 +31,9 @@
 * \ingroup core/crypto
 * Module: \ref core/crypto
 */
+#include <ctype.h>
+#include <errno.h>
+#include <stdlib.h>
 
 #include "../../core/crypto/md5.h"
 #include "../../core/ut.h"
@@ -90,4 +93,61 @@ void compute_sha512(char *dst, u_int8_t *src, int src_len)
 	sr_SHA512_Init(&ctx512);
 	sr_SHA512_Update(&ctx512, src, src_len);
 	sr_SHA512_End(&ctx512, dst);
+}
+
+
+/**
+ * Convert hex string to bytes
+ */
+int hex_to_bytes(const char *hex_str, unsigned char *bytes, int max_bytes)
+{
+	int len;
+	int byte_len;
+	int i;
+	char *endptr;
+	long val;
+
+	len = strlen(hex_str);
+	if(len % 2 != 0)
+		return -1; /* Invalid hex string */
+
+	byte_len = len / 2;
+	if(byte_len > max_bytes)
+		return -1; /* Too many bytes */
+
+	for(i = 0; i < byte_len; i++) {
+		char hex_byte[3] = {hex_str[i * 2], hex_str[i * 2 + 1], '\0'};
+
+		/* Check for valid hex characters before calling strtol */
+		if(!isxdigit((unsigned char)hex_byte[0])
+				|| !isxdigit((unsigned char)hex_byte[1])) {
+			return -1; /* Invalid hex character */
+		}
+
+		errno = 0;
+		val = strtol(hex_byte, &endptr, 16);
+
+		/* Check for conversion errors */
+		if(errno != 0 || endptr == hex_byte || *endptr != '\0') {
+			return -1; /* Conversion failed */
+		}
+
+		/* Check for out of range values */
+		if(val < 0 || val > 255) {
+			return -1; /* Value out of byte range */
+		}
+
+		bytes[i] = (unsigned char)val;
+	}
+
+	return byte_len;
+}
+
+/* Convert bytes to hex string */
+void bytes_to_hex(const unsigned char *bytes, size_t len, char *hex)
+{
+	for(size_t i = 0; i < len; i++) {
+		sprintf(hex + 2 * i, "%02x", bytes[i]);
+	}
+	hex[2 * len] = '\0';
 }
