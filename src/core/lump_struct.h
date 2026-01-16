7a6813bc623f008e15f0e1df25c029a76f31fcd3
Daniel-Constantin Mierla <miconda@gmail.com>
2026-01-16 10:24:02 +0100

    core: data lump - flag for record-route lumps

--- src/core/lump_struct.h
+++ src/core/lump_struct.h
@@ -93,11 +93,17 @@ enum lump_flag
 	LUMPFLAG_SHMEM = 2,		/* lump stored in shared memory (e.g., tm) */
 	LUMPFLAG_BRANCH = 4,	/* not in use ?!? */
 	LUMPFLAG_COND_TRUE = 8, /* conditional lump processing */
-	LUMPFLAG_APPLIED = 16	/* lump already applied */
+	LUMPFLAG_APPLIED = 16,	/* lump already applied */
+	LUMPFLAG_RR = 32		/* lump related to record-route */
 };
 
 #define LUMP_SET_COND_TRUE(_lump) (_lump)->flags |= LUMPFLAG_COND_TRUE
 #define LUMP_IS_COND_TRUE(_lump) ((_lump)->flags & LUMPFLAG_COND_TRUE)
+
+#define LUMP_SET_FLAG(_lump, _flag) \
+	do {                            \
+		(_lump)->flags |= _flag;    \
+	} while(0)
 
 typedef struct lump
 {
