206c7bc8c04ff68435bc040e211e97af21af1f57
Victor Seva <vseva@sipwork.pro>
2026-02-10 10:38:39 +0100

    ims_registrar_pcscf: fix small typo in debug log
    (cherry picked from commit b4d38643209b8876345b2120994fb8860313b325)

--- src/modules/ims_registrar_pcscf/ul_callback.c
+++ src/modules/ims_registrar_pcscf/ul_callback.c
@@ -282,7 +282,7 @@ void callback_pcscf_contact_cb(struct pcontact *c, int type, void *param)
 	ppublic_t *ptr;
 
 	LM_DBG("----------------------!\n");
-	LM_DBG("PCSCF Contact Callback in regsitrar!\n");
+	LM_DBG("PCSCF Contact Callback in registrar!\n");
 	LM_DBG("Contact AOR: [%.*s]\n", c->aor.len, c->aor.s);
 	LM_DBG("Callback type [%d]\n", type);
 	LM_DBG("Reg state [%s]\n", reg_state_to_string(c->reg_state));
