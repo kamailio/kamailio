ffb4a62f8b62aa30d59c69f6c9ee610b54382eaa
Daniel-Constantin Mierla <miconda@gmail.com>
2026-04-19 17:25:10 +0200

    tls: use SSL_CTX_set_ciphersuites() for TLS 1.3 ciphers from libssl 1.1.1
    (cherry picked from commit 8ee596f6972ff52648c8fea6246ff20972b67e52)

--- src/modules/tls/tls_domain.c
+++ src/modules/tls/tls_domain.c
@@ -738,7 +738,7 @@ static int set_cipher_list(tls_domain_t *d)
 		return 0;
 	procs_no = get_max_procs();
 	for(i = 0; i < procs_no; i++) {
-#if OPENSSL_VERSION_NUMBER < 0x030000000L
+#if OPENSSL_VERSION_NUMBER < 0x010101000L
 		if(SSL_CTX_set_cipher_list(d->ctx[i], cipher_list) == 0) {
 			ERR("%s: Failure to set SSL context cipher list \"%s\"\n",
 					tls_domain_str(d), cipher_list);
