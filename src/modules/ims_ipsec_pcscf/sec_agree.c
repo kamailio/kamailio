c257ac9aeabadabc24be8e59dee6e85b74d556e2
Victor Seva <linuxmaniac@torreviejawireless.org>
2026-05-04 11:40:10 +0200

    ims_ipsec_pcscf: Fix problems with double mem free, take three
    related #4670
    
    * free paramas->data.ipsec always

--- src/modules/ims_ipsec_pcscf/sec_agree.c
+++ src/modules/ims_ipsec_pcscf/sec_agree.c
@@ -311,6 +311,8 @@ cleanup:
 			if(params->data.ipsec->mod.s)
 				shm_free(params->data.ipsec->mod.s);
 			shm_free(params->data.ipsec);
+		} else {
+			shm_free(params->data.ipsec);
 		}
 
 		shm_free(params);
