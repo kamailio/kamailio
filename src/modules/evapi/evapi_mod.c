4f3129f44186dd4014383c3dcb1e0456f3f67ff1
Daniel-Constantin Mierla <miconda@gmail.com>
2026-06-07 14:30:49 +0200

    evapi: check parameters for kemi functions

--- src/modules/evapi/evapi_mod.c
+++ src/modules/evapi/evapi_mod.c
@@ -659,6 +659,11 @@ static int ki_evapi_relay(sip_msg_t *msg, str *sdata)
 {
 	int ret;
 
+	if(sdata == NULL || sdata->s == NULL || sdata->len <= 0) {
+		LM_ERR("invalid data parameter\n");
+		return -1;
+	}
+
 	ret = evapi_relay(sdata);
 
 	if(ret < 0)
@@ -817,6 +822,16 @@ static int ki_evapi_relay_unicast(sip_msg_t *msg, str *sdata, str *stag)
 {
 	int ret;
 
+	if(sdata == NULL || sdata->s == NULL || sdata->len <= 0) {
+		LM_ERR("invalid data parameter\n");
+		return -1;
+	}
+
+	if(stag == NULL || stag->s == NULL || stag->len <= 0) {
+		LM_ERR("invalid tag parameter\n");
+		return -1;
+	}
+
 	ret = evapi_relay_unicast(sdata, stag);
 
 	if(ret < 0)
@@ -831,6 +846,16 @@ static int ki_evapi_relay_unicast(sip_msg_t *msg, str *sdata, str *stag)
 static int ki_evapi_relay_multicast(sip_msg_t *msg, str *sdata, str *stag)
 {
 	int ret;
+
+	if(sdata == NULL || sdata->s == NULL || sdata->len <= 0) {
+		LM_ERR("invalid data parameter\n");
+		return -1;
+	}
+
+	if(stag == NULL || stag->s == NULL || stag->len <= 0) {
+		LM_ERR("invalid tag parameter\n");
+		return -1;
+	}
 
 	ret = evapi_relay_multicast(sdata, stag);
 
