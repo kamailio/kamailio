14b2490e85b2562dce5b006c1bfc5e4f7a2910a5
harish <toharishs@gmail.com>
2026-06-17 09:34:05 +0530

    lib: srdb1 - add GRUU fields to pcscf location schema
    Extend pcscf_location to version 8 with instance_id, pub_gruu, temp_gruu,
    and public_ids_barred columns. Add pcscf_temp_gruu_history table for
    temporary GRUU lifecycle tracking. Regenerate kamctl SQL and provisioning
    artifacts for supported database backends.

--- utils/kamctl/mysql/ims_usrloc_pcscf-create.sql
+++ utils/kamctl/mysql/ims_usrloc_pcscf-create.sql
@@ -44,9 +44,28 @@ CREATE TABLE `pcscf_location` (
     `t_spi_pc` INT(11) UNSIGNED DEFAULT NULL,
     `t_spi_ps` INT(11) UNSIGNED DEFAULT NULL,
     `t_spi_uc` INT(11) UNSIGNED DEFAULT NULL,
-    `t_spi_us` INT(11) UNSIGNED DEFAULT NULL
+    `t_spi_us` INT(11) UNSIGNED DEFAULT NULL,
+    `instance_id` VARCHAR(255) DEFAULT NULL,
+    `pub_gruu` VARCHAR(255) DEFAULT NULL,
+    `temp_gruu` VARCHAR(255) DEFAULT NULL,
+    `public_ids_barred` VARCHAR(2048) DEFAULT NULL
 );
 
 CREATE INDEX aor_idx ON pcscf_location (`aor`);
 
-INSERT INTO version (table_name, table_version) values ('pcscf_location','7');
+INSERT INTO version (table_name, table_version) values ('pcscf_location','8');
+
+CREATE TABLE `pcscf_temp_gruu_history` (
+    `id` INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    `location_id` INT(10) UNSIGNED NOT NULL,
+    `temp_gruu` VARCHAR(255) NOT NULL,
+    `created` DATETIME NOT NULL,
+    `expires` DATETIME NOT NULL
+);
+
+CREATE INDEX idx_loc ON pcscf_temp_gruu_history (`location_id`);
+CREATE INDEX idx_tgruu ON pcscf_temp_gruu_history (`temp_gruu`);
+CREATE INDEX idx_exp ON pcscf_temp_gruu_history (`expires`);
+
+INSERT INTO version (table_name, table_version) values ('pcscf_temp_gruu_history','1');
+
