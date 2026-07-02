93e2ea6e9516b06b9dd0f9e0faea6e81400d61fa
Stefan Mititelu <stefan.mititelu@net2phone.com>
2026-07-02 12:04:57 +0300

    dialog: revert replicate if same state
    This reverts commit b3b35bc55aa825a5a0aab98ba705fda9afb4fb06.

--- src/modules/dialog/dlg_handlers.c
+++ src/modules/dialog/dlg_handlers.c
@@ -599,7 +599,7 @@ static void dlg_onreply(struct cell *t, int type, struct tmcb_params *param)
 
 done:
 	if(dlg_enable_dmq && (dlg->iflags & DLG_IFLAG_DMQ_SYNC)
-			&& new_state >= old_state) {
+			&& new_state > old_state) {
 		dlg_dmq_replicate_action(DLG_DMQ_STATE, dlg, 0, 0);
 	}
 
@@ -1580,7 +1580,7 @@ void dlg_onroute(struct sip_msg *req, str *route_params, void *param)
 
 done:
 	if(dlg_enable_dmq && (dlg->iflags & DLG_IFLAG_DMQ_SYNC)
-			&& new_state >= old_state) {
+			&& new_state > old_state) {
 		dlg_dmq_replicate_action(DLG_DMQ_STATE, dlg, 0, 0);
 	}
 
@@ -1700,7 +1700,7 @@ void dlg_ontimeout(struct dlg_tl *tl)
 	}
 
 	if(dlg_enable_dmq && (dlg->iflags & DLG_IFLAG_DMQ_SYNC)
-			&& new_state >= old_state) {
+			&& new_state > old_state) {
 		dlg_dmq_replicate_action(DLG_DMQ_STATE, dlg, 0, 0);
 	}
 
