kamailio/kamailio

commit c9adcbec6375c2c2d913b7a6ceb44ed569f4c095

core: check size of the message on receive()
(cherry picked from commit 2fae35767398535fdfe2185327a0757cbca66fb7)
(cherry picked from commit 0eec7c78a2e711899c0b95d855ec3cb2d389fe02)

src/modules/ndb_cassandra/test-cpp/Cassandra.cpp
@@ -1,1 +1,1 @@
-../Cassandra.cpp
+/home/ubuntu/saturnc/./work/sip/esr/sip-router/src/modules/ndb_cassandra/Cassandra.cpp

src/modules/ndb_cassandra/test-cpp/cassandra_constants.cpp
@@ -1,1 +1,1 @@
-../cassandra_constants.cpp
+/home/ubuntu/saturnc/./work/sip/esr/sip-router/src/modules/ndb_cassandra/cassandra_constants.cpp

src/modules/ndb_cassandra/test-cpp/cassandra_types.cpp
@@ -1,1 +1,1 @@
-../cassandra_types.cpp
+/home/ubuntu/saturnc/./work/sip/esr/sip-router/src/modules/ndb_cassandra/cassandra_types.cpp

src/modules/ndb_cassandra/test-cpp/thrift_wrapper.cpp
@@ -1,1 +1,1 @@
-../thrift_wrapper.cpp
+/home/ubuntu/saturnc/./work/sip/esr/sip-router/src/modules/ndb_cassandra/thrift_wrapper.cpp

src/core/receive.c
@@ -323,6 +323,11 @@ int receive_msg(char *buf, unsigned int len, receive_info_t *rcv_info)
 		return -1;
 	}
 
+	if(len >= BUF_SIZE) {
+		LM_ERR("message exceeding maximum size: %d\n", (int)BUF_SIZE);
+		return -1;
+	}
+
 	if(ksr_evrt_received_mode & KSR_EVRT_RECEIVED_MESSAGE) {
 		if(ksr_evrt_received(buf, &len, rcv_info, KSR_EVRT_RECEIVED_MESSAGE)
 				< 0) {

commit 2335d3b000f56a57ec29fe8ab7418e694bc452d2

ndb_cassandra: Restore previous symbolic link
(cherry picked from commit 69475dab0b8f8b13f33871c656c1d7df4dbec450)

Fixes: c9adcbec63 ("core: check size of the message on receive()")

src/modules/ndb_cassandra/test-cpp/Cassandra.cpp
@@ -1,1 +1,1 @@
-/home/ubuntu/saturnc/./work/sip/esr/sip-router/src/modules/ndb_cassandra/Cassandra.cpp
+../Cassandra.cpp

src/modules/ndb_cassandra/test-cpp/cassandra_constants.cpp
@@ -1,1 +1,1 @@
-/home/ubuntu/saturnc/./work/sip/esr/sip-router/src/modules/ndb_cassandra/cassandra_constants.cpp
+../cassandra_constants.cpp

src/modules/ndb_cassandra/test-cpp/cassandra_types.cpp
@@ -1,1 +1,1 @@
-/home/ubuntu/saturnc/./work/sip/esr/sip-router/src/modules/ndb_cassandra/cassandra_types.cpp
+../cassandra_types.cpp

src/modules/ndb_cassandra/test-cpp/thrift_wrapper.cpp
@@ -1,1 +1,1 @@
-/home/ubuntu/saturnc/./work/sip/esr/sip-router/src/modules/ndb_cassandra/thrift_wrapper.cpp
+../thrift_wrapper.cpp