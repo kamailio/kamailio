2a5632251c2e56f01567ad48bb26515c40460751
Daniel-Constantin Mierla <miconda@gmail.com>
2026-09-22 10:27:38 +0200

    pv: proper size of $oul definition

--- src/modules/pv/pv.c
+++ src/modules/pv/pv.c
@@ -284,7 +284,7 @@ static pv_export_t mod_pvs[] = {
 			PVT_OTHER, pv_get_ouri_attr, 0, 0, 0, pv_init_iname, 4},
 	{{"ou", (sizeof("ou") - 1)}, /* */
 			PVT_OURI, pv_get_ouri, 0, 0, 0, 0, 0},
-	{{"oul", (sizeof("ou") - 1)}, /* */
+	{{"oul", (sizeof("oul") - 1)}, /* */
 			PVT_OURI, pv_get_ouri_len, 0, 0, 0, 0, 0},
 	{{"ouri", (sizeof("ouri") - 1)}, /* */
 			PVT_OURI, pv_get_ouri, 0, 0, 0, 0, 0},
